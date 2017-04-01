bbb5f65feb4f61650e6d7fb888147b65c2a3c3dd
John Marino <draco@marino.st>
2017-04-01 01:53:57 -0500

    add libarchive (we need shared version of xz for one variant)

---
+++ bucket_12/libarchive/patches/patch-libarchive_archive__util.c
@@ -0,0 +1,23 @@
+--- libarchive/archive_util.c.orig	2016-07-17 15:29:23 UTC
++++ libarchive/archive_util.c
+@@ -46,16 +46,16 @@ __FBSDID("$FreeBSD: head/lib/libarchive/
+ #include <wincrypt.h>
+ #endif
+ #ifdef HAVE_ZLIB_H
+-#include <zlib.h>
++#include "zlib.h"
+ #endif
+ #ifdef HAVE_LZMA_H
+-#include <lzma.h>
++#include "lzma.h"
+ #endif
+ #ifdef HAVE_BZLIB_H
+-#include <bzlib.h>
++#include "bzlib.h"
+ #endif
+ #ifdef HAVE_LZ4_H
+-#include <lz4.h>
++#include "lz4.h"
+ #endif
+ 
+ #include "archive.h"
