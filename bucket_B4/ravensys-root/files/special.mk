9b37d4fc22d1a0236f4a53b5f444e7c852bfd5ea
John Marino <draco@marino.st>
2017-04-10 23:13:13 -0500

    Add raven-sysroot port, split pkg into shared/static subpackages

---
+++ bucket_B4/ravensys-root/files/special.mk
@@ -0,0 +1,13 @@
+Libc_DragonFly=		libc.so.8
+Libm_DragonFly=		libm.so.4
+Libutil_DragonFly=	libutil.so.4
+Libc_FreeBSD=		libc.so.7
+Libm_FreeBSD=		libm.so.5
+Libutil_FreeBSD=	libutil.so.9
+Libusb_DragonFly=	libusb.so.2
+Libusb_FreeBSD=		libusb.so.3
+Libusbhid_DragonFly=	libusbhid.so.4
+Libusbhid_FreeBSD=	libusbhid.so.4
+
+RSYS=			${PREFIX}/share/raven/sysroot/${OPSYS}
+FOS=			${WRKDIR}/${OPSYS:tl}
