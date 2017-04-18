3b232a2f7251480bb3d97908bfeff8fa3b2c5b28
John Marino <draco@marino.st>
2017-04-18 14:35:17 -0500

    add libcrypt transfer to ravensys-root

--- bucket_B4/ravensys-root/files/special.mk
+++ bucket_B4/ravensys-root/files/special.mk
@@ -1,13 +1,20 @@
 Libc_DragonFly=		libc.so.8
-Libm_DragonFly=		libm.so.4
-Libutil_DragonFly=	libutil.so.4
 Libc_FreeBSD=		libc.so.7
+
+Libm_DragonFly=		libm.so.4
 Libm_FreeBSD=		libm.so.5
+
+Libutil_DragonFly=	libutil.so.4
 Libutil_FreeBSD=	libutil.so.9
+
 Libusb_DragonFly=	libusb.so.2
 Libusb_FreeBSD=		libusb.so.3
+
 Libusbhid_DragonFly=	libusbhid.so.4
 Libusbhid_FreeBSD=	libusbhid.so.4
+
+Libcrypt_DragonFly=	libcrypt.so.4
+Libcrypt_FreeBSD=	libcrypt.so.3
 
 RSYS=			${PREFIX}/share/raven/sysroot/${OPSYS}
 FOS=			${WRKDIR}/${OPSYS:tl}
