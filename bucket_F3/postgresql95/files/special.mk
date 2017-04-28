a0e9bc9e62557ffb05ecf6e9d366520a5cf99056
John Marino <draco@marino.st>
2017-04-28 02:01:39 -0500

    add postgresql 94 and 95

---
+++ bucket_F3/postgresql95/files/special.mk
@@ -0,0 +1,23 @@
+.if ${OPSYS:MFreeBSD}
+TEMPLATE_ARG=	--with-template=freebsd
+UUID_ARG=	--with-uuid=bsd
+.elif ${OPSYS:MDragonFly}
+TEMPLATE_ARG=	--with-template=freebsd
+UUID_ARG=	--with-uuid=bsd
+.elif ${OPSYS:MNetBSD}
+TEMPLATE_ARG=	--with-template=netbsd
+UUID_ARG=	--with-uuid=bsd
+.elif ${OPSYS:MOpenBSD}
+TEMPLATE_ARG=	--with-template=openbsd
+UUID_ARG=	--with-uuid=bsd
+.elif ${OPSYS:MDarwin}
+TEMPLATE_ARG=	--with-template=darwin
+UUID_ARG=	--with-uuid=e2fs
+.elif ${OPSYS:MSunOS}
+TEMPLATE_ARG=	--with-template=solaris
+# UUID?
+.elif ${OPSYS:Linux}
+TEMPLATE_ARG=	--with-template=linux
+UUID_ARG=	--with-uuid=e2fs
+.endif
+
