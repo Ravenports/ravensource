f5c8ab0ef1773748c811158cd237b526fe5b8927
John Marino <draco@marino.st>
2017-05-10 14:54:26 -0500

    update ravenadm to latest for testing reasons (and remove unneeded ravenports)

--- bucket_05/ravenports/files/special.mk
+++
@@ -1,6 +0,0 @@
-POST_PLIST_TARGET=	ravenports-autolist
-
-ravenports-autolist:
-	@(cd ${STAGEDIR}${PREFIX} && ${FIND} share \
-	\( -type f -o -type l \) 2>/dev/null | ${SORT}) \
-	>> ${WRKDIR}/.manifest.single.mktmp
