a3f5046c6cd60c1320d37cc833d917389e3d7b3d
John Marino <draco@marino.st>
2017-05-19 17:21:34 -0500

    Temporary status for gcc6
    I need to remove the ALLSTAGES option for two reasons:
    1) libcc1 will not build now under a single stage
    2) It causes dep check failure if the option is toggled.  as a core
       component of toolchain, we need option-less port here.

--- bucket_97/gcc6/patches/patch-gcc_config_i386_gnu-user64.h
+++ bucket_97/gcc6/patches/patch-gcc_config_i386_gnu-user64.h
@@ -1,11 +1,10 @@
 --- gcc/config/i386/gnu-user64.h.orig	2016-01-04 08:30:50.000000000 -0600
-+++ gcc/config/i386/gnu-user64.h	2017-05-19 09:16:28.204939715 -0500
-@@ -59,6 +59,8 @@
++++ gcc/config/i386/gnu-user64.h	2017-05-19 15:55:41.756608223 -0500
+@@ -57,6 +57,7 @@
+                    %{" SPEC_32 ":-m " GNU_USER_LINK_EMULATION32 "} \
+                    %{" SPEC_X32 ":-m " GNU_USER_LINK_EMULATIONX32 "} \
    %{shared:-shared} \
++  %{!static:--enable-new-dtags -rpath @PREFIX@/@GCCAUX@/lib64} \
    %{!shared: \
      %{!static: \
-+      --enable-new-dtags \
-+      -rpath @PREFIX@/@GCCAUX@/lib \
        %{rdynamic:-export-dynamic} \
-       %{" SPEC_32 ":-dynamic-linker " GNU_USER_DYNAMIC_LINKER32 "} \
-       %{" SPEC_64 ":-dynamic-linker " GNU_USER_DYNAMIC_LINKER64 "} \
