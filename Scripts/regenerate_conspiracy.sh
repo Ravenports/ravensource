fc7d03847320ba38308b73556af6754025fa6c99
John Marino <draco@marino.st>
2017-05-24 20:09:41 -0500

    add conspiracy regeneration script

---
+++ Scripts/regenerate_conspiracy.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+maintainer=marino
+repo=/home/marino/github/Ravenports
+myid=$(id -un)
+today=$(date "+%d %h %Y %H:%M")
+message="Ravenports generated: ${today}"
+
+if [ "${myid}" != "root" ]; then
+   echo "Only root is allowed to regenerate ports (which requires using root-only ravenadm)"
+   exit 1
+fi
+
+RPATH=$(realpath $0)
+SCRIPTSDIR=$(dirname ${RPATH})
+RAVENSRC=$(dirname ${SCRIPTSDIR})
+HEXRANGE="0 1 2 3 4 5 6 7 8 9 A B C D E F"
+
+for D1 in ${HEXRANGE}; do
+  for D0 in ${HEXRANGE}; do
+    BK=${RAVENSRC}/bucket_${D1}${D0}
+    if [ -d ${BK} ]; then
+      queue=$(find ${BK} -depth -maxdepth 1 -mindepth 1 -type d)
+      for port in ${queue}; do
+         /raven/bin/ravenadm dev buildsheet ${port} save
+      done
+    fi
+  done
+done
+
+/raven/bin/ravenadm dev generate-index
+
+chown -R ${maintainer} ${repo}
+(cd ${repo} && git add * && git commit -m "${message}")
