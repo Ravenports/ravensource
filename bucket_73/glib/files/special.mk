6d92e17d5d510b5fdda5e390f745b770fa1b7210
John Marino <draco@marino.st>
2017-06-03 00:26:31 -0500

    add glib port (this one was tough)

---
+++ bucket_73/glib/files/special.mk
@@ -0,0 +1,12 @@
+glib_MAN=	gtester.1 gtester-report.1 glib-gettextize.1
+gio_MAN=	gdbus.1 gsettings.1 gio-querymodules.1 \
+		glib-compile-schemas.1 gapplication.1 \
+		glib-compile-resources.1 gresource.1 gdbus-codegen.1
+gobject_MAN=	glib-genmarshal.1 glib-mkenums.1 gobject-query.1
+
+POST_PLIST_TARGET=	my-autolist
+
+my-autolist:
+	(cd ${STAGEDIR}${PREFIX} && ${FIND} share/glib-2.0/codegen \
+	\( -type f -o -type l \) 2>/dev/null | ${SORT}) \
+	>> ${WRKDIR}/.manifest.single.mktmp
