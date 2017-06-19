81cfc581d99f167639929b87ae0a0d74ee68a59b
John Marino <draco@marino.st>
2017-06-19 10:38:14 -0500

    add utility script to quickly check FPC equivalent values

---
+++ Scripts/check_fpc_equivalents.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+#
+# The purpose of this script is to catch FPC_EQUIVALENT values of "N/A"
+# that are not on a whitelist.  This usually means they were forgotten
+# to be defined.
+
+location=/usr/raven
+tmpfile=/tmp/fpc.notlist
+PROG='{if ($3 == "N/A") print}'
+awk "${PROG}" ${location}/Mk/Misc/fpc_equivalents > ${tmpfile}
+
+echo "List of ports with undefined FPC_EQUIVALENT values:"
+while read ravenport version fpc_port; do
+   case "${ravenport}" in
+	libbsd) ;; 		# linux only
+	libusb) ;; 		# linux only
+	shadow) ;;		# linux only
+
+	ravenports) ;;		# unique to ravenports
+	libdl) ;;		# unique to ravenports
+	ravensys-root) ;;	# unique to ravenports
+	ravensys-toolchain) ;;	# unique to ravenports
+	ravensys-uname) ;;	# unique to ravenports
+	bmake-mk-files) ;;	# unique to ravenports
+	ravenadm) ;;		# unique to ravenports
+
+	xz) ;;			# deleted from FPC
+	zlib) ;;		# deleted from FPC
+
+	fetch) ;;		# From DragonFly
+	makewhatis) ;;		# From DragonFly
+	mtree) ;;		# From DragonFly
+	rpcgen) ;;		# From DragonFly
+	sha256) ;;		# From DragonFly
+
+	libedit) ;;		# Different version scheme from FPC
+	gnat-util) ;;		# Different version scheme from FPC
+	gcc6) ;;		# Different version scheme from FPC
+	gprbuild) ;;		# Different version scheme from FPC
+
+	libexecinfo) ;;		# Different source from FPC
+	*) echo "${ravenport} ${version}"
+   esac
+done < ${tmpfile}
+
+rm -f ${tmpfile}
