2883587d3b8a8bedf60667c397c47356372d66d8
John Marino <draco@marino.st>
2017-06-19 16:25:33 -0500

    add sparforte port

---
+++ bucket_FF/sparforte/files/special.mk
@@ -0,0 +1,27 @@
+.if ${SETMYSQL} == 1 || ${SETPGSQL} == 1
+SFARG+=		-DAPQ=true
+SETAPQ=		1
+.else
+SFARG+=		-DAPQ=false
+SETAPG=		0
+.endif
+
+SFARG+=		-DREADLINE=true -DBDB=true
+
+# Currently Ada is only available for x86, so we can assume the following
+SF_ARCH=		${ARCH_STANDARD:S/x86_64/athlon64/}
+SF_CPU_FLAG=		-march
+
+.if ${OPSYS} == FreeBSD || ${OPSYS} == DragonFly
+SF_CFLAGSTYPE=		-DFREEBSD
+OSBIND=			os_bindings/spar_freebsd
+.else
+SF_CFLAGSTYPE=		-DLINUX
+OSBIND=			os_bindings/spar_linux
+.endif
+
+.if ${ARCH_STANDARD} == x86_64 || ${ARCH_STANDARD} == aarch64
+CPU_SDL=		spar_os-sdl-64bit
+.else
+CPU_SDL=		spar_os-sdl-32bit
+.endif
