c49bdae852856e06c236c435260931993b33cde2
John Marino <draco@marino.st>
2017-06-21 22:58:58 -0500

    add wxwidgets port

---
+++ bucket_3D/wxWidgets/patches/patch-src_stc_scintilla_src_Editor.cxx
@@ -0,0 +1,10 @@
+--- src/stc/scintilla/src/Editor.cxx.orig	2017-04-29 12:00:38 UTC
++++ src/stc/scintilla/src/Editor.cxx
+@@ -17,6 +17,7 @@
+ #include <map>
+ #include <algorithm>
+ #include <memory>
++#include <cmath>
+ 
+ #include "Platform.h"
+ 
