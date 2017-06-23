e1179208dc29c4311af7fdf0a595d7331db677c9
John Marino <draco@marino.st>
2017-06-22 21:05:01 -0500

    fix w3m on linux

---
+++ bucket_83/w3m/patches/patch-table.c
@@ -0,0 +1,10 @@
+--- table.c.orig	2017-01-06 14:20:43 UTC
++++ table.c
+@@ -6,6 +6,7 @@
+ #include <stdio.h>
+ #include <string.h>
+ #include <math.h>
++#include <limits.h>
+ #include "fm.h"
+ #include "html.h"
+ #include "parsetagx.h"
