cb67ddb0a26378e06119b9071329fecdeded3cc3
John Marino <draco@marino.st>
2017-09-20 16:06:37 -0500

    add shared-mime-info

--- bucket_EC/shared-mime-info/patches/patch-update-mime-database.c
+++ bucket_EC/shared-mime-info/patches/patch-update-mime-database.c
@@ -1,6 +1,6 @@
---- update-mime-database.c.orig	2007-07-02 04:00:13.000000000 -0500
-+++ update-mime-database.c	2008-01-28 23:42:30.000000000 -0600
-@@ -445,7 +446,8 @@
+--- update-mime-database.c.orig	2017-02-11 17:26:13 UTC
++++ update-mime-database.c
+@@ -633,7 +633,8 @@ static void remove_old(Type *type, xmlNo
  		}
  	}
  
@@ -10,7 +10,7 @@
  }
  
  /* 'node' is a <mime-type> node from a source file, whose type is 'type'.
-@@ -1510,7 +1512,7 @@
+@@ -2168,7 +2169,7 @@ static void check_in_path_xdg_data(const
  
  	env = getenv("XDG_DATA_DIRS");
  	if (!env)
