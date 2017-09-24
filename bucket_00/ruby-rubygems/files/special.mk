8751701a32d03bb8ece5c95df16b6630712684ce
John Marino <draco@marino.st>
2017-09-23 19:22:36 -0500

    simplify rubygems fix

--- bucket_00/ruby-rubygems/files/special.mk
+++ bucket_00/ruby-rubygems/files/special.mk
@@ -1,11 +1,1 @@
-.if "${VARIANT}" == "v23"
-GEMLINK2.4="@comment "
-GEMLINK2.3=""
-.endif
-
-.if "${VARIANT}" == "v24"
-GEMLINK2.3="@comment "
-GEMLINK2.4=""
-.endif
-
 PLIST_SUB+= GEMLINK=${GEMLINK${RUBY_DEFAULT}}
