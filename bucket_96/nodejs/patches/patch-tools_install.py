0ba02085d54a71a8ca35af5ba996f0f098d4f9e6
John Marino <draco@marino.st>
2017-10-13 08:56:53 -0500

    update a bunch + gnatcross progress

--- bucket_96/nodejs/patches/patch-tools_install.py
+++ bucket_96/nodejs/patches/patch-tools_install.py
@@ -1,8 +1,8 @@
---- tools/install.py.orig	2014-12-22 21:22:32 UTC
+--- tools/install.py.orig	2017-10-11 12:36:15 UTC
 +++ tools/install.py
-@@ -132,7 +132,7 @@ def files(action):
-   if 'true' == variables.get('node_use_dtrace'):
-     action(['out/Release/node.d'], 'lib/dtrace/node.d')
+@@ -148,7 +148,7 @@ def files(action):
+   action(['deps/v8/tools/lldbinit'], 'share/doc/node/')
+   action(['deps/v8/tools/lldb_commands.py'], 'share/doc/node/')
  
 -  if 'freebsd' in sys.platform or 'openbsd' in sys.platform:
 +  if 'skipme' in sys.platform:
