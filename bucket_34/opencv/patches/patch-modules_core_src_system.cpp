2bd7ae9bdf2b419b80363b2779fed6734e56ee8d
John Marino <draco@marino.st>
2017-10-26 10:31:26 -0500

    update opencv

--- bucket_34/opencv/patches/patch-modules_core_src_system.cpp
+++ bucket_34/opencv/patches/patch-modules_core_src_system.cpp
@@ -1,11 +1,11 @@
---- modules/core/src/system.cpp.orig	2017-08-03 23:58:23 UTC
+--- modules/core/src/system.cpp.orig	2017-10-23 15:41:12 UTC
 +++ modules/core/src/system.cpp
-@@ -66,7 +66,7 @@ Mutex* __initialization_mutex_initialize
+@@ -67,7 +67,7 @@ Mutex* __initialization_mutex_initialize
  # endif
  #endif
  
--#if defined __ANDROID__ || defined __linux__ || defined __FreeBSD__
-+#if defined __ANDROID__ || defined __linux__ || defined __FreeBSD__ || defined __DragonFly__
+-#if defined __ANDROID__ || defined __linux__ || defined __FreeBSD__ || defined __HAIKU__
++#if defined __ANDROID__ || defined __linux__ || defined __FreeBSD__ || defined __HAIKU__ || defined __DragonFly__
  #  include <unistd.h>
  #  include <fcntl.h>
  #  include <elf.h>
