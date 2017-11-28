24bcd6d468fede4d4525665bc38944b76b3d4f2a
John Marino <draco@marino.st>
2017-11-28 12:50:00 -0600

    regen less patch

--- bucket_0B/less/patches/patch-charset.c
+++ bucket_0B/less/patches/patch-charset.c
@@ -1,6 +1,6 @@
---- charset.c.orig	2013-04-04 16:55:05 UTC
+--- charset.c.orig	2016-10-25 14:37:35 UTC
 +++ charset.c
-@@ -407,6 +407,10 @@ binary_char(c)
+@@ -409,6 +409,10 @@ binary_char(c)
  control_char(c)
  	LWCHAR c;
  {
@@ -11,7 +11,7 @@
  	c &= 0377;
  	return (chardef[c] & IS_CONTROL_CHAR);
  }
-@@ -422,6 +426,20 @@ prchar(c)
+@@ -424,6 +428,20 @@ prchar(c)
  	/* {{ This buffer can be overrun if LESSBINFMT is a long string. }} */
  	static char buf[32];
  
@@ -32,7 +32,7 @@
  	c &= 0377;
  	if ((c < 128 || !utf_mode) && !control_char(c))
  		SNPRINTF1(buf, sizeof(buf), "%c", (int) c);
-@@ -445,6 +463,7 @@ prchar(c)
+@@ -447,6 +465,7 @@ prchar(c)
  #endif
  	else
  		SNPRINTF1(buf, sizeof(buf), binfmt, c);
