7ac849e90df7f923093f4d8610896b2e324a1c3a
John Marino <draco@marino.st>
2017-12-23 14:07:42 -0600

    upgrade tcl86

--- bucket_E3/tcl86/patches/patch-unix-tclUnixInit.c
+++ bucket_E3/tcl86/patches/patch-unix-tclUnixInit.c
@@ -1,6 +1,6 @@
---- unix/tclUnixInit.c.orig    2016-09-08 20:22:10.064101000 -0400
-+++ unix/tclUnixInit.c 2016-09-08 20:22:42.345592000 -0400
-@@ -391,14 +391,6 @@
+--- unix/tclUnixInit.c.orig	2017-10-13 13:36:31 UTC
++++ unix/tclUnixInit.c
+@@ -391,14 +391,6 @@ TclpInitPlatform(void)
  #endif /* SIGPIPE */
  
  #if defined(__FreeBSD__) && defined(__GNUC__)
