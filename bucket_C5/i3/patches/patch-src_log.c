5dc2292210358d6490488a293421e27831669927
John Marino <draco@marino.st>
2017-12-25 13:20:03 -0600

    add i3 port

---
+++ bucket_C5/i3/patches/patch-src_log.c
@@ -0,0 +1,20 @@
+--- src/log.c.orig	2017-09-24 17:21:26 UTC
++++ src/log.c
+@@ -128,7 +128,7 @@ void open_logbuffer(void) {
+          * At the moment (2011-12-10), no testcase leads to an i3 log
+          * of more than ~ 600 KiB. */
+     logbuffer_size = min(physical_mem_bytes * 0.01, shmlog_size);
+-#if defined(__FreeBSD__)
++#if defined(__FreeBSD__) || defined(__DragonFly__)
+     sasprintf(&shmlogname, "/tmp/i3-log-%d", getpid());
+ #else
+     sasprintf(&shmlogname, "/i3-log-%d", getpid());
+@@ -139,7 +139,7 @@ void open_logbuffer(void) {
+         return;
+     }
+ 
+-#if defined(__OpenBSD__) || defined(__APPLE__)
++#if defined(__OpenBSD__) || defined(__APPLE__) || defined(__DragonFly__)
+     if (ftruncate(logbuffer_shm, logbuffer_size) == -1) {
+         fprintf(stderr, "Could not ftruncate SHM segment for the i3 log: %s\n", strerror(errno));
+ #else
