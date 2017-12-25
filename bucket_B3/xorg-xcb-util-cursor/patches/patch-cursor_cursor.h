d4282a24bc1c385417bb7c585287fabdd06a7733
John Marino <draco@marino.st>
2017-12-25 12:32:58 -0600

    add xorg-xcb-util-cursor port

---
+++ bucket_B3/xorg-xcb-util-cursor/patches/patch-cursor_cursor.h
@@ -0,0 +1,11 @@
+--- cursor/cursor.h.orig	2016-05-12 07:51:59 UTC
++++ cursor/cursor.h
+@@ -161,7 +161,7 @@ typedef struct xcint_image_t {
+ } __attribute__((packed)) xcint_image_t;
+ 
+ /* shape_to_id.c */
+-const int cursor_shape_to_id(const char *name);
++int cursor_shape_to_id(const char *name);
+ 
+ /* parse_cursor_file.c */
+ int parse_cursor_file(xcb_cursor_context_t *c, const int fd, xcint_image_t **images, int *nimg);
