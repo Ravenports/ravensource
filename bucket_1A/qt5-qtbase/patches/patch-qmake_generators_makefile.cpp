3692de9282a93315e7dfa4167dd386423eadb507
John Marino <draco@marino.st>
2018-01-04 11:58:01 -0600

    finish qt5-qtbase

---
+++ bucket_1A/qt5-qtbase/patches/patch-qmake_generators_makefile.cpp
@@ -0,0 +1,11 @@
+--- qmake/generators/makefile.cpp.orig	2017-11-30 13:49:46 UTC
++++ qmake/generators/makefile.cpp
+@@ -3294,7 +3294,7 @@ MakefileGenerator::writePkgConfigFile()
+         pkgConfiglibName = bundle.toQString();
+     } else {
+         if (!project->values("QMAKE_DEFAULT_LIBDIRS").contains(libDir))
+-            t << "-L${libdir} ";
++            t << "-L${libdir} -Wl,-rpath,${libdir}";
+         pkgConfiglibName = "-l" + project->first("QMAKE_ORIG_TARGET");
+         if (project->isActiveConfig("shared"))
+             pkgConfiglibName += project->first("TARGET_VERSION_EXT").toQString();
