fd5e95426850ddf63fab227f417fc5edcfb0c20c
John Marino <draco@marino.st>
2018-01-05 17:42:55 -0600

    fix ssl linking in qtbase

---
+++ bucket_1A/qt5-qtbase/patches/patch-config.tests_unix_openssl11_openssl.cpp
@@ -0,0 +1,11 @@
+--- config.tests/unix/openssl11/openssl.cpp.orig	2017-11-30 13:49:46 UTC
++++ config.tests/unix/openssl11/openssl.cpp
+@@ -39,7 +39,7 @@
+ 
+ #include <openssl/opensslv.h>
+ 
+-#if !defined(OPENSSL_VERSION_NUMBER) || OPENSSL_VERSION_NUMBER-0 < 0x10100000L
++#if !defined(OPENSSL_VERSION_NUMBER) || OPENSSL_VERSION_NUMBER-0 < 0x10100000L || defined(LIBRESSL_VERSION_NUMBER)
+ #  error "OpenSSL >= 1.1 is required"
+ #endif
+ 
