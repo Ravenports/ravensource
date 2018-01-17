04abe6487d5bdba2fc9952ab3172954232f0b7ba
John Marino <draco@marino.st>
2018-01-17 09:11:14 -0600

    add opencollada port

---
+++ bucket_D1/opencollada/patches/patch-COLLADABaseUtils_include_COLLADABUhash__map.h
@@ -0,0 +1,11 @@
+--- COLLADABaseUtils/include/COLLADABUhash_map.h.orig	2018-01-13 00:23:52 UTC
++++ COLLADABaseUtils/include/COLLADABUhash_map.h
+@@ -60,7 +60,7 @@
+         #define COLLADABU_HASH_NAMESPACE_CLOSE }
+         #define COLLADABU_HASH_FUN hash
+     #endif
+-#elif (defined(__APPLE__) || defined(__FreeBSD__)) && defined(_LIBCPP_VERSION)
++#elif (defined(__APPLE__) || defined(__FreeBSD__) || defined(__DragonFly__)) && defined(_LIBCPP_VERSION)
+     #include <unordered_map>
+     #include <unordered_set>
+     #define COLLADABU_HASH_MAP std::unordered_map
---
+++ bucket_D1/opencollada/patches/patch-common_libBuffer_src_CommonFWriteBufferFlusher.cpp
@@ -0,0 +1,29 @@
+--- common/libBuffer/src/CommonFWriteBufferFlusher.cpp.orig	2018-01-13 00:23:52 UTC
++++ common/libBuffer/src/CommonFWriteBufferFlusher.cpp
+@@ -90,7 +90,7 @@ namespace Common
+ 		FilePosType currentPos = ftello64(mStream);
+ #elif defined( _WIN32)
+ 		FilePosType currentPos = _ftelli64(mStream);
+-#elif defined (__APPLE__) || defined(__FreeBSD__)
++#elif defined (__APPLE__) || defined(__FreeBSD__) || defined(__DragonFly__)
+ 		FilePosType currentPos = ftello(mStream);
+ #else
+ 		FilePosType currentPos = ftello64(mStream);
+@@ -115,7 +115,7 @@ namespace Common
+   			return (fseeko64(mStream,0,SEEK_END) == 0);
+ #elif defined( _WIN32)
+ 			return (_fseeki64(mStream, 0, SEEK_END) == 0);
+-#elif defined (__APPLE__) || defined(__FreeBSD__)
++#elif defined (__APPLE__) || defined(__FreeBSD__) || defined(__DragonFly__)
+ 			return (fseeko(mStream, 0, SEEK_END) == 0);
+ #else
+ 			return (fseeko64(mStream, 0, SEEK_END) == 0);
+@@ -135,7 +135,7 @@ namespace Common
+   				bool success = (fseeko64(mStream,pos,SEEK_SET) == 0);
+ #elif defined( _WIN32)
+ 				bool success = (_fseeki64(mStream, pos, SEEK_SET) == 0);
+-#elif defined (__APPLE__) || defined(__FreeBSD__)
++#elif defined (__APPLE__) || defined(__FreeBSD__) || defined(__DragonFly__)
+ 				bool success = (fseeko(mStream, pos, SEEK_SET) == 0);
+ #else
+ 				bool success = (fseeko64(mStream, pos, SEEK_SET) == 0);
