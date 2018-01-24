0dd0ae50f89115077ee860d6a261f0f61d0a7b9b
John Marino <draco@marino.st>
2018-01-24 17:14:40 -0600

    libedit: fix build on sunos

---
+++ bucket_BC/libedit/patches/patch-src_sys.h
@@ -0,0 +1,27 @@
+--- src/sys.h.orig	2017-03-29 18:08:21 UTC
++++ src/sys.h
+@@ -44,6 +44,10 @@
+ #include <sys/cdefs.h>
+ #endif
+ 
++#ifdef __sun__
++#include <sys/types.h>
++#endif
++
+ #if !defined(__attribute__) && (defined(__cplusplus) || !defined(__GNUC__)  || __GNUC__ == 2 && __GNUC_MINOR__ < 8)
+ # define __attribute__(A)
+ #endif
+@@ -115,13 +119,4 @@ typedef unsigned int	u_int32_t;
+ #define	REGEX		/* Use POSIX.2 regular expression functions */
+ #undef	REGEXP		/* Use UNIX V8 regular expression functions */
+ 
+-#if defined(__sun)
+-extern int tgetent(char *, const char *);
+-extern int tgetflag(char *);
+-extern int tgetnum(char *);
+-extern int tputs(const char *, int, int (*)(int));
+-extern char* tgoto(const char*, int, int);
+-extern char* tgetstr(char*, char**);
+-#endif
+-
+ #endif /* _h_sys */
---
+++ bucket_BC/libedit/patches/patch-src_terminal.h
@@ -0,0 +1,17 @@
+--- src/terminal.h.orig	2017-03-29 18:08:21 UTC
++++ src/terminal.h
+@@ -80,14 +80,6 @@ typedef struct {
+ #define	A_K_DE		6
+ #define	A_K_NKEYS	7
+ 
+-#ifdef __sun
+-extern int tgetent(char *, const char *);
+-extern int tgetflag(char *);
+-extern int tgetnum(char *);
+-extern int tputs(const char *, int, int (*)(int));
+-extern char* tgoto(const char*, int, int);
+-extern char* tgetstr(char*, char**);
+-#endif
+ 
+ libedit_private void	terminal_move_to_line(EditLine *, int);
+ libedit_private void	terminal_move_to_char(EditLine *, int);
