c01e6a9d20943c30b86288ccf9904b11d6d64feb
John Marino <draco@marino.st>
2018-01-27 23:27:05 -0600

    sunos fixes

---
+++ bucket_CA/libressl-devel/patches/patch-crypto_compat_arc4random.c
@@ -0,0 +1,11 @@
+--- crypto/compat/arc4random.c.orig	2017-11-05 23:15:17 UTC
++++ crypto/compat/arc4random.c
+@@ -88,7 +88,7 @@ _rs_stir(void)
+ 	u_char rnd[KEYSZ + IVSZ];
+ 
+ 	if (getentropy(rnd, sizeof rnd) == -1)
+-		_getentropy_fail();
++	; /*	_getentropy_fail(); */
+ 
+ 	if (!rs)
+ 		_rs_init(rnd, sizeof(rnd));
