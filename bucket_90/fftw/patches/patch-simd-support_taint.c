f6dc4c1f2b3144d007127c74fbfec1b0cc222d87
John Marino <draco@marino.st>
2018-01-27 18:37:00 -0600

    postgresql: xslt deps different on sunos
    while here, fix liboa, tcl85, php-sockets, and fftw

---
+++ bucket_90/fftw/patches/patch-simd-support_taint.c
@@ -0,0 +1,11 @@
+--- simd-support/taint.c.orig	2017-10-29 12:14:40 UTC
++++ simd-support/taint.c
+@@ -40,4 +40,8 @@ R *X(join_taint)(R *p1, R *p2)
+      A(UNTAINT(p1) == UNTAINT(p2));
+      return (R *)(PTRINT(p1) | PTRINT(p2));
+ }
++#else
++# ifdef __sun__
++int Solaris_requires_a_symbol= 0;
++# endif
+ #endif
