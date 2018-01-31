e7f7a2e5d24c703453b1d99bfb87f3ae50850608
John Marino <draco@marino.st>
2018-01-31 12:19:43 -0600

    libva: fix build on sunos

--- bucket_1A/libva/patches/patch-va_va__trace.c
+++ bucket_1A/libva/patches/patch-va_va__trace.c
@@ -10,7 +10,7 @@
  #include <unistd.h>
  #include <time.h>
  #include <errno.h>
-@@ -291,7 +294,13 @@ static void add_trace_config_info(
+@@ -291,7 +294,15 @@ static void add_trace_config_info(
  {
      struct trace_config_info *pconfig_info;
      int idx = 0;
@@ -18,13 +18,15 @@
 +    pid_t thd_id = pthread_getthreadid_np();
 +#elif defined __DragonFly__
 +    pid_t thd_id = syscall(SYS_lwp_gettid);
++#elif defined __sun__
++    pid_t thd_id = pthread_self();
 +#else
      pid_t thd_id = syscall(__NR_gettid);
 +#endif
  
      LOCK_RESOURCE(pva_trace);
  
-@@ -315,7 +324,13 @@ static void delete_trace_config_info(
+@@ -315,7 +326,15 @@ static void delete_trace_config_info(
  {
      struct trace_config_info *pconfig_info;
      int idx = 0;
@@ -32,13 +34,34 @@
 +    pid_t thd_id = pthread_getthreadid_np();
 +#elif defined __DragonFly__
 +    pid_t thd_id = syscall(SYS_lwp_gettid);
++#elif defined __sun__
++    pid_t thd_id = pthread_self();
 +#else
      pid_t thd_id = syscall(__NR_gettid);
 +#endif
  
      LOCK_RESOURCE(pva_trace);
  
-@@ -662,7 +677,13 @@ static struct trace_log_file *start_trac
+@@ -489,6 +508,18 @@ static int get_valid_ctx_idx(
+     return idx;
+ }
+ 
++#ifdef __sun__
++static size_t
++strnlen(const char *s, size_t maxlen)
++{
++	size_t len;
++	for (len = 0; len < maxlen; len++, s++) {
++		if (!*s) break;
++	}
++	return (len);
++}
++#endif
++
+ static void FILE_NAME_SUFFIX(
+     char *env_value,
+     int max_size,
+@@ -662,7 +693,15 @@ static struct trace_log_file *start_trac
  {
      struct trace_log_files_manager *plog_files_mgr = NULL;
      struct trace_log_file *plog_file = NULL;
@@ -46,13 +69,15 @@
 +    pid_t thd_id = pthread_getthreadid_np();
 +#elif defined __DragonFly__
 +    pid_t thd_id = syscall(SYS_lwp_gettid);
++#elif defined __sun__
++    pid_t thd_id = pthread_self();
 +#else
      pid_t thd_id = syscall(__NR_gettid);
 +#endif
      int i = 0;
  
      LOCK_RESOURCE(pva_trace);
-@@ -701,7 +722,13 @@ static void refresh_log_file(
+@@ -701,7 +740,15 @@ static void refresh_log_file(
      struct trace_context *ptra_ctx)
  {
      struct trace_log_file *plog_file = NULL;
@@ -60,13 +85,15 @@
 +    pid_t thd_id = pthread_getthreadid_np();
 +#elif defined __DragonFly__
 +    pid_t thd_id = syscall(SYS_lwp_gettid);
++#elif defined __sun__
++    pid_t thd_id = pthread_self();
 +#else
      pid_t thd_id = syscall(__NR_gettid);
 +#endif
      int i = 0;
  
      plog_file = ptra_ctx->plog_file;
-@@ -1224,7 +1251,13 @@ static void internal_TraceUpdateContext
+@@ -1224,7 +1271,15 @@ static void internal_TraceUpdateContext
  {
      struct trace_context *trace_ctx = NULL;
      int i = 0, delete = 1;
@@ -74,6 +101,8 @@
 +    pid_t thd_id = pthread_getthreadid_np();
 +#elif defined __DragonFly__
 +    pid_t thd_id = syscall(SYS_lwp_gettid);
++#elif defined __sun__
++    pid_t thd_id = pthread_self();
 +#else
      pid_t thd_id = syscall(__NR_gettid);
 +#endif
