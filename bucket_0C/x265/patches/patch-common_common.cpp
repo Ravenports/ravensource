b2135b85d148dab3d104cdd366366ece835f1b8b
John Marino <draco@marino.st>
2018-02-01 11:23:57 -0600

    x265: fix build on sunos

---
+++ bucket_0C/x265/patches/patch-common_common.cpp
@@ -0,0 +1,30 @@
+--- common/common.cpp.orig	2017-11-29 03:21:40 UTC
++++ common/common.cpp
+@@ -35,6 +35,27 @@
+ #include <sys/time.h>
+ #endif
+ 
++#ifdef __sun__
++#define posix_memalign	illumos_memalign
++static int
++posix_memalign(void **memptr, size_t alignment, size_t size)
++{
++	void *ptr = NULL;
++	int error = 0;
++
++	if (alignment == 0 ||
++	    (alignment & (sizeof (void *) - 1)) != 0 ||
++	    (alignment & (alignment - 1)) != 0)
++		error = EINVAL;
++	else if (size != 0 &&
++	    (ptr = memalign(alignment, size)) == NULL)
++		error = ENOMEM;
++
++	*memptr = ptr;
++	return (error);
++}
++#endif
++
+ namespace X265_NS {
+ 
+ #if CHECKED_BUILD || _DEBUG
