8fed207f9822f07b98cc2641cb13713c6e3bc537
John Marino <draco@marino.st>
2018-02-23 15:33:33 -0600

    add gdal port

---
+++ bucket_9F/gdal/patches/patch-frmts-mrf-mrf_band.cpp
@@ -0,0 +1,11 @@
+--- frmts/mrf/mrf_band.cpp.orig	2017-11-20 10:45:39 UTC
++++ frmts/mrf/mrf_band.cpp
+@@ -49,7 +49,7 @@
+ 
+ #include <vector>
+ #include <assert.h>
+-#include "../zlib/zlib.h"
++#include <zlib.h>
+ 
+ CPL_CVSID("$Id: mrf_band.cpp 37663 2017-03-09 20:42:19Z lplesea $");
+ 
