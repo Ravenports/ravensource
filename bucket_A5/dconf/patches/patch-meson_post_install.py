6ea05bab2ba247c2d32998a62212472e5539b5f9
John Marino <draco@marino.st>
2018-03-22 10:56:24 -0500

    dconf: use updated SHEBANGFIX instead of patch (#43)
    Note that meson executes the post script so if the permissions don't
    indicate executable status, it fails.  This might be a recent meson
    change because the pre-patched meson_post_install.py script doesn't
    have exec bit set either.

--- bucket_A5/dconf/patches/patch-meson_post_install.py
+++
@@ -1,8 +0,0 @@
---- meson_post_install.py.orig	2018-03-16 10:49:08.955597000 +0100
-+++ meson_post_install.py	2018-03-16 10:49:39.411538000 +0100
-@@ -1,4 +1,4 @@
--#!/usr/bin/env python3
-+#!/raven/bin/python3.6
- 
- import os
- import subprocess
