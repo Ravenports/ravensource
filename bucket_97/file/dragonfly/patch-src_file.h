fa9420f42ae076695ece547c19d5e637a9050a6e
John Marino <draco@marino.st>
2018-04-16 08:35:36 -0500

    file: upgrade

--- bucket_97/file/dragonfly/patch-src_file.h
+++ bucket_97/file/dragonfly/patch-src_file.h
@@ -1,6 +1,6 @@
---- src/file.h.orig	2015-01-01 17:07:34 UTC
+--- src/file.h.orig	2017-08-28 13:39:18 UTC
 +++ src/file.h
-@@ -491,6 +491,7 @@ protected int file_os2_apptype(struct ma
+@@ -501,6 +501,7 @@ protected int file_os2_apptype(struct ma
  #include <locale.h>
  #endif
  #if defined(HAVE_XLOCALE_H)
