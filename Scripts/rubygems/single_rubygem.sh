eb80bf1af05abdc6c899a71a4a7c9e878235a218
John Marino <draco@marino.st>
2018-05-10 12:51:23 -0500

    upgrade rubygems

--- Scripts/rubygems/single_rubygem.sh
+++ Scripts/rubygems/single_rubygem.sh
@@ -144,7 +144,7 @@ get_filtered_url() {
    case ${url} in
       http://github.com/* | http://bitbucket.org/* | \
       http://docs.openstack.org* | http://banisterfiend.wordpress.com* | \
-      http://*.github.io)
+      http://*.github.io | http://rubyonrails.org*)
          echo ${url} | sed -e "s/^http:/https:/"
          ;;
       *) echo ${url} ;;
