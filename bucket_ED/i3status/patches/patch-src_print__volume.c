70a625a3b22879898be9e5936e505562af659ba3
John Marino <draco@marino.st>
2018-05-13 11:37:05 -0500

    i3status: upgrade

--- bucket_ED/i3status/patches/patch-src_print__volume.c
+++ bucket_ED/i3status/patches/patch-src_print__volume.c
@@ -1,4 +1,4 @@
---- src/print_volume.c.orig	2017-01-21 15:00:31 UTC
+--- src/print_volume.c.orig	2018-05-11 09:10:46 UTC
 +++ src/print_volume.c
 @@ -61,7 +61,7 @@ void print_volume(yajl_gen json_gen, cha
          free(instance);
