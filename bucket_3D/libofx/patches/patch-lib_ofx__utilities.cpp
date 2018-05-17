6c66682f898e77d2db9b7c8d430e49e5c943b557
John Marino <draco@marino.st>
2018-05-17 06:46:32 -0500

    add libofx port

---
+++ bucket_3D/libofx/patches/patch-lib_ofx__utilities.cpp
@@ -0,0 +1,14 @@
+--- lib/ofx_utilities.cpp.orig	2018-05-02 19:39:38 UTC
++++ lib/ofx_utilities.cpp
+@@ -138,6 +138,11 @@ time_t ofxdate_to_time_t(const string of
+   char time_zone_specified = false;
+   string ofxdate_whole;
+   time_t temptime;
++  int daylight = 0;
++
++  tzset();
++  if(tzname[1][0] != ' ')
++    daylight = 1;
+ 
+   std::time(&temptime);
+   local_offset = difftime(mktime(localtime(&temptime)), mktime(gmtime(&temptime)));
---
+++ bucket_3D/libofx/patches/patch-ofxconnect_ofxpartner.cpp
@@ -0,0 +1,10 @@
+--- ofxconnect/ofxpartner.cpp.orig	2016-09-10 08:01:22 UTC
++++ ofxconnect/ofxpartner.cpp
+@@ -37,6 +37,7 @@
+ #include <vector>
+ #include <algorithm>
+ #include <string.h>
++#include <unistd.h>
+ 
+ using std::string;
+ using std::vector;
