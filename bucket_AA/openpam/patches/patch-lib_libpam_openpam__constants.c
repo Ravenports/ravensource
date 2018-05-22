5a0e64333346a1804d053558d44b79f1eef5a9ef
John Marino <draco@marino.st>
2018-05-21 20:39:32 -0500

    openpam: use system pam modules (DF and FreeBSD)

---
+++ bucket_AA/openpam/patches/patch-lib_libpam_openpam__constants.c
@@ -0,0 +1,24 @@
+--- lib/libpam/openpam_constants.c.orig	2017-04-30 21:34:49 UTC
++++ lib/libpam/openpam_constants.c
+@@ -169,17 +169,13 @@ const char *pam_sm_func_name[PAM_NUM_PRI
+ const char *openpam_policy_path[] = {
+ 	"/etc/pam.d/",
+ 	"/etc/pam.conf",
+-	"/usr/local/etc/pam.d/",
+-	"/usr/local/etc/pam.conf",
++	"@PREFIX@/etc/pam.d/",
++	"@PREFIX@/etc/pam.conf",
+ 	NULL
+ };
+ 
+ const char *openpam_module_path[] = {
+-#ifdef OPENPAM_MODULES_DIRECTORY
+-	OPENPAM_MODULES_DIRECTORY,
+-#else
+-	"/usr/lib",
+-	"/usr/local/lib",
+-#endif
++	"@PREFIX@/lib/openpam",
++	@SYSTEM_MODULES@
+ 	NULL
+ };
