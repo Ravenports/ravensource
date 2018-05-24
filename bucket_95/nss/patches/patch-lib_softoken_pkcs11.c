1ed89938c39e0466237d92aabc2be5915b5c7f87
John Marino <draco@marino.st>
2018-05-24 14:40:36 -0500

    nss: fix build on sunos

---
+++ bucket_95/nss/patches/patch-lib_softoken_pkcs11.c
@@ -0,0 +1,13 @@
+--- lib/softoken/pkcs11.c.orig	2018-05-04 16:40:51 UTC
++++ lib/softoken/pkcs11.c
+@@ -3077,8 +3077,8 @@ nsc_CommonInitialize(CK_VOID_PTR pReserv
+         char buf[200];
+         int major = 0, minor = 0;
+ 
+-        long rv = sysinfo(SI_RELEASE, buf, sizeof(buf));
+-        if (rv > 0 && rv < sizeof(buf)) {
++        long sunrv = sysinfo(SI_RELEASE, buf, sizeof(buf));
++        if (sunrv > 0 && sunrv < sizeof(buf)) {
+             if (2 == sscanf(buf, "%d.%d", &major, &minor)) {
+                 /* Are we on Solaris 10 or greater ? */
+                 if (major > 5 || (5 == major && minor >= 10)) {
