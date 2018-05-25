fb4b6d2d6e752cd84b90bed1470dfa2088e8073e
John Marino <draco@marino.st>
2018-05-24 21:12:36 -0500

    openpam: finish sunos support (?)

---
+++ bucket_AA/openpam/patches/patch-lib_libpam_openpam__ttyconv.c
@@ -0,0 +1,33 @@
+--- lib/libpam/openpam_ttyconv.c.orig	2017-04-30 21:34:49 UTC
++++ lib/libpam/openpam_ttyconv.c
+@@ -57,6 +57,30 @@
+ #include "openpam_impl.h"
+ #include "openpam_strlset.h"
+ 
++#ifndef timeradd
++#define timeradd(tvp, uvp, vvp)						\
++	do {								\
++		(vvp)->tv_sec = (tvp)->tv_sec + (uvp)->tv_sec;		\
++		(vvp)->tv_usec = (tvp)->tv_usec + (uvp)->tv_usec;	\
++		if ((vvp)->tv_usec >= 1000000) {			\
++			(vvp)->tv_sec++;				\
++			(vvp)->tv_usec -= 1000000;			\
++		}							\
++	} while (0)
++#endif
++
++#ifndef timersub
++#define timersub(tvp, uvp, vvp)						\
++	do {								\
++		(vvp)->tv_sec = (tvp)->tv_sec - (uvp)->tv_sec;		\
++		(vvp)->tv_usec = (tvp)->tv_usec - (uvp)->tv_usec;	\
++		if ((vvp)->tv_usec < 0) {				\
++			(vvp)->tv_sec--;				\
++			(vvp)->tv_usec += 1000000;			\
++		}							\
++	} while (0)
++#endif
++
+ int openpam_ttyconv_timeout = 0;
+ 
+ static volatile sig_atomic_t caught_signal;
