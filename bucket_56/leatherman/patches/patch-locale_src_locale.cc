0ab41070814ed971c3856be66f50f7381ddbdeee
John Marino <draco@marino.st>
2018-06-18 12:08:20 +0200

    add leatherman port

---
+++ bucket_56/leatherman/patches/patch-locale_src_locale.cc
@@ -0,0 +1,12 @@
+--- locale/src/locale.cc.orig	2018-03-30 23:01:25 UTC
++++ locale/src/locale.cc
+@@ -43,6 +43,9 @@ namespace leatherman { namespace locale
+             gen.add_messages_domain(domain);
+         }
+ 
++        if (id.empty())
++            return g_locales.insert(make_pair(domain, std::locale())).first->second;
++
+         // Ensure creating and adding a new locale is thread-safe.
+         try {
+             return g_locales.insert(make_pair(domain, gen(id))).first->second;
