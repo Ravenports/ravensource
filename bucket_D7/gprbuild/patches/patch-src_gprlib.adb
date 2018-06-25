b7f2db2766e1b3c41e66aea4586e958936bd1c07
John Marino <draco@marino.st>
2018-06-24 19:46:02 -0500

    Revert "gprbuild: upgrade to 2018 version"
    This reverts commit 400c9a1add4ed70120a73b4b970803a723b24e30.
    
    GPRBuild 2018 fails with error frequently:
    raised STORAGE_ERROR : stack overflow or erroneous memory access
    seems to be centered around gprlib
    
    Stick to 2017 for another year, I guess.

--- bucket_D7/gprbuild/patches/patch-src_gprlib.adb
+++ bucket_D7/gprbuild/patches/patch-src_gprlib.adb
@@ -1,51 +1,52 @@
---- src/gprlib.adb.orig	2018-05-25 06:23:18 UTC
+--- src/gprlib.adb.orig	2016-12-23 15:59:49 UTC
 +++ src/gprlib.adb
-@@ -271,6 +271,11 @@ procedure Gprlib is
+@@ -413,6 +413,11 @@ procedure Gprlib is
  
-    Separate_Run_Path_Options     : Boolean := False;
+    Separate_Run_Path_Options : Boolean := False;
  
-+   --  If -R is passed through the library options for the linker, it will
-+   --  prevent the implemented libraries portion of the rpath switch from
-+   --  being built, even if the linker is capable of supporting rpath.
-+   Rpath_Disabled                : Boolean := False;
++      Rpath_Disabled : Boolean := False;
++      --  If -R is passed through the library options for the linker, it will
++      --  prevent the implemented libraries portion of the rpath switch from
++      --  being built, even if the linker is capable of supporting rpath.
 +
-    Rpath_Origin                  : String_Access := null;
+    Rpath : String_List_Access := null;
+    --  Allocated only if Path Option is supported
  
-    Rpath                         : String_Vectors.Vector;
-@@ -720,7 +725,7 @@ procedure Gprlib is
+@@ -908,7 +913,7 @@ procedure Gprlib is
+          Library_Switches_Table.Append
+            (new String'("-L" & Imported_Library_Directories.Table (J).all));
  
-       for Dir of Imported_Library_Directories loop
-          Library_Switches_Table.Append ("-L" & Dir);
 -         if Path_Option /= null then
 +         if not Rpath_Disabled and then Path_Option /= null then
-             Add_Rpath (Dir);
+             Add_Rpath (Imported_Library_Directories.Table (J));
           end if;
-       end loop;
-@@ -1468,9 +1473,7 @@ procedure Gprlib is
-                if Is_Regular_File (Object_Path) then
+ 
+@@ -1721,9 +1726,7 @@ procedure Gprlib is
+                if Is_Regular_File (Object_Path.all) then
                    Object_Files.Append (Object_Path);
                 else
 -                  Fail_Program
 -                    (null,
--                     "unknown object file """ & Object_Path & """");
-+                  Put_Line ("WARNING: unknown object '" & Object_Path & "'");
+-                     "unknown object file """ & Object_Path.all & """");
++                  Put_Line ("WARNING: unknown object """ & Object_Path.all & """");
                 end if;
              end;
           end loop;
-@@ -1872,7 +1875,11 @@ procedure Gprlib is
+@@ -2160,7 +2163,12 @@ procedure Gprlib is
                    Use_GNAT_Lib := False;
                 end if;
  
--               Library_Options_Table.Append (Line (1 .. Last));
+-               Library_Options_Table.Append (new String'(Line (1 .. Last)));
 +               if Line (1 .. Last) = "-R" then
 +                  Rpath_Disabled := True;
 +               else
-+                  Library_Options_Table.Append (Line (1 .. Last));
++                  Library_Options_Table.Append
++                    (new String'(Line (1 .. Last)));
 +               end if;
  
              when Gprexch.Library_Rpath_Options =>
-                Library_Rpath_Options_Table.Append (Line (1 .. Last));
-@@ -2002,10 +2009,10 @@ procedure Gprlib is
+                Library_Rpath_Options_Table.Append
+@@ -2295,10 +2303,10 @@ procedure Gprlib is
  
                       Libgnat :=
                         new String'
