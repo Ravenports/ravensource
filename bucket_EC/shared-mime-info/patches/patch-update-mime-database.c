1fa7803aa7bf6729fb6c8e456928ba76050b6bf0
John Marino <draco@marino.st>
2018-06-28 14:46:21 -0500

    shared-mime-info: upgrade and add license

--- bucket_EC/shared-mime-info/patches/patch-update-mime-database.c
+++ bucket_EC/shared-mime-info/patches/patch-update-mime-database.c
@@ -1,6 +1,6 @@
---- update-mime-database.c.orig	2017-02-11 17:26:13 UTC
+--- update-mime-database.c.orig	2018-04-24 13:09:19 UTC
 +++ update-mime-database.c
-@@ -633,7 +633,8 @@ static void remove_old(Type *type, xmlNo
+@@ -636,7 +636,8 @@ static void remove_old(Type *type, xmlNo
  		}
  	}
  
@@ -10,7 +10,7 @@
  }
  
  /* 'node' is a <mime-type> node from a source file, whose type is 'type'.
-@@ -2168,7 +2169,7 @@ static void check_in_path_xdg_data(const
+@@ -2173,7 +2174,7 @@ static void check_in_path_xdg_data(const
  
  	env = getenv("XDG_DATA_DIRS");
  	if (!env)
