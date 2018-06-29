afb68750f233af9246934f1a920d48e9631ee50e
John Marino <draco@marino.st>
2018-06-28 19:56:15 -0500

    freetds: fix build with libressl 2.7

--- bucket_93/freetds/patches/patch-src_tds_sec__negotiate__openssl.h
+++ bucket_93/freetds/patches/patch-src_tds_sec__negotiate__openssl.h
@@ -4,7 +4,7 @@
  #error HAVE_OPENSSL not defines, this file should not be included
  #endif
  
-+#ifdef LIBRESSL_VERSION_NUMBER
++#if defined(LIBRESSL_VERSION_NUMBER) && LIBRESSL_VERSION_NUMBER < 0x2070000fL
 +#undef OPENSSL_VERSION_NUMBER
 +#define OPENSSL_VERSION_NUMBER 0x1000107fL
 +#endif
