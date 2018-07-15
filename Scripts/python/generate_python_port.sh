bbaee1a06fa14467fc4194462a0eb8e5dbd2a922
John Marino <draco@marino.st>
2018-07-15 10:57:55 -0500

    fix pyyaml's redirect 'problem'

--- Scripts/python/generate_python_port.sh
+++ Scripts/python/generate_python_port.sh
@@ -193,7 +193,7 @@ get_filtered_url() {
       url=$(get_url)
       case ${url} in
          http://github.com/* | http://pypi.python.org/* | http://bitbucket.org/* | \
-         http://lxml.de/* | \
+         http://lxml.de/* | http://pyyaml.org/* | \
          http://certifi.io/* | http://docs.openstack.org/* | http://msgpack.org/* )
             echo ${url} | sed -e "s/^http:/https:/"
             ;;
