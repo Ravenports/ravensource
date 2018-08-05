7dfb9024f0443bfe87170593342a6fe109325bf7
Super-User <draco@marino.st>
2018-08-04 22:19:06 -0500

    libvolume: fix build on sunos

--- bucket_2F/libvolume_id/patches/patch-ddf__raid.c
+++ bucket_2F/libvolume_id/patches/patch-ddf__raid.c
@@ -1,11 +1,14 @@
---- linux_raid.c.orig	2007-12-27 01:03:43 UTC
-+++ linux_raid.c
-@@ -22,7 +22,7 @@
+--- ddf_raid.c.orig	2007-12-27 01:05:32 UTC
++++ ddf_raid.c
+@@ -22,9 +22,9 @@
  #include <string.h>
  #include <errno.h>
  #include <ctype.h>
 -#ifdef __FreeBSD__
 +#if defined(__FreeBSD__) || defined(__DragonFly__)
  #include <sys/endian.h>
- #else
+-#else
++#elif !defined(__APPLE__) && !defined(__sun__)
  #include <byteswap.h>
+ #endif /* __FreeBSD__ */
+ 
--- bucket_2F/libvolume_id/patches/patch-util.h
+++ bucket_2F/libvolume_id/patches/patch-util.h
@@ -9,3 +9,37 @@
  #include <sys/endian.h>
  
  #define __BYTE_ORDER            _BYTE_ORDER
+@@ -29,6 +29,33 @@
+ #define bswap_16                bswap16
+ #define bswap_32                bswap32
+ #define bswap_64                bswap64
++#elif defined(__APPLE__)
++#include <machine/endian.h>
++#include <libkern/OSByteOrder.h>
++
++#define __BYTE_ORDER            BYTE_ORDER
++#define __LITTLE_ENDIAN         LITTLE_ENDIAN
++#define __BIG_ENDIAN            BIG_ENDIAN
++
++#define bswap_16                OSSwapInt16
++#define bswap_32                OSSwapInt32
++#define bswap_64                OSSwapInt64
++#elif defined(__sun__)
++#include <sys/isa_defs.h>
++#include <sys/byteorder.h>
++
++#define __LITTLE_ENDIAN		1234
++#define __BIG_ENDIAN		4321
++#if defined(_BIG_ENDIAN)
++#define	__BYTE_ORDER		__BIG_ENDIAN
++#elif defined(_LITTLE_ENDIAN)
++#define __BYTE_ORDER		__LITTLE_ENDIAN
++#endif
++
++#define bswap_16		BSWAP_16
++#define bswap_32		BSWAP_32
++#define bswap_64		BSWAP_64
++
+ #else
+ #include <endian.h>
+ #include <byteswap.h>
