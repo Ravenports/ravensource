38873ac26a67fa3927e97176b2240e6691bb7bc2
John Marino <draco@marino.st>
2018-08-08 15:10:06 -0500

    move dynamic Ravenports/Mk files to ravensource

---
+++ Scripts/Ravenports_Mk/raven.information.mk
@@ -0,0 +1,202 @@
+# RESERVED_WARNING
+# This file is not directly used by the Raven framework.
+# It's sole purpose is to provide definitions that multiple specifications
+# need in a single location.  The data is extracted by using the
+# EXTRACT_INFO function on a specification definition, e.g.
+#
+# DEF[PORTVERSION]=		EXTRACT_INFO(GCC6_VERSION)
+# 
+# This file is only used at "compile-time", that is when the port
+# specification is used to generate the buildsheet.
+
+# ------------------------------------------------------------------------
+# APQ
+# ------------------------------------------------------------------------
+
+APQ_VERSION=			3.2.0
+APQ_BASE_REVISION=		0
+APQ_MYSQL_REVISION=		0
+APQ_PGSQL_REVISION=		0
+APQ_ODBC_REVISION=		0
+
+# ------------------------------------------------------------------------
+# boost
+# ------------------------------------------------------------------------
+
+BOOST_VERSION=			1.67.0
+BOOST_JAM_REVISION=		0
+BOOST_LIBRARIES_REVISION=	0
+
+# ------------------------------------------------------------------------
+# gcc6
+# ------------------------------------------------------------------------
+
+GCC6_BRANCH=			6
+GCC6_POINT=			4.0
+GCC6_VERSION=			${GCC6_BRANCH}.${GCC6_POINT}
+GCC6_SNAPSHOT=			20170704
+GCC6_BUILD_RELEASE=		yes
+GCC6_REVISION=			0
+GCC6_GNAT_UTIL_REVISION=	0
+
+.if ${GCC6_BUILD_RELEASE:Mno}
+GCC6_PORTVERSION=		${GCC6_BRANCH}.${GCC6_SNAPSHOT}
+GCC6_IDENTIFICATION=		gcc-${GCC6_BRANCH}-${GCC6_SNAPSHOT}
+GCC6_MS_SUBDIR=			snapshots/${GCC6_BRANCH}-${GCC6_SNAPSHOT}
+GCC6_PHASE=			snapshot
+.else
+GCC6_PORTVERSION=		${GCC6_VERSION}
+GCC6_IDENTIFICATION=		gcc-${GCC6_VERSION}
+GCC6_MS_SUBDIR=			releases/gcc-${GCC6_VERSION}
+GCC6_PHASE=			release
+.endif
+
+# ------------------------------------------------------------------------
+# gcc7
+# ------------------------------------------------------------------------
+
+GCC7_BRANCH=			7
+GCC7_POINT=			3.0
+GCC7_VERSION=			${GCC7_BRANCH}.${GCC7_POINT}
+GCC7_SNAPSHOT=			20180125
+GCC7_BUILD_RELEASE=		yes
+GCC7_REVISION=			0
+GCC7_GNAT_UTIL_REVISION=	0
+GCC7_GNATCROSS_REVISION=	0
+GCC7_BOOTSTRAP_REVISION=	0
+
+.if ${GCC7_BUILD_RELEASE:Mno}
+GCC7_PORTVERSION=		${GCC7_BRANCH}.${GCC7_SNAPSHOT}
+GCC7_IDENTIFICATION=		gcc-${GCC7_BRANCH}-${GCC7_SNAPSHOT}
+GCC7_MS_SUBDIR=			snapshots/${GCC7_BRANCH}-${GCC7_SNAPSHOT}
+GCC7_PHASE=			snapshot
+.else
+GCC7_PORTVERSION=		${GCC7_VERSION}
+GCC7_IDENTIFICATION=		gcc-${GCC7_VERSION}
+GCC7_MS_SUBDIR=			releases/gcc-${GCC7_VERSION}
+GCC7_PHASE=			release
+.endif
+
+# ------------------------------------------------------------------------
+# gcc8
+# ------------------------------------------------------------------------
+
+GCC8_BRANCH=			8
+GCC8_POINT=			2.0
+GCC8_VERSION=			${GCC8_BRANCH}.${GCC8_POINT}
+GCC8_SNAPSHOT=			20180726
+GCC8_BUILD_RELEASE=		yes
+GCC8_REVISION=			0
+GCC8_GNAT_UTIL_REVISION=	0
+GCC8_GNATCROSS_REVISION=	0
+GCC8_BOOTSTRAP_REVISION=	0
+
+.if ${GCC8_BUILD_RELEASE:Mno}
+GCC8_PORTVERSION=		${GCC8_BRANCH}.${GCC8_SNAPSHOT}
+GCC8_IDENTIFICATION=		gcc-${GCC8_BRANCH}-${GCC8_SNAPSHOT}
+GCC8_MS_SUBDIR=			snapshots/${GCC8_BRANCH}-${GCC8_SNAPSHOT}
+GCC8_PHASE=			snapshot
+.else
+GCC8_PORTVERSION=		${GCC8_VERSION}
+GCC8_IDENTIFICATION=		gcc-${GCC8_VERSION}
+GCC8_MS_SUBDIR=			releases/gcc-${GCC8_VERSION}
+GCC8_PHASE=			release
+.endif
+
+# ------------------------------------------------------------------------
+# icu
+# ------------------------------------------------------------------------
+
+ICU_VERSION=			62.1
+ICU_REVISION=			0
+ICU_LX_REVISION=		0
+
+# ------------------------------------------------------------------------
+# libxml2
+# ------------------------------------------------------------------------
+
+LIBXML2_VERSION=		2.9.8
+LIBXML2_REVISION=		0
+LIBXML2_PYTHON_REVISION=	0
+
+# ------------------------------------------------------------------------
+# libxslt
+# ------------------------------------------------------------------------
+
+LIBXSLT_VERSION=		1.1.32
+LIBXSLT_REVISION=		0
+LIBXSLT_PYTHON_REVISION=	0
+
+# ------------------------------------------------------------------------
+# ruby
+# ------------------------------------------------------------------------
+
+RUBY_2.3_REVISION=		0
+RUBY_2.3_PATCHLEVEL=		0
+
+RUBY_2.4_REVISION=		0
+RUBY_2.4_PATCHLEVEL=		0
+
+RUBY_2.5_REVISION=		0
+RUBY_2.5_PATCHLEVEL=		0
+
+# ------------------------------------------------------------------------
+# gnome
+# ------------------------------------------------------------------------
+
+GTK2_VERSION=			2.10.0
+GTK3_VERSION=			3.0.0
+GTK2_PORT_VERSION=		2.24.32
+GTK3_PORT_VERSION=		3.22.30
+
+# ------------------------------------------------------------------------
+# default gcc and binutils
+# ------------------------------------------------------------------------
+
+CURRENT_GCC=			gcc8	# mirror in Uses/meson.mk
+CURRENT_GCC_VERSION=		${GCC8_VERSION}
+BINUTILS_VERSION=		2.31.1
+
+# ------------------------------------------------------------------------
+# aspell
+# ------------------------------------------------------------------------
+
+ASPELL_CORE_VERSION=		0.60.6.1
+
+# ------------------------------------------------------------------------
+# apr1 and other apache projects
+# ------------------------------------------------------------------------
+
+APR1_VERSION=			1.6.3
+APR1_UTIL_VERSION=		1.6.1
+SUBVERSION_VERSION=		1.10.2
+
+# ------------------------------------------------------------------------
+# Miscellaneous master versions
+# ------------------------------------------------------------------------
+
+LIBFM_VERSION=			1.3.0.2
+LIBTOOL_VERSION=		2.4.6
+FREI0R_VERSION=			1.6.1
+M17N_VERSION=			1.8.0
+GLIBC_VERSION=			2.26
+GSTREAMER1_VERSION=		1.14.2
+GSTREAMER1_GST=			1.0
+GSTREAMER1_SOVERSION=		0.1402.0
+XORG_VERSION=			7.7
+FIREFOX52_VERSION=		52.9.0
+
+# ------------------------------------------------------------------------
+# Gnatcross options
+# ------------------------------------------------------------------------
+
+GNATCROSS_FREEBSD_11=		11.1
+GNATCROSS_FREEBSD_12=		12.0
+
+# ------------------------------------------------------------------------
+# Qt and Lumina versions
+# ------------------------------------------------------------------------
+
+QT5_VERSION=			5.11.1
+LUMINA_VERSION=			1.4.1
+LUMINA_RELEASE_TAG=		b6536c1
---
+++ Scripts/Ravenports_Mk/raven.versions.mk
@@ -0,0 +1,136 @@
+# RESERVED_WARNING
+# Provide default versions for ports with multiple versions selectable
+# by the user.
+#
+# Users who want to override these defaults can easily do so by defining
+# DEFAULT_VERSIONS in their make.conf as follows:
+#
+# DEFAULT_VERSIONS=	perl5=5.20 ruby=2.0
+#
+
+.if !defined(_INCLUDE_BSD_DEFAULT_VERSIONS_MK)
+_INCLUDE_BSD_DEFAULT_VERSIONS_MK=	yes
+
+.  for base in FIREBIRD LUA MYSQL PERL5 PHP PGSQL PYTHON3 RUBY SSL TCL
+.undef ${base}_DEFAULT
+.  endfor
+
+.  for base in ${DEFAULT_VERSIONS}
+_l=			${base:C/=.*//g}
+${_l:tu}_DEFAULT=	${base:C/.*=//g}
+.  endfor
+
+#-------------------------------------------------------------------------
+# PERL5
+# Possible values: 5.24, 5.26
+#-------------------------------------------------------------------------
+
+PERL5_DEFAULT?=		5.28
+
+PERL_5.26_VERSION=	5.26.2
+PERL_5.28_VERSION=	5.28.0
+
+#-------------------------------------------------------------------------
+# RUBY
+# Possible values: 2.3, 2.4, 2.5
+#-------------------------------------------------------------------------
+
+RUBY_DEFAULT?=		2.5
+
+RUBY_2.3_VERSION=	2.3.7
+RUBY_2.4_VERSION=	2.4.4
+RUBY_2.5_VERSION=	2.5.1
+
+#-------------------------------------------------------------------------
+# PYTHON
+# Possible values: 2.7, 3.4, 3.5
+#-------------------------------------------------------------------------
+
+PYTHON3_DEFAULT?=	3.7
+
+PYTHON_2.7_VERSION=	2.7.15
+PYTHON_3.6_VERSION=	3.6.6
+PYTHON_3.7_VERSION=	3.7.0
+
+#-------------------------------------------------------------------------
+# SSL
+# Possible values: openssl, openssl-devel, libressl, libressl-devel
+#-------------------------------------------------------------------------
+
+SSL_DEFAULT?=		libressl
+
+#-------------------------------------------------------------------------
+# LUA
+# Possible values: 5.2, 5.3
+#-------------------------------------------------------------------------
+
+LUA_DEFAULT?=		5.3
+
+LUA_5.2_VERSION=	5.2.4
+LUA_5.3_VERSION=	5.3.5
+
+#-------------------------------------------------------------------------
+# TCL/TK
+# Possible values: 8.5, 8.6
+#-------------------------------------------------------------------------
+
+TCL_DEFAULT?=		8.6
+
+TCL_8.5_VERSION=	8.5.19
+TCL_8.6_VERSION=	8.6.8
+
+#-------------------------------------------------------------------------
+# PGSQL
+# Possible values: 9.2, 9.3, 9.4, 9.5, 9.6, 10.0
+#-------------------------------------------------------------------------
+
+PGSQL_DEFAULT?=		10
+
+PGSQL_9.3_VERSION=	9.3.23
+PGSQL_9.4_VERSION=	9.4.18
+PGSQL_9.5_VERSION=	9.5.13
+PGSQL_9.6_VERSION=	9.6.9
+PGSQL_10_VERSION=	10.4
+
+#-------------------------------------------------------------------------
+# MYSQL and derivatives
+# Possible values: oracle-(5.5,5.6,5.7), mariadb-(10.1,10.2),
+#                  percona-(5.5,5.6,5.7), galera-(5.5,5.6,5.7)
+#-------------------------------------------------------------------------
+
+MYSQL_DEFAULT=			oracle-5.7
+
+MYSQL_oracle-5.7_VERSION=	5.7.23
+MYSQL_oracle-5.6_VERSION=	5.6.41
+MYSQL_oracle-5.5_VERSION=	5.5.61
+MYSQL_mariadb-10.2_VERSION=	10.2.5
+MYSQL_mariadb-10.1_VERSION=	10.1.22
+MYSQL_percona-5.7_VERSION=	5.7.17
+MYSQL_percona-5.6_VERSION=	5.6.35
+MYSQL_percona-5.5_VERSION=	5.5.54
+MYSQL_galera-5.7_VERSION=	5.7.17
+MYSQL_galera-5.6_VERSION=	5.6.35
+MYSQL_galera-5.5_VERSION=	5.5.54
+
+#-------------------------------------------------------------------------
+# Firebird database server
+# Possible values: 2.5, 3.0
+#-------------------------------------------------------------------------
+
+FIREBIRD_DEFAULT?=	2.5
+
+FIREBIRD_2.5_VERSION=	2.5.8
+FIREBIRD_3.0_VERSION=	3.0.3
+
+#-------------------------------------------------------------------------
+# PHP (restricted to branches still receiving updates)
+# Possible values: 5.6, 7.1, 7.2
+#-------------------------------------------------------------------------
+
+PHP_DEFAULT?=		7.2
+
+PHP_5.6_VERSION=	5.6.37
+PHP_7.1_VERSION=	7.1.20
+PHP_7.2_VERSION=	7.2.8
+
+.endif # defined (_INCLUDE_BSD_DEFAULT_VERSIONS_MK)
