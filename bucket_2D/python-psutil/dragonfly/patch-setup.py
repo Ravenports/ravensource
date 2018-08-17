591b73231da5afbf551bdf4023d34280c6e7bbd1
John Marino <draco@marino.st>
2018-08-17 10:18:16 -0500

    python-psutil: upgrade

--- bucket_2D/python-psutil/dragonfly/patch-setup.py
+++ bucket_2D/python-psutil/dragonfly/patch-setup.py
@@ -1,13 +1,13 @@
---- setup.py.orig	2018-05-08 15:56:36 UTC
+--- setup.py.orig	2018-08-13 23:51:39 UTC
 +++ setup.py
-@@ -33,6 +33,7 @@ from _common import FREEBSD  # NOQA
- from _common import LINUX  # NOQA
+@@ -35,6 +35,7 @@ from _common import LINUX  # NOQA
+ from _common import MACOS  # NOQA
  from _common import NETBSD  # NOQA
  from _common import OPENBSD  # NOQA
 +from _common import DRAGONFLY  # NOQA
- from _common import OSX  # NOQA
  from _common import POSIX  # NOQA
  from _common import SUNOS  # NOQA
+ from _common import WINDOWS  # NOQA
 @@ -193,6 +194,17 @@ elif NETBSD:
          ],
          define_macros=macros,
