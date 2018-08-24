0f2f9f7351a0f93315dcf24b2f4054d0e01e4052
John Marino <draco@marino.st>
2018-08-24 09:27:22 -0500

    add mg port

---
+++ bucket_19/mg/patches/patch-main.c
@@ -0,0 +1,13 @@
+--- main.c.orig	2018-07-13 09:16:01 UTC
++++ main.c
+@@ -34,6 +34,10 @@ struct mgwin	*curwp;				/* current windo
+ struct mgwin	*wheadp;			/* MGWIN listhead	*/
+ char		 pat[NPAT];			/* pattern		*/
+ 
++#ifndef __dead
++#define __dead __dead2
++#endif
++
+ static void	 edinit(struct buffer *);
+ static __dead void usage(void);
+ 
