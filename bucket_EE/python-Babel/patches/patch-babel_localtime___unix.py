e5b7297fec9539fb8fa54aff94b19e4944725d37
John Marino <john.marino@eltoro.com>
2018-09-15 20:15:09 -0500

    python-Babel: fix runtime on MacOS

---
+++ bucket_EE/python-Babel/patches/patch-babel_localtime___unix.py
@@ -0,0 +1,11 @@
+--- babel/localtime/_unix.py.orig	2018-05-28 09:13:13.000000000 +0000
++++ babel/localtime/_unix.py	2018-09-16 01:04:34.000000000 +0000
+@@ -71,7 +71,7 @@
+                              stdout=subprocess.PIPE)
+         sys_result = c.communicate()[0]
+         c.wait()
+-        tz_match = _systemconfig_tz.search(sys_result)
++        tz_match = _systemconfig_tz.search(sys_result.decode('utf-8'))
+         if tz_match is not None:
+             zone_name = tz_match.group(1)
+             try:
