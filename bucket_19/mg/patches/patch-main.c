3b000fa3756d8610bac6ec54319f1ec4fcc0a667
Leonid Bobrov <mazocomp@disroot.org>
2018-09-28 18:43:12 +0300

    mg: upgrade (#104)

--- bucket_19/mg/patches/patch-main.c
+++ bucket_19/mg/patches/patch-main.c
@@ -1,13 +1,11 @@
---- main.c.orig	2018-07-13 09:16:01 UTC
-+++ main.c
-@@ -34,6 +34,10 @@ struct mgwin	*curwp;				/* current windo
- struct mgwin	*wheadp;			/* MGWIN listhead	*/
+--- main.c.orig	2018-09-28 18:34:33.627045000 +0300
++++ main.c	2018-09-28 18:34:57.797531000 +0300
+@@ -35,7 +35,7 @@ struct mgwin	*wheadp;			/* MGWIN listhea
  char		 pat[NPAT];			/* pattern		*/
  
-+#ifndef __dead
+ #ifndef __dead
+-#define __dead __dead2
 +#define __dead __attribute__ ((__noreturn__))
-+#endif
-+
- static void	 edinit(struct buffer *);
- static __dead void usage(void);
+ #endif
  
+ static void	 edinit(struct buffer *);
