e2ad9211e68b2228ee7bdf42fa55175f0bbf7302
John Marino <draco@marino.st>
2018-10-05 08:38:19 -0500

    leatherman: upgrade

--- bucket_56/leatherman/patches/patch-locale_src_locale.cc
+++ bucket_56/leatherman/patches/patch-locale_src_locale.cc
@@ -1,6 +1,6 @@
---- locale/src/locale.cc.orig	2018-03-30 23:01:25 UTC
+--- locale/src/locale.cc.orig	2018-09-29 16:21:31 UTC
 +++ locale/src/locale.cc
-@@ -43,6 +43,9 @@ namespace leatherman { namespace locale
+@@ -44,6 +44,9 @@ namespace leatherman { namespace locale
              gen.add_messages_domain(domain);
          }
  
