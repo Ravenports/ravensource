6479c722bac1a6f85f888fef9b8ae245fb85d7aa
John Marino <john.marino@eltoro.com>
2018-10-08 21:26:10 -0500

    python36: fix build on macos

--- bucket_8E/python36/patches/patch-setup.py
+++ bucket_8E/python36/patches/patch-setup.py
@@ -1,4 +1,4 @@
---- setup.py.orig	2017-12-19 04:53:56 UTC
+--- setup.py.orig	2018-06-26 23:39:50 UTC
 +++ setup.py
 @@ -43,7 +43,10 @@ host_platform = get_platform()
  COMPILED_WITH_PYDEBUG = ('--with-pydebug' in sysconfig.get_config_var("CONFIG_ARGS"))
@@ -12,7 +12,18 @@
  
  def add_dir_to_list(dirlist, dir):
      """Add the directory 'dir' to the list 'dirlist' (after any relative
-@@ -1613,6 +1616,10 @@ class PyBuildExt(build_ext):
+@@ -1390,10 +1393,6 @@ class PyBuildExt(build_ext):
+                 # for OS X but we need _XOPEN_SOURCE_EXTENDED here for
+                 # ncurses wide char support
+                 curses_defines.append(('_XOPEN_SOURCE_EXTENDED', '1'))
+-        elif host_platform == 'darwin' and curses_library == 'ncurses':
+-            # Building with the system-suppied combined libncurses/libpanel
+-            curses_defines.append(('HAVE_NCURSESW', '1'))
+-            curses_defines.append(('_XOPEN_SOURCE_EXTENDED', '1'))
+ 
+         if curses_library.startswith('ncurses'):
+             curses_libs = [curses_library]
+@@ -1607,6 +1606,10 @@ class PyBuildExt(build_ext):
              macros = dict()
              libraries = []
  
@@ -23,7 +34,7 @@
          elif host_platform in ('freebsd4', 'freebsd5', 'freebsd6', 'freebsd7', 'freebsd8'):
              # FreeBSD's P1003.1b semaphore support is very experimental
              # and has many known problems. (as of June 2008)
-@@ -1652,7 +1659,7 @@ class PyBuildExt(build_ext):
+@@ -1646,7 +1649,7 @@ class PyBuildExt(build_ext):
          # End multiprocessing
  
          # Platform-specific libraries
