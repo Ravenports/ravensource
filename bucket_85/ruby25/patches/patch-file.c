94dcc939a9ad81605ab7babd6d403f035e3cd6ed
John Marino <john.marino@eltoro.com>
2018-10-11 16:28:26 -0500

    ruby25: add support for macos

---
+++ bucket_85/ruby25/patches/patch-file.c
@@ -0,0 +1,30 @@
+--- file.c.orig	2018-03-02 08:25:51 UTC
++++ file.c
+@@ -19,9 +19,6 @@
+ #include <sys/cygwin.h>
+ #include <wchar.h>
+ #endif
+-#ifdef __APPLE__
+-#include <CoreFoundation/CFString.h>
+-#endif
+ 
+ #include "id.h"
+ #include "internal.h"
+@@ -242,7 +239,7 @@ rb_str_encode_ospath(VALUE path)
+     return path;
+ }
+ 
+-#ifdef __APPLE__
++#if 0
+ # define NORMALIZE_UTF8PATH 1
+ static VALUE
+ rb_str_append_normalized_ospath(VALUE str, const char *ptr, long len)
+@@ -3680,7 +3677,7 @@ rb_file_expand_path_internal(VALUE fname
+ 	    b = ++s;
+ 	    break;
+ 	  default:
+-#ifdef __APPLE__
++#if 0
+ 	    {
+ 		int n = ignored_char_p(s, fend, enc);
+ 		if (n) {
