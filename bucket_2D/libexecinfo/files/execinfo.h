5a878029477b162d1b8674a7e138f4715160b3f8
John Marino <draco@marino.st>
2018-10-16 13:22:05 -0500

    libexecinfo: ensure *DECLS is defined

--- bucket_2D/libexecinfo/files/execinfo.h
+++ bucket_2D/libexecinfo/files/execinfo.h
@@ -3,6 +3,16 @@
 
 #include <stddef.h>
 
+# ifndef __BEGIN_DECLS
+#   ifdef __cplusplus
+#     define __BEGIN_DECLS	extern "C" {
+#     define __END_DECLS	}
+#   else
+#     define __BEGIN_DECLS
+#     define __END_DECLS
+#   endif
+# endif
+
 __BEGIN_DECLS
 size_t backtrace(void **, size_t);
 char **backtrace_symbols(void *const *, size_t);
