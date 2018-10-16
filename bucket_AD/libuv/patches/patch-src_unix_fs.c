66946b75c38cd9806a4c447ccc70939b096c8dde
John Marino <draco@marino.st>
2018-10-16 11:30:00 -0500

    libuv: fix linking on sunos

--- bucket_AD/libuv/patches/patch-src_unix_fs.c
+++ bucket_AD/libuv/patches/patch-src_unix_fs.c
@@ -16,3 +16,11 @@
  }
  
  
+@@ -656,7 +664,6 @@ static ssize_t uv__fs_sendfile(uv_fs_t*
+ static ssize_t uv__fs_utime(uv_fs_t* req) {
+ #if defined(__linux__)                                                         \
+     || defined(_AIX71)                                                         \
+-    || defined(__sun)
+   /* utimesat() has nanosecond resolution but we stick to microseconds
+    * for the sake of consistency with other platforms.
+    */
