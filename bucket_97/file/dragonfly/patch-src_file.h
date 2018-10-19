cfa2bbca26999fb701761aa31e44bd2fce347027
John Marino <draco@marino.st>
2018-10-19 08:28:08 -0500

    file: upgrade

--- bucket_97/file/dragonfly/patch-src_file.h
+++ bucket_97/file/dragonfly/patch-src_file.h
@@ -1,8 +1,8 @@
---- src/file.h.orig	2018-05-24 18:09:17 UTC
+--- src/file.h.orig	2018-10-15 16:29:16 UTC
 +++ src/file.h
-@@ -515,6 +515,7 @@ protected int buffer_fill(const struct b
+@@ -518,6 +518,7 @@ protected int buffer_fill(const struct b
+ 
  #include <locale.h>
- #endif
  #if defined(HAVE_XLOCALE_H)
 +#include <wchar.h>
  #include <xlocale.h>
