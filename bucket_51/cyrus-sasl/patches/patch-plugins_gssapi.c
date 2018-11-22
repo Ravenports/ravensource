540e44e59be8154ffc99e62552836c783c00b94e
John Marino <draco@marino.st>
2018-11-22 10:55:14 -0600

    cyrus-sasl: upgrade

--- bucket_51/cyrus-sasl/patches/patch-plugins_gssapi.c
+++ bucket_51/cyrus-sasl/patches/patch-plugins_gssapi.c
@@ -1,11 +1,11 @@
---- plugins/gssapi.c.orig	2012-01-27 23:31:36 UTC
+--- plugins/gssapi.c.orig	2018-11-08 17:29:57 UTC
 +++ plugins/gssapi.c
-@@ -1490,8 +1490,10 @@ static int gssapi_client_mech_step(void
-     *clientout = NULL;
-     *clientoutlen = 0;
+@@ -1668,8 +1668,10 @@ static int gssapi_client_mech_step(void
+     if (clientoutlen)
+         *clientoutlen = 0;
      
 +#if 0
-     params->utils->log(NULL, SASL_LOG_DEBUG,
+     params->utils->log(params->utils->conn, SASL_LOG_DEBUG,
  		       "GSSAPI client step %d", text->state);
 +#endif
  
