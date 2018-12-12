c92840990c8ad86123337856bf87cf713f2bec72
John Marino <draco@marino.st>
2018-12-12 08:16:45 -0600

    rust: WIP

---
+++ bucket_7B/rust/patches/patch-src_bootstrap_bin_rustc.rs
@@ -0,0 +1,11 @@
+--- src/bootstrap/bin/rustc.rs.orig	2018-12-04 23:41:40 UTC
++++ src/bootstrap/bin/rustc.rs
+@@ -233,7 +233,7 @@ fn main() {
+                 cmd.arg("-Z").arg("osx-rpath-install-name");
+                 Some("-Wl,-rpath,@loader_path/../lib")
+             } else if !target.contains("windows") && !target.contains("wasm32") {
+-                Some("-Wl,-rpath,$ORIGIN/../lib")
++                Some("-Wl,-rpath,@PREFIX@/lib")
+             } else {
+                 None
+             };
