7875098173ac93f4d30c7c19c90035a8449114c4
John Marino <draco@marino.st>
2018-12-24 11:37:51 -0600

    boost: upgrade

--- bucket_31/boost-libraries/patches/patch-boost_math_tools_config.hpp
+++ bucket_31/boost-libraries/patches/patch-boost_math_tools_config.hpp
@@ -1,4 +1,4 @@
---- boost/math/tools/config.hpp.orig	2017-09-02 09:56:14 UTC
+--- boost/math/tools/config.hpp.orig	2018-12-05 19:58:20 UTC
 +++ boost/math/tools/config.hpp
 @@ -28,7 +28,7 @@
  
@@ -8,4 +8,4 @@
 +#if (defined(__CYGWIN__) || defined(__NetBSD__) \
     || (defined(__hppa) && !defined(__OpenBSD__)) || (defined(__NO_LONG_DOUBLE_MATH) && (DBL_MANT_DIG != LDBL_MANT_DIG))) \
     && !defined(BOOST_MATH_NO_LONG_DOUBLE_MATH_FUNCTIONS)
- //#  define BOOST_MATH_NO_LONG_DOUBLE_MATH_FUNCTIONS
+ #  define BOOST_MATH_NO_LONG_DOUBLE_MATH_FUNCTIONS
