25fc77d2cd4baf6a06f2f336a0517d48b7e4477e
John Marino <draco@marino.st>
2019-01-03 14:06:54 -0600

    meson: restore build on sunos

--- bucket_B3/meson/sunos/patch-mesonbuild_compilers_compilers.py
+++ bucket_B3/meson/sunos/patch-mesonbuild_compilers_compilers.py
@@ -1,6 +1,6 @@
---- mesonbuild/compilers/compilers.py.orig	2018-11-08 22:43:05 UTC
+--- mesonbuild/compilers/compilers.py.orig	2018-12-09 14:27:23 UTC
 +++ mesonbuild/compilers/compilers.py
-@@ -166,7 +166,7 @@ apple_buildtype_linker_args = {'plain':
+@@ -177,7 +177,7 @@ apple_buildtype_linker_args = {'plain':
  gnulike_buildtype_linker_args = {'plain': [],
                                   'debug': [],
                                   'debugoptimized': [],
@@ -9,10 +9,10 @@
                                   'minsize': [],
                                   'custom': [],
                                   }
-@@ -1136,19 +1136,6 @@ class Compiler:
-             # linked against local libraries will fail to resolve them.
-             args.append('-Wl,-z,origin')
-         args.append('-Wl,-rpath,' + paths)
+@@ -1210,19 +1210,6 @@ class Compiler:
+                     paths = paths + ':' + padding
+             args.append('-Wl,-rpath,' + paths)
+ 
 -        if get_compiler_is_linuxlike(self):
 -            # Rpaths to use while linking must be absolute. These are not
 -            # written to the binary. Needed only with GNU ld:
@@ -29,7 +29,7 @@
          return args
  
      def thread_flags(self, env):
-@@ -1236,15 +1223,7 @@ def get_compiler_is_linuxlike(compiler):
+@@ -1354,15 +1341,7 @@ def get_compiler_is_linuxlike(compiler):
  def get_compiler_uses_gnuld(c):
      # FIXME: Perhaps we should detect the linker in the environment?
      # FIXME: Assumes that *BSD use GNU ld, but they might start using lld soon
@@ -46,12 +46,23 @@
  
  def get_largefile_args(compiler):
      '''
-@@ -1315,8 +1294,6 @@ class GnuLikeCompiler(abc.ABC):
+@@ -1433,8 +1412,6 @@ class GnuLikeCompiler(abc.ABC):
          self.compiler_type = compiler_type
          self.base_options = ['b_pch', 'b_lto', 'b_pgo', 'b_sanitize', 'b_coverage',
-                              'b_ndebug', 'b_staticpic', 'b_asneeded']
--        if not self.compiler_type.is_osx_compiler:
+                              'b_ndebug', 'b_staticpic', 'b_pie']
+-        if not self.compiler_type.is_osx_compiler and not self.compiler_type.is_windows_compiler:
 -            self.base_options.append('b_lundef')
+         if not self.compiler_type.is_windows_compiler:
+             self.base_options.append('b_asneeded')
          # All GCC-like backends can do assembly
-         self.can_compile_suffixes.add('s')
+@@ -1542,8 +1519,8 @@ class GnuLikeCompiler(abc.ABC):
+             # For PE/COFF this is impossible
+             return []
+         else:
+-            # GNU ld and LLVM lld
+-            return ['-Wl,--allow-shlib-undefined']
++            # sun linker
++            return []
  
+ 
+ class GnuCompiler(GnuLikeCompiler):
