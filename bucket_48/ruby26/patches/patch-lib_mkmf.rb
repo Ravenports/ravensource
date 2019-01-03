341a5034921d559ec27505076d5f7e817f23dea3
John Marino <draco@marino.st>
2019-01-02 19:02:36 -0600

    Add ruby26 (requires new ravenadm)

---
+++ bucket_48/ruby26/patches/patch-lib_mkmf.rb
@@ -0,0 +1,11 @@
+--- lib/mkmf.rb.orig	2018-12-04 08:22:10 UTC
++++ lib/mkmf.rb
+@@ -240,7 +240,7 @@ module MakeMakefile
+   end
+   $extmk ||= false
+   if not $extmk and File.exist?(($hdrdir = RbConfig::CONFIG["rubyhdrdir"]) + "/ruby/ruby.h")
+-    $topdir = $hdrdir
++    $topdir = $hdrdir + "/" + "#{CONFIG['arch']}/ruby/"
+     $top_srcdir = $hdrdir
+     $arch_hdrdir = RbConfig::CONFIG["rubyarchhdrdir"]
+   elsif File.exist?(($hdrdir = ($top_srcdir ||= topdir) + "/include")  + "/ruby.h")
