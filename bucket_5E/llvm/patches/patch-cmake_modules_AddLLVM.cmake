21da0c46831d2361b3996b3f6f2ad081ac5e03a8
John Marino <draco@marino.st>
2019-01-04 08:59:53 -0600

    llvm: fix linker detection on sunos

--- bucket_5E/llvm/patches/patch-cmake_modules_AddLLVM.cmake
+++ bucket_5E/llvm/patches/patch-cmake_modules_AddLLVM.cmake
@@ -1,6 +1,21 @@
 --- cmake/modules/AddLLVM.cmake.orig	2018-08-01 07:51:55 UTC
 +++ cmake/modules/AddLLVM.cmake
-@@ -218,9 +218,6 @@ function(add_link_opts target_name)
+@@ -183,13 +183,9 @@ elseif(NOT WIN32)
+   elseif("${stdout}" MATCHES "GNU ld")
+     set(LLVM_LINKER_IS_GNULD YES)
+     message(STATUS "Linker detection: GNU ld")
+-  elseif("${stderr}" MATCHES "Solaris Link Editors" OR
+-         "${stdout}" MATCHES "Solaris Link Editors")
++  else()
+     set(LLVM_LINKER_IS_SOLARISLD YES)
+     message(STATUS "Linker detection: Solaris ld")
+-  else()
+-    set(LLVM_LINKER_DETECTED NO)
+-    message(STATUS "Linker detection: unknown")
+   endif()
+ endif()
+ 
+@@ -218,9 +214,6 @@ function(add_link_opts target_name)
          # ld64's implementation of -dead_strip breaks tools that use plugins.
          set_property(TARGET ${target_name} APPEND_STRING PROPERTY
                       LINK_FLAGS " -Wl,-dead_strip")
