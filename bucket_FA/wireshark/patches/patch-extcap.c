c54bbc3dd6ba41cb1bed7ba8a7e6d2a77d40dee0
John Marino <draco@marino.st>
2019-01-04 12:51:20 -0600

    add wireshark port

---
+++ bucket_FA/wireshark/patches/patch-extcap.c
@@ -0,0 +1,10 @@
+--- extcap.c.orig	2018-11-28 18:28:19 UTC
++++ extcap.c
+@@ -21,6 +21,7 @@
+ #include <process.h>
+ #include <time.h>
+ #else
++#include <sys/wait.h>
+ /* Include for unlink */
+ #include <unistd.h>
+ #endif
