bbf0112844f40ee9fdee3679c8099145276d38e7
John Marino <draco@marino.st>
2019-01-05 09:03:41 -0600

    add python-borgbackup port

--- Scripts/python/generate_python_port.sh
+++ Scripts/python/generate_python_port.sh
@@ -527,6 +527,7 @@ function filter(dep) {\
    else if (dep == "python-sphinx") return "python-Sphinx";\
    else if (dep == "python-setuptools-scm") return "python-setuptools_scm";\
    else if (dep == "python-requests_toolbelt") return "python-requests-toolbelt";\
+   else if (dep == "python-msgpack-python") return "python-msgpack";\
    else return dep;\
 }\
 BEGIN {\
