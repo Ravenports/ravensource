60dc39cdd43407da2be60ca98cee0f927368eb47
John Marino <draco@marino.st>
2019-01-08 11:31:32 -0600

    add kerberos (MIT) port

---
+++ bucket_AC/krb5/patches/patch-plugins_preauth_pkinit_pkinit__crypto__openssl.h
@@ -0,0 +1,12 @@
+--- plugins/preauth/pkinit/pkinit_crypto_openssl.h.orig	2019-01-07 17:19:42 UTC
++++ plugins/preauth/pkinit/pkinit_crypto_openssl.h
+@@ -46,7 +46,8 @@
+ #include <openssl/asn1.h>
+ #include <openssl/pem.h>
+ 
+-#if OPENSSL_VERSION_NUMBER >= 0x10100000L
++#if (defined(OPENSSL_VERSION_NUMBER) && OPENSSL_VERSION_NUMBER >= 0x10100000L) && \
++     !defined(LIBRESSL_VERSION_NUMBER)
+ #include <openssl/asn1t.h>
+ #else
+ #include <openssl/asn1_mac.h>
