1a394f2dde0d887267fc02631c68c6c9bc08e98b
John Marino <draco@marino.st>
2019-01-14 12:25:16 -0600

    neovim: upgrade

--- bucket_03/neovim/patches/patch-src_nvim_os_pty__process__unix.c
+++ bucket_03/neovim/patches/patch-src_nvim_os_pty__process__unix.c
@@ -1,4 +1,4 @@
---- src/nvim/os/pty_process_unix.c.orig	2019-01-04 19:05:23 UTC
+--- src/nvim/os/pty_process_unix.c.orig	2019-01-13 13:25:08 UTC
 +++ src/nvim/os/pty_process_unix.c
 @@ -16,6 +16,78 @@
  # include <libutil.h>
