fd0fbd95ce2ed8238d22df70e89bb573c6c62938
John Marino <draco@marino.st>
2019-01-18 10:33:03 -0600

    tdb: upgrade

--- bucket_5F/tdb/patches/patch-buildtools_wafsamba_samba__autoconf.py
+++ bucket_5F/tdb/patches/patch-buildtools_wafsamba_samba__autoconf.py
@@ -1,30 +1,49 @@
 Disable libssp -- it causes segfaults on several platforms, likly some
 kind of missing os-specific support or consideration.
 
---- buildtools/wafsamba/samba_autoconf.py.orig	2018-07-12 05:51:30 UTC
+--- buildtools/wafsamba/samba_autoconf.py.orig	2019-01-14 22:24:44 UTC
 +++ buildtools/wafsamba/samba_autoconf.py
-@@ -673,25 +673,6 @@ def SAMBA_CONFIG_H(conf, path=None):
+@@ -685,44 +685,6 @@ def SAMBA_CONFIG_H(conf, path=None):
      if not IN_LAUNCH_DIR(conf):
          return
  
 -    # we need to build real code that can't be optimized away to test
--    if conf.check(fragment='''
--        #include <stdio.h>
+-    stack_protect_list = ['-fstack-protector-strong', '-fstack-protector']
+-    for stack_protect_flag in stack_protect_list:
+-        flag_supported = conf.check(fragment='''
+-                                    #include <stdio.h>
 -
--        int main(void)
--        {
--            char t[100000];
--            while (fgets(t, sizeof(t), stdin));
--            return 0;
--        }
--        ''',
--        execute=0,
--        ccflags='-fstack-protector',
--        ldflags='-fstack-protector',
--        mandatory=False,
--        msg='Checking if toolchain accepts -fstack-protector'):
--            conf.ADD_CFLAGS('-fstack-protector')
--            conf.ADD_LDFLAGS('-fstack-protector')
+-                                    int main(void)
+-                                    {
+-                                        char t[100000];
+-                                        while (fgets(t, sizeof(t), stdin));
+-                                        return 0;
+-                                    }
+-                                    ''',
+-                                    execute=0,
+-                                    cflags=[ '-Werror', '-Wp,-D_FORTIFY_SOURCE=2', stack_protect_flag],
+-                                    mandatory=False,
+-                                    msg='Checking if compiler accepts %s' % (stack_protect_flag))
+-        if flag_supported:
+-            conf.ADD_CFLAGS('%s' % (stack_protect_flag))
+-            break
+-
+-    flag_supported = conf.check(fragment='''
+-                                #include <stdio.h>
+-
+-                                int main(void)
+-                                {
+-                                    char t[100000];
+-                                    while (fgets(t, sizeof(t), stdin));
+-                                    return 0;
+-                                }
+-                                ''',
+-                                execute=0,
+-                                cflags=[ '-Werror', '-fstack-clash-protection'],
+-                                mandatory=False,
+-                                msg='Checking if compiler accepts -fstack-clash-protection')
+-    if flag_supported:
+-        conf.ADD_CFLAGS('-fstack-clash-protection')
 -
      if Options.options.debug:
          conf.ADD_CFLAGS('-g', testflags=True)
