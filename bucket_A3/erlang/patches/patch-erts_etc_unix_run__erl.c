e2d3b49f8ab6d2526fb9d55e572145918b43083b
John Marino <draco@marino.st>
2019-01-20 12:32:36 -0600

    erlang: upgrade

--- bucket_A3/erlang/patches/patch-erts_etc_unix_run__erl.c
+++ bucket_A3/erlang/patches/patch-erts_etc_unix_run__erl.c
@@ -1,4 +1,4 @@
---- erts/etc/unix/run_erl.c.orig	2018-03-09 10:01:10 UTC
+--- erts/etc/unix/run_erl.c.orig	2019-01-15 18:35:10 UTC
 +++ erts/etc/unix/run_erl.c
 @@ -81,9 +81,6 @@
  #ifdef HAVE_LIBUTIL_H
--- bucket_A3/erlang/patches/patch-lib_erl__interface_src_connect_ei__resolve.c
+++ bucket_A3/erlang/patches/patch-lib_erl__interface_src_connect_ei__resolve.c
@@ -1,6 +1,6 @@
---- lib/erl_interface/src/connect/ei_resolve.c.orig	2018-03-09 10:01:10 UTC
+--- lib/erl_interface/src/connect/ei_resolve.c.orig	2019-01-15 18:35:10 UTC
 +++ lib/erl_interface/src/connect/ei_resolve.c
-@@ -650,7 +650,8 @@ struct hostent *ei_gethostbyname_r(const
+@@ -653,7 +653,8 @@ struct hostent *ei_gethostbyname_r(const
  
    return result;
  #else
