df63492351d5258b646d014fa6a5060286d48a4b
John Marino <draco@marino.st>
2019-01-20 12:59:55 -0600

    facter: upgrade

--- bucket_4C/facter/patches/patch-lib_inc_internal_util_freebsd_geom.hpp
+++ bucket_4C/facter/patches/patch-lib_inc_internal_util_freebsd_geom.hpp
@@ -1,4 +1,4 @@
---- lib/inc/internal/util/freebsd/geom.hpp.orig	2018-09-14 15:27:42 UTC
+--- lib/inc/internal/util/freebsd/geom.hpp.orig	2018-12-03 19:36:37 UTC
 +++ lib/inc/internal/util/freebsd/geom.hpp
 @@ -6,6 +6,7 @@
  
