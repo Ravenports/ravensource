d6175f96ae5c19542fafb724d30c1216c8da43ac
John Marino <draco@marino.st>
2019-01-28 12:19:58 -0600

    libva: upgrade

--- bucket_1A/libva/patches/patch-va_va.c
+++ bucket_1A/libva/patches/patch-va_va.c
@@ -1,8 +1,8 @@
---- va/va.c.orig	2018-02-12 06:32:11 UTC
+--- va/va.c.orig	2019-01-25 18:53:40 UTC
 +++ va/va.c
-@@ -383,7 +383,7 @@ static VAStatus va_openDriver(VADisplay
-         strncat( driver_path, DRIVER_EXTENSION, strlen(DRIVER_EXTENSION) );
-         
+@@ -393,7 +393,7 @@ static VAStatus va_openDriver(VADisplay
+         }
+ 
          va_infoMessage(dpy, "Trying to open %s\n", driver_path);
 -#ifndef ANDROID
 +#ifdef RTLD_NODELETE
