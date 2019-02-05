1c73e87138fa8d6fa597625762090ca7fb940038
John Marino <draco@marino.st>
2019-02-04 22:44:13 -0600

    meson: upgrade

--- bucket_B3/meson/patches/patch-mesonbuild_dependencies_base.py
+++ bucket_B3/meson/patches/patch-mesonbuild_dependencies_base.py
@@ -1,7 +1,7 @@
---- mesonbuild/dependencies/base.py.orig	2019-01-23 16:46:09 UTC
+--- mesonbuild/dependencies/base.py.orig	2019-02-03 17:41:01 UTC
 +++ mesonbuild/dependencies/base.py
-@@ -1941,7 +1941,7 @@ class ExtraFrameworkDependency(ExternalD
- 
+@@ -1936,7 +1936,7 @@ class ExtraFrameworkDependency(ExternalD
+     def detect(self, name, path):
          lname = name.lower()
          if path is None:
 -            paths = ['/System/Library/Frameworks', '/Library/Frameworks']
