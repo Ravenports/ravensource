d1f3e48aa09b59c39a10d6c23c4a2d0d0f79cbfa
John Marino <draco@marino.st>
2019-02-12 08:39:10 -0600

    nodejs: rework
    A) allow functionality on dragonfly 5.4.1 and earlier (no relro)
    B) allow assembly version of bundled ssl
    C) regenerate patches

---
+++ bucket_96/nodejs/patches/patch-deps_openssl_config_dso__conf__asm.h
@@ -0,0 +1,11 @@
+--- deps/openssl/config/dso_conf_asm.h.orig	2019-01-30 20:43:12 UTC
++++ deps/openssl/config/dso_conf_asm.h
+@@ -23,7 +23,7 @@
+ # include "./archs/VC-WIN64A/asm/crypto/include/internal/dso_conf.h"
+ #elif (defined(__FreeBSD__) || defined(__OpenBSD__)) && defined(__i386__)
+ # include "./archs/BSD-x86/asm/crypto/include/internal/dso_conf.h"
+-#elif (defined(__FreeBSD__) || defined(__OpenBSD__)) && defined(__x86_64__)
++#elif (defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__DragonFly__)) && defined(__x86_64__)
+ # include "./archs/BSD-x86_64/asm/crypto/include/internal/dso_conf.h"
+ #elif defined(__sun) && defined(__i386__)
+ # include "./archs/solaris-x86-gcc/asm/crypto/include/internal/dso_conf.h"
--- bucket_96/nodejs/patches/patch-tools_gyp_pylib_gyp_common.py
+++ bucket_96/nodejs/patches/patch-tools_gyp_pylib_gyp_common.py
@@ -1,4 +1,4 @@
---- tools/gyp/pylib/gyp/common.py.orig	2019-01-24 23:19:09 UTC
+--- tools/gyp/pylib/gyp/common.py.orig	2019-01-30 20:43:39 UTC
 +++ tools/gyp/pylib/gyp/common.py
 @@ -423,6 +423,8 @@ def GetFlavor(params):
      return 'solaris'
