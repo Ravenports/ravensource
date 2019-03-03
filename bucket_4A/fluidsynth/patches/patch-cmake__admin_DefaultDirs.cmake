4d1a84deba86c517d07ef915b4fd5007fb5b928c
John Marino <draco@marino.st>
2019-03-03 12:11:48 -0600

    fluidsynth: upgrade

---
+++ bucket_4A/fluidsynth/patches/patch-cmake__admin_DefaultDirs.cmake
@@ -0,0 +1,11 @@
+--- cmake_admin/DefaultDirs.cmake.orig	2019-02-22 09:15:18 UTC
++++ cmake_admin/DefaultDirs.cmake
+@@ -53,7 +53,7 @@ set (INFO_INSTALL_DIR "share/info" CACHE
+ mark_as_advanced (INFO_INSTALL_DIR) 
+ 
+ # MAN_INSTALL_DIR - the man pages install dir
+-if ( CMAKE_SYSTEM_NAME MATCHES "FreeBSD|DragonFly")
++if ( CMAKE_SYSTEM_NAME MATCHES "nothing")
+   set (MAN_INSTALL_DIR "man/man1" CACHE STRING "The man pages install dir")
+ else()
+   set (MAN_INSTALL_DIR "share/man/man1" CACHE STRING "The man pages install dir")
