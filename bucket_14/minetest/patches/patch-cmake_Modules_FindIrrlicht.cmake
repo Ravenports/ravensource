c8c8796494fcabf0064db82eeb6d832e26da3efc
Leonid Bobrov <mazocomp@disroot.org>
2019-03-10 22:12:43 +0200

    Update Minetest to 5.0.0 (#175)

--- bucket_14/minetest/patches/patch-cmake_Modules_FindIrrlicht.cmake
+++ bucket_14/minetest/patches/patch-cmake_Modules_FindIrrlicht.cmake
@@ -1,4 +1,4 @@
---- cmake/Modules/FindIrrlicht.cmake.orig	2018-06-10 15:18:11 UTC
+--- cmake/Modules/FindIrrlicht.cmake.orig	2019-03-04 22:43:14 UTC
 +++ cmake/Modules/FindIrrlicht.cmake
 @@ -7,7 +7,7 @@ set(IRRLICHT_SOURCE_DIR "" CACHE PATH "P
  
@@ -9,18 +9,19 @@
  	)
  
  	set(IRRLICHT_LIBRARY_NAMES libIrrlicht.a Irrlicht Irrlicht.lib)
-@@ -41,12 +41,14 @@ else()
+@@ -41,6 +41,7 @@ else()
  	find_path(IRRLICHT_INCLUDE_DIR NAMES irrlicht.h
  		PATHS
  		/usr/local/include/irrlicht
 +		/raven/include/irrlicht
  		/usr/include/irrlicht
+ 		/system/develop/headers/irrlicht #Haiku
  	)
- 
+@@ -48,6 +49,7 @@ else()
  	find_library(IRRLICHT_LIBRARY NAMES libIrrlicht.so libIrrlicht.a Irrlicht
  		PATHS
  		/usr/local/lib
 +		/raven/lib
  		/usr/lib
+ 		/system/develop/lib # Haiku
  	)
- endif()
