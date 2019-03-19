d6a31f6c34029d25c075ab8bfc6c7ecca198b2a1
Super-User <draco@marino.st>
2019-03-19 15:22:16 -0500

    boost-libraries: fix build on sunos

---
+++ bucket_31/boost-libraries/patches/patch-boost_thread_pthread_thread__data.hpp
@@ -0,0 +1,20 @@
+--- boost/thread/pthread/thread_data.hpp.orig	2018-12-05 19:58:23 UTC
++++ boost/thread/pthread/thread_data.hpp
+@@ -32,6 +32,7 @@
+ # endif
+ #endif
+ 
++#include <limits.h>
+ #include <pthread.h>
+ #include <unistd.h>
+ 
+@@ -57,9 +58,7 @@ namespace boost
+ #else
+           std::size_t page_size = ::sysconf( _SC_PAGESIZE);
+ #endif
+-#if PTHREAD_STACK_MIN > 0
+           if (size<PTHREAD_STACK_MIN) size=PTHREAD_STACK_MIN;
+-#endif
+           size = ((size+page_size-1)/page_size)*page_size;
+           int res = pthread_attr_setstacksize(&val_, size);
+           BOOST_VERIFY(!res && "pthread_attr_setstacksize failed");
