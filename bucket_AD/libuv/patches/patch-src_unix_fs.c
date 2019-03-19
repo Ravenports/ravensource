62d511156581f11569a102a8718f49fd49f78a0c
Super-User <draco@marino.st>
2019-03-19 00:10:05 -0500

    libuv: simplify (ravenadm 1.27)

--- bucket_AD/libuv/patches/patch-src_unix_fs.c
+++ bucket_AD/libuv/patches/patch-src_unix_fs.c
@@ -1,21 +1,5 @@
 --- src/unix/fs.c.orig	2019-02-10 16:44:47 UTC
 +++ src/unix/fs.c
-@@ -221,7 +221,15 @@ static ssize_t uv__fs_futime(uv_fs_t* re
- 
- 
- static ssize_t uv__fs_mkdtemp(uv_fs_t* req) {
-+#ifdef __sun__
-+  char *tmplt;
-+  tmplt = mktemp((char*) req->path);
-+  if (tmplt == NULL)
-+	return (-1);
-+  return (mkdir (tmplt, 0700));
-+#else
-   return mkdtemp((char*) req->path) ? 0 : -1;
-+#endif
- }
- 
- 
 @@ -704,7 +712,6 @@ static ssize_t uv__fs_sendfile(uv_fs_t*
  static ssize_t uv__fs_utime(uv_fs_t* req) {
  #if defined(__linux__)                                                         \
