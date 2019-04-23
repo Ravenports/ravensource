75f4692c978e40f45547608dd6e5f34dbe0d26f0
John Marino <draco@marino.st>
2019-04-22 23:53:26 -0500

    minizip: fix build on dragonfly

---
+++ bucket_C3/minizip/patches/patch-contrib_minizip_ioapi.h
@@ -0,0 +1,11 @@
+--- ioapi.h.orig	2012-01-17 02:51:31 UTC
++++ ioapi.h
+@@ -50,7 +50,7 @@
+ #define ftello64 ftell
+ #define fseeko64 fseek
+ #else
+-#ifdef __FreeBSD__
++#if defined(__FreeBSD__) || defined(__DragonFly__)
+ #define fopen64 fopen
+ #define ftello64 ftello
+ #define fseeko64 fseeko
