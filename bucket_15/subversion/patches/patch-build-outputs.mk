855b88debf2e034aa23967d47e2487f9298f8216
John Marino <draco@marino.st>
2019-04-27 15:45:38 -0500

    upgrade subversion

--- bucket_15/subversion/patches/patch-build-outputs.mk
+++ bucket_15/subversion/patches/patch-build-outputs.mk
@@ -1,8 +1,8 @@
---- build-outputs.mk.orig	2015-10-15 01:24:35.493881000 +0300
-+++ build-outputs.mk	2015-10-15 01:25:35.653321000 +0300
-@@ -1316,11 +1316,11 @@
+--- build-outputs.mk.orig	2019-04-11 13:21:27 UTC
++++ build-outputs.mk
+@@ -1354,11 +1354,11 @@ install-swig-rb-lib: subversion/bindings
  
- install-tools: tools/diff/diff$(EXEEXT) tools/diff/diff3$(EXEEXT) tools/diff/diff4$(EXEEXT) tools/dev/fsfs-access-map$(EXEEXT) tools/server-side/mod_dontdothat/mod_dontdothat.la tools/server-side/svn-populate-node-origins-index$(EXEEXT) tools/server-side/svnauthz$(EXEEXT) tools/server-side/svnauthz-validate$(EXEEXT) tools/dev/svnraisetreeconflict/svnraisetreeconflict$(EXEEXT) tools/dev/x509-parser$(EXEEXT)
+ install-tools: tools/diff/diff$(EXEEXT) tools/diff/diff3$(EXEEXT) tools/diff/diff4$(EXEEXT) tools/dev/fsfs-access-map$(EXEEXT) tools/server-side/mod_dontdothat/mod_dontdothat.la tools/client-side/svn-mergeinfo-normalizer/svn-mergeinfo-normalizer$(EXEEXT) tools/server-side/svn-populate-node-origins-index$(EXEEXT) tools/server-side/svnauthz$(EXEEXT) tools/server-side/svnauthz-validate$(EXEEXT) tools/client-side/svnconflict/svnconflict$(EXEEXT) tools/dev/svnmover/svnmover$(EXEEXT) tools/dev/svnraisetreeconflict/svnraisetreeconflict$(EXEEXT) tools/dev/x509-parser$(EXEEXT)
  	$(MKDIR) $(DESTDIR)$(toolsdir)
 -	cd tools/diff ; $(INSTALL_TOOLS) diff$(EXEEXT) $(DESTDIR)$(toolsdir)/diff$(EXEEXT)
 +	cd tools/diff ; $(INSTALL_TOOLS) diff$(EXEEXT) $(DESTDIR)$(toolsdir)/svndiff$(EXEEXT)
