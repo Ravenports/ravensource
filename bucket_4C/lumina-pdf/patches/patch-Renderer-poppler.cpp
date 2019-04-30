eca5577e0230e24a5d865e1bdcef6a243fdc278b
John Marino <draco@marino.st>
2019-04-30 15:15:41 -0500

    upgrade lumina (multimedia pending)

---
+++ bucket_4C/lumina-pdf/patches/patch-Renderer-poppler.cpp
@@ -0,0 +1,7 @@
+--- Renderer-poppler.cpp.orig	2019-04-27 17:41:41 UTC
++++ Renderer-poppler.cpp
+@@ -1,3 +1,4 @@
++#include <memory>
+ #include "Renderer.h"
+ #include "drawablepage.h"
+ #include "link.h"
