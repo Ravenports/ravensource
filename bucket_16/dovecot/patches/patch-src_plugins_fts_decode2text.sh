97fbe40b33f57a62cb16f9286a6efde7cacc9f40
John Marino <draco@marino.st>
2019-04-30 13:36:18 -0500

    dovecot: upgrade

--- bucket_16/dovecot/patches/patch-src_plugins_fts_decode2text.sh
+++ bucket_16/dovecot/patches/patch-src_plugins_fts_decode2text.sh
@@ -1,7 +1,7 @@
 /usr gets changed to ${LOCALBASE} in post-patch:, so we
 cheat and set xpdf's path to /usr/lib.
 
---- src/plugins/fts/decode2text.sh.orig	2019-03-25 08:43:27 UTC
+--- src/plugins/fts/decode2text.sh.orig	2019-04-30 12:25:06 UTC
 +++ src/plugins/fts/decode2text.sh
 @@ -79,16 +79,20 @@ wait_timeout() {
  LANG=en_US.UTF-8
