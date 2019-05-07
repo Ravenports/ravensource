d9014b5ee90c59a94387152c387c7dfe30b69635
John Marino <draco@marino.st>
2019-05-06 22:31:31 -0500

    mupdf: upgrade

--- bucket_E0/mupdf/patches/patch-platform__x11__pdfapp.c
+++ bucket_E0/mupdf/patches/patch-platform__x11__pdfapp.c
@@ -1,6 +1,6 @@
---- platform/x11/pdfapp.c.orig	2018-10-03 11:29:14 UTC
+--- platform/x11/pdfapp.c.orig	2019-05-06 11:53:09 UTC
 +++ platform/x11/pdfapp.c
-@@ -1310,6 +1310,8 @@ void pdfapp_onkey(pdfapp_t *app, int c,
+@@ -1191,6 +1191,8 @@ void pdfapp_onkey(pdfapp_t *app, int c,
  	case 'j':
  		{
  			int h = fz_pixmap_height(app->ctx, app->image);
@@ -9,7 +9,7 @@
  			if (h <= app->winh || app->pany <= app->winh - h)
  			{
  				panto = PAN_TO_TOP;
-@@ -1326,6 +1328,8 @@ void pdfapp_onkey(pdfapp_t *app, int c,
+@@ -1207,6 +1209,8 @@ void pdfapp_onkey(pdfapp_t *app, int c,
  	case 'k':
  		{
  			int h = fz_pixmap_height(app->ctx, app->image);
@@ -18,7 +18,7 @@
  			if (h <= app->winh || app->pany == 0)
  			{
  				panto = PAN_TO_BOTTOM;
-@@ -1401,6 +1405,7 @@ void pdfapp_onkey(pdfapp_t *app, int c,
+@@ -1282,6 +1286,7 @@ void pdfapp_onkey(pdfapp_t *app, int c,
  	 */
  
  	case ',':
@@ -26,7 +26,7 @@
  		panto = PAN_TO_BOTTOM;
  		if (app->numberlen > 0)
  			app->pageno -= atoi(app->number);
-@@ -1409,6 +1414,7 @@ void pdfapp_onkey(pdfapp_t *app, int c,
+@@ -1290,6 +1295,7 @@ void pdfapp_onkey(pdfapp_t *app, int c,
  		break;
  
  	case '.':
