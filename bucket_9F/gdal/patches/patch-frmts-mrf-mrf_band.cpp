f77cdcf2519cd60832a2e21b484eb3720fc0651f
John Marino <draco@marino.st>
2019-05-09 17:16:44 -0500

    gdal: upgrade

--- bucket_9F/gdal/patches/patch-frmts-mrf-mrf_band.cpp
+++ bucket_9F/gdal/patches/patch-frmts-mrf-mrf_band.cpp
@@ -1,4 +1,4 @@
---- frmts/mrf/mrf_band.cpp.orig	2018-05-04 16:07:31 UTC
+--- frmts/mrf/mrf_band.cpp.orig	2019-05-05 09:46:29 UTC
 +++ frmts/mrf/mrf_band.cpp
 @@ -49,7 +49,7 @@
  
@@ -7,5 +7,5 @@
 -#include "../zlib/zlib.h"
 +#include <zlib.h>
  
- CPL_CVSID("$Id: mrf_band.cpp 7e07230bbff24eb333608de4dbd460b7312839d0 2017-12-11 19:08:47Z Even Rouault $")
+ CPL_CVSID("$Id: mrf_band.cpp 09abe392b2b229ee2ee7625cfc5d6509c57f4f17 2019-02-02 13:13:07 -0800 Lucian Plesea $")
  
