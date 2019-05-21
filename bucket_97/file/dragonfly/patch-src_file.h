e2105dcecdc64f2a628afcdd45c20b692fbb4f8f
John Marino <draco@marino.st>
2019-05-21 16:16:07 -0500

    file: upgrade

--- bucket_97/file/dragonfly/patch-src_file.h
+++ bucket_97/file/dragonfly/patch-src_file.h
@@ -1,6 +1,6 @@
---- src/file.h.orig	2019-02-20 02:32:11 UTC
+--- src/file.h.orig	2019-05-07 02:27:11 UTC
 +++ src/file.h
-@@ -518,6 +518,7 @@ protected int buffer_fill(const struct b
+@@ -520,6 +520,7 @@ protected int buffer_fill(const struct b
  
  #include <locale.h>
  #if defined(HAVE_XLOCALE_H)
