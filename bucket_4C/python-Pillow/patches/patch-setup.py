a17da9eee1144b3766545e1db3ababf9e40e49a1
John Marino <draco@marino.st>
2019-07-07 15:30:46 -0500

    python-Pillow: regen patch

--- bucket_4C/python-Pillow/patches/patch-setup.py
+++ bucket_4C/python-Pillow/patches/patch-setup.py
@@ -1,13 +1,13 @@
---- setup.py.orig	2019-01-01 10:29:12 UTC
+--- setup.py.orig	2019-07-02 19:13:39 UTC
 +++ setup.py
-@@ -410,9 +410,7 @@ class pil_build_ext(build_ext):
-                 _add_directory(library_dirs, "/usr/X11/lib")
+@@ -465,9 +465,7 @@ class pil_build_ext(build_ext):
                  _add_directory(include_dirs, "/usr/X11/include")
  
--        elif sys.platform.startswith("linux") or \
--                sys.platform.startswith("gnu") or \
--                sys.platform.startswith("freebsd"):
-+        elif sys.platform.startswith("nothing"):
+         elif (
+-            sys.platform.startswith("linux")
+-            or sys.platform.startswith("gnu")
+-            or sys.platform.startswith("freebsd")
++            sys.platform.startswith("nothing")
+         ):
              for dirname in _find_library_dirs_ldconfig():
                  _add_directory(library_dirs, dirname)
-             if sys.platform.startswith("linux") and \
