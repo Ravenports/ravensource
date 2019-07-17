80da3a3b598c6686cde267bf28fbb88a03671005
Michael Reim <kraileth@elderlinux.org>
2019-07-17 06:33:22 +0200

    cmake: upgrade

--- bucket_93/cmake/patches/patch-Modules_FindSDL.cmake
+++ bucket_93/cmake/patches/patch-Modules_FindSDL.cmake
@@ -1,4 +1,4 @@
---- Modules/FindSDL.cmake.orig	2019-05-14 16:43:26 UTC
+--- Modules/FindSDL.cmake.orig	2019-07-16 11:19:54 UTC
 +++ Modules/FindSDL.cmake
 @@ -86,6 +86,11 @@ else()
    set(VC_LIB_PATH_SUFFIX lib/x86)
