17c9444e7583f8315ca29023d8b8a8b7716b1605
Michael Reim <kraileth@elderlinux.org>
2019-07-30 18:31:24 +0200

    puppet: upgrade

--- bucket_98/puppet/patches/patch-lib_puppet_provider_service_daemontools.rb
+++ bucket_98/puppet/patches/patch-lib_puppet_provider_service_daemontools.rb
@@ -1,6 +1,6 @@
 Daemontools program locations
 
---- lib/puppet/provider/service/daemontools.rb.orig	2019-04-17 20:35:03 UTC
+--- lib/puppet/provider/service/daemontools.rb.orig	2019-07-25 16:45:47 UTC
 +++ lib/puppet/provider/service/daemontools.rb
 @@ -39,7 +39,7 @@ Puppet::Type.type(:service).provide :dae
  
