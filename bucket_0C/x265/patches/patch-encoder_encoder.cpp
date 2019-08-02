ba95fae0139b86865d906c689617f074295b1aaf
Michael Reim <kraileth@elderlinux.org>
2019-08-02 05:33:52 +0200

    x265: upgrade

--- bucket_0C/x265/patches/patch-encoder_encoder.cpp
+++ bucket_0C/x265/patches/patch-encoder_encoder.cpp
@@ -2,9 +2,9 @@ $NetBSD: patch-encoder_encoder.cpp,v 1.3 2018/05/25 14:42:07 jperkin Exp $
 
 Fix error: call of overloaded 'pow(int, int)' is ambiguous
 
---- encoder/encoder.cpp.orig	2019-01-23 09:47:18 UTC
+--- encoder/encoder.cpp.orig	2019-07-08 11:10:19 UTC
 +++ encoder/encoder.cpp
-@@ -83,6 +83,7 @@ DolbyVisionProfileSpec dovi[] =
+@@ -84,6 +84,7 @@ DolbyVisionProfileSpec dovi[] =
  static const char* defaultAnalysisFileName = "x265_analysis.dat";
  
  using namespace X265_NS;
