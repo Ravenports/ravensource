07946dfa4aec2f75e4b4224ea3ccab9aa5355ad2
Michael Reim <kraileth@elderlinux.org>
2019-08-30 17:02:48 +0200

    nano: upgrade

--- bucket_E6/nano/patches/patch-lib_cdefs.h
+++ bucket_E6/nano/patches/patch-lib_cdefs.h
@@ -1,4 +1,4 @@
---- lib/cdefs.h.orig	2019-04-24 09:02:29 UTC
+--- lib/cdefs.h.orig	2019-06-18 08:28:30 UTC
 +++ lib/cdefs.h
 @@ -292,7 +292,7 @@
     for portability when this file is used in Gnulib.  */
