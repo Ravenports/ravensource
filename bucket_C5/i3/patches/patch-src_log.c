7b0e69e0d92eb635354226c6ccb6edfab13987f1
Michael Reim <kraileth@elderlinux.org>
2019-09-02 20:18:00 +0200

    i3: upgrade

--- bucket_C5/i3/patches/patch-src_log.c
+++ bucket_C5/i3/patches/patch-src_log.c
@@ -1,8 +1,8 @@
---- src/log.c.orig	2019-01-27 15:45:19 UTC
+--- src/log.c.orig	2019-08-03 13:14:38 UTC
 +++ src/log.c
 @@ -128,7 +128,7 @@ void open_logbuffer(void) {
-          * At the moment (2011-12-10), no testcase leads to an i3 log
-          * of more than ~ 600 KiB. */
+      * At the moment (2011-12-10), no testcase leads to an i3 log
+      * of more than ~ 600 KiB. */
      logbuffer_size = min(physical_mem_bytes * 0.01, shmlog_size);
 -#if defined(__FreeBSD__)
 +#if defined(__FreeBSD__) || defined(__DragonFly__)
