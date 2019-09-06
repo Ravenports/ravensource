0a8a2acb297cbe44eb4f304c8f4e6989d7b2b94f
Michael Reim <kraileth@elderlinux.org>
2019-09-06 18:12:49 +0200

    upower: upgrade (and switch back to official distfiles now that they are releasing them again)

--- bucket_53/upower/freebsd/patch-src_freebsd_up-devd.c
+++ bucket_53/upower/freebsd/patch-src_freebsd_up-devd.c
@@ -1,5 +1,5 @@
---- src/freebsd/up-devd.c.orig	2013-09-15 13:08:35.000000000 -0400
-+++ src/freebsd/up-devd.c	2013-09-15 13:10:13.000000000 -0400
+--- src/freebsd/up-devd.c.orig	2019-03-05 14:18:32 UTC
++++ src/freebsd/up-devd.c
 @@ -195,7 +195,7 @@ up_devd_event_cb (GIOChannel *source, GI
  		event[terminator] = 0;
  		up_devd_process_event(event, user_data);
