5a07b56c6c862534e06414843597ee9cd042dc8c
John Marino <draco@marino.st>
2019-09-20 14:44:08 -0500

    nspr: upgrade

--- bucket_D2/nspr/dragonfly/patch-nsprpub_pr_include_md__pth.h
+++ bucket_D2/nspr/dragonfly/patch-nsprpub_pr_include_md__pth.h
@@ -1,28 +1,28 @@
---- pr/include/md/_pth.h.orig	2019-03-07 10:04:05 UTC
+--- pr/include/md/_pth.h.orig	2019-08-19 14:12:12 UTC
 +++ pr/include/md/_pth.h
 @@ -86,7 +86,7 @@
   */
- #if defined(IRIX) || defined(OSF1) || defined(AIX) || defined(SOLARIS) \
+ #if defined(AIX) || defined(SOLARIS) \
  	|| defined(LINUX) || defined(__GNU__) || defined(__GLIBC__) \
 -	|| defined(HPUX) || defined(FREEBSD) \
 +	|| defined(HPUX) || defined(FREEBSD) || defined(__DragonFly__) \
  	|| defined(NETBSD) || defined(OPENBSD) || defined(BSDI) \
  	|| defined(NTO) || defined(DARWIN) \
- 	|| defined(UNIXWARE) || defined(RISCOS)	|| defined(SYMBIAN)
-@@ -140,7 +140,7 @@
+ 	|| defined(UNIXWARE) || defined(RISCOS)
+@@ -133,7 +133,7 @@
  #define PT_PRIO_MIN            sched_get_priority_min(SCHED_OTHER)
  #define PT_PRIO_MAX            sched_get_priority_max(SCHED_OTHER)
  #elif defined(LINUX) || defined(__GNU__) || defined(__GLIBC__) \
--	|| defined(FREEBSD) || defined(SYMBIAN)
-+	|| defined(FREEBSD) || defined(SYMBIAN) || defined(__DragonFly__)
+-	|| defined(FREEBSD)
++	|| defined(FREEBSD) || defined(__DragonFly__)
  #define PT_PRIO_MIN            sched_get_priority_min(SCHED_OTHER)
  #define PT_PRIO_MAX            sched_get_priority_max(SCHED_OTHER)
  #elif defined(NTO)
-@@ -197,6 +197,7 @@ extern int (*_PT_aix_yield_fcn)();
+@@ -176,6 +176,7 @@ extern int (*_PT_aix_yield_fcn)();
  #elif defined(HPUX) || defined(SOLARIS) \
  	|| defined(LINUX) || defined(__GNU__) || defined(__GLIBC__) \
  	|| defined(FREEBSD) || defined(NETBSD) || defined(OPENBSD) \
 +	|| defined(__DragonFly__) \
  	|| defined(BSDI) || defined(NTO) || defined(DARWIN) \
- 	|| defined(UNIXWARE) || defined(RISCOS) || defined(SYMBIAN)
+ 	|| defined(UNIXWARE) || defined(RISCOS)
  #define _PT_PTHREAD_YIELD()            	sched_yield()
