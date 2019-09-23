6642d72c10dcbe30a362cc02d0e05e961a760b73
John Marino <draco@marino.st>
2019-09-23 13:29:03 -0500

    findutils: upgrade

--- bucket_BE/findutils/patches/patch-gnulib-lib-fdopendir.c
+++ bucket_BE/findutils/patches/patch-gnulib-lib-fdopendir.c
@@ -1,6 +1,6 @@
---- gl/lib/fdopendir.c.orig	2015-12-24 16:41:42 UTC
+--- gl/lib/fdopendir.c.orig	2019-08-29 19:14:53 UTC
 +++ gl/lib/fdopendir.c
-@@ -191,9 +191,11 @@ fd_clone_opendir (int fd, struct saved_c
+@@ -227,9 +227,11 @@ fd_clone_opendir (int fd, struct saved_c
  
  # include <errno.h>
  # include <sys/stat.h>
--- bucket_BE/findutils/patches/patch-locate__updatedb.sh
+++ bucket_BE/findutils/patches/patch-locate__updatedb.sh
@@ -1,6 +1,6 @@
---- locate/updatedb.sh.orig	2015-12-28 21:10:23 UTC
+--- locate/updatedb.sh.orig	2019-06-23 09:00:40 UTC
 +++ locate/updatedb.sh
-@@ -295,7 +295,7 @@ cd "$changeto"
+@@ -292,7 +292,7 @@ cd "$changeto"
  if test -n "$SEARCHPATHS"; then
    if [ "$LOCALUSER" != "" ]; then
      # : A1
@@ -9,28 +9,10 @@
      "$find $SEARCHPATHS $FINDOPTIONS \
       \\( $prunefs_exp \
       -type d -regex '$PRUNEREGEX' \\) -prune -o $print_option"
-@@ -311,7 +311,7 @@ if test -n "$NETPATHS"; then
+@@ -308,7 +308,7 @@ if test -n "$NETPATHS"; then
  myuid=`getuid`
  if [ "$myuid" = 0 ]; then
      # : A3
--    su $NETUSER `select_shell $NETUSER` -c \
-+    su -m $NETUSER -c \
-      "$find $NETPATHS $FINDOPTIONS \\( -type d -regex '$PRUNEREGEX' -prune \\) -o $print_option" ||
-     exit $?
-   else
-@@ -362,7 +362,7 @@ cd "$changeto"
- if test -n "$SEARCHPATHS"; then
-   if [ "$LOCALUSER" != "" ]; then
-     # : A5
--    su $LOCALUSER `select_shell $LOCALUSER` -c \
-+    su -m $LOCALUSER -c \
-     "$find $SEARCHPATHS $FINDOPTIONS \
-      \( $prunefs_exp \
-      -type d -regex '$PRUNEREGEX' \) -prune -o $print_option" || exit $?
-@@ -378,7 +378,7 @@ if test -n "$NETPATHS"; then
-   myuid=`getuid`
-   if [ "$myuid" = 0 ]; then
-     # : A7
 -    su $NETUSER `select_shell $NETUSER` -c \
 +    su -m $NETUSER -c \
       "$find $NETPATHS $FINDOPTIONS \\( -type d -regex '$PRUNEREGEX' -prune \\) -o $print_option" ||
