749094af59532617a03ac140ed1e90b6fa8ceee2
John Marino <draco@marino.st>
2019-09-26 11:48:38 -0500

    graphviz: upgrade

---
+++ bucket_45/graphviz/patches/patch-lib-ortho-trapezoid.c
@@ -0,0 +1,14 @@
+--- lib/ortho/trapezoid.c.orig	2019-09-12 02:11:30 UTC
++++ lib/ortho/trapezoid.c
+@@ -26,6 +26,11 @@
+ #include <memory.h>
+ #include <trap.h>
+ 
++#include <sys/param.h>
++#if __FreeBSD_version <= 704101 || (__FreeBSD_version >= 800000 && __FreeBSD_version < 802502) || (__FreeBSD_version >= 900000 && __FreeBSD_version < 900027)
++#define log2(x)  (log(x)/log(2))
++#endif
++
+ /* Node types */
+ 
+ #define T_X     1
