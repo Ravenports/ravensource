afc4d4deaa3d2f9849f77e358d6ed928c50bdba3
John Marino <draco@marino.st>
2019-10-14 13:10:01 -0500

    rdesktop: upgrade

--- bucket_C0/rdesktop/patches/patch-xwin.c
+++ bucket_C0/rdesktop/patches/patch-xwin.c
@@ -1,11 +1,20 @@
---- xwin.c.orig	2019-05-09 08:14:04 UTC
+--- xwin.c.orig	2019-06-13 12:10:15 UTC
 +++ xwin.c
-@@ -2160,6 +2160,8 @@ ui_resize_window()
- 		XFreePixmap(g_display, g_backstore);
- 		g_backstore = bs;
+@@ -48,7 +48,7 @@
+ #define HOST_NAME_MAX MAXHOSTNAMELEN
+ #endif
+ 
+-#ifdef __FreeBSD__
++#if defined __FreeBSD__ || defined __DragonFly__
+ #define HOST_NAME_MAX _POSIX_HOST_NAME_MAX
+ #endif
+ 
+@@ -2167,6 +2167,8 @@ get_sizehints(XSizeHints * sizehints, ui
+ 		sizehints->min_width = sizehints->max_width = width;
+ 		sizehints->min_height = sizehints->max_height = height;
  	}
 +
 +	ui_reset_clip();
  }
  
- RD_BOOL
+ void
