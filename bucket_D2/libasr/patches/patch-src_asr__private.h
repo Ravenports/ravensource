33a53b46240ee40a3d0a3c29b70c7f3c2ef5ac06
John Marino <draco@marino.st>
2019-10-28 13:50:17 -0500

    libasr: upgrade

---
+++ bucket_D2/libasr/patches/patch-src_asr__private.h
@@ -0,0 +1,10 @@
+--- src/asr_private.h.orig	2019-09-28 14:09:47 UTC
++++ src/asr_private.h
+@@ -16,6 +16,7 @@
+  */
+ 
+ #include <stdio.h>
++#include <netinet/in.h>
+ 
+ #include "asr_compat.h"
+ 
---
+++ bucket_D2/libasr/patches/patch-src_res__search__async.c
@@ -0,0 +1,12 @@
+--- src/res_search_async.c.orig	2019-09-28 14:09:47 UTC
++++ src/res_search_async.c
+@@ -22,6 +22,9 @@
+ #include <sys/uio.h>
+ #include <arpa/nameser.h>
+ #include <netdb.h>
++#ifdef __DragonFly__
++#include <netinet/in.h>
++#endif
+ 
+ #include <asr.h>
+ #include <errno.h>
