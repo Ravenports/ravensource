29f398789a74cab2f14aa6cfb93337c969267465
John Marino <draco@marino.st>
2019-10-29 09:15:22 -0500

    keepassxc: upgrade

--- bucket_A2/keepassxc/patches/patch-src_core_Alloc.cpp
+++ bucket_A2/keepassxc/patches/patch-src_core_Alloc.cpp
@@ -1,15 +1,19 @@
---- src/core/Alloc.cpp.orig	2019-05-31 20:03:47 UTC
+--- src/core/Alloc.cpp.orig	2019-10-26 19:34:32 UTC
 +++ src/core/Alloc.cpp
-@@ -21,7 +21,7 @@
- #ifdef Q_OS_MACOS
+@@ -21,10 +21,10 @@
+ #include <sodium.h>
+ #if defined(Q_OS_MACOS)
  #include <malloc/malloc.h>
+-#elif defined(Q_OS_FREEBSD)
++#elif defined(__FreeBSD__)
+ #include <malloc_np.h>
  #else
 -#include <malloc.h>
 +#include <stdlib.h>
  #endif
  
  #if defined(NDEBUG) && !defined(__cpp_sized_deallocation)
-@@ -61,8 +61,6 @@ void operator delete(void* ptr) noexcept
+@@ -64,8 +64,6 @@ void operator delete(void* ptr) noexcept
      ::operator delete(ptr, _msize(ptr));
  #elif defined(Q_OS_MACOS)
      ::operator delete(ptr, malloc_size(ptr));
