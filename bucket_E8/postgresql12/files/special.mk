8ab4d8ffd491c4d0505fb05d598e2dc6ff008132
John Marino <draco@marino.st>
2019-10-29 17:17:34 -0500

    add new postgresql 12 port

---
+++ bucket_E8/postgresql12/files/special.mk
@@ -0,0 +1,22 @@
+.if ${OPSYS:MFreeBSD}
+TEMPLATE_ARG=	--with-template=freebsd
+UUID_ARG=	--with-uuid=bsd
+.elif ${OPSYS:MDragonFly}
+TEMPLATE_ARG=	--with-template=freebsd
+UUID_ARG=	--with-uuid=bsd
+.elif ${OPSYS:MNetBSD}
+TEMPLATE_ARG=	--with-template=netbsd
+UUID_ARG=	--with-uuid=bsd
+.elif ${OPSYS:MOpenBSD}
+TEMPLATE_ARG=	--with-template=openbsd
+UUID_ARG=	--with-uuid=bsd
+.elif ${OPSYS:MDarwin}
+TEMPLATE_ARG=	--with-template=darwin
+UUID_ARG=	--with-uuid=e2fs
+.elif ${OPSYS:MSunOS}
+TEMPLATE_ARG=	--with-template=solaris
+UUID_ARG=	--with-uuid=ossp
+.elif ${OPSYS:MLinux}
+TEMPLATE_ARG=	--with-template=linux
+UUID_ARG=	--with-uuid=ossp
+.endif
