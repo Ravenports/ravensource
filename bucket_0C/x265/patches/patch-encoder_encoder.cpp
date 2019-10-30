d692c12ce66b975c8a5f646438827f335c8e64ed
John Marino <draco@marino.st>
2019-10-30 10:22:15 -0500

    x265: upgrade

--- bucket_0C/x265/patches/patch-encoder_encoder.cpp
+++ bucket_0C/x265/patches/patch-encoder_encoder.cpp
@@ -2,7 +2,7 @@ $NetBSD: patch-encoder_encoder.cpp,v 1.3 2018/05/25 14:42:07 jperkin Exp $
 
 Fix error: call of overloaded 'pow(int, int)' is ambiguous
 
---- encoder/encoder.cpp.orig	2019-07-31 04:59:54 UTC
+--- encoder/encoder.cpp.orig	2019-10-29 11:55:05 UTC
 +++ encoder/encoder.cpp
 @@ -84,6 +84,7 @@ DolbyVisionProfileSpec dovi[] =
  static const char* defaultAnalysisFileName = "x265_analysis.dat";
