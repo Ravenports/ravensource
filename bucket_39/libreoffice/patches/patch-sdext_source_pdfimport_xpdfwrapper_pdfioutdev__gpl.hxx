d44f61cb9716b9726e42d2b5bc61b9838011caa5
John Marino <draco@marino.st>
2019-10-31 09:53:18 -0500

    libreoffice: fix build after update to poppler 0.82

---
+++ bucket_39/libreoffice/patches/patch-sdext_source_pdfimport_xpdfwrapper_pdfioutdev__gpl.hxx
@@ -0,0 +1,20 @@
+--- sdext/source/pdfimport/xpdfwrapper/pdfioutdev_gpl.hxx.orig	2019-09-18 14:12:06 UTC
++++ sdext/source/pdfimport/xpdfwrapper/pdfioutdev_gpl.hxx
+@@ -235,7 +235,7 @@ namespace pdfi
+         virtual void drawChar(GfxState *state, double x, double y,
+                               double dx, double dy,
+                               double originX, double originY,
+-                              CharCode code, int nBytes, Unicode *u, int uLen) override;
++                              CharCode code, int nBytes, Unicode *u, int uLen);
+ #if POPPLER_CHECK_VERSION(0, 64, 0)
+         virtual void drawString(GfxState *state, const GooString *s) override;
+ #else
+@@ -251,7 +251,7 @@ namespace pdfi
+         virtual void drawImage(GfxState *state, Object *ref, Stream *str,
+                                int width, int height, GfxImageColorMap *colorMap,
+                                poppler_bool interpolate,
+-                               int* maskColors, poppler_bool inlineImg) override;
++                               int* maskColors, poppler_bool inlineImg);
+         virtual void drawMaskedImage(GfxState *state, Object *ref, Stream *str,
+                                      int width, int height,
+                                      GfxImageColorMap *colorMap,
