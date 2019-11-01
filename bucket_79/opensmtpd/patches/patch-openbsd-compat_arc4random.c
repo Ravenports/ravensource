301c074ba42ec134e4848a3d98f020909ddfd088
John Marino <draco@marino.st>
2019-11-01 12:28:11 -0500

    opensmtpd: fix build on linux

---
+++ bucket_79/opensmtpd/patches/patch-openbsd-compat_arc4random.c
@@ -0,0 +1,18 @@
+--- openbsd-compat/arc4random.c.orig	2019-10-26 11:54:19 UTC
++++ openbsd-compat/arc4random.c
+@@ -163,6 +163,7 @@ _rs_random_u32(uint32_t *val)
+ 	return;
+ }
+ 
++#if !defined(HAVE_ARC4RANDOM) && !defined(LIBRESSL_VERSION_NUMBER)
+ void
+ arc4random_stir(void)
+ {
+@@ -170,6 +171,7 @@ arc4random_stir(void)
+ 	_rs_stir();
+ 	_ARC4_UNLOCK();
+ }
++#endif
+ 
+ void
+ arc4random_addrandom(u_char *dat, int datlen)
