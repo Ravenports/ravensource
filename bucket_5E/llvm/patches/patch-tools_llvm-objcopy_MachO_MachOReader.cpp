45fdd4d7565c65adda4e178fa94c694df05f552c
Super-User <draco@marino.st>
2019-11-03 07:03:17 -0600

    llvm: fix build on sunos

---
+++ bucket_5E/llvm/patches/patch-tools_llvm-objcopy_MachO_MachOReader.cpp
@@ -0,0 +1,21 @@
+--- tools/llvm-objcopy/MachO/MachOReader.cpp.orig	2019-06-24 23:39:01 UTC
++++ tools/llvm-objcopy/MachO/MachOReader.cpp
+@@ -13,6 +13,18 @@
+ #include "llvm/Object/MachO.h"
+ #include <memory>
+ 
++#ifdef __sun__
++static size_t
++strnlen(const char *s, size_t maxlen)
++{
++	size_t len;
++	for (len = 0; len < maxlen; len++, s++) {
++		if (!*s) break;
++	}
++	return (len);
++}
++#endif
++
+ namespace llvm {
+ namespace objcopy {
+ namespace macho {
