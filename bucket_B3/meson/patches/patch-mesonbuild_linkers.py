e8c1633ae7fee3b9ccab9beda41700aac5f57211
John Marino <draco@marino.st>
2019-11-04 13:16:10 -0600

    fix solaris whole-archive correctly

--- bucket_B3/meson/patches/patch-mesonbuild_linkers.py
+++ bucket_B3/meson/patches/patch-mesonbuild_linkers.py
@@ -1,13 +1,16 @@
 --- mesonbuild/linkers.py.orig	2019-09-28 23:52:33 UTC
 +++ mesonbuild/linkers.py
-@@ -876,9 +876,7 @@ class SolarisDynamicLinker(PosixDynamicL
-     """Sys-V derived linker used on Solaris and OpenSolaris."""
- 
+@@ -878,7 +878,12 @@ class SolarisDynamicLinker(PosixDynamicL
      def get_link_whole_for(self, args: typing.List[str]) -> typing.List[str]:
--        if not args:
--            return args
+         if not args:
+             return args
 -        return self._apply_prefix('--whole-archive') + args + self._apply_prefix('--no-whole-archive')
-+        return args
++        result = [self.prefix_arg + '-z', self.prefix_arg + 'allextract']
++        for a in args:
++            result.append(a)
++        result.append(self.prefix_arg + '-z')
++        result.append(self.prefix_arg + 'defaultextract')
++        return result
  
      def no_undefined_args(self) -> typing.List[str]:
          return ['-z', 'defs']
