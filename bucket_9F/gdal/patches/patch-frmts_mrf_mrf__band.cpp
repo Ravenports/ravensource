05b416ab18d43214afe37845c04ba03dcfd467d2
John Marino <draco@marino.st>
2019-11-05 12:47:14 -0600

    gdal: upgrade

---
+++ bucket_9F/gdal/patches/patch-frmts_mrf_mrf__band.cpp
@@ -0,0 +1,11 @@
+--- frmts/mrf/mrf_band.cpp.orig	2019-10-28 10:02:13 UTC
++++ frmts/mrf/mrf_band.cpp
+@@ -49,7 +49,7 @@
+ 
+ #include <vector>
+ #include <assert.h>
+-#include "zlib.h"
++#include <zlib.h>
+ 
+ CPL_CVSID("$Id: mrf_band.cpp c91d6b1341d26132fbf6e85f0350230a45406db1 2019-10-18 18:50:38 +0200 Even Rouault $")
+ 
