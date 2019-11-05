919bdfb9fe81486a80ae28d439ee03a3094e3266
John Marino <draco@marino.st>
2019-11-05 16:47:45 -0600

    qemu: upgrade

--- bucket_A9/qemu/patches/patch-disas_libvixl_vixl_a64_disasm-a64.cc
+++ bucket_A9/qemu/patches/patch-disas_libvixl_vixl_a64_disasm-a64.cc
@@ -1,6 +1,6 @@
---- disas/libvixl/vixl/a64/disasm-a64.cc.orig	2019-10-01 22:31:44 UTC
+--- disas/libvixl/vixl/a64/disasm-a64.cc.orig	2019-04-23 18:14:45 UTC
 +++ disas/libvixl/vixl/a64/disasm-a64.cc
-@@ -2693,7 +2693,7 @@ void Disassembler::AppendPCRelativeOffse
+@@ -2693,7 +2693,7 @@ void Disassembler::AppendPCRelativeOffsetToOutput(cons
    if (offset < 0) {
      abs_offset = -abs_offset;
    }
