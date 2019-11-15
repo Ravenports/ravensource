d33a367ea598e7c45bce8b736a9b6f8769953a07
John Marino <draco@marino.st>
2019-11-15 09:39:11 -0600

    upgrade perl ports

--- Scripts/python/generate_python_port.sh
+++ Scripts/python/generate_python_port.sh
@@ -217,6 +217,7 @@ get_filtered_url() {
          http://*djangosuit.com* | \
          http://*.readthedocs.io/* | \
          http://*.nicfit.net/* | \
+         http://*pygments.org/* | \
          http://certifi.io/* | http://docs.openstack.org/* | http://msgpack.org/* )
             echo ${url} | sed -e "s/^http:/https:/"
             ;;
