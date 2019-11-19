926262846f9d859963c3f15dcc5e780b00596450
John Marino <draco@marino.st>
2019-11-18 20:09:43 -0600

    norm: follow move to github

--- bucket_63/norm/patches/patch-protolib_setup.py
+++ bucket_63/norm/patches/patch-protolib_setup.py
@@ -1,4 +1,4 @@
---- protolib/setup.py.orig	2014-03-01 16:17:04 UTC
+--- protolib/setup.py.orig	2019-11-11 14:54:17 UTC
 +++ protolib/setup.py
 @@ -23,7 +23,7 @@ system = platform.system().lower()
  sys_macros = [('HAVE_ASSERT',None), ('HAVE_IPV6',None), ('PROTO_DEBUG', None)]
