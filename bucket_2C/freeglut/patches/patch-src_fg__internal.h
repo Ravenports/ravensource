29f3472c917f9d7e026cae61b81cb7368d43cd79
John Marino <draco@marino.st>
2019-11-19 00:04:42 -0600

    freeglut: disable joystick on freebsd to fix build

--- bucket_2C/freeglut/patches/patch-src_fg__internal.h
+++ bucket_2C/freeglut/patches/patch-src_fg__internal.h
@@ -1,6 +1,16 @@
 --- src/fg_internal.h.orig	2019-09-25 10:08:07 UTC
 +++ src/fg_internal.h
-@@ -1067,6 +1067,8 @@ int         fgJoystickDetect( void );
+@@ -137,9 +137,6 @@
+  * If so, perhaps there should be a config_guess.h for them. Alternatively,
+  * config guesses could be placed above, just after the config.h exclusion.
+  */
+-#if defined(__FreeBSD__) || defined(__NetBSD__)
+-#    define HAVE_USB_JS 1
+-#endif
+ 
+ #if defined(_MSC_VER) || defined(__WATCOMC__)
+ /* strdup() is non-standard, for all but POSIX-2001 */
+@@ -1067,6 +1064,8 @@ int         fgJoystickDetect( void );
  void        fgInitialiseJoysticks( void );
  void        fgJoystickClose( void );
  void        fgJoystickPollWindow( SFG_Window* window );
