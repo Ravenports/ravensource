646cb7ef43aeade083c5cfd33d8b43f27902df29
John Marino <draco@marino.st>
2019-11-26 06:55:58 -0600

    libtickit: upgrade

--- bucket_39/libtickit/patches/patch-src_term.c
+++ bucket_39/libtickit/patches/patch-src_term.c
@@ -1,6 +1,6 @@
---- src/term.c.orig	2019-04-12 15:53:40 UTC
+--- src/term.c.orig	2019-11-18 17:09:53 UTC
 +++ src/term.c
-@@ -33,6 +33,10 @@
+@@ -27,6 +27,10 @@
  #include <sys/select.h>
  #include <sys/time.h>
  
