b6d018428a6b7088bc50c42bbf546bfc789f9d23
John Marino <draco@marino.st>
2019-11-26 08:49:39 -0600

    clean up libublio (fix build on dragonfly post-gcc9)

--- bucket_EE/libublio/files/compile.sh
+++ bucket_EE/libublio/files/compile.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-cc -O2 -pipe -MD -MF.depend.ublio.o -MTublio.o -std=gnu99 -fstack-protector-strong -c ublio.c -o ublio.o
+cc ${CFLAGS} -MD -MF.depend.ublio.o -MTublio.o -c ublio.c -o ublio.o
 ar -crD libublio.a `NM='nm' NMFLAGS='' lorder ublio.o  | tsort -q` 
 ranlib -D libublio.a
-cc -pg  -O2 -pipe -MD -MF.depend.ublio.po -MTublio.po -std=gnu99 -fstack-protector-strong -c ublio.c -o ublio.po
+cc -p  ${CFLAGS} -MD -MF.depend.ublio.po -MTublio.po -c ublio.c -o ublio.po
 ar -crD libublio_p.a `NM='nm' NMFLAGS='' lorder ublio.po  | tsort -q` 
 ranlib -D libublio_p.a
-cc -fpic -DPIC -g -O2 -pipe -MD -MF.depend.ublio.pico -MTublio.pico -std=gnu99 -fstack-protector-strong -c ublio.c -o ublio.pico
-cc -fstack-protector-strong -shared -Wl,-x -Wl,--fatal-warnings -Wl,--warn-shared-textrel -o libublio.so.1.full -Wl,-soname,libublio.so.1  `NM='nm' NMFLAGS='' lorder ublio.pico | tsort -q` 
+cc -fpic -DPIC ${CFLAGS} -MD -MF.depend.ublio.pico -MTublio.pico -c ublio.c -o ublio.pico
+cc -shared ${CFLAGS} -Wl,-x -Wl,--fatal-warnings -Wl,--warn-shared-textrel -o libublio.so.1.full -Wl,-soname,libublio.so.1  `NM='nm' NMFLAGS='' lorder ublio.pico | tsort -q` 
 objcopy --only-keep-debug libublio.so.1.full libublio.so.1.debug
 objcopy --strip-debug --add-gnu-debuglink=libublio.so.1.debug  libublio.so.1.full libublio.so.1
