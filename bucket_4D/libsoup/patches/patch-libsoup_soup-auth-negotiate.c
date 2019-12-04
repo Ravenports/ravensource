fa5fc2e29673f6d33fe1bf24c36a6d43cd47e895
John Marino <draco@marino.st>
2019-12-04 12:18:15 -0600

    libsoup: upgrade

--- bucket_4D/libsoup/patches/patch-libsoup_soup-auth-negotiate.c
+++ bucket_4D/libsoup/patches/patch-libsoup_soup-auth-negotiate.c
@@ -1,11 +1,14 @@
---- libsoup/soup-auth-negotiate.c.orig	2019-10-09 09:49:52 UTC
+--- libsoup/soup-auth-negotiate.c.orig	2019-12-04 10:50:12 UTC
 +++ libsoup/soup-auth-negotiate.c
-@@ -13,7 +13,7 @@
+@@ -13,7 +13,11 @@
  #include <string.h>
  
  #ifdef LIBSOUP_HAVE_GSSAPI
--#include <gssapi/gssapi.h>
++#ifdef __FreeBSD__
 +#include <gssapi/gssapi_krb5.h>
++#else
+ #include <gssapi/gssapi.h>
++#endif
  #endif /* LIBSOUP_HAVE_GSSAPI */
  
  #include "soup-auth-negotiate.h"
