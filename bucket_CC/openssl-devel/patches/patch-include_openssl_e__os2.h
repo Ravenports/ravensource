45f6b1782ad6685ee07baeb2515ff32073ceeb4f
John Marino <draco@marino.st>
2019-12-09 17:43:22 -0600

    openssl-devel: fix c++ usage on sunos

---
+++ bucket_CC/openssl-devel/patches/patch-include_openssl_e__os2.h
@@ -0,0 +1,13 @@
+--- include/openssl/e_os2.h.orig	2019-09-10 13:13:07 UTC
++++ include/openssl/e_os2.h
+@@ -279,7 +279,9 @@ typedef unsigned __int64 uint64_t;
+ #  define ossl_inline inline
+ # endif
+ 
+-# if defined(__STDC_VERSION__) && __STDC_VERSION__ >= 201112L
++# if (defined(__cplusplus) && __cplusplus >= 201103L)
++#  define ossl_noreturn [[noreturn]]
++# elif defined(__STDC_VERSION__) && __STDC_VERSION__ >= 201112L
+ #  define ossl_noreturn _Noreturn
+ # elif defined(__GNUC__) && __GNUC__ >= 2
+ #  define ossl_noreturn __attribute__((noreturn))
