209ebd5a4cf544e98b65fb77d6a74f25095b2368
John Marino <draco@marino.st>
2019-12-12 15:14:51 -0600

    ghostscript: restore build on sunos

---
+++ bucket_78/ghostscript/patches/patch-base_ttobjs.h
@@ -0,0 +1,15 @@
+Avoid GS conflict on older SunOS.
+
+--- base/ttobjs.h.orig	2019-10-15 09:05:37 UTC
++++ base/ttobjs.h
+@@ -43,6 +43,10 @@
+ #include "setjmp_.h"
+ #include "ttfoutl.h"
+ 
++#ifdef __sun
++#undef GS
++#endif
++
+ #ifdef __cplusplus
+   extern "C" {
+ #endif
