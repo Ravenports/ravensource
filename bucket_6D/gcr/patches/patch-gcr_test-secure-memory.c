631f07138b5b988330c45f7ef5ad8c5e365975eb
John Marino <draco@marino.st>
2019-12-11 23:05:37 -0600

    gcr: fix build on sunos

---
+++ bucket_6D/gcr/patches/patch-gcr_test-secure-memory.c
@@ -0,0 +1,21 @@
+--- gcr/test-secure-memory.c.orig	2019-10-14 15:36:19 UTC
++++ gcr/test-secure-memory.c
+@@ -55,6 +55,10 @@ extern int egg_secure_warnings;
+ static gsize
+ get_rlimit_memlock (void)
+ {
++#ifdef __sun__
++	g_test_skip ("test unsupported on solaris");
++	return 0;
++#else
+ 	struct rlimit memlock;
+ 
+ 	/* If the test program is running as a privileged user, it is
+@@ -83,6 +87,7 @@ get_rlimit_memlock (void)
+ 	}
+ 
+ 	return memlock.rlim_cur;
++#endif
+ }
+ 
+ static void
