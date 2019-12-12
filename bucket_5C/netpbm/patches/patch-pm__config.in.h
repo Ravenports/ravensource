4b409c7e8a1f42914b97c370f00f073507063b68
John Marino <draco@marino.st>
2019-12-12 15:04:06 -0600

    netpbm: build on sunos

---
+++ bucket_5C/netpbm/patches/patch-pm__config.in.h
@@ -0,0 +1,13 @@
+Remove bogus SYSV section (fixes build on SunOS)
+
+--- pm_config.in.h.orig	2019-12-03 17:32:14 UTC
++++ pm_config.in.h
+@@ -74,7 +74,7 @@
+ ** for the format of the color database file.
+ */
+ 
+-#if (defined(SYSV) || defined(__amigaos__))
++#if defined(__amigaos__)
+ 
+ #include <string.h>
+ 
