f4eb5ac3d66278c7ea1d3b7f4873c2867728d812
John Marino <draco@marino.st>
2019-12-11 19:18:21 -0600

    cups: fix build on sunos (snmp not built though)

--- bucket_BA/cups/patches/patch-cups_getifaddrs-internal.h
+++ bucket_BA/cups/patches/patch-cups_getifaddrs-internal.h
@@ -1,11 +1,15 @@
 --- cups/getifaddrs-internal.h.orig	2019-08-23 15:19:38 UTC
 +++ cups/getifaddrs-internal.h
-@@ -28,6 +28,10 @@
+@@ -28,6 +28,14 @@
  #    define CUPS_SOCAST
  #  endif /* _WIN32 */
  
 +#  if defined(__DragonFly__) || defined(__FreeBSD__)
 +#    include <netinet/in.h>
++#  endif
++
++#  if !defined(_WIN32) && !defined(HAVE_GETIFADDRS)
++#    include <cups/versioning.h>
 +#  endif
 +
  #  if defined(__APPLE__) && !defined(_SOCKLEN_T)
