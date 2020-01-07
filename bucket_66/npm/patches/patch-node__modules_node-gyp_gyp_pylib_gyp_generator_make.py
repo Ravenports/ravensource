75af5c08539b9953f5df6578d58fe0c4165a845b
John Marino <draco@marino.st>
2020-01-07 21:31:42 +0000

    npm: build it yourself now (drop freebsd package)

---
+++ bucket_66/npm/patches/patch-node__modules_node-gyp_gyp_pylib_gyp_generator_make.py
@@ -0,0 +1,15 @@
+$NetBSD: patch-node__modules_node-gyp_gyp_pylib_gyp_generator_make.py,v 1.1 2018/05/02 17:03:09 fhajny Exp $
+
+Ensure we use the system libtool on OSX.
+
+--- node_modules/node-gyp/gyp/pylib/gyp/generator/make.py.orig	2019-12-11 18:59:51 UTC
++++ node_modules/node-gyp/gyp/pylib/gyp/generator/make.py
+@@ -176,7 +176,7 @@ cmd_solink_module = $(LINK.$(TOOLSET)) -
+ 
+ LINK_COMMANDS_MAC = """\
+ quiet_cmd_alink = LIBTOOL-STATIC $@
+-cmd_alink = rm -f $@ && ./gyp-mac-tool filter-libtool libtool $(GYP_LIBTOOLFLAGS) -static -o $@ $(filter %.o,$^)
++cmd_alink = rm -f $@ && ./gyp-mac-tool filter-libtool /usr/bin/libtool $(GYP_LIBTOOLFLAGS) -static -o $@ $(filter %.o,$^)
+ 
+ quiet_cmd_link = LINK($(TOOLSET)) $@
+ cmd_link = $(LINK.$(TOOLSET)) $(GYP_LDFLAGS) $(LDFLAGS.$(TOOLSET)) -o "$@" $(LD_INPUTS) $(LIBS)
