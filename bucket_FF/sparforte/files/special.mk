daad57c5d405b99672dad91c3c12ef72b4fe8833
John Marino <draco@marino.st>
2020-01-10 22:17:00 +0000

    sparforte: simplify (requires ravenadm 1.41)

--- bucket_FF/sparforte/files/special.mk
+++ bucket_FF/sparforte/files/special.mk
@@ -15,17 +15,3 @@ SFARG+=		-Dconfigreleased=true
 # Currently Ada is only available for x86, so we can assume the following
 SF_ARCH=		${ARCH_STANDARD:S/x86_64/athlon64/}
 SF_CPU_FLAG=		-march
-
-.if ${OPSYS} == FreeBSD || ${OPSYS} == DragonFly
-SF_CFLAGSTYPE=		-DFREEBSD
-OSBIND=			os_bindings/spar_freebsd
-.else
-SF_CFLAGSTYPE=		-DLINUX
-OSBIND=			os_bindings/spar_linux
-.endif
-
-.if ${ARCH_STANDARD} == x86_64 || ${ARCH_STANDARD} == aarch64
-CPU_SDL=		spar_os-sdl-64bit
-.else
-CPU_SDL=		spar_os-sdl-32bit
-.endif
