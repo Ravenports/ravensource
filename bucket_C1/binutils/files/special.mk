1d4be81bc587ec952c7016bf602a41f630b1ce75
John Marino <draco@marino.st>
2020-01-10 22:34:22 +0000

    binutils: simplify (requires ravenadm 1.41)

--- bucket_C1/binutils/files/special.mk
+++ bucket_C1/binutils/files/special.mk
@@ -1,46 +1,9 @@
-# Consider making gold the default linker on DF and Linux
-
-.if "${OPSYS}" == "DragonFly"
-BUILD_GOLD=		yes
-GOLD_CONFIG=		--enable-gold
-.elif "${OPSYS}" == "FreeBSD"
-.  if "${ARCH_STANDARD}" == "x86_64" || "${ARCH_STANDARD}" == "aarch64"
-BUILD_GOLD=		yes
-GOLD_CONFIG=		--enable-gold=default
-.  endif
-.elif "${OPSYS}" == "Linux"
-.  if "${ARCH_STANDARD}" == "x86_64" || "${ARCH_STANDARD}" == "aarch64"
-BUILD_GOLD=		yes
-GOLD_CONFIG=		--enable-gold
-.  endif
-.elif "${OPSYS}" == "SunOS"
-BUILD_GOLD=		yes
-GOLD_CONFIG=		--enable-gold
-.endif
-
-
 .if defined(BUILD_GOLD)
 CONFIGURE_ARGS+=	${GOLD_CONFIG}
 CONFIGURE_ARGS+=	--enable-plugins
 PLIST_SUB+=		GOLD=""
-PLIST_SUB+=		PLUGINAPI=""
 .else
 CONFIGURE_ARGS+=	--disable-gold
 CONFIGURE_ARGS+=	--disable-plugins
 PLIST_SUB+=		GOLD="@comment "
-.  if "${OPSYS}" == "Darwin"
-PLIST_SUB+=		PLUGINAPI=""
-.  else
-PLIST_SUB+=		PLUGINAPI="@comment "
-.  endif
-.endif
-
-.if "${OPSYS}" == "Darwin"
-PLIST_SUB+=		LINKER="@comment "
-PLIST_SUB+=		ASSEMBLER="@comment "
-PLIST_SUB+=		GPROF="@comment "
-.else
-PLIST_SUB+=		LINKER=""
-PLIST_SUB+=		ASSEMBLER=""
-PLIST_SUB+=		GPROF=""
 .endif
