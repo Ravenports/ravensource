fcfcf40c99fd98b1746ecc5964391436d68f7456
John Marino <draco@marino.st>
2020-01-15 14:09:04 +0000

    meson: untested fix for pkgconfig pc file locations on freebsd

---
+++ bucket_B3/meson/patches/patch-mesonbuild_modules_pkgconfig.py
@@ -0,0 +1,14 @@
+--- mesonbuild/modules/pkgconfig.py.orig	2019-12-04 18:45:50 UTC
++++ mesonbuild/modules/pkgconfig.py
+@@ -437,10 +437,7 @@ class PkgConfigModule(ExtensionModule):
+         pcfile = filebase + '.pc'
+         pkgroot = kwargs.get('install_dir', default_install_dir)
+         if pkgroot is None:
+-            if mesonlib.is_freebsd():
+-                pkgroot = os.path.join(state.environment.coredata.get_builtin_option('prefix'), 'libdata', 'pkgconfig')
+-            else:
+-                pkgroot = os.path.join(state.environment.coredata.get_builtin_option('libdir'), 'pkgconfig')
++            pkgroot = os.path.join(state.environment.coredata.get_builtin_option('libdir'), 'pkgconfig')
+         if not isinstance(pkgroot, str):
+             raise mesonlib.MesonException('Install_dir must be a string.')
+         self.generate_pkgconfig_file(state, deps, subdirs, name, description, url,
