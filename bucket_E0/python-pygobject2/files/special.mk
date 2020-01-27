2b4e7ac29cb495ccb454bd8c1e6135b1e58e044c
John Marino <draco@marino.st>
2020-01-27 18:36:17 +0000

    delete python-pygobject2 port (unused, requires python2)

--- bucket_E0/python-pygobject2/files/special.mk
+++
@@ -1,7 +0,0 @@
-POST_PLIST_TARGET=	autolist
-
-autolist:
-	@${ECHO_MSG} "autogenerating manifest ..."
-	@(cd ${STAGEDIR}${PREFIX} && ${FIND} bin share/py* include/py* lib \
-	\( -type f -o -type l \) 2>/dev/null | ${SORT}) \
-	>> ${WRKDIR}/.manifest.single.mktmp
