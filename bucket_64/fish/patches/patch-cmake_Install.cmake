034e6d197e6d420f9f02ff63aa44697a6643a20b
John Marino <draco@marino.st>
2020-02-17 00:05:46 +0000

    fish: upgrade

--- bucket_64/fish/patches/patch-cmake_Install.cmake
+++ bucket_64/fish/patches/patch-cmake_Install.cmake
@@ -1,14 +1,25 @@
---- cmake/Install.cmake.orig	2019-02-19 13:39:17 UTC
+--- cmake/Install.cmake.orig	2020-02-12 14:04:07 UTC
 +++ cmake/Install.cmake
-@@ -113,7 +113,7 @@ INSTALL(FILES share/config.fish
+@@ -108,7 +108,7 @@ INSTALL(FILES share/config.fish
+               ${CMAKE_CURRENT_BINARY_DIR}/share/__fish_build_paths.fish
+         DESTINATION ${rel_datadir}/fish)
+ 
+-# $v $(INSTALL) -m 755 -d $(DESTDIR)$(datadir)/pkgconfig
++# $v $(INSTALL) -m 755 -d $(DESTDIR)$(datadir)/../lib/pkgconfig
+ # @echo "Creating placeholder vendor/'extra_' directories"
  # -$v $(INSTALL) -m 755 -d $(DESTDIR)$(extra_completionsdir)
  # -$v $(INSTALL) -m 755 -d $(DESTDIR)$(extra_functionsdir)
- # -$v $(INSTALL) -m 755 -d $(DESTDIR)$(extra_confdir)
--FISH_CREATE_DIRS(${rel_datadir}/pkgconfig)
-+FISH_CREATE_DIRS(${rel_datadir}/../lib/pkgconfig)
- # Don't try too hard to create these directories as they may be outside our writeable area
- # https://github.com/Homebrew/homebrew-core/pull/2813
- FISH_TRY_CREATE_DIRS(${extra_completionsdir} ${extra_functionsdir} ${extra_confdir})
+@@ -118,8 +118,8 @@ FISH_CREATE_DIRS(${rel_datadir}/fish/ven
+     ${rel_datadir}/fish/vendor_conf.d)
+ 
+ # @echo "Installing pkgconfig file"
+-# $v $(INSTALL) -m 644 fish.pc $(DESTDIR)$(datadir)/pkgconfig
+-FISH_TRY_CREATE_DIRS(${rel_datadir}/pkgconfig)
++# $v $(INSTALL) -m 644 fish.pc $(DESTDIR)$(datadir)/../lib/pkgconfig
++FISH_TRY_CREATE_DIRS(${rel_datadir}/../lib/pkgconfig)
+ CONFIGURE_FILE(fish.pc.in fish.pc.noversion)
+ 
+ ADD_CUSTOM_COMMAND(OUTPUT fish.pc
 @@ -132,7 +132,7 @@ ADD_CUSTOM_COMMAND(OUTPUT fish.pc
  ADD_CUSTOM_TARGET(build_fish_pc ALL DEPENDS fish.pc)
  
