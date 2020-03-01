25bb548705be122c581baa21f8008033a55bfb7c
John Marino <draco@marino.st>
2020-03-01 23:04:23 +0000

    upgrade poppler (requires libreoffice fix)

--- bucket_39/libreoffice/patches/patch-sdext_source_pdfimport_xpdfwrapper_pdfioutdev__gpl.cxx
+++ bucket_39/libreoffice/patches/patch-sdext_source_pdfimport_xpdfwrapper_pdfioutdev__gpl.cxx
@@ -15,3 +15,14 @@
          const int nPoints = pSub->getNumPoints();
  
          printf( " subpath %d", pSub->isClosed() );
+@@ -556,7 +556,9 @@ void PDFOutDev::processLink(Link* link,
+     if (!(pAction && pAction->getKind() == actionURI))
+ return;
+ 
+-#if POPPLER_CHECK_VERSION(0, 72, 0)
++#if POPPLER_CHECK_VERSION(0, 86, 0)
++    const char* pURI = static_cast<LinkURI*>(pAction)->getURI().c_str();
++#elif POPPLER_CHECK_VERSION(0, 72, 0)
+     const char* pURI = static_cast<LinkURI*>(pAction)->getURI()->c_str();
+ #else
+     const char* pURI = static_cast<LinkURI*>(pAction)->getURI()->getCString();
