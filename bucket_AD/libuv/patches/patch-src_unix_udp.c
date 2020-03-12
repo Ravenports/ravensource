4c9e69eb953a5622add61d5b7f16d2b19ad2c57f
Michael Reim <kraileth@elderlinux.org>
2020-03-12 16:21:23 +0100

    libuv: upgrade

--- bucket_AD/libuv/patches/patch-src_unix_udp.c
+++ bucket_AD/libuv/patches/patch-src_unix_udp.c
@@ -1,20 +1,20 @@
---- src/unix/udp.c.orig	2020-01-23 14:20:02 UTC
-+++ src/unix/udp.c
-@@ -653,7 +653,7 @@ static int uv__udp_set_membership6(uv_ud
+--- src/unix/udp.c.orig	2020-03-12 15:18:05.269264000 +0100
++++ src/unix/udp.c	2020-03-12 15:18:29.952654000 +0100
+@@ -851,7 +851,7 @@
  }
  
  
--#if !defined(__OpenBSD__) && !defined(__NetBSD__)
-+#if !defined(__OpenBSD__) && !defined(__NetBSD__) && !defined(__DragonFly__)
+-#if !defined(__OpenBSD__) && !defined(__NetBSD__) && !defined(__ANDROID__)
++#if !defined(__OpenBSD__) && !defined(__NetBSD__) && !defined(__ANDROID__) && !defined(__DragonFly__)
  static int uv__udp_set_source_membership4(uv_udp_t* handle,
                                            const struct sockaddr_in* multicast_addr,
                                            const char* interface_addr,
-@@ -842,7 +842,7 @@ int uv_udp_set_source_membership(uv_udp_
+@@ -1040,7 +1040,7 @@
                                   const char* interface_addr,
                                   const char* source_addr,
                                   uv_membership membership) {
--#if !defined(__OpenBSD__) && !defined(__NetBSD__)
-+#if !defined(__OpenBSD__) && !defined(__NetBSD__) && !defined(__DragonFly__)
+-#if !defined(__OpenBSD__) && !defined(__NetBSD__) && !defined(__ANDROID__)
++#if !defined(__OpenBSD__) && !defined(__NetBSD__) && !defined(__ANDROID__) && !defined(__DragonFly__)
    int err;
    struct sockaddr_storage mcast_addr;
    struct sockaddr_in* mcast_addr4;
