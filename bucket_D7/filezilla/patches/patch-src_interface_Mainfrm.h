f3eb9e602d5e5abda6921561aea882dfcb59b69a
John Marino <draco@marino.st>
2020-03-21 15:49:11 +0000

    upgrade lib/filezilla together

---
+++ bucket_D7/filezilla/patches/patch-src_interface_Mainfrm.h
@@ -0,0 +1,10 @@
+--- src/interface/Mainfrm.h.orig	2020-01-23 11:18:25 UTC
++++ src/interface/Mainfrm.h
+@@ -1,6 +1,7 @@
+ #ifndef FILEZILLA_INTERFACE_MAINFRM_HEADER
+ #define FILEZILLA_INTERFACE_MAINFRM_HEADER
+ 
++#include <list>
+ #include "statusbar.h"
+ #include "engine_context.h"
+ #include "notification.h"
