cad2b43f9d3977fc985f102811946870ff56d348
John Marino <draco@marino.st>
2020-03-31 12:38:39 -0500

    vde2: fix build on linux

--- bucket_27/vde2/patches/patch-src_dpipe.c
+++ bucket_27/vde2/patches/patch-src_dpipe.c
@@ -1,11 +1,14 @@
 --- src/dpipe.c.orig	2011-11-23 16:41:18 UTC
 +++ src/dpipe.c
-@@ -201,7 +201,7 @@ int main(int argc, char *argv[])
+@@ -201,7 +201,11 @@ int main(int argc, char *argv[])
  
  	if (daemonize != 0)
  		daemon(0,0);
--	else if (setpgrp() != 0) {
++#ifdef __linux__
+ 	else if (setpgrp() != 0) {
++#else
 +	else if (setpgrp(0,getpid()) != 0) {
++#endif
  		fprintf(stderr,"Err: cannot create pgrp\n");
  		exit(1);
  	}
