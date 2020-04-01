16ffe49fbd8a80272ccde8d7d5ee5c83c359e28f
John Marino <draco@marino.st>
2020-04-01 15:04:14 +0000

    unrar: upgrade

--- bucket_1B/unrar/patches/patch-os.hpp
+++ bucket_1B/unrar/patches/patch-os.hpp
@@ -1,4 +1,4 @@
---- os.hpp.orig	2020-01-28 15:03:59 UTC
+--- os.hpp.orig	2020-03-26 10:04:33 UTC
 +++ os.hpp
 @@ -166,6 +166,10 @@
  #include <locale.h>
--- bucket_1B/unrar/patches/patch-rijndael.hpp
+++ bucket_1B/unrar/patches/patch-rijndael.hpp
@@ -1,4 +1,4 @@
---- rijndael.hpp.orig	2020-01-28 15:03:59 UTC
+--- rijndael.hpp.orig	2020-03-26 10:04:34 UTC
 +++ rijndael.hpp
 @@ -16,6 +16,9 @@
  class Rijndael
