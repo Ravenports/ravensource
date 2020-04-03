d4803c1ef1b3bacb3df1a63794a98f167eef6e07
John Marino <draco@marino.st>
2020-04-03 00:57:04 +0000

    add openexr port (+convert ilmbase to raven-info)

---
+++ bucket_40/openexr/patches/patch-IlmImfTest_bswap__32.h
@@ -0,0 +1,11 @@
+--- IlmImfTest/bswap_32.h.orig	2020-02-10 23:01:19 UTC
++++ IlmImfTest/bswap_32.h
+@@ -12,7 +12,7 @@
+ #elif defined(__sun) || defined(sun)
+ #include <sys/byteorder.h>
+ #define bswap_32(x) BSWAP_32(x)
+-#elif defined(__FreeBSD__)
++#elif defined(__FreeBSD__) || defined(__DragonFly__)
+ #include <sys/endian.h>
+ #define bswap_32(x) bswap32(x)
+ #elif defined(__OpenBSD__)
