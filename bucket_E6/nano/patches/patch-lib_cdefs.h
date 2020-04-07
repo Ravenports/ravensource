71a4e8064206725c2a141ec7ec56be262b2479de
John Marino <draco@marino.st>
2020-04-07 19:26:02 +0000

    nano: upgrade

--- bucket_E6/nano/patches/patch-lib_cdefs.h
+++ bucket_E6/nano/patches/patch-lib_cdefs.h
@@ -1,4 +1,4 @@
---- lib/cdefs.h.orig	2020-03-31 10:10:28 UTC
+--- lib/cdefs.h.orig	2020-04-07 09:24:34 UTC
 +++ lib/cdefs.h
 @@ -292,7 +292,7 @@
     for portability when this file is used in Gnulib.  */
