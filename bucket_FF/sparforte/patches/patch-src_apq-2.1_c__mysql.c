896e2739d703465e671e8dd49ba3045f0a33af80
John Marino <draco@marino.st>
2020-04-11 15:03:45 +0000

    sparforte: fix build with mysql-8.0

---
+++ bucket_FF/sparforte/patches/patch-src_apq-2.1_c__mysql.c
@@ -0,0 +1,27 @@
+--- src/apq-2.1/c_mysql.c.orig	2019-07-23 10:48:03 UTC
++++ src/apq-2.1/c_mysql.c
+@@ -21,10 +21,10 @@
+  */
+ 
+ #include <stdio.h>
++#include <mysql_version.h>
++#if MYSQL_VERSION_ID < 80000
+ #include <my_global.h>
+ #include <my_sys.h>
+-#ifdef _WINDOWS
+-#include <my_global.h>
+ #endif
+ #include <mysql.h>
+ #include <errmsg.h>
+@@ -326,7 +326,11 @@ c_mysql_free_result(MYSQL_RES *result) {
+ 
+ EXPORT void
+ c_my_init(void) {
++#if MYSQL_VERSION_ID < 80000
+ 	my_init();
++#else
++	return;
++#endif
+ }
+ 
+ EXPORT my_ulonglong
