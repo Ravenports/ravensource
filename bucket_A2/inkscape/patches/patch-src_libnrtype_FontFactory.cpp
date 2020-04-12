1d0ffdd135fca5f870b179a2be603fc5e25eca34
John Marino <draco@marino.st>
2020-04-12 16:39:34 +0000

    inkscape: upgrade, split out nls files

--- bucket_A2/inkscape/patches/patch-src_libnrtype_FontFactory.cpp
+++ bucket_A2/inkscape/patches/patch-src_libnrtype_FontFactory.cpp
@@ -1,6 +1,6 @@
---- src/libnrtype/FontFactory.cpp.orig	2018-03-11 20:38:09 UTC
+--- src/libnrtype/FontFactory.cpp.orig	2020-04-09 21:37:16 UTC
 +++ src/libnrtype/FontFactory.cpp
-@@ -704,8 +704,9 @@ font_instance *font_factory::Face(PangoFontDescription
+@@ -704,8 +704,9 @@ font_instance *font_factory::Face(PangoF
              guint script_index = -1;
              if( pango_ot_info_find_script( info, PANGO_OT_TABLE_GSUB, scripts[i], &script_index )) {
  
