33c1ec8cda418c36cef2df27f6edcf0e3f6dce92
John Marino <draco@marino.st>
2020-04-29 23:26:52 +0000

    python-salt: upgrade

--- bucket_F8/python-salt/patches/patch-salt_utils_rsax931.py
+++ bucket_F8/python-salt/patches/patch-salt_utils_rsax931.py
@@ -1,11 +1,11 @@
---- salt/utils/rsax931.py.orig	2018-08-03 14:17:17.391935000 +0200
-+++ salt/utils/rsax931.py	2018-08-03 14:18:20.948802000 +0200
-@@ -43,7 +43,7 @@
+--- salt/utils/rsax931.py.orig	2020-04-29 18:29:44 UTC
++++ salt/utils/rsax931.py
+@@ -43,7 +43,7 @@ def _load_libcrypto():
              # This could be /opt/tools/lib (Global Zone)
              # or /opt/local/lib (non-Global Zone), thus the
              # two checks below
 -            lib = glob.glob('/opt/local/lib/libcrypto.so*') + glob.glob('/opt/tools/lib/libcrypto.so*')
 +            lib = glob.glob('/opt/local/lib/libcrypto.so*') + glob.glob('/opt/tools/lib/libcrypto.so*') + glob.glob('__PREFIX__/lib/libcrypto.so*')
              lib = lib[0] if len(lib) > 0 else None
-         if lib:
-             return cdll.LoadLibrary(lib)
+         if not lib and salt.utils.platform.is_aix():
+             if os.path.isdir('/opt/salt/lib'):
