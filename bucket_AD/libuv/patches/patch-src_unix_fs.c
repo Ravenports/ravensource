93992924f3edab2035550d49ece02ebc27d66630
Super-User <draco@marino.st>
2020-04-28 23:22:46 -0500

    libuv: fix linking on sunos

--- bucket_AD/libuv/patches/patch-src_unix_fs.c
+++ bucket_AD/libuv/patches/patch-src_unix_fs.c
@@ -1,6 +1,6 @@
---- src/unix/fs.c.orig	2020-04-19 16:15:57 UTC
+--- src/unix/fs.c.orig	2020-04-29 04:16:51 UTC
 +++ src/unix/fs.c
-@@ -976,7 +976,6 @@ static ssize_t uv__fs_sendfile(uv_fs_t*
+@@ -988,7 +988,6 @@ static ssize_t uv__fs_sendfile(uv_fs_t*
  static ssize_t uv__fs_utime(uv_fs_t* req) {
  #if defined(__linux__)                                                         \
      || defined(_AIX71)                                                         \
@@ -8,7 +8,7 @@
      || defined(__HAIKU__)
    /* utimesat() has nanosecond resolution but we stick to microseconds
     * for the sake of consistency with other platforms.
-@@ -986,6 +985,7 @@ static ssize_t uv__fs_utime(uv_fs_t* req
+@@ -998,6 +997,7 @@ static ssize_t uv__fs_utime(uv_fs_t* req
    ts[1] = uv__fs_to_timespec(req->mtime);
    return utimensat(AT_FDCWD, req->path, ts, 0);
  #elif defined(__APPLE__)                                                      \
@@ -16,3 +16,11 @@
      || defined(__DragonFly__)                                                 \
      || defined(__FreeBSD__)                                                   \
      || defined(__FreeBSD_kernel__)                                            \
+@@ -1031,7 +1031,6 @@ static ssize_t uv__fs_utime(uv_fs_t* req
+ static ssize_t uv__fs_lutime(uv_fs_t* req) {
+ #if defined(__linux__)            ||                                           \
+     defined(_AIX71)               ||                                           \
+-    defined(__sun)                ||                                           \
+     defined(__HAIKU__)
+   struct timespec ts[2];
+   ts[0] = uv__fs_to_timespec(req->atime);
