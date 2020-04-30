19df150ab5f6f8c5661afc0191c7737ad9581fe7
John Marino <draco@marino.st>
2020-04-30 01:55:37 +0000

    leatherman: upgrade

--- bucket_56/leatherman/patches/patch-json__container_tests_json__container__test.cc
+++ bucket_56/leatherman/patches/patch-json__container_tests_json__container__test.cc
@@ -1,4 +1,4 @@
---- json_container/tests/json_container_test.cc.orig	2020-03-26 20:23:09 UTC
+--- json_container/tests/json_container_test.cc.orig	2020-04-21 23:04:52 UTC
 +++ json_container/tests/json_container_test.cc
 @@ -22,11 +22,13 @@ TEST_CASE("JsonContainer::JsonContainer
      std::string json_value {};
