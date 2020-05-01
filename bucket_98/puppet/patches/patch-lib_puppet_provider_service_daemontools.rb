87c47fda0968a080a7af838f02791d632febec6f
John Marino <draco@marino.st>
2020-05-01 18:47:13 -0500

    puppet: upgrade (no longer locked to ruby26)

--- bucket_98/puppet/patches/patch-lib_puppet_provider_service_daemontools.rb
+++ bucket_98/puppet/patches/patch-lib_puppet_provider_service_daemontools.rb
@@ -1,6 +1,6 @@
 Daemontools program locations
 
---- lib/puppet/provider/service/daemontools.rb.orig	2020-02-12 09:09:36 UTC
+--- lib/puppet/provider/service/daemontools.rb.orig	2020-03-06 09:46:03 UTC
 +++ lib/puppet/provider/service/daemontools.rb
 @@ -39,7 +39,7 @@ Puppet::Type.type(:service).provide :dae
  
