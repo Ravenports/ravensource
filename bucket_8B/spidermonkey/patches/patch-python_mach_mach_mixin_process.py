a70c0fbc75042195518f96e594121f98aef99583
John Marino <draco@marino.st>
2020-05-05 01:28:59 +0000

    spidermonkey: upgrade

--- bucket_8B/spidermonkey/patches/patch-python_mach_mach_mixin_process.py
+++ bucket_8B/spidermonkey/patches/patch-python_mach_mach_mixin_process.py
@@ -1,4 +1,4 @@
---- ../../python/mach/mach/mixin/process.py.orig	2020-04-03 19:30:02 UTC
+--- ../../python/mach/mach/mixin/process.py.orig	2020-04-29 21:49:45 UTC
 +++ ../../python/mach/mach/mixin/process.py
 @@ -27,7 +27,7 @@ elif 'MOZILLABUILD' in os.environ:
  elif 'COMSPEC' in os.environ:
