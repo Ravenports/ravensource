9f4b89732ba35e4ba966a7bba2a18fb912577c89
John Marino <draco@marino.st>
2020-05-05 15:14:40 +0000

    thunderbird: upgrade

--- bucket_67/thunderbird/patches/patch-mail-app-nsMailApp.cpp
+++ bucket_67/thunderbird/patches/patch-mail-app-nsMailApp.cpp
@@ -1,4 +1,4 @@
---- comm/mail/app/nsMailApp.cpp.orig	2020-04-07 18:05:26 UTC
+--- comm/mail/app/nsMailApp.cpp.orig	2020-05-04 17:22:31 UTC
 +++ comm/mail/app/nsMailApp.cpp
 @@ -273,6 +273,9 @@ int main(int argc, char* argv[], char* e
    }
