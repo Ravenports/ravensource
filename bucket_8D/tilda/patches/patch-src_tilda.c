6f8f80208c57500313f20abb97fc56ff67dc7870
John Marino <draco@marino.st>
2020-05-05 13:08:22 +0000

    tilda: upgrade

--- bucket_8D/tilda/patches/patch-src_tilda.c
+++ bucket_8D/tilda/patches/patch-src_tilda.c
@@ -1,19 +1,25 @@
---- src/tilda.c.orig	2018-02-08 19:55:42 UTC
+--- src/tilda.c.orig	2020-05-03 19:20:55 UTC
 +++ src/tilda.c
-@@ -12,6 +12,7 @@
+@@ -13,6 +13,10 @@
   * You should have received a copy of the GNU Library General Public
   * License along with this library. If not, see <http://www.gnu.org/licenses/>.
   */
++#if defined(__FreeBSD__) || defined(__DragonFly__)
++#define USE_PGREP
 +#include <fcntl.h>
++#endif
  #define _POSIX_SOURCE /* feature test macro for signal functions */
  #define _XOPEN_SOURCE /* feature test macro for popen */
  
-@@ -230,7 +231,7 @@ nomatch:
+@@ -232,7 +236,11 @@ nomatch:
  static GSList *getPids() {
      GSList *pids = NULL;
      FILE *ps_output;
--    const gchar ps_command[] = "ps -C tilda -o pid=";
++#ifdef USE_PGREP
 +    const gchar ps_command[] = "/usr/bin/pgrep tilda";
++#else
+     const gchar ps_command[] = "ps -C tilda -o pid=";
++#endif
      gchar buf[16]; /* Really shouldn't need more than 6 */
  
      if ((ps_output = popen (ps_command, "r")) == NULL) {
