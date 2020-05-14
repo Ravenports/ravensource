c16da457907d822447d4e2bd478b5b870dc3f153
John Marino <draco@marino.st>
2020-05-14 00:13:07 +0000

    vim: upgrade (deduplicate nls plist)

--- bucket_E1/vim/patches/patch-src-installml.sh
+++ bucket_E1/vim/patches/patch-src-installml.sh
@@ -1,4 +1,4 @@
---- src/installml.sh.orig	2020-05-07 21:20:21 UTC
+--- src/installml.sh.orig	2020-05-13 21:24:12 UTC
 +++ src/installml.sh
 @@ -53,16 +53,16 @@ if test $what = "install" -a \( -f $dest
     fi
