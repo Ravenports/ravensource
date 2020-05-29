5372c220c2cd53b021f1eb88444a867332fc0a08
John Marino <draco@marino.st>
2020-05-29 02:21:22 -0500

    llvm: fix build on sunos

---
+++ bucket_5E/llvm/patches/patch-tools_llvm-objcopy_MachO_Object.cpp
@@ -0,0 +1,21 @@
+--- tools/llvm-objcopy/MachO/Object.cpp.orig	2020-03-23 15:01:02 UTC
++++ tools/llvm-objcopy/MachO/Object.cpp
+@@ -1,6 +1,18 @@
+ #include "Object.h"
+ #include "../llvm-objcopy.h"
+ 
++#ifdef __sun__
++static size_t
++strnlen(const char *s, size_t maxlen)
++{
++       size_t len;
++       for (len = 0; len < maxlen; len++, s++) {
++               if (!*s) break;
++       }
++       return (len);
++}
++#endif
++
+ namespace llvm {
+ namespace objcopy {
+ namespace macho {
