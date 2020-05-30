2ad48fd891bf450268dc7a37a47c20d5c3ae87f7
John Marino <draco@marino.st>
2020-05-30 14:37:08 +0000

    langkit: upgrade (upstream for python3 version)

--- bucket_A5/langkit/patches/patch-langkit_libmanage.py
+++ bucket_A5/langkit/patches/patch-langkit_libmanage.py
@@ -1,6 +1,6 @@
---- langkit/libmanage.py.orig	2019-05-11 05:16:01 UTC
+--- langkit/libmanage.py.orig	2020-05-29 13:42:27 UTC
 +++ langkit/libmanage.py
-@@ -826,6 +826,7 @@ class ManageScript(object):
+@@ -950,6 +950,7 @@ class ManageScript(object):
          base_argv = ['gprinstall', '-p',
                       '-P{}'.format(project_file),
                       '--prefix={}'.format(self.dirs.install_dir()),
