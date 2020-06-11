48ffde541e46560897ab2fe1a93480d09725b971
John Marino <draco@marino.st>
2020-06-11 13:21:50 +0000

    netpbm: upgrade

--- bucket_5C/netpbm/patches/patch-pm__config.in.h
+++ bucket_5C/netpbm/patches/patch-pm__config.in.h
@@ -1,6 +1,6 @@
 Remove bogus SYSV section (fixes build on SunOS)
 
---- pm_config.in.h.orig	2020-04-20 03:31:29 UTC
+--- pm_config.in.h.orig	2020-06-10 00:29:07 UTC
 +++ pm_config.in.h
 @@ -74,7 +74,7 @@
  ** for the format of the color database file.
