a870772d00ab0e9a72eddf91ea5b07010dc76386
Michael Reim <kraileth@elderlinux.org>
2020-06-18 09:58:22 +0200

    python-salt: upgrade (and remove raet as it was dropped by upstream)

--- bucket_F8/python-salt/patches/patch-salt_utils_rsax931.py
+++ bucket_F8/python-salt/patches/patch-salt_utils_rsax931.py
@@ -1,11 +1,12 @@
---- salt/utils/rsax931.py.orig	2020-04-29 18:29:44 UTC
-+++ salt/utils/rsax931.py
-@@ -43,7 +43,7 @@ def _load_libcrypto():
-             # This could be /opt/tools/lib (Global Zone)
-             # or /opt/local/lib (non-Global Zone), thus the
-             # two checks below
--            lib = glob.glob('/opt/local/lib/libcrypto.so*') + glob.glob('/opt/tools/lib/libcrypto.so*')
-+            lib = glob.glob('/opt/local/lib/libcrypto.so*') + glob.glob('/opt/tools/lib/libcrypto.so*') + glob.glob('__PREFIX__/lib/libcrypto.so*')
-             lib = lib[0] if len(lib) > 0 else None
-         if not lib and salt.utils.platform.is_aix():
-             if os.path.isdir('/opt/salt/lib'):
+--- salt/utils/rsax931.py.orig	2020-06-18 09:31:44.611887000 +0200
++++ salt/utils/rsax931.py	2020-06-18 09:34:12.705293000 +0200
+@@ -43,7 +43,8 @@
+                 # This could be /opt/tools/lib (Global Zone) or
+                 # /opt/local/lib (non-Global Zone), thus the two checks
+                 # below
+-                lib = glob.glob("/opt/local/lib/libcrypto.so*")
++		lib = glob.glob('__PREFIX__/lib/libcrypto.so*')
++                lib = lib or glob.glob("/opt/local/lib/libcrypto.so*")
+                 lib = lib or glob.glob("/opt/tools/lib/libcrypto.so*")
+                 lib = lib[0] if lib else None
+             elif salt.utils.platform.is_aix():
