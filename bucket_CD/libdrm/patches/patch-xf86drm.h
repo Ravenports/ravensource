c29494b5d0aa6ed17582ee8ce4ffb47ecc8a01b8
John Marino <draco@marino.st>
2020-06-18 23:30:29 -0500

    libdrm: restore build on sunos

---
+++ bucket_CD/libdrm/patches/patch-xf86drm.h
@@ -0,0 +1,12 @@
+--- xf86drm.h.orig	2020-05-26 20:37:16 UTC
++++ xf86drm.h
+@@ -59,6 +59,9 @@ extern "C" {
+ #else /* One of the *BSDs */
+ 
+ #include <sys/ioccom.h>
++#ifdef __sun__
++#define _IOC(inout,group,num,len)	(inout | ((len & IOCPARM_MASK) << 16) | ((group) << 8) | (num))
++#endif
+ #define DRM_IOCTL_NR(n)         ((n) & 0xff)
+ #define DRM_IOC_VOID            IOC_VOID
+ #define DRM_IOC_READ            IOC_OUT
