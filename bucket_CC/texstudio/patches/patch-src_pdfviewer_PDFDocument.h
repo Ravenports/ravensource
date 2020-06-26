db9b19628cf81abc1e41380b57b3a3a6ba6774d4
John Marino <draco@marino.st>
2020-06-26 11:33:43 +0000

    texstudio: fix build with qt 5.15.0

---
+++ bucket_CC/texstudio/patches/patch-src_pdfviewer_PDFDocument.h
@@ -0,0 +1,10 @@
+--- src/pdfviewer/PDFDocument.h.orig	2020-06-26 11:30:32 UTC
++++ src/pdfviewer/PDFDocument.h
+@@ -27,6 +27,7 @@
+ 
+ #include "mostQtHeaders.h"
+ 
++#include <QPainterPath>
+ #include <QGestureEvent>
+ #include <QPinchGesture>
+ #include <QTapGesture>
