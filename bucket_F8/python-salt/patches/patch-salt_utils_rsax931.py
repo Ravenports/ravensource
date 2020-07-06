ce38610dcba01b5742ba47c7c056ca9e59c1e89e
Michael Reim <kraileth@elderlinux.org>
2020-07-06 22:16:43 +0200

    python-salt: Add distro dependency and msgpack version fix

--- bucket_F8/python-salt/patches/patch-salt_utils_rsax931.py
+++ bucket_F8/python-salt/patches/patch-salt_utils_rsax931.py
@@ -5,7 +5,7 @@
                  # /opt/local/lib (non-Global Zone), thus the two checks
                  # below
 -                lib = glob.glob("/opt/local/lib/libcrypto.so*")
-+		lib = glob.glob('__PREFIX__/lib/libcrypto.so*')
++                lib = glob.glob('__PREFIX__/lib/libcrypto.so*')
 +                lib = lib or glob.glob("/opt/local/lib/libcrypto.so*")
                  lib = lib or glob.glob("/opt/tools/lib/libcrypto.so*")
                  lib = lib[0] if lib else None
