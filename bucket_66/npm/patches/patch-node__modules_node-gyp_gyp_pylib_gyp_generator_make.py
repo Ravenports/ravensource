73706c145c91b774aced1050144999e273815a86
John Marino <draco@marino.st>
2020-07-08 13:36:00 +0000

    npm: upgrade

--- bucket_66/npm/patches/patch-node__modules_node-gyp_gyp_pylib_gyp_generator_make.py
+++ bucket_66/npm/patches/patch-node__modules_node-gyp_gyp_pylib_gyp_generator_make.py
@@ -2,7 +2,7 @@ $NetBSD: patch-node__modules_node-gyp_gyp_pylib_gyp_generator_make.py,v 1.1 2018
 
 Ensure we use the system libtool on OSX.
 
---- node_modules/node-gyp/gyp/pylib/gyp/generator/make.py.orig	2020-05-04 15:55:16 UTC
+--- node_modules/node-gyp/gyp/pylib/gyp/generator/make.py.orig	2020-07-07 16:54:05 UTC
 +++ node_modules/node-gyp/gyp/pylib/gyp/generator/make.py
 @@ -176,7 +176,7 @@ cmd_solink_module = $(LINK.$(TOOLSET)) -
  
--- bucket_66/npm/patches/patch-node__modules_node-gyp_gyp_pylib_gyp_xcode__emulation.py
+++ bucket_66/npm/patches/patch-node__modules_node-gyp_gyp_pylib_gyp_xcode__emulation.py
@@ -2,7 +2,7 @@ $NetBSD: patch-node__modules_node-gyp_gyp_pylib_gyp_xcode__emulation.py,v 1.1 20
 
 Do not generate debugging symbols (uses lots of disk space).
 
---- node_modules/node-gyp/gyp/pylib/gyp/xcode_emulation.py.orig	2020-05-04 15:55:16 UTC
+--- node_modules/node-gyp/gyp/pylib/gyp/xcode_emulation.py.orig	2020-07-07 16:54:05 UTC
 +++ node_modules/node-gyp/gyp/pylib/gyp/xcode_emulation.py
 @@ -507,7 +507,7 @@ class XcodeSettings(object):
  
