af33f6b447ebacdaa508eec03aa632bae2983dce
John Marino <draco@marino.st>
2020-07-11 01:42:21 +0000

    add lua54 (for future swap with lua5)

---
+++ bucket_BE/lua54/patches/patch-src__liolib.c
@@ -0,0 +1,10 @@
+--- src/liolib.c.orig	2020-06-18 14:25:53 UTC
++++ src/liolib.c
+@@ -16,6 +16,7 @@
+ #include <stdio.h>
+ #include <stdlib.h>
+ #include <string.h>
++#include <unistd.h>
+ 
+ #include "lua.h"
+ 
