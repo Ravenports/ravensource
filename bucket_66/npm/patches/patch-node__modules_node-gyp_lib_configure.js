11f701763d6118d31831a4cd0d20377ede80919e
John Marino <draco@marino.st>
2020-07-22 14:46:00 +0000

    npm: upgrade

--- bucket_66/npm/patches/patch-node__modules_node-gyp_lib_configure.js
+++ bucket_66/npm/patches/patch-node__modules_node-gyp_lib_configure.js
@@ -2,7 +2,7 @@ $NetBSD: patch-node__modules_node-gyp_lib_configure.js,v 1.3 2019/11/24 20:21:23
 
 Skip Python detection, version 2.7 is required and we know where it is.
 
---- node_modules/node-gyp/lib/configure.js.orig	2020-07-07 16:54:05 UTC
+--- node_modules/node-gyp/lib/configure.js.orig	2020-07-21 19:24:54 UTC
 +++ node_modules/node-gyp/lib/configure.js
 @@ -15,21 +15,14 @@ if (win) {
  }
