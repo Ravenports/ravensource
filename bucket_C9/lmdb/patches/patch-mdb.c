5b855730b3cb78c3cd7e84dbfb8f0d3b715a4ecb
John Marino <draco@marino.st>
2020-08-12 15:15:13 +0000

    lmdb: upgrade

--- bucket_C9/lmdb/patches/patch-mdb.c
+++ bucket_C9/lmdb/patches/patch-mdb.c
@@ -1,6 +1,6 @@
---- mdb.c.orig	2019-07-19 16:41:12 UTC
+--- mdb.c.orig	2020-08-11 18:24:07 UTC
 +++ mdb.c
-@@ -129,6 +129,8 @@ typedef SSIZE_T	ssize_t;
+@@ -132,6 +132,8 @@ typedef SSIZE_T	ssize_t;
  # define MDB_FDATASYNC		fsync
  #elif defined(ANDROID)
  # define MDB_FDATASYNC		fsync
