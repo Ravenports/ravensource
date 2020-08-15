b5b2e5312dbf39d2d4a62ac9b846256a1c8a85c5
John Marino <draco@marino.st>
2020-08-15 16:00:48 +0000

    libetonyek: fix build failure (reported by kraileth)

---
+++ bucket_6D/libetonyek/patches/patch-src_lib_NUM3Parser.cpp
@@ -0,0 +1,10 @@
+--- src/lib/NUM3Parser.cpp.orig	2018-12-28 17:45:35 UTC
++++ src/lib/NUM3Parser.cpp
+@@ -8,6 +8,7 @@
+  */
+ 
+ #include <functional>
++#include <algorithm>
+ 
+ #include "NUM3Parser.h"
+ 
