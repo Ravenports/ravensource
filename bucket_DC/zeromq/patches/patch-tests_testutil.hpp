c99ef4b11fcf5bcac44b472b8afe420aad08c081
John Marino <draco@marino.st>
2020-09-15 20:04:34 +0000

    zeromq: upgrade

---
+++ bucket_DC/zeromq/patches/patch-tests_testutil.hpp
@@ -0,0 +1,13 @@
+--- tests/testutil.hpp.orig	2020-09-07 16:01:31 UTC
++++ tests/testutil.hpp
+@@ -46,6 +46,10 @@
+ #include <unistd.h>
+ #include <stdlib.h>
+ #endif
++#ifdef __DragonFly__
++#include <netinet/in.h>
++#include <sys/socket.h>
++#endif
+ 
+ //  This defines the settle time used in tests; raise this if we
+ //  get test failures on slower systems due to binds/connects not
