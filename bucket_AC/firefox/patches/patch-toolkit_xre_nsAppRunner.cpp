70434f97e43ca44caede27c4025dbddf7f81746f
John Marino <draco@marino.st>
2020-09-23 15:51:03 +0000

    firefox: upgrade (remove python2 dep)

--- bucket_AC/firefox/patches/patch-toolkit_xre_nsAppRunner.cpp
+++ bucket_AC/firefox/patches/patch-toolkit_xre_nsAppRunner.cpp
@@ -1,16 +1,20 @@
---- toolkit/xre/nsAppRunner.cpp.orig	2020-04-29 21:55:18 UTC
+--- toolkit/xre/nsAppRunner.cpp.orig	2020-09-17 00:32:53 UTC
 +++ toolkit/xre/nsAppRunner.cpp
-@@ -5003,7 +5003,13 @@ bool BrowserTabsRemoteAutostart() {
+@@ -5156,15 +5156,10 @@ bool BrowserTabsRemoteAutostart() {
+     gBrowserTabsRemoteAutostart = true;
    }
  
-   // Uber override pref for emergency blocking
-+#ifdef __DragonFly__
+-  // Uber override pref for emergency blocking
+-  if (gBrowserTabsRemoteAutostart) {
+-    const char* forceDisable = PR_GetEnv("MOZ_FORCE_DISABLE_E10S");
+-    // The environment variable must match the application version to apply.
+-    if (forceDisable && gAppData && !strcmp(forceDisable, gAppData->version)) {
 +  // Disable MultiProcessing for DragonFly. Slowness and many crashes that require
 +  // quite some debugging and investigation.
-+  {
-+#else
-   if (gBrowserTabsRemoteAutostart && EnvHasValue("MOZ_FORCE_DISABLE_E10S")) {
-+#endif
-     gBrowserTabsRemoteAutostart = false;
-     status = kE10sForceDisabled;
-   }
+       gBrowserTabsRemoteAutostart = false;
+       status = kE10sForceDisabled;
+-    }
+-  }
+ 
+   gBrowserTabsRemoteStatus = status;
+ 
