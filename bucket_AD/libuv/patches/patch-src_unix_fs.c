05e6f0079494c2f94108ca6667b5f5455c5af20b
John Marino <draco@marino.st>
2020-09-26 03:22:55 +0000

    libuv: upgrade

--- bucket_AD/libuv/patches/patch-src_unix_fs.c
+++ bucket_AD/libuv/patches/patch-src_unix_fs.c
@@ -1,6 +1,6 @@
---- src/unix/fs.c.orig	2020-08-25 00:25:03 UTC
+--- src/unix/fs.c.orig	2020-09-25 00:34:43 UTC
 +++ src/unix/fs.c
-@@ -995,7 +995,6 @@ ok:
+@@ -1008,7 +1008,6 @@ ok:
  static ssize_t uv__fs_utime(uv_fs_t* req) {
  #if defined(__linux__)                                                         \
      || defined(_AIX71)                                                         \
@@ -8,7 +8,7 @@
      || defined(__HAIKU__)
    /* utimesat() has nanosecond resolution but we stick to microseconds
     * for the sake of consistency with other platforms.
-@@ -1005,6 +1004,7 @@ static ssize_t uv__fs_utime(uv_fs_t* req
+@@ -1018,6 +1017,7 @@ static ssize_t uv__fs_utime(uv_fs_t* req
    ts[1] = uv__fs_to_timespec(req->mtime);
    return utimensat(AT_FDCWD, req->path, ts, 0);
  #elif defined(__APPLE__)                                                      \
@@ -16,7 +16,7 @@
      || defined(__DragonFly__)                                                 \
      || defined(__FreeBSD__)                                                   \
      || defined(__FreeBSD_kernel__)                                            \
-@@ -1038,7 +1038,6 @@ static ssize_t uv__fs_utime(uv_fs_t* req
+@@ -1051,7 +1051,6 @@ static ssize_t uv__fs_utime(uv_fs_t* req
  static ssize_t uv__fs_lutime(uv_fs_t* req) {
  #if defined(__linux__)            ||                                           \
      defined(_AIX71)               ||                                           \
