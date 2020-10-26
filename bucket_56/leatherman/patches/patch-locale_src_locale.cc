9afc2b1f103804b512ba74e1d06e489123bd299c
John Marino <draco@marino.st>
2020-10-26 00:42:31 +0000

    leatherman: upgrade

--- bucket_56/leatherman/patches/patch-locale_src_locale.cc
+++ bucket_56/leatherman/patches/patch-locale_src_locale.cc
@@ -1,4 +1,4 @@
---- locale/src/locale.cc.orig	2020-04-21 23:04:52 UTC
+--- locale/src/locale.cc.orig	2020-09-08 07:08:47 UTC
 +++ locale/src/locale.cc
 @@ -44,6 +44,9 @@ namespace leatherman { namespace locale
              gen.add_messages_domain(domain);
