062b1fc8533fa62838f412fe2fa4c606faad18e4
John Marino <draco@marino.st>
2020-11-02 02:03:46 +0000

    add preview ruby30 port

---
+++ bucket_AD/ruby30/patches/patch-lib_mkmf.rb
@@ -0,0 +1,11 @@
+--- lib/mkmf.rb.orig	2020-09-25 13:16:55 UTC
++++ lib/mkmf.rb
+@@ -240,7 +240,7 @@ module MakeMakefile
+   end
+   $extmk ||= false
+   if not $extmk and File.exist?(($hdrdir = RbConfig::CONFIG["rubyhdrdir"]) + "/ruby/ruby.h")
+-    $topdir = $hdrdir
++    $topdir = $hdrdir + "/" + "#{CONFIG['arch']}/ruby/"
+     $top_srcdir = $hdrdir
+     $arch_hdrdir = RbConfig::CONFIG["rubyarchhdrdir"]
+   elsif File.exist?(($hdrdir = ($top_srcdir ||= topdir) + "/include")  + "/ruby.h")
