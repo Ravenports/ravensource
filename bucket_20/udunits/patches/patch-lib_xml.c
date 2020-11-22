f7d9cd6631f077cd9cb0ccebe8447b76089ec8ff
John Marino <draco@marino.st>
2020-11-22 19:19:10 +0000

    udunits: upgrade

--- bucket_20/udunits/patches/patch-lib_xml.c
+++ bucket_20/udunits/patches/patch-lib_xml.c
@@ -1,32 +1,21 @@
---- lib/xml.c.orig	2020-05-29 20:22:11 UTC
+--- lib/xml.c.orig	2020-11-20 20:55:28 UTC
 +++ lib/xml.c
-@@ -14,9 +14,11 @@
- 
- #include <config.h>
- 
-+#if !(defined(__DragonFly__) || defined(__FreeBSD__))
- #ifndef	_XOPEN_SOURCE
- #   define _XOPEN_SOURCE 500
- #endif
-+#endif
- 
- #if defined(__linux__)
- #   ifndef _GNU_SOURCE
-@@ -40,7 +42,7 @@
+@@ -38,7 +38,8 @@
  #endif
  #include <sys/stat.h>
  #include <sys/types.h>
 -#if defined(__linux__)
 +#if defined(__linux__) || defined(__DragonFly__) || defined(__FreeBSD__)
++#define __BSD_VISIBLE 1
  #include <dlfcn.h>
  #elif defined(__APPLE__)
  #define _DARWIN_C_SOURCE
-@@ -2137,7 +2139,7 @@ default_udunits2_xml_path()
-     const char* path;
-     char const* soname = 0;
+@@ -2135,7 +2136,7 @@ default_udunits2_xml_path()
+     static char absXmlPathname[PATH_MAX];
  
--#if defined(__APPLE__) || defined(__linux__)
-+#if defined(__APPLE__) || defined(__linux__) || defined(__FreeBSD__) || defined(__DragonFly__)
-     #define END_BIT "/share/udunits/udunits2.xml"
-     #define SEP '/'
-     Dl_info info;
+     if (absXmlPathname[0] == 0) {
+-#       if defined(__APPLE__) || defined(__linux__)
++#       if defined(__APPLE__) || defined(__linux__) || defined(__FreeBSD__) || defined(__DragonFly__)
+             Dl_info     info;
+             const char  sep = '/'; ///< Pathname component separator
+             char        buf[PATH_MAX];
