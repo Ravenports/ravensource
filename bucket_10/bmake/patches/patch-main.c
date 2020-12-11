646b9d5f74a65ccd5321b4f94b143dca317f05f1
John Marino <draco@marino.st>
2020-12-11 17:15:58 +0000

    bmake: upgrade

--- bucket_10/bmake/patches/patch-main.c
+++ bucket_10/bmake/patches/patch-main.c
@@ -1,9 +1,9 @@
---- main.c.orig	2020-11-18 03:58:43 UTC
+--- main.c.orig	2020-11-30 19:27:53 UTC
 +++ main.c
-@@ -1469,6 +1469,12 @@ main_Init(int argc, char **argv)
- 		Var_Set(".MAKE.PID", tmp, VAR_GLOBAL);
- 		snprintf(tmp, sizeof tmp, "%u", getppid());
- 		Var_Set(".MAKE.PPID", tmp, VAR_GLOBAL);
+@@ -1473,6 +1473,12 @@ main_Init(int argc, char **argv)
+ 		Var_Set(".MAKE.UID", tmp, VAR_GLOBAL);
+ 		snprintf(tmp, sizeof tmp, "%u", getgid());
+ 		Var_Set(".MAKE.GID", tmp, VAR_GLOBAL);
 +		Var_Set(".MAKE.OS.NAME",          OSNAME,    VAR_GLOBAL);
 +		Var_Set(".MAKE.OS.VERSION",       OSVERSION, VAR_GLOBAL);
 +		Var_Set(".MAKE.OS.RELEASE",       OSRELEASE, VAR_GLOBAL);
