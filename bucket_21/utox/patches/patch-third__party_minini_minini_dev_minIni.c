bbe44c5d14a29fcb99e9f367f9938dda0d014199
John Marino <draco@marino.st>
2020-12-11 21:26:30 +0000

    utox: upgrade

--- bucket_21/utox/patches/patch-third__party_minini_minini_dev_minIni.c
+++ bucket_21/utox/patches/patch-third__party_minini_minini_dev_minIni.c
@@ -1,11 +1,11 @@
---- third-party/minini/dev/minIni.c.orig	2020-04-14 17:53:20 UTC
-+++ third-party/minini/dev/minIni.c
+--- third_party/minini/minini/dev/minIni.c.orig	2020-12-11 21:24:06 UTC
++++ third_party/minini/minini/dev/minIni.c
 @@ -71,7 +71,7 @@
    #pragma warning(disable: 4996)	/* for Microsoft Visual C/C++ */
  #endif
  #if !defined strnicmp && !defined PORTABLE_STRNICMP
 -  #if defined __LINUX__ || defined __FreeBSD__ || defined __OpenBSD__ || defined __APPLE__
-+  #if defined __LINUX__ || defined __FreeBSD__ || defined __OpenBSD__ || defined __APPLE__ || defined __DragonFly__ || defined __NetBSD__
++  #if defined __LINUX__ || defined __FreeBSD__ || defined __OpenBSD__ || defined __APPLE__ || defined __DragonFly__ || defined __NetBSD_
      #define strnicmp  strncasecmp
    #endif
  #endif
