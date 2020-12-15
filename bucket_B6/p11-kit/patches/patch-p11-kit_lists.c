6f8edf127f2718f4773acfb71456720f1475038c
John Marino <draco@marino.st>
2020-12-15 06:46:35 -0600

    p11-kit: patch to fix build on freebsd

---
+++ bucket_B6/p11-kit/patches/patch-p11-kit_lists.c
@@ -0,0 +1,10 @@
+--- p11-kit/lists.c.orig	2020-12-11 15:25:36 UTC
++++ p11-kit/lists.c
+@@ -41,6 +41,7 @@
+ #include <ctype.h>
+ #include <string.h>
+ #include <stdio.h>
++#include <stdint.h>
+ #include <stdlib.h>
+ #include <unistd.h>
+ 
