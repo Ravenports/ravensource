a380435446bb015dc36f9aaf2ced60b38216d191
John Marino <draco@marino.st>
2020-12-19 16:15:58 +0000

    fluent-bit: upgrade

--- bucket_12/fluent-bit/patches/patch-lib_chunkio_deps_crc32_crc32.c
+++ bucket_12/fluent-bit/patches/patch-lib_chunkio_deps_crc32_crc32.c
@@ -1,4 +1,4 @@
---- lib/chunkio/deps/crc32/crc32.c.orig	2020-11-24 18:24:33 UTC
+--- lib/chunkio/deps/crc32/crc32.c.orig	2020-12-18 00:40:07 UTC
 +++ lib/chunkio/deps/crc32/crc32.c
 @@ -31,7 +31,7 @@
  #  define htole16(x) (x)
