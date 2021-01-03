89a27f93215648854c28d3bd90c45deeb46dc814
John Marino <draco@marino.st>
2021-01-03 23:01:48 +0000

    upgrade tcl/tk 8.6

--- bucket_E3/tcl86/patches/patch-generic-tclPort.h
+++ bucket_E3/tcl86/patches/patch-generic-tclPort.h
@@ -1,4 +1,4 @@
---- generic/tclPort.h.orig	2019-06-17 18:39:57 UTC
+--- generic/tclPort.h.orig	2020-07-06 20:41:07 UTC
 +++ generic/tclPort.h
 @@ -20,7 +20,7 @@
  #if defined(_WIN32)
--- bucket_E3/tcl86/patches/patch-unix-tclUnixInit.c
+++ bucket_E3/tcl86/patches/patch-unix-tclUnixInit.c
@@ -1,6 +1,6 @@
---- unix/tclUnixInit.c.orig	2019-08-26 15:44:14 UTC
+--- unix/tclUnixInit.c.orig	2020-12-11 17:46:23 UTC
 +++ unix/tclUnixInit.c
-@@ -391,14 +391,6 @@ TclpInitPlatform(void)
+@@ -395,14 +395,6 @@ TclpInitPlatform(void)
  #endif /* SIGPIPE */
  
  #if defined(__FreeBSD__) && defined(__GNUC__)
