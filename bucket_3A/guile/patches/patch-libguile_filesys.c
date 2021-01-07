93984745b2c12c05a7978b8e2cfc2a79a76422d7
John Marino <draco@marino.st>
2021-01-07 22:00:48 +0000

    guile: upgrade

--- bucket_3A/guile/patches/patch-libguile_filesys.c
+++ bucket_3A/guile/patches/patch-libguile_filesys.c
@@ -1,4 +1,4 @@
---- libguile/filesys.c.orig	2020-06-19 10:16:12 UTC
+--- libguile/filesys.c.orig	2020-05-30 20:06:44 UTC
 +++ libguile/filesys.c
 @@ -1519,6 +1519,9 @@ SCM_DEFINE (scm_i_mkstemp, "mkstemp!", 1
        mode_bits = scm_i_mode_bits (mode);
