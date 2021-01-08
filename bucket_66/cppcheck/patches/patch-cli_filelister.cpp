33847ad26fa1b0005aeb073d97fffef110fc3a72
John Marino <draco@marino.st>
2021-01-08 05:03:38 +0000

    add cppcheck tool and gui

---
+++ bucket_66/cppcheck/patches/patch-cli_filelister.cpp
@@ -0,0 +1,10 @@
+--- cli/filelister.cpp.orig	2020-12-05 09:20:59 UTC
++++ cli/filelister.cpp
+@@ -24,6 +24,7 @@
+ 
+ #include <cstddef>
+ #include <cstring>
++#include <climits>
+ 
+ #ifdef _WIN32
+ 
