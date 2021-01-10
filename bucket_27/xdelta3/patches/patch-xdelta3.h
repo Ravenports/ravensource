a3c7860d85ba7cf368f4e9ea90075163161a229f
John Marino <draco@marino.st>
2021-01-10 00:51:39 +0000

    add xdelta3 port

---
+++ bucket_27/xdelta3/patches/patch-xdelta3.h
@@ -0,0 +1,12 @@
+--- xdelta3.h.orig	2016-01-08 07:58:54 UTC
++++ xdelta3.h
+@@ -24,9 +24,7 @@
+ #ifndef _XDELTA3_H_
+ #define _XDELTA3_H_
+ 
+-#define _POSIX_SOURCE 200112L
+ #define _ISOC99_SOURCE
+-#define _C99_SOURCE
+ 
+ #if HAVE_CONFIG_H
+ #include "config.h"
