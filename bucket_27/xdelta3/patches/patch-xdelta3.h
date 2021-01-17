1d74e1260c7c8b67339e1baab99fc06d48a7219c
John Marino <draco@marino.st>
2021-01-17 12:10:29 -0600

    xdelta3: fix build on linux

--- bucket_27/xdelta3/patches/patch-xdelta3.h
+++ bucket_27/xdelta3/patches/patch-xdelta3.h
@@ -1,12 +1,15 @@
 --- xdelta3.h.orig	2016-01-08 07:58:54 UTC
 +++ xdelta3.h
-@@ -24,9 +24,7 @@
+@@ -24,9 +24,11 @@
  #ifndef _XDELTA3_H_
  #define _XDELTA3_H_
  
--#define _POSIX_SOURCE 200112L
- #define _ISOC99_SOURCE
--#define _C99_SOURCE
++#ifdef __linux__
+ #define _POSIX_SOURCE 200112L
+-#define _ISOC99_SOURCE
+ #define _C99_SOURCE
++#endif
++#define _ISOC99_SOURCE
  
  #if HAVE_CONFIG_H
  #include "config.h"
