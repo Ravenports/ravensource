aacdea7257174ad138a093b979f2bb538e0e0d52
John Marino <draco@marino.st>
2021-01-25 02:49:34 +0000

    pinentry: upgrade

--- bucket_78/pinentry/patches/patch-secmem_secmem.c
+++ bucket_78/pinentry/patches/patch-secmem_secmem.c
@@ -1,4 +1,4 @@
---- secmem/secmem.c.orig	2017-12-03 16:13:05 UTC
+--- secmem/secmem.c.orig	2017-11-21 22:43:51 UTC
 +++ secmem/secmem.c
 @@ -88,7 +88,7 @@ log_fatal(char *template, ...)
  #  define MAP_ANONYMOUS MAP_ANON
