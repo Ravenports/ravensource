ee2a2fc0ae566e6d74e12e567fef77e352205f5a
John Marino <draco@marino.st>
2021-02-18 06:18:57 +0000

    npm: upgrade

--- bucket_66/npm/patches/patch-node__modules_@npmcli_config_lib_index.js
+++ bucket_66/npm/patches/patch-node__modules_@npmcli_config_lib_index.js
@@ -1,4 +1,4 @@
---- node_modules/@npmcli/config/lib/index.js.orig	2021-02-08 20:49:00 UTC
+--- node_modules/@npmcli/config/lib/index.js.orig	2021-02-12 18:11:07 UTC
 +++ node_modules/@npmcli/config/lib/index.js
 @@ -296,6 +296,9 @@ class Config {
  
