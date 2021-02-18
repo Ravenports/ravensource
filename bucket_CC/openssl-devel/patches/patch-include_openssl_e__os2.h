afed0527c4fee5cf208bcec96052461b3ed89965
John Marino <draco@marino.st>
2021-02-18 04:14:11 +0000

    openssl-devel: upgrade

--- bucket_CC/openssl-devel/patches/patch-include_openssl_e__os2.h
+++ bucket_CC/openssl-devel/patches/patch-include_openssl_e__os2.h
@@ -1,4 +1,4 @@
---- include/openssl/e_os2.h.orig	2020-12-08 13:20:59 UTC
+--- include/openssl/e_os2.h.orig	2021-02-16 15:24:01 UTC
 +++ include/openssl/e_os2.h
 @@ -279,7 +279,9 @@ typedef unsigned __int64 uint64_t;
  #  define ossl_inline inline
