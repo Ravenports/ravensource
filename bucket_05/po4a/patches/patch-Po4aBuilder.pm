05d54be554208f44ec6a59fa3976f9d2dcad2f32
Michael Reim <kraileth@elderlinux.org>
2021-02-27 09:27:15 +0100

    po4a: upgrade and restructure port accordingly

--- bucket_05/po4a/patches/patch-Po4aBuilder.pm
+++ bucket_05/po4a/patches/patch-Po4aBuilder.pm
@@ -1,6 +1,6 @@
---- Po4aBuilder.pm.orig	2020-08-05 13:03:49 UTC
+--- Po4aBuilder.pm.orig	2020-12-07 15:24:54 UTC
 +++ Po4aBuilder.pm
-@@ -116,8 +116,8 @@ sub ACTION_install {
+@@ -118,8 +118,8 @@ sub ACTION_install {
      $mandir =~ s,/man3$,,;
      $self->install_path(man => $mandir);
  
@@ -11,7 +11,7 @@
      $self->install_path(po => $localedir);
  
      ExtUtils::Install::install($self->install_map, !$self->quiet, 0, $self->{args}{uninst}||0);
-@@ -248,7 +248,7 @@ sub ACTION_man {
+@@ -250,7 +250,7 @@ sub ACTION_man {
  		system("xsltproc -o $outdir/$outfile.$section --nonet /usr/share/xml/docbook/stylesheet/docbook-xsl/manpages/docbook.xsl $file") and die;
  	    } else { # Not found locally, use the XSL file online
  		print "Convert $outdir/$outfile.$section (online docbook.xsl file). ";
