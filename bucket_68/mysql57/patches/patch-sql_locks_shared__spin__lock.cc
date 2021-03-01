72ab4caddc8106cbd3af46796589e1d5af1c8b0c
John Marino <draco@marino.st>
2021-03-01 11:44:16 -0600

    mysql57: fix build on linux

--- bucket_68/mysql57/patches/patch-sql_locks_shared__spin__lock.cc
+++ bucket_68/mysql57/patches/patch-sql_locks_shared__spin__lock.cc
@@ -1,11 +1,14 @@
 --- sql/locks/shared_spin_lock.cc.orig	2020-12-10 03:01:55 UTC
 +++ sql/locks/shared_spin_lock.cc
-@@ -239,7 +239,7 @@ lock::Shared_spin_lock &lock::Shared_spi
+@@ -239,7 +239,11 @@ lock::Shared_spin_lock &lock::Shared_spi
    {
      this->spin_exclusive_lock();
    }
--  my_atomic_store64(&this->m_exclusive_owner, self);
++#if defined(__FreeBSD__) || defined(__DragonFly__)
 +  my_atomic_store64(&this->m_exclusive_owner, reinterpret_cast<int64>(self));
++#else
+   my_atomic_store64(&this->m_exclusive_owner, self);
++#endif
    return (*this);
  }
  
