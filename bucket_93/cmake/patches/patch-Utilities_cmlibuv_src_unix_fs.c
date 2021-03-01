64aa26d847828d90c3136818807dec5c8f3e302d
John Marino <draco@marino.st>
2021-03-01 04:43:27 -0600

    cmake: fix build on sunos

--- bucket_93/cmake/patches/patch-Utilities_cmlibuv_src_unix_fs.c
+++ bucket_93/cmake/patches/patch-Utilities_cmlibuv_src_unix_fs.c
@@ -1,4 +1,4 @@
---- Utilities/cmlibuv/src/unix/fs.c.orig	2021-02-15 16:48:33 UTC
+--- Utilities/cmlibuv/src/unix/fs.c.orig	2021-02-24 15:08:43 UTC
 +++ Utilities/cmlibuv/src/unix/fs.c
 @@ -1018,7 +1018,6 @@ ok:
  static ssize_t uv__fs_utime(uv_fs_t* req) {
@@ -16,3 +16,11 @@
      || defined(__OpenBSD__)
    struct timeval tv[2];
    tv[0] = uv__fs_to_timeval(req->atime);
+@@ -1061,7 +1061,6 @@ static ssize_t uv__fs_utime(uv_fs_t* req
+ static ssize_t uv__fs_lutime(uv_fs_t* req) {
+ #if defined(__linux__)            ||                                           \
+     defined(_AIX71)               ||                                           \
+-    defined(__sun)                ||                                           \
+     defined(__HAIKU__)
+   struct timespec ts[2];
+   ts[0] = uv__fs_to_timespec(req->atime);
