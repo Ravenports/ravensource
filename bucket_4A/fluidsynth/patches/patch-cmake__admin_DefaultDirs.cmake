32f31ac874d202825c606de83aaa44c59e4cae08
John Marino <draco@marino.st>
2021-03-18 19:23:52 +0000

    fluidsynth: upgrade

--- bucket_4A/fluidsynth/patches/patch-cmake__admin_DefaultDirs.cmake
+++ bucket_4A/fluidsynth/patches/patch-cmake__admin_DefaultDirs.cmake
@@ -1,4 +1,4 @@
---- cmake_admin/DefaultDirs.cmake.orig	2021-01-29 13:37:27 UTC
+--- cmake_admin/DefaultDirs.cmake.orig	2021-03-15 19:12:51 UTC
 +++ cmake_admin/DefaultDirs.cmake
 @@ -53,7 +53,7 @@ set (INFO_INSTALL_DIR "share/info" CACHE
  mark_as_advanced (INFO_INSTALL_DIR) 
