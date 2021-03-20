acba7f1152af0979a3dc7c855a898ee1ac3e0d84
Michael Reim <kraileth@elderlinux.org>
2021-03-20 15:54:11 +0100

    lua-sysctl: Upgrade and enable multiple language version variants

--- bucket_80/lua-sysctl/patches/patch-src_lua__sysctl.c
+++ bucket_80/lua-sysctl/patches/patch-src_lua__sysctl.c
@@ -1,6 +1,6 @@
---- src/lua_sysctl.c.orig	2016-12-23 20:56:25 UTC
-+++ src/lua_sysctl.c
-@@ -266,10 +266,12 @@ luaA_sysctl_set(lua_State *L)
+--- src/lua_sysctl.c.orig	2021-03-20 15:41:49.561385000 +0100
++++ src/lua_sysctl.c	2021-03-20 15:42:52.220425000 +0100
+@@ -273,10 +273,12 @@
  	if ((kind & CTLTYPE) == CTLTYPE_NODE)
  		return (luaL_error(L, "oid '%s' isn't a leaf node", bufp));
  	if (!(kind & CTLFLAG_WR)) {
