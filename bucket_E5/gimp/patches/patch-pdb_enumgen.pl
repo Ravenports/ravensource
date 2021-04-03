19a768240f06a3f760108fc62a07badc9b41947a
John Marino <draco@marino.st>
2021-04-03 18:29:35 +0000

    gimp: finish upgrade

--- bucket_E5/gimp/patches/patch-pdb_enumgen.pl
+++ bucket_E5/gimp/patches/patch-pdb_enumgen.pl
@@ -1,5 +1,5 @@
 Should be ok.
---- pdb/enumgen.pl.orig	2018-08-19 16:47:18 UTC
+--- pdb/enumgen.pl.orig	2021-03-28 21:52:56 UTC
 +++ pdb/enumgen.pl
 @@ -211,7 +211,7 @@ while (<>) {
         \*/@@gx;
