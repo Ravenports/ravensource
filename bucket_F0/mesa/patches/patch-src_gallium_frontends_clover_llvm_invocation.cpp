5e816d7771ec8b3a07cfedc21b632658c2cb2c1c
John Marino <draco@marino.st>
2021-04-20 14:00:05 +0000

    mesa: fix build with llvm 12.0

---
+++ bucket_F0/mesa/patches/patch-src_gallium_frontends_clover_llvm_invocation.cpp
@@ -0,0 +1,13 @@
+Fix build with llvm 12.0
+
+--- src/gallium/frontends/clover/llvm/invocation.cpp.orig	2021-04-07 16:36:56 UTC
++++ src/gallium/frontends/clover/llvm/invocation.cpp
+@@ -249,7 +249,7 @@ namespace {
+ 
+       c->getInvocation().setLangDefaults(c->getLangOpts(),
+                                 compat::ik_opencl, ::llvm::Triple(target.triple),
+-                                c->getPreprocessorOpts(),
++                                c->getPreprocessorOpts().Includes,
+                                 get_language_version(opts, device_clc_version));
+ 
+       c->createDiagnostics(new clang::TextDiagnosticPrinter(
