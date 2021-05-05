bd4bdbb58334bf0eaa15e9a6c7b3dabebb80a4a4
John Marino <draco@marino.st>
2021-05-05 00:41:55 +0000

    gtk4: upgrade

--- bucket_C8/gtk4/patches/patch-gtk_roaring_roaring.h
+++ bucket_C8/gtk4/patches/patch-gtk_roaring_roaring.h
@@ -1,4 +1,4 @@
---- gtk/roaring/roaring.h.orig	2021-03-30 15:07:36 UTC
+--- gtk/roaring/roaring.h.orig	2021-05-04 01:43:42 UTC
 +++ gtk/roaring/roaring.h
 @@ -59,7 +59,6 @@ enum {
  #include <stdint.h>
