f8cb17c759de77e6b325637ce936ec283209453f
John Marino <draco@marino.st>
2021-05-19 03:06:56 +0000

    fluent-bit: upgrade

--- bucket_12/fluent-bit/patches/patch-lib_monkey_mk__core_deps_libevent_kqueue.c
+++ bucket_12/fluent-bit/patches/patch-lib_monkey_mk__core_deps_libevent_kqueue.c
@@ -1,4 +1,4 @@
---- lib/monkey/mk_core/deps/libevent/kqueue.c.orig	2021-04-19 23:40:36 UTC
+--- lib/monkey/mk_core/deps/libevent/kqueue.c.orig	2021-05-13 23:32:16 UTC
 +++ lib/monkey/mk_core/deps/libevent/kqueue.c
 @@ -50,7 +50,7 @@
  /* Some platforms apparently define the udata field of struct kevent as
