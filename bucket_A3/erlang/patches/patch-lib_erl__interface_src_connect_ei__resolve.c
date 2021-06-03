362efa9e54e44cc42c2eb9ced495cb09ccd5173f
Michael Reim <kraileth@elderlinux.org>
2021-06-03 22:04:11 +0200

    erlang: upgrade

--- bucket_A3/erlang/patches/patch-lib_erl__interface_src_connect_ei__resolve.c
+++ bucket_A3/erlang/patches/patch-lib_erl__interface_src_connect_ei__resolve.c
@@ -1,4 +1,4 @@
---- lib/erl_interface/src/connect/ei_resolve.c.orig	2021-05-10 14:06:09 UTC
+--- lib/erl_interface/src/connect/ei_resolve.c.orig	2021-05-20 14:05:53 UTC
 +++ lib/erl_interface/src/connect/ei_resolve.c
 @@ -438,7 +438,8 @@ struct hostent *ei_gethostbyname_r(const
  
--- bucket_A3/erlang/patches/patch-lib_stdlib_src_calendar.erl
+++ bucket_A3/erlang/patches/patch-lib_stdlib_src_calendar.erl
@@ -1,4 +1,4 @@
---- lib/stdlib/src/calendar.erl.orig	2021-05-10 14:06:09 UTC
+--- lib/stdlib/src/calendar.erl.orig	2021-05-20 14:05:53 UTC
 +++ lib/stdlib/src/calendar.erl
 @@ -297,11 +297,19 @@ local_time_to_universal_time(DateTime, I
        DateTime1 :: datetime1970(),
