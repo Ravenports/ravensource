bb62f9464bf5d37030674eb67e54e036144d2d62
John Marino <draco@marino.st>
2021-06-20 06:26:02 +0000

    remove obsolete ansible-base port (replaced by python-ansible-core)

--- bucket_0C/ansible-base/files/special.mk
+++
@@ -1,8 +0,0 @@
-# POST_PLIST_TARGET=	autolist
-
-# override USES=python autolist
-setuptools-autolist:
-	@${ECHO_MSG} "autogenerating manifest for lib ..."
-	@(cd ${STAGEDIR}${PREFIX} && ${FIND} lib \
-	\( -type f -o -type l \) 2>/dev/null | ${SORT}) \
-	>> ${WRKDIR}/.manifest.primary.mktmp
