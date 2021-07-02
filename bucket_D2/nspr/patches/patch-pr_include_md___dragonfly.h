d15f5641e08b9a1a4809491647331996d0fc2d06
Michael Reim <kraileth@elderlinux.org>
2021-07-02 23:21:13 +0200

    nspr: upgrade

--- bucket_D2/nspr/patches/patch-pr_include_md___dragonfly.h
+++ bucket_D2/nspr/patches/patch-pr_include_md___dragonfly.h
@@ -1,4 +1,4 @@
---- /dev/null	2021-05-28 12:16:35 UTC
+--- /dev/null	2021-07-02 21:20:38 UTC
 +++ pr/include/md/_dragonfly.h
 @@ -0,0 +1,225 @@
 +/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
