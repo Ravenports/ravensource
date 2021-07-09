91cb642a67cf8f37e752dd7b871f4da56bc67fcc
John Marino <draco@marino.st>
2021-07-09 00:26:00 +0000

    fish: upgrade

--- bucket_64/fish/patches/patch-cmake_Install.cmake
+++ bucket_64/fish/patches/patch-cmake_Install.cmake
@@ -1,4 +1,4 @@
---- cmake/Install.cmake.orig	2021-06-28 14:46:46 UTC
+--- cmake/Install.cmake.orig	2021-07-06 14:45:37 UTC
 +++ cmake/Install.cmake
 @@ -102,7 +102,7 @@ install(FILES share/config.fish
  fish_create_dirs(${rel_datadir}/fish/vendor_completions.d ${rel_datadir}/fish/vendor_functions.d
