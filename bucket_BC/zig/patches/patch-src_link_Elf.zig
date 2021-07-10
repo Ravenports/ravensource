0f6a0c3648e8ffe92e0184aac2b91449d37afd30
John Marino <draco@marino.st>
2021-07-10 17:35:16 +0000

    zig: upgrade

---
+++ bucket_BC/zig/patches/patch-src_link_Elf.zig
@@ -0,0 +1,25 @@
+--- src/link/Elf.zig.orig	2021-06-04 18:21:32 UTC
++++ src/link/Elf.zig
+@@ -3339,19 +3339,13 @@ const CsuObjects = struct {
+             const crt_dir_path = lci.crt_dir orelse return error.LibCInstallationMissingCRTDir;
+             switch (link_options.target.os.tag) {
+                 .dragonfly => {
++                    const gcc_dir_path = "@GCC_TARGET_LIBDIR@";
+                     if (result.crt0) |*obj| obj.* = try fs.path.join(arena, &[_][]const u8{ crt_dir_path, obj.* });
+                     if (result.crti) |*obj| obj.* = try fs.path.join(arena, &[_][]const u8{ crt_dir_path, obj.* });
+                     if (result.crtn) |*obj| obj.* = try fs.path.join(arena, &[_][]const u8{ crt_dir_path, obj.* });
+ 
+-                    var gccv: []const u8 = undefined;
+-                    if (link_options.target.os.version_range.semver.isAtLeast(.{ .major = 5, .minor = 4 }) orelse true) {
+-                        gccv = "gcc80";
+-                    } else {
+-                        gccv = "gcc54";
+-                    }
+-
+-                    if (result.crtbegin) |*obj| obj.* = try fs.path.join(arena, &[_][]const u8{ crt_dir_path, gccv, obj.* });
+-                    if (result.crtend) |*obj| obj.* = try fs.path.join(arena, &[_][]const u8{ crt_dir_path, gccv, obj.* });
++                    if (result.crtbegin) |*obj| obj.* = try fs.path.join(arena, &[_][]const u8{ gcc_dir_path, obj.* });
++                    if (result.crtend) |*obj| obj.* = try fs.path.join(arena, &[_][]const u8{ gcc_dir_path, obj.* });
+                 },
+                 .haiku => {
+                     const gcc_dir_path = lci.gcc_dir orelse return error.LibCInstallationMissingCRTDir;
