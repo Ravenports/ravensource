5bf8997ed9dea56f9e95d95b3336f6baf49dee03
John Marino <draco@marino.st>
2021-07-15 22:17:02 +0000

    tdb: upgrade

--- bucket_5F/tdb/patches/patch-buildtools_wafsamba_samba__autoconf.py
+++ bucket_5F/tdb/patches/patch-buildtools_wafsamba_samba__autoconf.py
@@ -1,7 +1,7 @@
 Disable libssp -- it causes segfaults on several platforms, likly some
 kind of missing os-specific support or consideration.
 
---- buildtools/wafsamba/samba_autoconf.py.orig	2019-12-16 09:14:22 UTC
+--- buildtools/wafsamba/samba_autoconf.py.orig	2021-04-28 06:53:27 UTC
 +++ buildtools/wafsamba/samba_autoconf.py
 @@ -684,44 +684,6 @@ def SAMBA_CONFIG_H(conf, path=None):
      if not IN_LAUNCH_DIR(conf):
