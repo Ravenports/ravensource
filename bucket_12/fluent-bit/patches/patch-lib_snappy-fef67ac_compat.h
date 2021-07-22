0e9eeb9c646f7a0dfad959f3acf6d39ba4d6b53f
John Marino <draco@marino.st>
2021-07-22 23:40:21 +0000

    fluent-bit: upgrade

---
+++ bucket_12/fluent-bit/patches/patch-lib_snappy-fef67ac_compat.h
@@ -0,0 +1,11 @@
+--- lib/snappy-fef67ac/compat.h.orig	2021-07-20 21:38:15 UTC
++++ lib/snappy-fef67ac/compat.h
+@@ -4,6 +4,8 @@
+ 
+ #ifdef __FreeBSD__
+ #  include <sys/endian.h>
++#elif defined __DragonFly__
++#  include <sys/endian.h>
+ #elif defined(__APPLE_CC_) || defined(__MACH__)  /* MacOS/X support */
+ #  include <machine/endian.h>
+ 
