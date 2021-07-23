b4944256835ca51fa7776096e108ce255db2aabe
John Marino <draco@marino.st>
2021-07-23 02:13:08 +0000

    npm: upgrade

--- bucket_66/npm/patches/patch-node__modules_node-gyp_gyp_pylib_gyp_xcode__emulation.py
+++ bucket_66/npm/patches/patch-node__modules_node-gyp_gyp_pylib_gyp_xcode__emulation.py
@@ -1,6 +1,6 @@
 Do not generate debugging symbols (uses lots of disk space).
 
---- node_modules/node-gyp/gyp/pylib/gyp/xcode_emulation.py.orig	2021-06-17 18:00:51 UTC
+--- node_modules/node-gyp/gyp/pylib/gyp/xcode_emulation.py.orig	2021-07-22 20:16:44 UTC
 +++ node_modules/node-gyp/gyp/pylib/gyp/xcode_emulation.py
 @@ -610,7 +610,7 @@ class XcodeSettings(object):
  
