d63413fd848852afaa9aae4327dda9fee19bf7e3
Michael Reim <kraileth@elderlinux.org>
2021-07-29 22:33:18 +0200

    npm: upgrade

--- bucket_66/npm/patches/patch-node__modules_node-gyp_gyp_pylib_gyp_generator_make.py
+++ bucket_66/npm/patches/patch-node__modules_node-gyp_gyp_pylib_gyp_generator_make.py
@@ -2,7 +2,7 @@ $NetBSD: patch-node__modules_node-gyp_gyp_pylib_gyp_generator_make.py,v 1.1 2018
 
 Ensure we use the system libtool on OSX.
 
---- node_modules/node-gyp/gyp/pylib/gyp/generator/make.py.orig	2021-07-22 20:16:44 UTC
+--- node_modules/node-gyp/gyp/pylib/gyp/generator/make.py.orig	2021-07-01 17:10:11 UTC
 +++ node_modules/node-gyp/gyp/pylib/gyp/generator/make.py
 @@ -180,7 +180,7 @@ cmd_solink_module = $(LINK.$(TOOLSET)) -
  
