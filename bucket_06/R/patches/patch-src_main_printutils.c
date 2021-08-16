00badcd7dbff9dac7e59c4cd34cd8c8f7a636d7b
John Marino <draco@marino.st>
2021-08-16 03:34:23 +0000

    R: upgrade

--- bucket_06/R/patches/patch-src_main_printutils.c
+++ bucket_06/R/patches/patch-src_main_printutils.c
@@ -1,11 +1,11 @@
---- src/main/printutils.c.orig	2021-04-19 22:02:06 UTC
+--- src/main/printutils.c.orig	2021-07-02 22:15:02 UTC
 +++ src/main/printutils.c
-@@ -751,9 +751,6 @@ const char *EncodeString(SEXP s, int w,
+@@ -723,9 +723,6 @@ const char *EncodeString(SEXP s, int w,
  			*/
  			for(int j = 0; j < res; j++) *q++ = *p++;
  		    } else {
 -# if !defined (__STDC_ISO_10646__) && !defined (Win32)
--			if(!use_ucs) Unicode_warning = TRUE;
+-			Unicode_warning = TRUE;
 -# endif
  			if(k > 0xffff)
  			    snprintf(buf, 13, "\\U{%06x}", k);
