f58d9ba58a99e563769a5b8f96a8418fa545c6ed
John Marino <draco@marino.st>
2021-08-28 06:46:22 -0500

    ghostscript: fix build on sunos

---
+++ bucket_78/ghostscript/patches/patch-leptonica_src_environ.h
@@ -0,0 +1,11 @@
+--- leptonica/src/environ.h.orig	2021-03-30 07:40:28 UTC
++++ leptonica/src/environ.h
+@@ -172,7 +172,7 @@ typedef uintptr_t l_uintptr_t;
+  * To use them on MacOS, which does not support these functions, set it to 0.
+  *-------------------------------------------------------------------------*/
+ #if !defined(HAVE_CONFIG_H) && !defined(ANDROID_BUILD) && !defined(OS_IOS) && \
+-    !defined(_WIN32)
++    !defined(_WIN32) && !defined(__sun)
+ #define  HAVE_FMEMOPEN    1
+ #endif  /* ! HAVE_CONFIG_H etc. */
+ 
