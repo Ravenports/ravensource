dcdd7c45731df643dc5240a1a3a14767e2683d4b
John Marino <draco@marino.st>
2021-08-28 03:44:51 -0500

    libdrm: fix build on solaris

--- bucket_CD/libdrm/patches/patch-xf86drm.c
+++ bucket_CD/libdrm/patches/patch-xf86drm.c
@@ -19,7 +19,29 @@
  #include <sys/param.h>
  #include <sys/pciio.h>
  #endif
-@@ -898,7 +908,7 @@ static int drmGetMinorBase(int type)
+@@ -230,8 +240,10 @@ drmGetFormatModifierNameFromArm(uint64_t
+     if (type != DRM_FORMAT_MOD_ARM_TYPE_AFBC)
+         return NULL;
+ 
++#ifndef __sun
+     fp = open_memstream(&modifier_name, &size);
+     if (!fp)
++#endif
+         return NULL;
+ 
+     /* add block, can only have a (single) block */
+@@ -394,8 +406,10 @@ drmGetFormatModifierNameFromAmd(uint64_t
+     tile_version = AMD_FMT_MOD_GET(TILE_VERSION, modifier);
+     dcc = AMD_FMT_MOD_GET(DCC, modifier);
+ 
++#ifndef __sun
+     fp = open_memstream(&mod_amd, &size);
+     if (!fp)
++#endif
+         return NULL;
+ 
+     /* add tile  */
+@@ -898,7 +912,7 @@ static int drmGetMinorBase(int type)
  
  static int drmGetMinorType(int major, int minor)
  {
@@ -28,7 +50,7 @@
      char name[SPECNAMELEN];
      int id;
  
-@@ -3154,7 +3164,7 @@ drm_public int drmIsMaster(int fd)
+@@ -3154,7 +3168,7 @@ drm_public int drmIsMaster(int fd)
  
  drm_public char *drmGetDeviceNameFromFd(int fd)
  {
@@ -37,7 +59,7 @@
      struct stat sbuf;
      int maj, min;
      int nodetype;
-@@ -3201,7 +3211,7 @@ static bool drmNodeIsDRM(int maj, int mi
+@@ -3201,7 +3215,7 @@ static bool drmNodeIsDRM(int maj, int mi
      snprintf(path, sizeof(path), "/sys/dev/char/%d:%d/device/drm",
               maj, min);
      return stat(path, &sbuf) == 0;
@@ -46,7 +68,7 @@
      char name[SPECNAMELEN];
  
      if (!devname_r(makedev(maj, min), S_IFCHR, name, sizeof(name)))
-@@ -3314,7 +3324,7 @@ static char *drmGetMinorNameForFD(int fd
+@@ -3314,7 +3328,7 @@ static char *drmGetMinorNameForFD(int fd
  
      closedir(sysdir);
      return NULL;
@@ -55,7 +77,7 @@
      struct stat sbuf;
      char dname[SPECNAMELEN];
      const char *mname;
-@@ -3525,7 +3535,7 @@ get_pci_path(int maj, int min, char *pci
+@@ -3525,7 +3539,7 @@ get_pci_path(int maj, int min, char *pci
  }
  #endif
  
