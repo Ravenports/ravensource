5fad556dc86e1da16d8e49c8a4603bd9b38c6751
Super-User <draco@marino.st>
2021-08-29 17:40:39 -0500

    vimb: fix build on sunos

---
+++ bucket_47/vimb/patches/patch-src_history.c
@@ -0,0 +1,28 @@
+--- src/history.c.orig	2020-01-02 22:17:49 UTC
++++ src/history.c
+@@ -259,8 +259,14 @@ static GList *load(FileStorage *s)
+ static void write_to_file(GList *list, const char *file)
+ {
+     FILE *f;
++    struct flock lock;
++
++    lock.l_whence = SEEK_SET;
++    lock.l_start = 0;
++    lock.l_len = 0;
+     if ((f = fopen(file, "w"))) {
+-        flock(fileno(f), LOCK_EX);
++        lock.l_type = F_WRLCK;
++        fcntl(fileno(f), F_SETLKW, &lock);
+ 
+         /* overwrite the history file with new unique history items */
+         for (GList *link = list; link; link = link->next) {
+@@ -272,7 +278,8 @@ static void write_to_file(GList *list, c
+             }
+         }
+ 
+-        flock(fileno(f), LOCK_UN);
++        lock.l_type = F_UNLCK;
++        fcntl(fileno(f), F_SETLK, &lock);
+         fclose(f);
+     }
+ }
