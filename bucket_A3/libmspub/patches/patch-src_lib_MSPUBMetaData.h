d25390429ff4874eefd7d224df58c200b052ff86
John Marino <draco@marino.st>
2021-09-01 23:50:34 +0000

    libmspub: fix build on gcc11

---
+++ bucket_A3/libmspub/patches/patch-src_lib_MSPUBMetaData.h
@@ -0,0 +1,10 @@
+--- src/lib/MSPUBMetaData.h.orig	2018-02-28 11:54:49 UTC
++++ src/lib/MSPUBMetaData.h
+@@ -13,6 +13,7 @@
+ #include <map>
+ #include <utility>
+ #include <vector>
++#include <cstdint>
+ 
+ #include <librevenge/librevenge.h>
+ 
