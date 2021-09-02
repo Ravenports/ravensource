12c970ecebbf6cf93adf9ba24300f38b329a1667
John Marino <draco@marino.st>
2021-09-02 01:48:03 +0000

    libwpd: fix build on gcc11

---
+++ bucket_89/libwpd/patches/patch-src_lib_WPXTable.h
@@ -0,0 +1,10 @@
+--- src/lib/WPXTable.h.orig	2018-12-25 16:19:19 UTC
++++ src/lib/WPXTable.h
+@@ -36,6 +36,7 @@
+ #ifndef _WPXTABLE_H
+ #define _WPXTABLE_H
+ 
++#include <stddef.h>
+ #include <vector>
+ 
+ struct WPXTableCell
