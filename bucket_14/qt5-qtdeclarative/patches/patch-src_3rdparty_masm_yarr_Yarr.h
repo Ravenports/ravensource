2ea1b71bd6639d9a6ab0c9801eac493d2e6749d7
John Marino <draco@marino.st>
2021-09-02 02:34:04 +0000

    qt5-qtdeclarative: fix build on gcc11

---
+++ bucket_14/qt5-qtdeclarative/patches/patch-src_3rdparty_masm_yarr_Yarr.h
@@ -0,0 +1,10 @@
+--- src/3rdparty/masm/yarr/Yarr.h.orig	2020-10-27 08:02:12 UTC
++++ src/3rdparty/masm/yarr/Yarr.h
+@@ -27,6 +27,7 @@
+ 
+ #pragma once
+ 
++#include <limits>
+ #include <limits.h>
+ #include "YarrErrorCode.h"
+ 
