6eeba9cdf8e1d30ec50a423dbb7dfe0daa8b14fb
John Marino <draco@marino.st>
2021-09-18 03:09:31 +0000

    gimp: upgrade

--- bucket_E5/gimp/patches/patch-pdb_enumgen.pl
+++ bucket_E5/gimp/patches/patch-pdb_enumgen.pl
@@ -1,5 +1,5 @@
 Should be ok.
---- pdb/enumgen.pl.orig	2021-09-05 21:59:42 UTC
+--- pdb/enumgen.pl.orig	2021-09-14 15:53:09 UTC
 +++ pdb/enumgen.pl
 @@ -211,7 +211,7 @@ while (<>) {
         \*/@@gx;
