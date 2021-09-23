668b6b3aef04acef144fc19f2a757c32c8c7e82b
John Marino <draco@marino.st>
2021-09-23 21:25:18 +0000

    libsoup-2.4: bring in earlier version of libsoup

---
+++ bucket_84/libsoup-2.4/patches/patch-libsoup_soup-auth-negotiate.c
@@ -0,0 +1,14 @@
+--- libsoup/soup-auth-negotiate.c.orig	2021-08-22 22:32:34 UTC
++++ libsoup/soup-auth-negotiate.c
+@@ -13,7 +13,11 @@
+ #include <string.h>
+ 
+ #ifdef LIBSOUP_HAVE_GSSAPI
++#ifdef __FreeBSD__
++#include <gssapi/gssapi_krb5.h>
++#else
+ #include <gssapi/gssapi.h>
++#endif
+ #endif /* LIBSOUP_HAVE_GSSAPI */
+ 
+ #include "soup-auth-negotiate.h"
