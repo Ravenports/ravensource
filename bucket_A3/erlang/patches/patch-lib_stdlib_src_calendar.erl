041f960c6d47c0ce2c83dfd6fc421c12814534f5
John Marino <draco@marino.st>
2021-09-23 19:38:21 +0000

    erlang: upgrade

--- bucket_A3/erlang/patches/patch-lib_stdlib_src_calendar.erl
+++ bucket_A3/erlang/patches/patch-lib_stdlib_src_calendar.erl
@@ -1,4 +1,4 @@
---- lib/stdlib/src/calendar.erl.orig	2021-09-02 10:21:16 UTC
+--- lib/stdlib/src/calendar.erl.orig	2021-09-17 08:20:27 UTC
 +++ lib/stdlib/src/calendar.erl
 @@ -297,11 +297,19 @@ local_time_to_universal_time(DateTime, I
        DateTime1 :: datetime1970(),
