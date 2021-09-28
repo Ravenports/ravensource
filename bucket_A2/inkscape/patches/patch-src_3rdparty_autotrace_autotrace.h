78073c3460897f8de2bd6ab244ada98042d2136b
John Marino <draco@marino.st>
2021-09-28 01:55:44 +0000

    inkscape: upgrade

--- bucket_A2/inkscape/patches/patch-src_3rdparty_autotrace_autotrace.h
+++ bucket_A2/inkscape/patches/patch-src_3rdparty_autotrace_autotrace.h
@@ -1,4 +1,4 @@
---- src/3rdparty/autotrace/autotrace.h.orig	2021-05-17 19:25:49 UTC
+--- src/3rdparty/autotrace/autotrace.h.orig	2021-05-25 11:35:00 UTC
 +++ src/3rdparty/autotrace/autotrace.h
 @@ -22,8 +22,8 @@
  #define AUTOTRACE_H
