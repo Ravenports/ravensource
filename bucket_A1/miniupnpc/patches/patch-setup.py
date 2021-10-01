b1f2077b9d68e15c7bf8130cdc79ffc3548cdd65
John Marino <draco@marino.st>
2021-10-01 02:04:26 +0000

    miniupnpc: upgrade

--- bucket_A1/miniupnpc/patches/patch-setup.py
+++ bucket_A1/miniupnpc/patches/patch-setup.py
@@ -1,10 +1,10 @@
---- setup.py.orig	2020-05-10 18:11:12 UTC
+--- setup.py.orig	2021-09-28 21:11:31 UTC
 +++ setup.py
 @@ -30,6 +30,6 @@ setup(name="miniupnpc",
        cmdclass={'build_ext': make_then_build_ext},
        ext_modules=[
-          Extension(name="miniupnpc", sources=["miniupnpcmodule.c"],
--                   extra_objects=EXT)
-+                   libraries=["miniupnpc"])
+          Extension(name="miniupnpc", sources=["src/miniupnpcmodule.c"],
+-                   include_dirs=['include'], extra_objects=EXT)
++                   include_dirs=['include'], libraries=["miniupnpc"])
        ])
  
