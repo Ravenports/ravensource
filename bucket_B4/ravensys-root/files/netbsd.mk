68759c856278fd95f4ea627101ba2760db97d922
John Marino <draco@marino.st>
2021-10-18 04:10:42 +0000

    ravensys-root: Modularize and add NetBSD support
    The Makefile had gotten very unwieldy.
    It's been modularized which will make adding new platforms
    a lot more straight-forward.  The first draft of NetBSD
    support has been added.
    
    Tested:
    DragonFly, Linux, NetBSD
    
    Needs testing:
    FreeBSD, Solaris
    
    Needs testing but won't get it:
    Darwin

---
+++ bucket_B4/ravensys-root/files/netbsd.mk
@@ -0,0 +1,295 @@
+# For NetBSD
+
+Libexec_NetBSD=		/libexec/ld.elf_so
+
+# no realpath in NetBSD
+
+Bin_NetBSD=\
+	/bin/[ \
+	/usr/bin/chflags \
+	/bin/chmod \
+	/bin/cp \
+	/bin/date \
+	/bin/echo \
+	/bin/expr \
+	/bin/hostname \
+	/bin/kill \
+	/usr/sbin/link \
+	/bin/ln \
+	/bin/ls \
+	/bin/mkdir \
+	/bin/mv \
+	/bin/pax \
+	/bin/pwd \
+	/bin/rm \
+	/bin/rmdir \
+	/bin/sh \
+	/bin/sleep \
+	/bin/test \
+	/usr/sbin/unlink \
+	# end
+
+# chgrp is /bin/symlink to /sbin/chown
+# no tsort
+
+Usr_Bin_NetBSD= \
+	/usr/bin/basename \
+	/sbin/chown \
+	/usr/bin/comm \
+	/usr/bin/cut \
+	/usr/bin/dirname \
+	/usr/bin/env \
+	/usr/bin/head \
+	/usr/bin/id \
+	/usr/bin/join \
+	/usr/bin/ldd \
+	/usr/bin/mkfifo \
+	/usr/bin/mktemp \
+	/usr/bin/printenv \
+	/usr/bin/printf \
+	/usr/bin/readlink \
+	/usr/bin/stat \
+	/usr/bin/tail \
+	/usr/bin/tee \
+	/usr/bin/touch \
+	/usr/bin/tr \
+	/usr/bin/uniq \
+	/usr/bin/which \
+	/usr/bin/whoami \
+	# end
+
+Usr_Sbin_NetBSD=\
+	/usr/sbin/pwd_mkdb \
+	# end
+
+static_lib_NetBSD=\
+	/usr/lib/libc.a \
+	/usr/lib/libm.a \
+	/usr/lib/librt.a \
+	/usr/lib/libutil.a \
+	/usr/lib/libcrypt.a \
+	/usr/lib/libdevmapper.a \
+	/usr/lib/librpcsvc.a \
+	/usr/lib/libpthread.a \
+	/usr/lib/libkvm.a \
+	/usr/lib/libusbhid.a \
+	/usr/lib/crt0.o \
+	/usr/lib/crtbegin.o \
+	/usr/lib/crtbeginS.o \
+	/usr/lib/crtend.o \
+	/usr/lib/crti.o \
+	/usr/lib/crtn.o \
+	/usr/lib/gcrt0.o \
+	# end
+
+# /bin/sh (for now)
+ONLY_SO=\
+	/usr/lib/libedit.so.3 \
+	/usr/lib/libterminfo.so.1 \
+	/usr/lib/libexecinfo.so.0 \
+	# end
+
+dynamic_lib_NetBSD=\
+	/usr/lib/libc.so.12 \
+	/usr/lib/libm.so.0 \
+	/usr/lib/libutil.so.7 \
+	/usr/lib/libcrypt.so.1 \
+	/usr/lib/libkvm.so.6 \
+	/usr/lib/librpcsvc.so.1 \
+	/usr/lib/librt.so.1 \
+	/usr/lib/libdevmapper.so.1 \
+	/usr/lib/libpthread.so.1 \
+	/usr/lib/libusbhid.so.1 \
+	${ONLY_SO} \
+	# end
+
+headers_NetBSD_dirs=\
+	amd64 \
+	arpa \
+	crypto \
+	dev \
+	event2 \
+	fs \
+	msdosfs \
+	net \
+	net80211 \
+	netbt \
+	netcan \
+	netinet \
+	netinet6 \
+	netipsec \
+	nfs \
+	ntfs \
+	prop \
+	protocols \
+	rpc \
+	rpcsvc \
+	security \
+	sys \
+	ufs \
+	uvm \
+	x86 \
+	# end
+
+posix_headers=\
+	aio.h \
+	assert.h \
+	complex.h \
+	cpio.h \
+	ctype.h \
+	dirent.h \
+	dlfcn.h \
+	errno.h \
+	fcntl.h \
+	fenv.h \
+	float.h \
+	fmtmsg.h \
+	fnmatch.h \
+	ftw.h \
+	glob.h \
+	grp.h \
+	iconv.h \
+	inttypes.h \
+	iso646.h \
+	langinfo.h \
+	libgen.h \
+	limits.h \
+	locale.h \
+	math.h \
+	monetary.h \
+	mqueue.h \
+	ndbm.h \
+	netdb.h \
+	nl_types.h \
+	poll.h \
+	pthread.h \
+	pwd.h \
+	regex.h \
+	sched.h \
+	search.h \
+	semaphore.h \
+	setjmp.h \
+	signal.h \
+	spawn.h \
+	stdarg.h \
+	stdbool.h \
+	stddef.h \
+	stdint.h \
+	stdio.h \
+	stdlib.h \
+	string.h \
+	strings.h \
+	syslog.h \
+	tar.h \
+	termios.h \
+	time.h \
+	ulimit.h \
+	unistd.h \
+	utime.h \
+	utmpx.h \
+	wchar.h \
+	wctype.h \
+	wordexp.h \
+	# end
+
+headers_BSD=\
+	${posix_headers} \
+	ar.h \
+	bitstring.h \
+	bluetooth.h \
+	elf.h \
+	err.h \
+	fstab.h \
+	fts.h \
+	getopt.h \
+	ieeefp.h \
+	ifaddrs.h \
+	kvm.h \
+	link.h \
+	login_cap.h \
+	memory.h \
+	mpool.h \
+	netconfig.h \
+	nlist.h \
+	paths.h \
+	res_update.h \
+	resolv.h \
+	sdp.h \
+	stdalign.h \
+	stdnoreturn.h \
+	stringlist.h \
+	sysexits.h \
+	syslog.h \
+	ttyent.h \
+	ucontext.h \
+	usbhid.h \
+	vis.h \
+	# end
+
+headers_NetBSD=\
+	${headers_BSD} \
+	disktab.h \
+	mntopts.h \
+	re_comp.h \
+	util.h \
+	# end
+
+pcfiles_NetBSD=\
+	# end
+
+
+
+platform-skeleton: common-skeleton
+	@echo "============================================="
+	@echo "===  Create extra directories for NetBSD  ==="
+	@echo "============================================="
+	${MKDIR} \
+		${DESTDIR}${BASE}/libexec \
+		${DESTDIR}${BASE}/usr/share/etc \
+		${DESTDIR}${BASE}/usr/share/locale/en_US.UTF-8/LC_MESSAGES \
+		# end
+
+install-platform: install-common
+	@echo "=================================="
+	@echo "===  NetBSD file installation  ==="
+	@echo "=================================="
+
+	(cd ${DESTDIR}${BASE}/usr/lib && \
+		ln -s libc.so.12 libc.so && \
+		ln -s libm.so.0 libm.so && \
+		ln -s libutil.so.7 libutil.so && \
+		ln -s libcrypt.so.1 libcrypt.so && \
+		ln -s libkvm.so.6 libkvm.so && \
+		ln -s librpcsvc.so.1 librpcsvc.so && \
+		ln -s librt.so.1 librt.so && \
+		ln -s libdevmapper.so.1 libdevmapper.so && \
+		ln -s libpthread.so.1 libpthread.so && \
+		ln -s libusbhid.so.1 libusbhid.so)
+
+.for SCRIPT in lorder false true
+	${BSD_INSTALL_SCRIPT} ../${OPSYS:tl}/usr/bin/${SCRIPT} ${DESTDIR}${BASE}/usr/bin/
+.endfor
+	${BSD_INSTALL_PROGRAM} ../${OPSYS:tl}/sbin/chown ${DESTDIR}${BASE}/usr/bin/chgrp
+	
+	cp ${LOCALBASE}/lib/libdl.a ${DESTDIR}${BASE}/usr/lib/
+
+	# locale information
+.  for LC in CTYPE MONETARY NUMERIC TIME
+	cp ../${OPSYS:tl}/usr/share/locale/en_US.UTF-8/LC_${LC} \
+		${DESTDIR}${BASE}/usr/share/locale/en_US.UTF-8/
+.  endfor
+	cp ../${OPSYS:tl}/usr/share/locale/en_US.UTF-8/LC_MESSAGES/SYS_LC_MESSAGES \
+		${DESTDIR}${BASE}/usr/share/locale/en_US.UTF-8/LC_MESSAGES/
+
+	# former "post-install" target
+	${BSD_INSTALL_DATA} ../${OPSYS:tl}/etc/group \
+		${DESTDIR}${BASE}/usr/share/group
+	${BSD_INSTALL_DATA} ../${OPSYS:tl}/etc/master.passwd \
+		${DESTDIR}${BASE}/usr/share/master.passwd
+	${BSD_INSTALL_DATA} ../${OPSYS:tl}/etc/defaults/rc.conf \
+		${DESTDIR}${BASE}/usr/share/rc.conf
+	/usr/sbin/pwd_mkdb -p -d ${DESTDIR}${BASE}/usr/share \
+		${DESTDIR}${BASE}/usr/share/master.passwd
+	mv ${DESTDIR}${BASE}/usr/share/etc/* ${DESTDIR}${BASE}/usr/share
+	rmdir ${DESTDIR}${BASE}/usr/share/etc
+	rmdir ${DESTDIR}${BASE}/usr/include/dev/ieee1394
--- bucket_B4/ravensys-root/files/special.mk
+++ bucket_B4/ravensys-root/files/special.mk
@@ -1,41 +1,11 @@
-Libc_DragonFly=		libc.so.8
-Libc_FreeBSD=		libc.so.7
-
-Libm_DragonFly=		libm.so.4
-Libm_FreeBSD=		libm.so.5
-
-Libutil_DragonFly=	libutil.so.4
-Libutil_FreeBSD=	libutil.so.9
-
-Libusb_DragonFly=	libusb.so.2
-Libusb_FreeBSD=		libusb.so.3
-
-Libusbhid_DragonFly=	libusbhid.so.4
-Libusbhid_FreeBSD=	libusbhid.so.4
-
-Libcrypt_DragonFly=	libcrypt.so.4
-Libcrypt_FreeBSD=	libcrypt.so.5
-
-Libkvm_DragonFly=	libkvm.so.3
-Libkvm_FreeBSD=		libkvm.so.7
-
-Librt_DragonFly=	librt.so.0
-Librt_FreeBSD=		librt.so.1
-
-Librpcsvc_DragonFly=	librpcsvc.so.3
-Librpcsvc_FreeBSD=	librpcsvc.so.5
-
-Libcam_DragonFly=	libcam.so.3
-Libcam_FreeBSD=		libcam.so.7
-
 # Override bmake settings (important for bootstraps and sysroot rebases)
-OPSYS=			${SYSTEM:H:H:H:H:H}
-ARCH=			${SYSTEM:H:H:H:H:T}
+OPSYS=				${SYSTEM:H:H:H:H:H}
+ARCH=				${SYSTEM:H:H:H:H:T}
 ARCH_STANDARD=		${SYSTEM:H:H:H:T}
-OSVERSION=		${SYSTEM:H:H:T}
-OSREL=			${SYSTEM:H:T}
-MAJOR=			${SYSTEM:T}
+OSVERSION=			${SYSTEM:H:H:T}
+OSREL=				${SYSTEM:H:T}
+MAJOR=				${SYSTEM:T}
 
-SYSPATH=		share${RAVENBASE}/sysroot/${OPSYS}
-RSYS=			${PREFIX}/${SYSPATH}
-FOS=			${WRKDIR}/${OPSYS:tl}
+SYSPATH=			share${RAVENBASE}/sysroot/${OPSYS}
+RSYS=				${PREFIX}/${SYSPATH}
+FOS=				${WRKDIR}/${OPSYS:tl}
