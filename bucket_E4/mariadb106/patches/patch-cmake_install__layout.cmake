1a8630a9dd386b17d4816192c9fc20266acf2496
John Marino <draco@marino.st>
2021-10-24 00:45:47 +0000

    mariadb106: add support for NetBSD

---
+++ bucket_E4/mariadb106/patches/patch-cmake_install__layout.cmake
@@ -0,0 +1,13 @@
+--- cmake/install_layout.cmake.orig	2021-08-03 08:29:07 UTC
++++ cmake/install_layout.cmake
+@@ -128,8 +128,8 @@ SET(INSTALL_SUPPORTFILESDIR_STANDALONE
+ SET(INSTALL_MYSQLDATADIR_STANDALONE     "data")
+ 
+ SET(INSTALL_UNIX_ADDRDIR_STANDALONE     "/tmp/mysql.sock")
+-SET(INSTALL_PAMDIR_STANDALONE           "share")
+-SET(INSTALL_PAMDATADIR_STANDALONE       "share")
++SET(INSTALL_PAMDIR_STANDALONE           "lib/mysql/security")
++SET(INSTALL_PAMDATADIR_STANDALONE       "etc/mariadb")
+ #
+ # RPM layout
+ #
