553105d4a0cc873a9d7f6a2565b318a5a140566b
John Marino <draco@marino.st>
2021-10-24 18:40:00 +0000

    exiv2: fix build on netbsd

---
+++ bucket_36/exiv2/patches/patch-src_convert.cpp
@@ -0,0 +1,26 @@
+$NetBSD: patch-src_convert.cpp,v 1.2 2019/10/25 09:44:26 kamil Exp $
+
+Support NetBSD and SunOS iconv being special.
+
+--- src/convert.cpp.orig	2021-06-15 14:08:36 UTC
++++ src/convert.cpp
+@@ -1545,11 +1545,19 @@ namespace {
+             char outbuf[256];
+             char* outptr = outbuf;
+             size_t outbytesleft = sizeof(outbuf);
++#if (defined(__NetBSD__) && !NETBSD_POSIX_ICONV) || defined(__sun)
++            size_t rc = iconv(cd,
++                              const_cast<char **>(&inptr),
++                              &inbytesleft,
++                              &outptr,
++                              &outbytesleft);
++#else
+             size_t rc = iconv(cd,
+                               &inptr,
+                               &inbytesleft,
+                               &outptr,
+                               &outbytesleft);
++#endif
+             const size_t outbytesProduced = sizeof(outbuf) - outbytesleft;
+             if (rc == size_t(-1) && errno != E2BIG) {
+ #ifndef SUPPRESS_WARNINGS
