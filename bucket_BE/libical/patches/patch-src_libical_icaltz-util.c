29dcc2ffc827da94da3f5545195f9162afb4fe61
John Marino <draco@marino.st>
2021-10-24 14:51:29 +0000

    libical: fix build on netbsd

--- bucket_BE/libical/patches/patch-src_libical_icaltz-util.c
+++ bucket_BE/libical/patches/patch-src_libical_icaltz-util.c
@@ -1,11 +1,14 @@
 --- src/libical/icaltz-util.c.orig	2021-04-17 13:12:32 UTC
 +++ src/libical/icaltz-util.c
-@@ -53,6 +53,10 @@
+@@ -53,6 +53,13 @@
  #define bswap_32 swap32
  #endif
  
-+#if defined(__FreeBSD__) || defined(__DragonFly__)
++#if defined(__FreeBSD__) || defined(__DragonFly__) || defined(__NetBSD__)
 +#define bswap_64 bswap64
++#endif
++#if defined(__NetBSD__)
++#define bswap_32 bswap32
 +#endif
 +
  #if defined(_MSC_VER)
