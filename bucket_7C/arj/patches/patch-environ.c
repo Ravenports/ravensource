670c077546c9d2bd8c5c52762e7f0a31211f628c
John Marino <draco@marino.st>
2021-10-27 00:29:05 +0000

    arj: fix build on netbsd (plus path fix, so bump)

--- bucket_7C/arj/patches/patch-environ.c
+++ bucket_7C/arj/patches/patch-environ.c
@@ -1,4 +1,4 @@
---- environ.c.intermediate	2021-01-10 04:42:23 UTC
+--- environ.c.orig	2021-10-27 00:22:50 UTC
 +++ environ.c
 @@ -58,7 +58,7 @@
    #include <sys/ioctl.h>
@@ -18,3 +18,30 @@
        {
         close(handle);
         return(NULL);
+@@ -2289,7 +2289,7 @@ unsigned long file_getfree(char *name)
+   else
+    return((LONG_MAX/(spclu*bps)<fclu)?LONG_MAX:spclu*bps*fclu);
+  #elif TARGET==UNIX
+-  #if defined(__QNXNTO__)||defined(__sco__)||defined(SUNOS)
++  #if defined(__QNXNTO__)||defined(__sco__)||defined(SUNOS) || defined(__NetBSD__)
+    struct statvfs vfs;
+ 
+    if(statvfs(name, &vfs)==-1)
+@@ -3008,7 +3008,7 @@ void get_exe_name(char *dest, char *arg)
+     they are missing altogether, the corresponding code will gracefully
+     terminate. */
+  #if SFX_LEVEL==ARJ
+-  strcpy(dest, "/usr/local/bin/arj");
++  strcpy(dest, "@PREFIX@/bin/arj");
+  #elif SFX_LEVEL==ARJSFXV
+   strcpy(dest, "./arjsfxv");
+  #elif SFX_LEVEL==ARJSFX
+@@ -3016,7 +3016,7 @@ void get_exe_name(char *dest, char *arg)
+  #elif SFX_LEVEL==ARJSFXJR
+   strcpy(dest, "./arjsfxjr");
+  #elif defined(REARJ)
+-  strcpy(dest, "/usr/local/bin/rearj");
++  strcpy(dest, "@PREFIX@/bin/rearj");
+  #else
+   dest[0]='\0';
+  #endif
