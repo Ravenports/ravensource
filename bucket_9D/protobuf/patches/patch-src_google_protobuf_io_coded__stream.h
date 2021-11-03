8ddb3ac1b44ce053b5b49bde101a4f25b72c763c
John Marino <draco@marino.st>
2021-11-03 04:02:37 +0000

    protobuf: upgrade

--- bucket_9D/protobuf/patches/patch-src_google_protobuf_io_coded__stream.h
+++ bucket_9D/protobuf/patches/patch-src_google_protobuf_io_coded__stream.h
@@ -1,12 +1,16 @@
---- ../src/google/protobuf/io/coded_stream.h.orig	2021-10-05 00:43:33 UTC
+--- ../src/google/protobuf/io/coded_stream.h.orig	2021-10-28 21:07:53 UTC
 +++ ../src/google/protobuf/io/coded_stream.h
-@@ -133,8 +133,10 @@
+@@ -134,12 +134,14 @@
  #else
  #ifdef __APPLE__
  #include <machine/endian.h>  // __BYTE_ORDER
 -#elif defined(__FreeBSD__)
 +#elif defined(__FreeBSD__) || defined(__DragonFly__)
  #include <sys/endian.h>  // __BYTE_ORDER
+ #elif (defined(sun) || defined(__sun)) && (defined(__SVR4) || defined(__svr4__))
+ #include <sys/isa_defs.h>  // __BYTE_ORDER
+ #elif defined(_AIX) || defined(__TOS_AIX__)
+ #include <sys/machine.h>  // BYTE_ORDER
 +#elif defined(__sun__)
 +#define __LITTLE_ENDIAN__
  #else
--- bucket_9D/protobuf/patches/patch-src_google_protobuf_stubs_port.h
+++ bucket_9D/protobuf/patches/patch-src_google_protobuf_stubs_port.h
@@ -1,18 +1,22 @@
---- ../src/google/protobuf/stubs/port.h.orig	2021-10-05 00:43:33 UTC
+--- ../src/google/protobuf/stubs/port.h.orig	2021-10-28 21:07:53 UTC
 +++ ../src/google/protobuf/stubs/port.h
-@@ -59,8 +59,10 @@
+@@ -59,12 +59,14 @@
  #else
  #ifdef __APPLE__
  #include <machine/endian.h>  // __BYTE_ORDER
 -#elif defined(__FreeBSD__)
 +#elif defined(__FreeBSD__) || defined(__DragonFly__)
  #include <sys/endian.h>  // __BYTE_ORDER
+ #elif (defined(sun) || defined(__sun)) && (defined(__SVR4) || defined(__svr4__))
+ #include <sys/isa_defs.h>  // __BYTE_ORDER
+ #elif defined(_AIX) || defined(__TOS_AIX__)
+ #include <sys/machine.h>  // BYTE_ORDER
 +#elif defined(__sun__)
 +#define __LITTLE_ENDIAN__
  #else
  #if !defined(__QNX__)
  #include <endian.h>  // __BYTE_ORDER
-@@ -78,6 +80,8 @@
+@@ -82,6 +84,8 @@
  #ifdef _MSC_VER
  #include <stdlib.h>  // NOLINT(build/include)
  #include <intrin.h>
@@ -21,7 +25,7 @@
  #elif defined(__APPLE__)
  #include <libkern/OSByteOrder.h>
  #elif defined(__linux__) || defined(__ANDROID__) || defined(__CYGWIN__)
-@@ -238,6 +242,11 @@ inline void GOOGLE_UNALIGNED_STORE64(voi
+@@ -242,6 +246,11 @@ inline void GOOGLE_UNALIGNED_STORE64(voi
  #define bswap_32(x) _byteswap_ulong(x)
  #define bswap_64(x) _byteswap_uint64(x)
  
