266c684d23735ad7f71552d498f87ded7e7238ea
John Marino <draco@marino.st>
2021-11-06 04:06:26 +0000

    i3: upgrade

--- bucket_C5/i3/patches/patch-src_log.c
+++ bucket_C5/i3/patches/patch-src_log.c
@@ -1,6 +1,6 @@
---- src/log.c.orig	2021-02-27 09:37:58 UTC
+--- src/log.c.orig	2021-11-03 08:23:08 UTC
 +++ src/log.c
-@@ -125,7 +125,7 @@ void open_logbuffer(void) {
+@@ -138,7 +138,7 @@ void open_logbuffer(void) {
       * At the moment (2011-12-10), no testcase leads to an i3 log
       * of more than ~ 600 KiB. */
      logbuffer_size = min(physical_mem_bytes * 0.01, shmlog_size);
@@ -9,7 +9,7 @@
      sasprintf(&shmlogname, "/tmp/i3-log-%d", getpid());
  #else
      sasprintf(&shmlogname, "/i3-log-%d", getpid());
-@@ -136,7 +136,7 @@ void open_logbuffer(void) {
+@@ -149,7 +149,7 @@ void open_logbuffer(void) {
          return;
      }
  
