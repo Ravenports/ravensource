b8c74bd8824c38d8a59217fcd37446cdf922a1bf
John Marino <draco@marino.st>
2021-11-13 20:12:40 +0000

    i3status: upgrade (convert to meson)

---
+++ bucket_ED/i3status/patches/patch-src_print__battery__info.c
@@ -0,0 +1,11 @@
+--- src/print_battery_info.c.orig	2021-11-09 07:27:11 UTC
++++ src/print_battery_info.c
+@@ -593,7 +593,7 @@ void print_battery_info(battery_info_ctx
+         ctx->format_percentage = "%.00f%s";
+     }
+ #endif
+-#if defined(__FreeBSD__) || defined(__FreeBSD_kernel__) || defined(__DragonFly__) || defined(__OpenBSD__)
++#if defined(__FreeBSD__) || defined(__FreeBSD_kernel__) || defined(__OpenBSD__)
+     /* These OSes report battery time in minutes. */
+     hide_seconds = true;
+ #endif
--- bucket_ED/i3status/patches/patch-src_print__volume.c
+++ bucket_ED/i3status/patches/patch-src_print__volume.c
@@ -1,11 +1,11 @@
---- src/print_volume.c.orig	2019-01-23 08:03:56 UTC
+--- src/print_volume.c.orig	2021-11-09 07:27:11 UTC
 +++ src/print_volume.c
-@@ -86,7 +86,7 @@ void print_volume(yajl_gen json_gen, cha
-         free(instance);
-     }
+@@ -243,7 +243,7 @@ void print_volume(volume_ctx_t *ctx) {
+     goto out_with_format;
  
--#if !defined(__DragonFly__) && !defined(__OpenBSD__)
-+#if !defined(__DragonFly__) && !defined(__OpenBSD__) &&!defined(__FreeBSD__)
-     /* Try PulseAudio first */
- 
-     /* If the device name has the format "pulse[:N]" where N is the
+ #endif
+-#if defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__) || defined(__DragonFly__)
++#if defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__)
+     char *mixerpath;
+     char defaultmixer[] = "/dev/mixer";
+     int mixfd, vol, devmask = 0;
