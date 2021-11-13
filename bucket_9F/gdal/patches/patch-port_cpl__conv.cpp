3e29a85e44d459ae09ae8f64ffdf31d052f2567b
John Marino <draco@marino.st>
2021-11-13 23:42:48 +0000

    gdal: upgrade

--- bucket_9F/gdal/patches/patch-port_cpl__conv.cpp
+++ bucket_9F/gdal/patches/patch-port_cpl__conv.cpp
@@ -1,18 +1,11 @@
 Avoid visibility define that masks usage that is apparently beyond XOPEN or C99.
 
---- port/cpl_conv.cpp.orig	2021-10-25 11:42:52 UTC
+--- port/cpl_conv.cpp.orig	2021-11-04 11:58:49 UTC
 +++ port/cpl_conv.cpp
-@@ -37,15 +37,7 @@
+@@ -48,8 +48,6 @@
  #endif
- #define _XOPEN_SOURCE 600
  #endif
--#else
--#ifdef _XOPEN_SOURCE
--#undef _XOPEN_SOURCE
- #endif
--#define _XOPEN_SOURCE 700
--#endif
--
+ 
 -// For atoll (at least for NetBSD)
 -#define _ISOC99_SOURCE
  
