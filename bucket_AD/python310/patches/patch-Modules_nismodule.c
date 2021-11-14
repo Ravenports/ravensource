36a5006da41b99eb85701e102373bbd65995bf5e
John Marino <draco@marino.st>
2021-11-14 18:41:59 +0000

    add python 3.10 port

---
+++ bucket_AD/python310/patches/patch-Modules_nismodule.c
@@ -0,0 +1,11 @@
+--- Modules/nismodule.c.orig	2021-10-04 17:40:46 UTC
++++ Modules/nismodule.c
+@@ -115,7 +115,7 @@ nis_mapname(char *map, int *pfix)
+     return map;
+ }
+ 
+-#if defined(__APPLE__) || defined(__OpenBSD__) || defined(__FreeBSD__)
++#if defined(__APPLE__) || defined(__OpenBSD__) || defined(__FreeBSD__) || defined(__DragonFly__)
+ typedef int (*foreachfunc)(unsigned long, char *, int, char *, int, void *);
+ #else
+ typedef int (*foreachfunc)(int, char *, int, char *, int, char *);
