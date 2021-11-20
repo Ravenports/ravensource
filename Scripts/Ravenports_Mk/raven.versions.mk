e8db07a2ee34b0a5ec9c81f06d975ea07f57cfa8
John Marino <draco@marino.st>
2021-11-20 23:59:31 +0000

    raven.versions.mk: missing python 3.8 setting

--- Scripts/Ravenports_Mk/raven.versions.mk
+++ Scripts/Ravenports_Mk/raven.versions.mk
@@ -46,7 +46,7 @@ RUBY_3.0_VERSION=	3.0.2
 # Possible values: 2.7, 3.9, 3.10
 #-------------------------------------------------------------------------
 
-PYTHON3_DEFAULT?=	3.8
+PYTHON3_DEFAULT?=	3.9
 
 PYTHON_2.7_VERSION=	2.7.18
 PYTHON_3.8_VERSION=	3.8.12
