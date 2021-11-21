d84347e6ec2a6c54ac9d140d6aebc48ca183daa5
John Marino <draco@marino.st>
2021-11-21 17:27:13 +0000

    qpdf: upgrade

--- bucket_F3/qpdf/patches/patch-make_libtool.mk
+++ bucket_F3/qpdf/patches/patch-make_libtool.mk
@@ -1,4 +1,4 @@
---- make/libtool.mk.orig	2021-03-11 17:59:41 UTC
+--- make/libtool.mk.orig	2021-11-16 20:44:09 UTC
 +++ make/libtool.mk
 @@ -1,7 +1,7 @@
  # --- Required interface definitions ---
@@ -10,15 +10,19 @@
  
  OBJ=o
  LOBJ=lo
-@@ -114,14 +114,14 @@ install: all
+@@ -108,7 +108,7 @@ endef
+ install-libs: build_libqpdf
+ 	./mkinstalldirs -m 0755 $(DESTDIR)$(libdir)/pkgconfig
  	./mkinstalldirs -m 0755 $(DESTDIR)$(includedir)/qpdf
- 	./mkinstalldirs -m 0755 $(DESTDIR)$(docdir)
- 	./mkinstalldirs -m 0755 $(DESTDIR)$(mandir)/man1
 -	$(LIBTOOL) --mode=install ./install-sh \
 +	$(LIBTOOL) --mode=install ./install-sh -s \
  		libqpdf/$(OUTPUT_DIR)/libqpdf.la \
  		$(DESTDIR)$(libdir)/libqpdf.la
  	$(LIBTOOL) --finish $(DESTDIR)$(libdir)
+@@ -124,13 +124,13 @@ install: all install-libs
+ 	./mkinstalldirs -m 0755 $(DESTDIR)$(bindir)
+ 	./mkinstalldirs -m 0755 $(DESTDIR)$(docdir)
+ 	./mkinstalldirs -m 0755 $(DESTDIR)$(mandir)/man1
 -	$(LIBTOOL) --mode=install ./install-sh \
 +	$(LIBTOOL) --mode=install ./install-sh -s \
  		qpdf/$(OUTPUT_DIR)/qpdf \
@@ -27,4 +31,8 @@
 +	$(LIBTOOL) --mode=install ./install-sh -s \
  		zlib-flate/$(OUTPUT_DIR)/zlib-flate \
  		$(DESTDIR)$(bindir)/zlib-flate
- 	$(LIBTOOL) --mode=install ./install-sh \
+-	$(LIBTOOL) --mode=install ./install-sh \
++	$(LIBTOOL) --mode=install ./install-sh -s \
+ 		qpdf/$(OUTPUT_DIR)/fix-qdf \
+ 		$(DESTDIR)$(bindir)/fix-qdf
+ 	./install-sh -m 0644 doc/stylesheet.css $(DESTDIR)$(docdir)
