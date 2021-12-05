93c55f1f3e0d4b5d6fc84fc291a14c671f2803e1
John Marino <draco@marino.st>
2021-12-05 17:20:30 +0000

    pangomm: fix build after pango upgrade

---
+++ bucket_19/pangomm/patches/patch-untracked_pango_pangomm_attrlist.cc
@@ -0,0 +1,10 @@
+--- untracked/pango/pangomm/attrlist.cc.orig	2021-05-21 11:14:18 UTC
++++ untracked/pango/pangomm/attrlist.cc
+@@ -3,6 +3,7 @@
+ 
+ #include <glibmm.h>
+ 
++#include <pango-1.0/pango/pango-markup.h> 
+ #include <pangomm/attrlist.h>
+ #include <pangomm/private/attrlist_p.h>
+ 
