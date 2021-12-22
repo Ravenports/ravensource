15a5de8f33a1a2bdfbdc92ade2afc122a4a3c224
John Marino <draco@marino.st>
2021-12-22 01:57:35 +0000

    zig: upgrade

--- bucket_BC/zig/patches/patch-src_link_Elf.zig
+++ bucket_BC/zig/patches/patch-src_link_Elf.zig
@@ -1,6 +1,6 @@
---- src/link/Elf.zig.orig	2021-09-07 02:41:54 UTC
+--- src/link/Elf.zig.orig	2021-12-20 20:13:19 UTC
 +++ src/link/Elf.zig
-@@ -3353,19 +3353,22 @@ const CsuObjects = struct {
+@@ -3487,19 +3487,22 @@ const CsuObjects = struct {
              const crt_dir_path = lci.crt_dir orelse return error.LibCInstallationMissingCRTDir;
              switch (link_options.target.os.tag) {
                  .dragonfly => {
