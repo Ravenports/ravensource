f7baa5197cdf6b3baebef0f7c809ef08fd1e1545
John Marino <draco@marino.st>
2022-01-16 14:58:41 +0000

    bmake: upgrade

--- bucket_10/bmake/patches/patch-main.c
+++ bucket_10/bmake/patches/patch-main.c
@@ -1,9 +1,9 @@
---- main.c.orig	2021-12-21 18:47:38 UTC
+--- main.c.orig	2022-01-11 03:15:32 UTC
 +++ main.c
-@@ -1483,6 +1483,12 @@ main_Init(int argc, char **argv)
- 		Global_Set(".MAKE.UID", tmp);
- 		snprintf(tmp, sizeof tmp, "%u", getgid());
- 		Global_Set(".MAKE.GID", tmp);
+@@ -1446,6 +1446,12 @@ main_Init(int argc, char **argv)
+ 		Global_Set(".MAKE.UID", buf);
+ 		snprintf(buf, sizeof buf, "%u", getgid());
+ 		Global_Set(".MAKE.GID", buf);
 +		Global_Set(".MAKE.OS.NAME",          OSNAME);
 +		Global_Set(".MAKE.OS.VERSION",       OSVERSION);
 +		Global_Set(".MAKE.OS.RELEASE",       OSRELEASE);
