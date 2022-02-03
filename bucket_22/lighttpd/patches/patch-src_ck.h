66f2a0597488723c2c8b3965960a5928447104fc
John Marino <draco@marino.st>
2022-02-03 19:36:57 +0000

    lighttpd: upgrade (and fix geoip option)

--- bucket_22/lighttpd/patches/patch-src_ck.h
+++ bucket_22/lighttpd/patches/patch-src_ck.h
@@ -1,11 +1,11 @@
---- src/ck.h.orig	2021-12-04 14:40:24 UTC
+--- src/ck.h.orig	2022-01-19 17:53:51 UTC
 +++ src/ck.h
 @@ -18,11 +18,12 @@
  #endif
  #endif
  #include "first.h"
--#ifdef __FreeBSD__
-+#if defined __FreeBSD__ || defined __DragonFly__
+-#if defined(__FreeBSD__) || defined(__sun)
++#if defined(__FreeBSD__) || defined(__sun) || defined(__DragonFly__)
  #ifndef _RSIZE_T_DEFINED /* expecting __EXT1_VISIBLE 1 and _RSIZE_T_DEFINED */
  #define _RSIZE_T_DEFINED
  typedef size_t rsize_t;
