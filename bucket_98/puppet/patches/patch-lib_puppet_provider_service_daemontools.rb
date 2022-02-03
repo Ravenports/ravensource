656e92c67ce4a69767e553c1f58555f346088733
John Marino <draco@marino.st>
2022-02-03 19:49:12 +0000

    puppet: upgrade

--- bucket_98/puppet/patches/patch-lib_puppet_provider_service_daemontools.rb
+++ bucket_98/puppet/patches/patch-lib_puppet_provider_service_daemontools.rb
@@ -1,6 +1,6 @@
 Daemontools program locations
 
---- lib/puppet/provider/service/daemontools.rb.orig	2021-12-09 11:25:54 UTC
+--- lib/puppet/provider/service/daemontools.rb.orig	2022-01-13 14:22:42 UTC
 +++ lib/puppet/provider/service/daemontools.rb
 @@ -39,7 +39,7 @@ Puppet::Type.type(:service).provide :dae
  
