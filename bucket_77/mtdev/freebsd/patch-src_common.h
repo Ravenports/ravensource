09869d50b49e0f775647c074483280509b4e3a97
Michael Reim <kraileth@elderlinux.org>
2022-02-18 20:10:38 +0100

    Add mtdev

---
+++ bucket_77/mtdev/freebsd/patch-src_common.h
@@ -0,0 +1,11 @@
+--- src/common.h.orig	2020-01-26 14:34:52.000000000 +0100
++++ src/common.h	2022-02-18 20:05:26.565513000 +0100
+@@ -33,7 +33,7 @@
+ 
+ #include <mtdev-mapping.h>
+ #include <mtdev-plumbing.h>
+-#include <malloc.h>
++#include <stdlib.h>
+ #include <string.h>
+ #include <errno.h>
+ 
