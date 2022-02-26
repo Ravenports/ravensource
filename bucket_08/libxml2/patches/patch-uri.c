6df74183ee1c8a7d1e5c19b4e7f6d1b3dda9e51c
John Marino <draco@marino.st>
2022-02-26 16:43:11 +0000

    libxml2: upgrade

--- bucket_08/libxml2/patches/patch-uri.c
+++ bucket_08/libxml2/patches/patch-uri.c
@@ -12,7 +12,7 @@ bogus values when called with URIs that have rootless paths
 (e.g. "urx:b:b" becomes "urx://b%3Ab" where "urx:b%3Ab" would be
 correct)
 
---- uri.c.orig	2021-05-13 12:53:51 UTC
+--- uri.c.orig	2022-02-12 15:36:48 UTC
 +++ uri.c
 @@ -1207,6 +1207,8 @@ xmlSaveUri(xmlURIPtr uri) {
                  if (temp == NULL) goto mem_error;
