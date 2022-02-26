696adf7bec816ce9ea4c641da48463c80925b6f8
Michael Reim <kraileth@elderlinux.org>
2022-02-26 09:39:47 +0100

    xorg-driver-video-amdgpu: upgrade

--- bucket_F0/xorg-driver-video-amdgpu/patches/patch-src_amdgpu__kms.c
+++ bucket_F0/xorg-driver-video-amdgpu/patches/patch-src_amdgpu__kms.c
@@ -1,6 +1,6 @@
---- src/amdgpu_kms.c.orig	2019-10-11 15:20:30 UTC
+--- src/amdgpu_kms.c.orig	2021-07-30 13:44:22 UTC
 +++ src/amdgpu_kms.c
-@@ -930,7 +930,7 @@ static void
+@@ -931,7 +931,7 @@ static void
  amdgpu_dirty_update(ScrnInfoPtr scrn)
  {
  	ScreenPtr screen = scrn->pScreen;
