803f7878723c0cd044ccf2ddb13cf5f6319450c9
John Marino <draco@marino.st>
2022-02-26 20:28:04 +0000

    python-numpy: fix build

--- bucket_E9/python-numpy/patches/patch-numpy_f2py_cfuncs.py
+++ bucket_E9/python-numpy/patches/patch-numpy_f2py_cfuncs.py
@@ -1,12 +1,12 @@
---- numpy/f2py/cfuncs.py.orig	2021-02-07 09:26:20 UTC
+--- numpy/f2py/cfuncs.py.orig	2022-01-29 12:20:32 UTC
 +++ numpy/f2py/cfuncs.py
-@@ -549,6 +549,9 @@ cppmacros["F2PY_THREAD_LOCAL_DECL"] = ""
- #define F2PY_THREAD_LOCAL_DECL __declspec(thread)
+@@ -578,6 +578,9 @@ cppmacros["F2PY_THREAD_LOCAL_DECL"] = ""
+ #define F2PY_THREAD_LOCAL_DECL __thread
  #elif defined(__STDC_VERSION__) \\
        && (__STDC_VERSION__ >= 201112L) \\
 +      && !defined(__DragonFly__) \\
 +      && !defined(__FreeBSD__) \\
 +      && !defined(__sun__) \\ 
        && !defined(__STDC_NO_THREADS__) \\
-       && (!defined(__GLIBC__) || __GLIBC__ > 2 || (__GLIBC__ == 2 && __GLIBC_MINOR__ > 12))
- /* __STDC_NO_THREADS__ was first defined in a maintenance release of glibc 2.12,
+       && (!defined(__GLIBC__) || __GLIBC__ > 2 || (__GLIBC__ == 2 && __GLIBC_MINOR__ > 12)) \\
+       && !defined(__OpenBSD__)
