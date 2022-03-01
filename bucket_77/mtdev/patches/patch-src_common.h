6b465b2d8e61ec6e58fca25de62fd74ed466affe
John Marino <draco@marino.st>
2022-03-01 02:48:06 +0000

    mtdev: fix build on DragonFly

--- bucket_77/mtdev/patches/patch-src_common.h
+++ bucket_77/mtdev/patches/patch-src_common.h
@@ -1,5 +1,5 @@
---- src/common.h.orig	2020-01-26 14:34:52.000000000 +0100
-+++ src/common.h	2022-02-18 20:05:26.565513000 +0100
+--- src/common.h.orig	2020-01-26 13:34:52 UTC
++++ src/common.h
 @@ -33,7 +33,7 @@
  
  #include <mtdev-mapping.h>
