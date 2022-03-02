c1d1e230b2cd2e44c2c33b741095b3c0a6a59fcd
John Marino <draco@marino.st>
2022-03-01 20:23:44 -0600

    screen: fix build on freebsd

--- bucket_35/screen/patches/patch-pty.c
+++ bucket_35/screen/patches/patch-pty.c
@@ -1,11 +1,14 @@
 --- pty.c.orig	2022-01-28 14:06:02 UTC
 +++ pty.c
-@@ -35,6 +35,10 @@
+@@ -35,6 +35,13 @@
  #include <utils.h>  /* for openpty() */
  #endif
  
-+#if defined(__FreeBSD__) || defined(__DragonFly__)
++#if defined(__DragonFly__)
 +#include <util.h>  /* for openpty() */
++#endif
++#if defined(__FreeBSD__)
++#include <libutil.h>  /* for openpty() */
 +#endif
 +
  #include "config.h"
