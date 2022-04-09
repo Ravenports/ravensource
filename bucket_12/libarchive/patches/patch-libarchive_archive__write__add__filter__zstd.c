0acc3765f5268cd44e76116cf28e5049c85127e6
Michael Reim <kraileth@elderlinux.org>
2022-04-09 16:02:30 +0200

    libarchive: upgrade

--- bucket_12/libarchive/patches/patch-libarchive_archive__write__add__filter__zstd.c
+++ bucket_12/libarchive/patches/patch-libarchive_archive__write__add__filter__zstd.c
@@ -1,6 +1,6 @@
---- libarchive/archive_write_add_filter_zstd.c.orig	2021-08-22 22:32:05 UTC
+--- libarchive/archive_write_add_filter_zstd.c.orig	2022-02-09 12:24:12 UTC
 +++ libarchive/archive_write_add_filter_zstd.c
-@@ -62,7 +62,7 @@ struct private_data {
+@@ -63,7 +63,7 @@ struct private_data {
  /* If we don't have the library use default range values (zstdcli.c v1.4.0) */
  #define CLEVEL_MIN -99
  #define CLEVEL_STD_MIN 0 /* prior to 1.3.4 and more recent without using --fast */
