7bd0110869f41d2c8711ad24da80e172699cafad
John Marino <draco@marino.st>
2022-04-20 03:58:11 +0000

    libgit2: upgrade

--- bucket_11/libgit2/patches/patch-src_unix_posix.h
+++ bucket_11/libgit2/patches/patch-src_unix_posix.h
@@ -1,4 +1,4 @@
---- src/unix/posix.h.orig	2022-02-26 19:50:04 UTC
+--- src/unix/posix.h.orig	2022-04-12 20:17:07 UTC
 +++ src/unix/posix.h
 @@ -94,6 +94,11 @@ GIT_INLINE(int) p_futimes(int f, const s
  	s[1].tv_nsec = t[1].tv_usec * 1000;
