616b210407cbeb7b6a1c6b006f39738dfe472b41
Michael Reim <kraileth@elderlinux.org>
2022-05-06 21:46:35 +0200

    openldap: upgrade

--- bucket_7F/openldap/patches/patch-include_ldap__defaults.h
+++ bucket_7F/openldap/patches/patch-include_ldap__defaults.h
@@ -1,11 +1,11 @@
---- include/ldap_defaults.h.orig	2022-01-19 18:32:34 UTC
-+++ include/ldap_defaults.h
-@@ -39,7 +39,7 @@
- #define LDAP_ENV_PREFIX "LDAP"
+--- include/ldap_defaults.h.orig	2022-05-06 21:40:32.972273000 +0200
++++ include/ldap_defaults.h	2022-05-06 21:41:20.793768000 +0200
+@@ -40,7 +40,7 @@
  
  /* default ldapi:// socket */
+ #ifndef LDAPI_SOCK
 -#define LDAPI_SOCK LDAP_RUNDIR LDAP_DIRSEP "run" LDAP_DIRSEP "ldapi"
 +#define LDAPI_SOCK "/var/run" LDAP_DIRSEP "openldap" LDAP_DIRSEP "ldapi"
+ #endif
  
  /*
-  * SLAPD DEFINITIONS
