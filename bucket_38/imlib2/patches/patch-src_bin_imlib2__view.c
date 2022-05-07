75737038e8898dbb1069c62940795c8ea92413d0
Michael Reim <kraileth@elderlinux.org>
2022-05-07 11:34:35 +0200

    imlib2: upgrade

--- bucket_38/imlib2/patches/patch-src_bin_imlib2__view.c
+++ bucket_38/imlib2/patches/patch-src_bin_imlib2__view.c
@@ -2,9 +2,9 @@ $NetBSD: patch-src_bin_imlib2__view.c,v 1.1 2020/02/20 15:14:57 nia Exp $
 
 Include sys/time.h for timeval.
 
---- src/bin/imlib2_view.c.orig	2022-02-06 14:52:21 UTC
+--- src/bin/imlib2_view.c.orig	2022-03-15 06:27:09 UTC
 +++ src/bin/imlib2_view.c
-@@ -2,6 +2,7 @@
+@@ -3,6 +3,7 @@
  
  #include <X11/Xlib.h>
  #include <X11/keysym.h>
