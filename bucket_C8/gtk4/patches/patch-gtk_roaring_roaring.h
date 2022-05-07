ef5f32daba230cafe82eee0c9a54284d2bef8495
Michael Reim <kraileth@elderlinux.org>
2022-05-07 15:58:20 +0200

    gtk4: upgrade

--- bucket_C8/gtk4/patches/patch-gtk_roaring_roaring.h
+++ bucket_C8/gtk4/patches/patch-gtk_roaring_roaring.h
@@ -1,4 +1,4 @@
---- gtk/roaring/roaring.h.orig	2021-12-30 15:33:48 UTC
+--- gtk/roaring/roaring.h.orig	2022-03-19 00:40:14 UTC
 +++ gtk/roaring/roaring.h
 @@ -59,7 +59,6 @@ enum {
  #include <stdint.h>
