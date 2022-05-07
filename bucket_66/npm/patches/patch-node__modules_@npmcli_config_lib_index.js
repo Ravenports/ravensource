ebba9cfd6f9444fe6eca455c2de2296ba59b05d5
Michael Reim <kraileth@elderlinux.org>
2022-05-07 07:26:30 +0200

    npm: upgrade, finally (at least one step)

--- bucket_66/npm/patches/patch-node__modules_@npmcli_config_lib_index.js
+++ bucket_66/npm/patches/patch-node__modules_@npmcli_config_lib_index.js
@@ -1,4 +1,4 @@
---- node_modules/@npmcli/config/lib/index.js.orig	2022-03-10 18:35:22 UTC
+--- node_modules/@npmcli/config/lib/index.js.orig	2022-03-17 19:56:54 UTC
 +++ node_modules/@npmcli/config/lib/index.js
 @@ -346,6 +346,9 @@ class Config {
  
--- bucket_66/npm/patches/patch-node__modules_node-gyp_gyp_pylib_gyp_xcode__emulation.py
+++ bucket_66/npm/patches/patch-node__modules_node-gyp_gyp_pylib_gyp_xcode__emulation.py
@@ -1,6 +1,6 @@
 Do not generate debugging symbols (uses lots of disk space).
 
---- node_modules/node-gyp/gyp/pylib/gyp/xcode_emulation.py.orig	2022-03-10 18:35:22 UTC
+--- node_modules/node-gyp/gyp/pylib/gyp/xcode_emulation.py.orig	2022-03-17 19:56:54 UTC
 +++ node_modules/node-gyp/gyp/pylib/gyp/xcode_emulation.py
 @@ -607,7 +607,7 @@ class XcodeSettings:
  
