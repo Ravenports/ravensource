4d19238cb0320a4f5b1c9b1b1a4adb04701784d1
Michael Reim <kraileth@elderlinux.org>
2022-05-08 20:35:16 +0200

    fluidsynth: upgrade

--- bucket_4A/fluidsynth/patches/patch-cmake__admin_DefaultDirs.cmake
+++ bucket_4A/fluidsynth/patches/patch-cmake__admin_DefaultDirs.cmake
@@ -1,4 +1,4 @@
---- cmake_admin/DefaultDirs.cmake.orig	2022-01-23 14:33:33 UTC
+--- cmake_admin/DefaultDirs.cmake.orig	2022-03-19 11:49:15 UTC
 +++ cmake_admin/DefaultDirs.cmake
 @@ -58,7 +58,7 @@ set (INFO_INSTALL_DIR "share/info" CACHE
  mark_as_advanced (INFO_INSTALL_DIR) 
