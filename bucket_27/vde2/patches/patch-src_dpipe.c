eedf6ca9a3fd9ac48bd8e0953e0c6125a58b1f5d
Michael Reim <kraileth@elderlinux.org>
2022-05-21 05:22:25 +0200

    vde2: upgrade

--- bucket_27/vde2/patches/patch-src_dpipe.c
+++ bucket_27/vde2/patches/patch-src_dpipe.c
@@ -1,14 +1,14 @@
---- src/dpipe.c.orig	2011-11-23 16:41:18 UTC
-+++ src/dpipe.c
-@@ -201,7 +201,11 @@ int main(int argc, char *argv[])
+--- src/dpipe.c.orig	2022-05-21 05:11:05.035482000 +0200
++++ src/dpipe.c	2022-05-21 05:12:17.785698000 +0200
+@@ -201,7 +201,11 @@
  
  	if (daemonize != 0)
  		daemon(0,0);
 +#ifdef __linux__
- 	else if (setpgrp() != 0) {
+ 	else if (setpgrp() == -1) {
 +#else
-+	else if (setpgrp(0,getpid()) != 0) {
++        else if (setpgrp(0,getpid()) != 0) {
 +#endif
  		fprintf(stderr,"Err: cannot create pgrp\n");
+ 		perror("setpgrp");
  		exit(1);
- 	}
