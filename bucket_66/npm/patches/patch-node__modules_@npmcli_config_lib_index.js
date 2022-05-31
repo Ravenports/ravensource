d12c8a7f69e0c027b7fc7b97f9931d678724a8dd
Michael Reim <kraileth@elderlinux.org>
2022-05-31 20:09:54 +0200

    npm: upgrade

--- bucket_66/npm/patches/patch-node__modules_@npmcli_config_lib_index.js
+++ bucket_66/npm/patches/patch-node__modules_@npmcli_config_lib_index.js
@@ -1,4 +1,4 @@
---- node_modules/@npmcli/config/lib/index.js.orig	2022-05-04 16:37:54 UTC
+--- node_modules/@npmcli/config/lib/index.js.orig	2022-05-11 16:33:05 UTC
 +++ node_modules/@npmcli/config/lib/index.js
 @@ -346,6 +346,9 @@ class Config {
  
