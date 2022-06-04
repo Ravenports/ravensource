6e268d645cd7a0bb759a947cb6fa3b7f52d6edb6
Michael Reim <kraileth@elderlinux.org>
2022-06-04 11:32:09 +0200

    webkit2: upgrade

--- bucket_58/webkit2/patches/patch-Source_bmalloc_bmalloc_AvailableMemory.cpp
+++ bucket_58/webkit2/patches/patch-Source_bmalloc_bmalloc_AvailableMemory.cpp
@@ -1,27 +1,31 @@
---- Source/bmalloc/bmalloc/AvailableMemory.cpp.orig	2022-02-23 11:41:53 UTC
-+++ Source/bmalloc/bmalloc/AvailableMemory.cpp
-@@ -44,7 +44,6 @@
+--- Source/bmalloc/bmalloc/AvailableMemory.cpp.orig	2022-02-23 12:41:53.294219500 +0100
++++ Source/bmalloc/bmalloc/AvailableMemory.cpp	2022-05-23 12:36:29.059839000 +0200
+@@ -44,8 +44,8 @@
  #import <mach/mach_error.h>
  #import <math.h>
  #elif BOS(UNIX)
 -#include <sys/sysinfo.h>
  #if BOS(LINUX)
++#include <sys/sysinfo.h>
  #include <algorithm>
  #include <fcntl.h>
-@@ -159,6 +158,12 @@ static size_t computeAvailableMemory()
+ #elif BOS(FREEBSD)
+@@ -159,7 +159,13 @@
      // Round up the memory size to a multiple of 128MB because max_mem may not be exactly 512MB
      // (for example) and we have code that depends on those boundaries.
      return ((sizeAccordingToKernel + multiple - 1) / multiple) * multiple;
+-#elif BOS(FREEBSD) || BOS(LINUX)
 +#elif defined(__DragonFly__)
 +    long pages = sysconf(_SC_PHYS_PAGES);
 +    long pageSize = sysconf(_SC_PAGE_SIZE);
 +    if (pages == -1 || pageSize == -1)
 +        return availableMemoryGuess;
 +    return pages * pageSize;
- #elif BOS(FREEBSD) || BOS(LINUX)
++#elif BOS(LINUX)
      struct sysinfo info;
      if (!sysinfo(&info))
-@@ -210,7 +215,11 @@ MemoryStatus memoryStatus()
+         return info.totalram * info.mem_unit;
+@@ -210,7 +216,11 @@
  
      size_t memoryFootprint = 0;
      if (!sysctl(mib, 4, &info, &infolen, nullptr, 0))
