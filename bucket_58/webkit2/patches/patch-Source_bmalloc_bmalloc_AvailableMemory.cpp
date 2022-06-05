5f349d0c35b58c19996b8654ad605b5c24740e34
John Marino <draco@marino.st>
2022-06-05 23:39:41 +0000

    webkit2: FreeBSD needs DragonFly solution to available memory
    Also, something happened between 2.36.2 and 2.36.3.
    It may take several attempts to build it.  It frequently but
    randomly fails with "no connection to socket" error.
    Limiting jobs to 8 didn't really help but I left it.

--- bucket_58/webkit2/patches/patch-Source_bmalloc_bmalloc_AvailableMemory.cpp
+++ bucket_58/webkit2/patches/patch-Source_bmalloc_bmalloc_AvailableMemory.cpp
@@ -1,5 +1,5 @@
---- Source/bmalloc/bmalloc/AvailableMemory.cpp.orig	2022-02-23 12:41:53.294219500 +0100
-+++ Source/bmalloc/bmalloc/AvailableMemory.cpp	2022-05-23 12:36:29.059839000 +0200
+--- Source/bmalloc/bmalloc/AvailableMemory.cpp.orig	2022-02-23 11:41:53 UTC
++++ Source/bmalloc/bmalloc/AvailableMemory.cpp
 @@ -44,8 +44,8 @@
  #import <mach/mach_error.h>
  #import <math.h>
@@ -10,12 +10,12 @@
  #include <algorithm>
  #include <fcntl.h>
  #elif BOS(FREEBSD)
-@@ -159,7 +159,13 @@
+@@ -159,7 +159,13 @@ static size_t computeAvailableMemory()
      // Round up the memory size to a multiple of 128MB because max_mem may not be exactly 512MB
      // (for example) and we have code that depends on those boundaries.
      return ((sizeAccordingToKernel + multiple - 1) / multiple) * multiple;
 -#elif BOS(FREEBSD) || BOS(LINUX)
-+#elif defined(__DragonFly__)
++#elif BOS(FREEBSD)
 +    long pages = sysconf(_SC_PHYS_PAGES);
 +    long pageSize = sysconf(_SC_PAGE_SIZE);
 +    if (pages == -1 || pageSize == -1)
@@ -25,7 +25,7 @@
      struct sysinfo info;
      if (!sysinfo(&info))
          return info.totalram * info.mem_unit;
-@@ -210,7 +216,11 @@
+@@ -210,7 +216,11 @@ MemoryStatus memoryStatus()
  
      size_t memoryFootprint = 0;
      if (!sysctl(mib, 4, &info, &infolen, nullptr, 0))
