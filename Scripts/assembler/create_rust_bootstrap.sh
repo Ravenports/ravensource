ded13760d883f38d398d8f9ac11edc8f0a281176
John Marino <draco@marino.st>
2022-06-13 01:08:30 +0000

    rust bootstrap script: no longer necessary to define CARGO_HTTP_CAINFO

--- Scripts/assembler/create_rust_bootstrap.sh
+++ Scripts/assembler/create_rust_bootstrap.sh
@@ -60,8 +60,9 @@ sleep 2
 # build rust-installer
 
 (cd ${BBASE} && git clone https://github.com/rust-lang/rust-installer.git)
-env PATH=${PATH}:${RASSY}/raven/bin CARGO_HTTP_CAINFO=/raven/share/certs/ca-root-nss.crt \
-   cargo build --manifest-path="${RINST}/Cargo.toml"
+# env PATH=${PATH}:${RASSY}/raven/bin CARGO_HTTP_CAINFO=/raven/share/certs/ca-root-nss.crt \
+#   cargo build --manifest-path="${RINST}/Cargo.toml"
+env PATH=${PATH}:${RASSY}/raven/bin cargo build --manifest-path="${RINST}/Cargo.toml"
 
 if [ $? -ne 0 ]; then
    echo "rust-installer build failed, aborting ..."
