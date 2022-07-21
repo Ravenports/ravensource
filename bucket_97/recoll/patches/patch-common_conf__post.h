0e525564fa17468cf4088f4b5122c65ef5402151
Michael Reim <kraileth@elderlinux.org>
2022-07-21 17:11:28 +0200

    recoll: upgrade

--- bucket_97/recoll/patches/patch-common_conf__post.h
+++ bucket_97/recoll/patches/patch-common_conf__post.h
@@ -1,4 +1,4 @@
---- common/conf_post.h.orig	2022-01-14 09:19:06 UTC
+--- common/conf_post.h.orig	2022-06-14 12:56:22 UTC
 +++ common/conf_post.h
 @@ -67,18 +67,4 @@ typedef int ssize_t;
  #  define PRETEND_USE(expr) ((void)(expr))
--- bucket_97/recoll/patches/patch-utils_fstreewalk.cpp
+++ bucket_97/recoll/patches/patch-utils_fstreewalk.cpp
@@ -1,4 +1,4 @@
---- utils/fstreewalk.cpp.orig	2022-01-14 09:19:07 UTC
+--- utils/fstreewalk.cpp.orig	2022-06-14 12:56:23 UTC
 +++ utils/fstreewalk.cpp
 @@ -22,6 +22,9 @@
  #include <errno.h>
