e7733fef382032ef974aba31fca205e4575481bd
John Marino <draco@marino.st>
2022-09-07 11:13:25 +0000

    po4a: upgrade

--- bucket_05/po4a/patches/patch-Po4aBuilder.pm
+++ bucket_05/po4a/patches/patch-Po4aBuilder.pm
@@ -1,4 +1,4 @@
---- Po4aBuilder.pm.orig	2022-01-02 00:10:18 UTC
+--- Po4aBuilder.pm.orig	2022-09-04 14:32:26 UTC
 +++ Po4aBuilder.pm
 @@ -118,8 +118,8 @@ sub ACTION_install {
      $mandir =~ s,/man3$,,;
@@ -11,12 +11,12 @@
      $self->install_path(po => $localedir);
  
      ExtUtils::Install::install($self->install_map, !$self->quiet, 0, $self->{args}{uninst}||0);
-@@ -250,7 +250,7 @@ sub ACTION_man {
- 		system("xsltproc -o $outdir/$outfile.$section --nonet /usr/share/xml/docbook/stylesheet/docbook-xsl/manpages/docbook.xsl $file") and die;
- 	    } else { # Not found locally, use the XSL file online
- 		print "Convert $outdir/$outfile.$section (online docbook.xsl file). ";
--		system("xsltproc -o $outdir/$outfile.$section --nonet http://docbook.sourceforge.net/release/xsl/current/manpages/docbook.xsl $file") and die;
-+		system("xsltproc -o $outdir/$outfile.$section --nonet http://cdn.docbook.org/release/xsl/current/manpages/docbook.xsl $file") and die;
- 	    }
-             system ("gzip -9 -f $outdir/$outfile.$section") and die;
-         }
+@@ -251,7 +251,7 @@ sub ACTION_man {
+             system("xsltproc -o $outdir/$outfile.$section --nonet /usr/share/xml/docbook/stylesheet/docbook-xsl/manpages/docbook.xsl $file") and die;
+             } else { # Not found locally, use the XSL file online
+             print "Convert $outdir/$outfile.$section (online docbook.xsl file). ";
+-            system("xsltproc -o $outdir/$outfile.$section --nonet http://docbook.sourceforge.net/release/xsl/current/manpages/docbook.xsl $file") and die;
++            system("xsltproc -o $outdir/$outfile.$section --nonet http://cdn.docbook.org/release/xsl/current/manpages/docbook.xsl $file") and die;
+             }
+                 system ("gzip -9 -f $outdir/$outfile.$section") and die;
+             }
