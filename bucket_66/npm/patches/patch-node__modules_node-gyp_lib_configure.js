6e7fc9584b6a78796991e226e8ab7c93d48e2a31
Michael Reim <kraileth@elderlinux.org>
2022-09-16 21:46:14 +0200

    npm: upgrade

--- bucket_66/npm/patches/patch-node__modules_node-gyp_lib_configure.js
+++ bucket_66/npm/patches/patch-node__modules_node-gyp_lib_configure.js
@@ -2,7 +2,7 @@ $NetBSD: patch-node__modules_node-gyp_lib_configure.js,v 1.3 2019/11/24 20:21:23
 
 Skip Python detection, version 2.7 is required and we know where it is.
 
---- node_modules/node-gyp/lib/configure.js.orig	2022-06-22 21:41:40 UTC
+--- node_modules/node-gyp/lib/configure.js.orig	2022-07-13 17:14:45 UTC
 +++ node_modules/node-gyp/lib/configure.js
 @@ -15,21 +15,14 @@ if (win) {
  }
