b13734242fadb79a0510f35307483949b0b8b08c
John Marino <draco@marino.st>
2022-09-18 22:46:27 +0000

    emacs: upgrade

--- bucket_4B/emacs/patches/patch-lisp_files.el
+++ bucket_4B/emacs/patches/patch-lisp_files.el
@@ -1,11 +1,11 @@
---- lisp/files.el.orig	2021-01-28 17:52:38 UTC
+--- lisp/files.el.orig	2022-09-06 21:31:53 UTC
 +++ lisp/files.el
-@@ -6766,7 +6766,7 @@ need to be passed verbatim to shell comm
+@@ -7244,7 +7244,7 @@ need to be passed verbatim to shell comm
        pattern))))
  
  
 -(defvar insert-directory-program (purecopy "ls")
 +(defvar insert-directory-program (purecopy "gls")
-   "Absolute or relative name of the `ls' program used by `insert-directory'.")
- 
- (defcustom directory-free-space-program (purecopy "df")
+   "Absolute or relative name of the `ls'-like program.
+ This is used by `insert-directory' and `dired-insert-directory'
+ \(thus, also by `dired').")
