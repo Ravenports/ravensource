27da145abcbf8339ff2d31d80cf957065c6b3994
John Marino <draco@marino.st>
2022-09-18 00:01:47 +0000

    ravensys-root: tweak
    - remove macos variant (the huge distfile isn't available)
    - for known missing ubuntu files, explicitly call out RP cache
    This should fix a lot of "problems" on repology.

--- bucket_B4/ravensys-root/files/special.mk
+++ bucket_B4/ravensys-root/files/special.mk
@@ -1,7 +1,7 @@
 # Override bmake settings (important for bootstraps and sysroot rebases)
 OPSYS=				${SYSTEM:H:H:H:H:H}
 ARCH=				${SYSTEM:H:H:H:H:T}
-ARCH_STANDARD=		${SYSTEM:H:H:H:T}
+ARCH_STANDARD=			${SYSTEM:H:H:H:T}
 OSVERSION=			${SYSTEM:H:H:T}
 OSREL=				${SYSTEM:H:T}
 MAJOR=				${SYSTEM:T}
