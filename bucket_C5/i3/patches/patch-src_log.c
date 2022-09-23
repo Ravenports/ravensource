a07b0d3e73bbaa196e58b7a0706ae400ff1ae9a8
John Marino <draco@marino.st>
2022-09-23 05:45:44 +0000

    I3: upgrade

--- bucket_C5/i3/patches/patch-src_log.c
+++ bucket_C5/i3/patches/patch-src_log.c
@@ -1,15 +1,15 @@
---- src/log.c.orig	2021-11-03 08:23:08 UTC
+--- src/log.c.orig	2022-09-21 16:26:43 UTC
 +++ src/log.c
-@@ -138,7 +138,7 @@ void open_logbuffer(void) {
-      * At the moment (2011-12-10), no testcase leads to an i3 log
-      * of more than ~ 600 KiB. */
-     logbuffer_size = min(physical_mem_bytes * 0.01, shmlog_size);
+@@ -142,7 +142,7 @@ void open_logbuffer(void) {
+         logbuffer_size = physical_mem_bytes * 0.01;
+     }
+ 
 -#if defined(__FreeBSD__)
 +#if defined(__FreeBSD__) || defined(__DragonFly__)
      sasprintf(&shmlogname, "/tmp/i3-log-%d", getpid());
  #else
      sasprintf(&shmlogname, "/i3-log-%d", getpid());
-@@ -149,7 +149,7 @@ void open_logbuffer(void) {
+@@ -153,7 +153,7 @@ void open_logbuffer(void) {
          return;
      }
  
