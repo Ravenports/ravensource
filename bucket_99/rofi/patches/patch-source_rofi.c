9993869c792332eb6187f570c2376621e0beb4ff
John Marino <draco@marino.st>
2022-09-28 00:09:55 +0000

    blind attempt to fix rofi build

---
+++ bucket_99/rofi/patches/patch-source_rofi.c
@@ -0,0 +1,12 @@
+--- source/rofi.c.orig	2022-08-09 13:47:23 UTC
++++ source/rofi.c
+@@ -37,6 +37,9 @@
+ #include <stdlib.h>
+ #include <string.h>
+ #include <sys/types.h>
++#if defined __FreeBSD__
++#include <sys/stat.h>
++#endif
+ #include <sysexits.h>
+ #include <time.h>
+ #include <unistd.h>
