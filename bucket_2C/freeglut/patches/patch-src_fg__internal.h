a0a517a537095f6097ec4267b4210c0d1ad30cab
John Marino <draco@marino.st>
2022-10-08 15:50:46 +0000

    freeglut: upgrade

--- bucket_2C/freeglut/patches/patch-src_fg__internal.h
+++ bucket_2C/freeglut/patches/patch-src_fg__internal.h
@@ -1,4 +1,4 @@
---- src/fg_internal.h.orig	2019-09-25 10:08:07 UTC
+--- src/fg_internal.h.orig	2022-10-07 16:12:51 UTC
 +++ src/fg_internal.h
 @@ -137,9 +137,6 @@
   * If so, perhaps there should be a config_guess.h for them. Alternatively,
@@ -10,12 +10,3 @@
  
  #if defined(_MSC_VER) || defined(__WATCOMC__)
  /* strdup() is non-standard, for all but POSIX-2001 */
-@@ -1067,6 +1064,8 @@ int         fgJoystickDetect( void );
- void        fgInitialiseJoysticks( void );
- void        fgJoystickClose( void );
- void        fgJoystickPollWindow( SFG_Window* window );
-+void        fgJoystickRawRead( SFG_Joystick* joy, int* buttons, float* axes );
-+void        fghJoystickRawRead( SFG_Joystick* joy, int* buttons, float* axes );
- 
- /* InputDevice Initialisation and Closure */
- int         fgInputDeviceDetect( void );
