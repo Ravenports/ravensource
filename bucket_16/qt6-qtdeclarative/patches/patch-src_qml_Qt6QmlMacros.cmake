db092dc987a875d6bd7b42d5d4a6978be843b841
John Marino <draco@marino.st>
2022-10-14 02:17:25 +0000

    qt6-qtdeclarative: fix rpath issue

---
+++ bucket_16/qt6-qtdeclarative/patches/patch-src_qml_Qt6QmlMacros.cmake
@@ -0,0 +1,19 @@
+--- src/qml/Qt6QmlMacros.cmake.orig	2022-09-23 07:21:41 UTC
++++ src/qml/Qt6QmlMacros.cmake
+@@ -1615,7 +1615,6 @@ function(qt6_add_qml_plugin target)
+         string(REPLACE "/" ";" path "qml/${arg_TARGET_PATH}")
+         list(LENGTH path path_count)
+         string(REPEAT "../" ${path_count} rel_path)
+-        string(APPEND rel_path "${CMAKE_INSTALL_LIBDIR}")
+         if(APPLE)
+             set(install_rpath
+                 # If embedded in an app bundle, search in a bundle-local path
+@@ -1631,7 +1630,7 @@ function(qt6_add_qml_plugin target)
+                 "@loader_path/${rel_path}"
+             )
+         else()
+-            set(install_rpath "$ORIGIN/${rel_path}")
++            set(install_rpath "${CMAKE_INSTALL_LIBDIR}:$ORIGIN/${rel_path}")
+         endif()
+         set_target_properties(${target} PROPERTIES INSTALL_RPATH "${install_rpath}")
+     endif()
