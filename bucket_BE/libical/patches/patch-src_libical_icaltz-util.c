8d542ba58b4ae40993a97a886d8781325ce9be47
John Marino <draco@marino.st>
2022-10-26 02:23:24 +0000

    libical: upgrade

--- bucket_BE/libical/patches/patch-src_libical_icaltz-util.c
+++ bucket_BE/libical/patches/patch-src_libical_icaltz-util.c
@@ -1,4 +1,4 @@
---- src/libical/icaltz-util.c.orig	2022-10-06 15:41:16 UTC
+--- src/libical/icaltz-util.c.orig	2022-10-17 20:53:57 UTC
 +++ src/libical/icaltz-util.c
 @@ -53,6 +53,13 @@
  #define bswap_32 swap32
