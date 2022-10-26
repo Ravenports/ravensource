de3df59f524486ae5a07ed660cdff756949d6ecc
John Marino <draco@marino.st>
2022-10-26 22:18:36 +0000

    erlang: upgrade

--- bucket_A3/erlang/patches/patch-erts_etc_unix_run__erl.c
+++ bucket_A3/erlang/patches/patch-erts_etc_unix_run__erl.c
@@ -1,4 +1,4 @@
---- erts/etc/unix/run_erl.c.orig	2022-10-03 08:12:56 UTC
+--- erts/etc/unix/run_erl.c.orig	2022-10-24 08:12:46 UTC
 +++ erts/etc/unix/run_erl.c
 @@ -81,9 +81,6 @@
  #ifdef HAVE_LIBUTIL_H
