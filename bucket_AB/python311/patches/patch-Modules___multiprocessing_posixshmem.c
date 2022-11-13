6a1d31107e6110c2d211931b4cc16886f7782494
John Marino <draco@marino.st>
2022-11-13 05:09:12 +0000

    add python 3.11 port (_crypt module wouldn't build, so disabled)

---
+++ bucket_AB/python311/patches/patch-Modules___multiprocessing_posixshmem.c
@@ -0,0 +1,12 @@
+--- Modules/_multiprocessing/posixshmem.c.orig	2022-10-24 17:35:39 UTC
++++ Modules/_multiprocessing/posixshmem.c
+@@ -8,6 +8,9 @@ posixshmem - A Python extension that pro
+ 
+ // for shm_open() and shm_unlink()
+ #ifdef HAVE_SYS_MMAN_H
++# ifdef __sun__
++# define _XPG4_2
++# endif
+ #include <sys/mman.h>
+ #endif
+ 
---
+++ bucket_AB/python311/patches/patch-Modules_nismodule.c
@@ -0,0 +1,11 @@
+--- Modules/nismodule.c.orig	2022-10-24 17:35:39 UTC
++++ Modules/nismodule.c
+@@ -116,7 +116,7 @@ nis_mapname(char *map, int *pfix)
+     return map;
+ }
+ 
+-#if defined(__APPLE__) || defined(__OpenBSD__) || defined(__FreeBSD__)
++#if defined(__APPLE__) || defined(__OpenBSD__) || defined(__FreeBSD__) || defined(__DragonFly__)
+ typedef int (*foreachfunc)(unsigned long, char *, int, char *, int, void *);
+ #else
+ typedef int (*foreachfunc)(int, char *, int, char *, int, char *);
