fb4e2e3642a65ba3df276b6fb771aba4523fd54f
John Marino <draco@marino.st>
2022-11-16 13:53:41 +0000

    lagrange: upgrade

--- bucket_6A/lagrange/patches/patch-build-tui.sh
+++ bucket_6A/lagrange/patches/patch-build-tui.sh
@@ -1,6 +1,6 @@
---- build-tui.sh.orig	2022-06-30 05:46:44 UTC
+--- build-tui.sh.orig	2022-11-02 17:11:20 UTC
 +++ build-tui.sh
-@@ -14,23 +14,15 @@
+@@ -14,25 +14,15 @@
  #
  # You can customize the install directory prefix here and build type here:
  
@@ -14,20 +14,22 @@
  echo "statically linked the_Foundation and SEALCurses. First, let's configure"
  echo "the build.\n"
  
--read -p "Build type? [${CMAKE_BUILD_TYPE}] " INPUT
+-echo "Build type? [${CMAKE_BUILD_TYPE}]"
+-read INPUT
 -if [ "${INPUT}." != "." ]; then
 -    CMAKE_BUILD_TYPE=${INPUT}
 -fi
 -
--read -p "Install prefix? [${INSTALL_PREFIX}] " INPUT
+-echo "Install prefix? [${INSTALL_PREFIX}]"
+-read INPUT
 -if [ "${INPUT}." != "." ]; then
 -    INSTALL_PREFIX=${INPUT}
 -fi
 -
  if [ ! -d lib/sealcurses ]; then
-     read -p "'lib/sealcurses' not found. Clone with Git? [Yn] " INPUT
-     if [ "${INPUT}." = "n." ]; then
-@@ -91,14 +83,11 @@ cmake .. \
+     echo "'lib/sealcurses' not found. Clone with Git? [Yn]"
+     read INPUT
+@@ -95,15 +85,11 @@ cmake .. \
      -DENABLE_HARFBUZZ=NO \
      -DENABLE_POPUP_MENUS=NO \
      -DENABLE_IDLE_SLEEP=NO \
@@ -38,7 +40,8 @@
  
  echo "-----"
  echo "clagrange and resources.lgr can be found in 'build-tui'."
--read -p "Do you want to install them to ${INSTALL_PREFIX}? (sudo) [yN] " CONFIRMED
+-echo "Do you want to install them to ${INSTALL_PREFIX}? (sudo) [yN]"
+-read CONFIRMED
 -if [ "${CONFIRMED}" = "y" ]; then
 -    sudo cmake --install .
 -    exit
