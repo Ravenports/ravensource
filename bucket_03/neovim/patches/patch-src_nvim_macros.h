0dde555e0332dadccefcea1f7a4c33dbc386a3a6
John Marino <draco@marino.st>
2022-11-16 00:58:33 +0000

    neovim: upgrade

--- bucket_03/neovim/patches/patch-src_nvim_macros.h
+++ bucket_03/neovim/patches/patch-src_nvim_macros.h
@@ -1,4 +1,4 @@
---- src/nvim/macros.h.orig	2022-09-30 15:15:13 UTC
+--- src/nvim/macros.h.orig	2022-11-14 09:12:05 UTC
 +++ src/nvim/macros.h
 @@ -137,7 +137,9 @@
  #define RGB_(r, g, b) (((r) << 16) | ((g) << 8) | (b))
