c79e61c2f803e9f762680c608dddffef1f8b89bf
John Marino <draco@marino.st>
2022-11-24 18:43:27 -0600

    rust: remove HardenedBSD check to fix FreeBSD operation inside RP sysroot (it requires sysctl)

---
+++ bucket_7B/rust/patches/patch-vendor_os__info_src_freebsd_mod.rs
@@ -0,0 +1,21 @@
+--- vendor/os_info/src/freebsd/mod.rs.orig	2022-11-02 16:21:42 UTC
++++ vendor/os_info/src/freebsd/mod.rs
+@@ -30,17 +30,7 @@ fn get_os() -> Type {
+         .expect("Failed to get OS");
+ 
+     match str::from_utf8(&os.stdout) {
+-        Ok("FreeBSD\n") => {
+-            let check_hardening = Command::new("sysctl")
+-                .arg("hardening.version")
+-                .output()
+-                .expect("Failed to check if is hardened");
+-            match str::from_utf8(&check_hardening.stderr) {
+-                Ok("0\n") => Type::HardenedBSD,
+-                Ok(_) => Type::FreeBSD,
+-                Err(_) => Type::FreeBSD,
+-            }
+-        }
++        Ok("FreeBSD\n") => Type::FreeBSD,
+         Ok("MidnightBSD\n") => Type::MidnightBSD,
+         Ok(_) => Type::Unknown,
+         Err(_) => Type::Unknown,
