7c78df78c689e7e431dd936604f034126a93d0e7
John Marino <draco@marino.st>
2022-11-26 04:22:03 +0000

    ada_spawn: implement ptsname_r on dragonfly

---
+++ bucket_9F/ada_spawn/files/ptsname_r.c
@@ -0,0 +1,45 @@
+#ifdef __DragonFly__
+
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+
+
+/* Store at most BUFLEN characters of the pathname of the slave pseudo
+   terminal associated with the master FD is open on in BUF.
+   Return 0 on success, otherwise an error number.  */
+int
+ptsname_r (int fd, char *buf, size_t buflen)
+{
+  int saved_errno = errno;
+  char tmpbuf[5 + 4 + 10 + 1];
+  int ret;
+  int n;
+  if (buf == NULL)
+    {
+      errno = EINVAL;
+      return errno;
+    }
+  /* The result of fdevname_r is typically of the form ptm/N.  */
+  ret = fdevname_r (fd, tmpbuf + 5, sizeof (tmpbuf) - 5);
+  if (ret < 0 || strncmp (tmpbuf + 5, "ptm/", 4) != 0)
+    {
+      errno = ENOTTY;
+      return errno;
+    }
+  /* Turn it into /dev/pts/N.  */
+  memcpy (tmpbuf, "/dev/pts/", 5 + 4);
+  n = strlen (tmpbuf);
+  if (n >= buflen)
+    {
+      errno = ERANGE;
+      return errno;
+    }
+  memcpy (buf, tmpbuf, n + 1);
+  /* Don't do a final stat(), since the file name /dev/pts/N does not actually
+     exist.  */
+  errno = saved_errno;
+  return 0;
+}
+
+#endif
