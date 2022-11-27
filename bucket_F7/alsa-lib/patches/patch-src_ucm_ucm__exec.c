2f15ce1f5d3bb64cc5f50c0ddc3eeb8ccc232a23
John Marino <draco@marino.st>
2022-11-27 15:35:47 +0000

    alsa-lib: regen patch, attempts to fix FreeBSD build

--- bucket_F7/alsa-lib/patches/patch-src_ucm_ucm__exec.c
+++ bucket_F7/alsa-lib/patches/patch-src_ucm_ucm__exec.c
@@ -1,15 +1,14 @@
 --- src/ucm/ucm_exec.c.orig	2022-10-24 08:17:27 UTC
 +++ src/ucm/ucm_exec.c
-@@ -32,6 +32,12 @@
- #include <sys/wait.h>
- #include <limits.h>
- #include <dirent.h>
-+#include <unistd.h>
-+#include <signal.h>
-+
-+#if defined(__FreeBSD__) || defined(__DragonFly__)
-+char *environ[] = { 0 };
-+#endif 
+@@ -35,8 +35,9 @@
  
  #if defined(__NetBSD__) || defined(__OpenBSD__) || defined(__FreeBSD__) || defined(__DragonFly__)
  #include <signal.h>
+-#if defined(__DragonFly__)
+-#define environ NULL /* XXX */
++#include <unistd.h>
++#if defined(__FreeBSD__) || defined(__DragonFly__)
++char *environ[] = { 0 };
+ #else
+ extern char **environ;
+ #endif
