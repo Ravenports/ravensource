b11daa335a013f56667ae82d8dbfd067b7ddbeb2
John Marino <draco@marino.st>
2022-12-09 23:31:37 +0000

    libarchive: upgrade

--- bucket_12/libarchive/patches/patch-libarchive_archive__write__add__filter__zstd.c
+++ bucket_12/libarchive/patches/patch-libarchive_archive__write__add__filter__zstd.c
@@ -1,4 +1,4 @@
---- libarchive/archive_write_add_filter_zstd.c.orig	2022-02-09 12:24:12 UTC
+--- libarchive/archive_write_add_filter_zstd.c.orig	2022-12-09 13:38:31 UTC
 +++ libarchive/archive_write_add_filter_zstd.c
 @@ -63,7 +63,7 @@ struct private_data {
  /* If we don't have the library use default range values (zstdcli.c v1.4.0) */
