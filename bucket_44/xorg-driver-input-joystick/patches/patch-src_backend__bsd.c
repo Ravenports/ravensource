003fc1b8ae6a5b2665637eb580fbd94e6c918ba2
John Marino <draco@marino.st>
2022-12-10 01:27:31 +0000

    joystick input driver upgrade

--- bucket_44/xorg-driver-input-joystick/patches/patch-src_backend__bsd.c
+++ bucket_44/xorg-driver-input-joystick/patches/patch-src_backend__bsd.c
@@ -1,6 +1,6 @@
---- src/backend_bsd.c.orig	2014-03-20 02:14:06 UTC
+--- src/backend_bsd.c.orig	2022-12-09 00:09:50 UTC
 +++ src/backend_bsd.c
-@@ -42,10 +42,16 @@
+@@ -42,6 +42,11 @@
  #include <xf86_OSproc.h>
  
  #include <usbhid.h>
@@ -11,10 +11,12 @@
 +#else
  #include <dev/usb/usb.h>
  #include <dev/usb/usbhid.h>
--#ifdef HAVE_DEV_USB_USB_IOCTL_H
-+# ifdef HAVE_DEV_USB_USB_IOCTL_H
+ #ifdef HAVE_DEV_HID_HID_H
+@@ -50,6 +55,7 @@
+ #ifdef HAVE_DEV_USB_USB_IOCTL_H
      #include <dev/usb/usb_ioctl.h>
-+# endif
  #endif
++#endif /* !dragonfly */
  
  #include "jstk.h"
+ #include "backend_bsd.h"
