e90c15a8a92096ac018c9edfa755de7d9dbd6933
John Marino <draco@marino.st>
2022-12-25 00:01:39 +0000

    skia: link without libdl on !linux

--- bucket_90/skia/files/special.mk
+++ bucket_90/skia/files/special.mk
@@ -1,5 +1,13 @@
 _USES_extract+=	501:make_extdir
 
+.if "${OPSYS}" != "Linux"
+_USES_configure+= 452:remove_libdl
+.endif
+
 make_extdir:
 	${MKDIR} ${WRKSRC}/third_party/externals
-	
+
+remove_libdl:
+	@echo "BSD: Remove -ldl linkage"
+	${REINPLACE_CMD} -e 's|-ldl||' ${WRKSRC}/public.bzl
+	${REINPLACE_CMD} -e 's|"dl"|""|' ${WRKSRC}/BUILD.gn
