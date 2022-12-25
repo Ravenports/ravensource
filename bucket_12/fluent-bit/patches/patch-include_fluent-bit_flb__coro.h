764c06c5f6932830173df34fa2a74bc309da995f
John Marino <draco@marino.st>
2022-12-25 17:58:26 +0000

    fluent-bit(18): add NetBSD support

---
+++ bucket_12/fluent-bit/patches/patch-include_fluent-bit_flb__coro.h
@@ -0,0 +1,30 @@
+NetBSD doesn't define THREAD_STACK_MIN, so pick an arbitrary 16 pages.
+_XOPEN_SOURCE is messing up sys/socket.h on NetBSD so remove it.
+
+--- include/fluent-bit/flb_coro.h.orig	2022-11-22 14:33:00 UTC
++++ include/fluent-bit/flb_coro.h
+@@ -20,11 +20,6 @@
+ #ifndef FLB_CORO_H
+ #define FLB_CORO_H
+ 
+-/* Required by OSX */
+-#ifndef _XOPEN_SOURCE
+-#define _XOPEN_SOURCE
+-#endif
+-
+ #ifndef _DEFAULT_SOURCE
+ #define _DEFAULT_SOURCE
+ #endif
+@@ -64,7 +59,12 @@ struct flb_coro {
+ #ifdef FLB_CORO_STACK_SIZE
+ #define FLB_CORO_STACK_SIZE_BYTE      FLB_CORO_STACK_SIZE
+ #else
++# if defined(__NetBSD__) && !defined(PTHREAD_STACK_MIN)
++#include <machine/vmparam.h>
++#define FLB_CORO_STACK_SIZE_BYTE      (16 * PAGE_SIZE)
++# else
+ #define FLB_CORO_STACK_SIZE_BYTE      ((3 * PTHREAD_STACK_MIN) / 2)
++# endif
+ #endif
+ 
+ #define FLB_CORO_DATA(coro)      (((char *) coro) + sizeof(struct flb_coro))
---
+++ bucket_12/fluent-bit/patches/patch-include_fluent-bit_flb__output.h
@@ -0,0 +1,13 @@
+--- include/fluent-bit/flb_output.h.orig	2022-11-22 14:33:00 UTC
++++ include/fluent-bit/flb_output.h
+@@ -20,10 +20,6 @@
+ #ifndef FLB_OUTPUT_H
+ #define FLB_OUTPUT_H
+ 
+-#ifndef _XOPEN_SOURCE
+-#define _XOPEN_SOURCE
+-#endif
+-
+ #ifndef _GNU_SOURCE
+ #define _GNU_SOURCE
+ #endif
