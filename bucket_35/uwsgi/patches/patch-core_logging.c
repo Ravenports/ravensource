e2af6e2c21b8418dc2c71ea079279b34abcc42f5
John Marino <draco@marino.st>
2022-12-26 03:07:23 +0000

    uwsgi: fix build on netbsd

---
+++ bucket_35/uwsgi/patches/patch-core_logging.c
@@ -0,0 +1,17 @@
+$NetBSD: patch-core_logging.c,v 1.5 2022/11/02 07:26:47 wiz Exp $
+
+Fix build for NetBSD.
+https://github.com/unbit/uwsgi/pull/2506
+
+--- core/logging.c.orig	2022-10-24 10:21:58 UTC
++++ core/logging.c
+@@ -2,7 +2,9 @@
+ #include "uwsgi.h"
+ #endif
+ #if defined(__FreeBSD__) || defined(__NetBSD__) || defined(__DragonFly__) || defined(__OpenBSD__)
++#  if !defined(__NetBSD__)
+ #include <sys/user.h>
++#  endif
+ #include <sys/sysctl.h>
+ #include <kvm.h>
+ #elif defined(__sun__)
