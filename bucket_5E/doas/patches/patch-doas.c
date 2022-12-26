edb5c9c4a014ab2deb2de335458bc33f78599fb4
John Marino <draco@marino.st>
2022-12-26 03:16:24 +0000

    doas: fix build on netbsd

--- bucket_5E/doas/patches/patch-doas.c
+++ bucket_5E/doas/patches/patch-doas.c
@@ -9,7 +9,16 @@
  	status = setresuid(uid, uid, uid);
  	#else
  	status = setreuid(uid, uid);
-@@ -536,7 +536,7 @@ main(int argc, char **argv)
+@@ -530,13 +530,16 @@ main(int argc, char **argv)
+ 
+         
+ #if defined(HAVE_LOGIN_CAP_H)
++# ifndef LOGIN_SETLOGINCLASS
++# define LOGIN_SETLOGINCLASS	0
++# endif
+         if (setusercontext(NULL, target_pw, target, 
+             LOGIN_SETGROUP | LOGIN_SETLOGINCLASS |
+             LOGIN_SETPRIORITY | LOGIN_SETRESOURCES | LOGIN_SETUMASK |
              LOGIN_SETUSER) != 0)
              errx(1, "failed to set user context for target");
  #else
@@ -18,7 +27,7 @@
  	if (setresgid(target_pw->pw_gid, target_pw->pw_gid, target_pw->pw_gid) == -1)
  		err(1, "setresgid");
  	#else
-@@ -545,7 +545,7 @@ main(int argc, char **argv)
+@@ -545,7 +548,7 @@ main(int argc, char **argv)
  	#endif
  	if (initgroups(target_pw->pw_name, target_pw->pw_gid) == -1)
  		err(1, "initgroups");
