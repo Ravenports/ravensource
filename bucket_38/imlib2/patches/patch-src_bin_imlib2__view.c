6d9f9d92dc1fafd4a3384c1da1a952a759a7581c
John Marino <draco@marino.st>
2022-12-30 00:34:03 +0000

    imlib2: upgrade

--- bucket_38/imlib2/patches/patch-src_bin_imlib2__view.c
+++ bucket_38/imlib2/patches/patch-src_bin_imlib2__view.c
@@ -2,7 +2,7 @@ $NetBSD: patch-src_bin_imlib2__view.c,v 1.1 2020/02/20 15:14:57 nia Exp $
 
 Include sys/time.h for timeval.
 
---- src/bin/imlib2_view.c.orig	2022-03-15 06:27:09 UTC
+--- src/bin/imlib2_view.c.orig	2022-12-17 09:12:10 UTC
 +++ src/bin/imlib2_view.c
 @@ -3,6 +3,7 @@
  
@@ -11,4 +11,4 @@ Include sys/time.h for timeval.
 +#include <sys/time.h>
  #include <stdio.h>
  #include <stdlib.h>
- #include <errno.h>
+ #include <string.h>
