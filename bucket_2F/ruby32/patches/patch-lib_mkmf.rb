c3894c184be1325e7507f6698084683df06e84c2
John Marino <draco@marino.st>
2022-12-30 19:35:41 +0000

    add ruby32 port

---
+++ bucket_2F/ruby32/patches/patch-lib_mkmf.rb
@@ -0,0 +1,11 @@
+--- lib/mkmf.rb.orig	2022-12-25 06:55:25 UTC
++++ lib/mkmf.rb
+@@ -232,7 +232,7 @@ module MakeMakefile
+   end
+   $extmk ||= false
+   if not $extmk and File.exist?(($hdrdir = RbConfig::CONFIG["rubyhdrdir"]) + "/ruby/ruby.h")
+-    $topdir = $hdrdir
++    $topdir = $hdrdir + "/" + "#{CONFIG['arch']}/ruby/"
+     $top_srcdir = $hdrdir
+     $arch_hdrdir = RbConfig::CONFIG["rubyarchhdrdir"]
+   elsif File.exist?(($hdrdir = ($top_srcdir ||= topdir) + "/include")  + "/ruby.h")
---
+++ bucket_2F/ruby32/patches/patch-tool_mkconfig.rb
@@ -0,0 +1,14 @@
+--- tool/mkconfig.rb.orig	2022-12-25 06:55:25 UTC
++++ tool/mkconfig.rb
+@@ -169,8 +169,9 @@ def vars.expand(val, config = self)
+   val.replace(newval) unless newval == val
+   val
+ end
+-prefix = vars.expand(vars["prefix"] ||= "")
+-rubyarchdir = vars.expand(vars["rubyarchdir"] ||= "")
++prefix = vars.expand(vars["rubyarchdir"])
++major, minor, *rest = RUBY_VERSION.split('.')
++rubyarchdir = "/lib/ruby/#{major}.#{minor}/#{arch}"
+ relative_archdir = rubyarchdir.rindex(prefix, 0) ? rubyarchdir[prefix.size..-1] : rubyarchdir
+ 
+ puts %[\
