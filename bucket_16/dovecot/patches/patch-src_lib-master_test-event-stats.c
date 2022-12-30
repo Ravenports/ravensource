0b07999b89fddc966afb60611b2835ff247e4bc1
John Marino <draco@marino.st>
2022-12-30 02:17:34 +0000

    dovecot: upgrade

--- bucket_16/dovecot/patches/patch-src_lib-master_test-event-stats.c
+++ bucket_16/dovecot/patches/patch-src_lib-master_test-event-stats.c
@@ -1,4 +1,4 @@
---- src/lib-master/test-event-stats.c.orig	2022-06-14 06:55:03 UTC
+--- src/lib-master/test-event-stats.c.orig	2022-12-21 08:49:12 UTC
 +++ src/lib-master/test-event-stats.c
 @@ -13,6 +13,7 @@
  #include "stats-client.h"
