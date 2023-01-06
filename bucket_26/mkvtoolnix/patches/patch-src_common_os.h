2f8d97a2644c44a0778d1aff39fbf50999274ba4
John Marino <draco@marino.st>
2023-01-06 20:35:39 +0000

    mktoolnix: fix build on NetBSD

--- bucket_26/mkvtoolnix/patches/patch-src_common_os.h
+++ bucket_26/mkvtoolnix/patches/patch-src_common_os.h
@@ -5,7 +5,7 @@
  # define COMP_GCC
  # define SYS_UNIX
 -# if defined(__bsdi__) || defined(__FreeBSD__)
-+# if defined(__bsdi__) || defined(__FreeBSD__) || defined(__DragonFly__)
++# if defined(__bsdi__) || defined(__FreeBSD__) || defined(__DragonFly__) || defined(__NetBSD__)
  #  define SYS_BSD
  # elif defined(__sun) && defined(__SUNPRO_CC)
  #  undef COMP_GCC
