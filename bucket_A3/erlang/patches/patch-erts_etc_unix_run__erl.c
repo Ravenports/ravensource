d924d65d4dccf19e31ee88e9ed0ad6b61d448e20
John Marino <draco@marino.st>
2023-01-06 13:25:21 +0000

    erlang: fix build on netbsd

--- bucket_A3/erlang/patches/patch-erts_etc_unix_run__erl.c
+++ bucket_A3/erlang/patches/patch-erts_etc_unix_run__erl.c
@@ -1,12 +1,11 @@
 --- erts/etc/unix/run_erl.c.orig	2022-12-12 07:30:54 UTC
 +++ erts/etc/unix/run_erl.c
-@@ -81,9 +81,6 @@
+@@ -81,7 +81,7 @@
  #ifdef HAVE_LIBUTIL_H
  #  include <libutil.h>
  #endif
 -#ifdef HAVE_UTIL_H
--#  include <util.h>
--#endif
- #ifdef HAVE_SYS_IOCTL_H
- #  include <sys/ioctl.h>
++#ifdef __NetBSD__
+ #  include <util.h>
  #endif
+ #ifdef HAVE_SYS_IOCTL_H
