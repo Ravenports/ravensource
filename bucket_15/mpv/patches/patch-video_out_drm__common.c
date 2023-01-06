dfb8f84bfcf4876c84785876a21099f4899a5aab
John Marino <draco@marino.st>
2023-01-06 05:09:31 +0000

    mpv: fix build on netbsd

---
+++ bucket_15/mpv/patches/patch-video_out_drm__common.c
@@ -0,0 +1,16 @@
+$NetBSD: patch-video_out_drm__common.c,v 1.4 2020/12/19 11:27:51 leot Exp $
+
+Enable support for DRM on NetBSD.
+
+--- video/out/drm_common.c.orig	2022-11-12 13:52:40 UTC
++++ video/out/drm_common.c
+@@ -31,6 +31,9 @@
+ 
+ #if HAVE_CONSIO_H
+ #include <sys/consio.h>
++#elif HAVE_WSDISPLAY_USL_IO_H
++#include <sys/types.h>
++#include <dev/wscons/wsdisplay_usl_io.h>
+ #else
+ #include <sys/vt.h>
+ #endif
