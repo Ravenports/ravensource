403b3953a7d18208970972eedd323c924264a9ce
John Marino <draco@marino.st>
2023-01-09 01:08:21 +0000

    add cups-filters port

---
+++ bucket_E5/cups-filters/patches/patch-filter_gstoraster.c
@@ -0,0 +1,11 @@
+--- filter/gstoraster.c.orig	2022-08-24 12:39:01 UTC
++++ filter/gstoraster.c
+@@ -50,6 +50,8 @@ MIT Open Source License  -  http://www.o
+ #include <errno.h>
+ #include "pdf.h"
+ 
++#define execvpe(a, b, c) execvp(a, b)
++
+ #define PDF_MAX_CHECK_COMMENT_LINES	20
+ 
+ typedef enum {
