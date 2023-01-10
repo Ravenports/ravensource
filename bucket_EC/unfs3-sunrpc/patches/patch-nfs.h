9ee7c9e251f447c3dba942b75d4b9bedb1d57cf6
John Marino <draco@marino.st>
2023-01-10 23:37:04 +0000

    unfs3-sunrpc: fix build on netbsd

---
+++ bucket_EC/unfs3-sunrpc/patches/patch-nfs.h
@@ -0,0 +1,12 @@
+--- nfs.h.orig	2022-11-07 12:11:48 UTC
++++ nfs.h
+@@ -65,6 +65,9 @@ typedef int32_t int32;
+ #ifdef __APPLE__
+ typedef off_t off64_t;
+ #endif
++#ifdef __NetBSD__
++typedef off_t off64_t;
++#endif
+ #endif
+ 
+ typedef char *filename3;
