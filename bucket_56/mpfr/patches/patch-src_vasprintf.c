619c2a3aca0a8ed47afe853a129954999fa2991d
John Marino <draco@marino.st>
2023-01-17 04:45:52 +0000

    mpfr: upgrade

--- bucket_56/mpfr/patches/patch-src_vasprintf.c
+++ bucket_56/mpfr/patches/patch-src_vasprintf.c
@@ -1,4 +1,4 @@
---- src/vasprintf.c.orig	2020-06-01 10:39:52 UTC
+--- src/vasprintf.c.orig	2023-01-05 17:09:48 UTC
 +++ src/vasprintf.c
 @@ -72,6 +72,7 @@ https://www.gnu.org/licenses/ or write t
  #endif /* HAVE_VA_COPY */
