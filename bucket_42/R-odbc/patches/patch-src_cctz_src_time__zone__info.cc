104bb6dd8c0844b4d3957a38ba73253aeafb918c
John Marino <draco@marino.st>
2023-01-17 14:54:12 +0000

    R-odbc: restore patch after netbsd fix to restore DF and FreeBSD

--- bucket_42/R-odbc/patches/patch-src_cctz_src_time__zone__info.cc
+++ bucket_42/R-odbc/patches/patch-src_cctz_src_time__zone__info.cc
@@ -5,7 +5,7 @@
    strerror_s(buf, buflen, errnum);
    return buf;
 -#elif defined(__APPLE__)
-+#elif defined(__APPLE__) || defined(__NetBSD__)
++#elif defined(__APPLE__) || defined(__NetBSD__) || defined(__FreeBSD__) || defined(__DragonFly__)
    strerror_r(errnum, buf, buflen);
    return buf;
  #elif ((_POSIX_C_SOURCE >= 200112L || _XOPEN_SOURCE >= 600) && !_GNU_SOURCE) || __MUSL__
