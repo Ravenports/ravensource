17e46149b6b8fd75094a0853bd90f6056a4ee772
John Marino <draco@marino.st>
2023-01-18 03:42:18 +0000

    libglvnd: fix desktop on netbsd!

--- bucket_58/libglvnd/patches/patch-src_GLdispatch_vnd-glapi_glapi.h
+++ bucket_58/libglvnd/patches/patch-src_GLdispatch_vnd-glapi_glapi.h
@@ -5,7 +5,7 @@
  _GLAPI_EXPORT extern const __thread void *
      _glapi_tls_Current[GLAPI_NUM_CURRENT_ENTRIES]
 -#if defined(__GLIBC__) || defined(__FreeBSD__)
-+#if defined(__GLIBC__) || defined(__FreeBSD__) || defined(__DragonFly__)
++#if defined(__GLIBC__) || defined(__FreeBSD__) || defined(__DragonFly__) || defined(__NetBSD__)
      __attribute__((tls_model("initial-exec")))
  #endif
      ;
--- bucket_58/libglvnd/patches/patch-src_GLdispatch_vnd-glapi_u__current__tls.c
+++ bucket_58/libglvnd/patches/patch-src_GLdispatch_vnd-glapi_u__current__tls.c
@@ -5,7 +5,7 @@
  
  __thread const void *_glapi_tls_Current[GLAPI_NUM_CURRENT_ENTRIES]
 -#if defined(__GLIBC__) || defined(__FreeBSD__)
-+#if defined(__GLIBC__) || defined(__FreeBSD__) || defined(__DragonFly__)
++#if defined(__GLIBC__) || defined(__FreeBSD__) || defined(__DragonFly__) || defined(__NetBSD__)
      __attribute__((tls_model("initial-exec")))
  #endif
      = {
