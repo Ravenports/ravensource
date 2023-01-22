5b36b6e4de8f34e545259327b8299504200271d4
John Marino <draco@marino.st>
2023-01-22 16:05:04 +0000

    pkgconf: upgrade

--- bucket_F2/pkgconf/patches/patch-libpkgconf_bsdstubs.c
+++ bucket_F2/pkgconf/patches/patch-libpkgconf_bsdstubs.c
@@ -1,4 +1,4 @@
---- libpkgconf/bsdstubs.c.orig	2022-08-08 00:58:54 UTC
+--- libpkgconf/bsdstubs.c.orig	2023-01-20 21:41:30 UTC
 +++ libpkgconf/bsdstubs.c
 @@ -17,6 +17,9 @@
   * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
