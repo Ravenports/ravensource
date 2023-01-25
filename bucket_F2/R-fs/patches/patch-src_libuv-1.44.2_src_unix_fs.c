4f5080e0f4b9a8a67817c4185be68f2764cb15be
John Marino <draco@marino.st>
2023-01-25 06:41:45 +0000

    repatch R-fs

--- bucket_F2/R-fs/patches/patch-src_libuv-1.44.2_src_unix_fs.c
+++ bucket_F2/R-fs/patches/patch-src_libuv-1.44.2_src_unix_fs.c
@@ -1,14 +1,13 @@
---- src/libuv-1.38.1/src/unix/fs.c.orig	2021-11-29 22:54:06 UTC
-+++ src/libuv-1.38.1/src/unix/fs.c
-@@ -972,7 +972,6 @@ static ssize_t uv__fs_sendfile(uv_fs_t*
+--- src/libuv-1.44.2/src/unix/fs.c.orig	2022-05-25 13:21:41 UTC
++++ src/libuv-1.44.2/src/unix/fs.c
+@@ -1139,13 +1139,13 @@ static ssize_t uv__fs_sendfile(uv_fs_t*
  static ssize_t uv__fs_utime(uv_fs_t* req) {
  #if defined(__linux__)                                                         \
      || defined(_AIX71)                                                         \
 -    || defined(__sun)                                                          \
      || defined(__HAIKU__)
-   /* utimesat() has nanosecond resolution but we stick to microseconds
-    * for the sake of consistency with other platforms.
-@@ -982,6 +981,7 @@ static ssize_t uv__fs_utime(uv_fs_t* req
+   struct timespec ts[2];
+   ts[0] = uv__fs_to_timespec(req->atime);
    ts[1] = uv__fs_to_timespec(req->mtime);
    return utimensat(AT_FDCWD, req->path, ts, 0);
  #elif defined(__APPLE__)                                                      \
@@ -16,11 +15,11 @@
      || defined(__DragonFly__)                                                 \
      || defined(__FreeBSD__)                                                   \
      || defined(__FreeBSD_kernel__)                                            \
-@@ -1015,7 +1015,6 @@ static ssize_t uv__fs_utime(uv_fs_t* req
+@@ -1179,7 +1179,6 @@ static ssize_t uv__fs_utime(uv_fs_t* req
  static ssize_t uv__fs_lutime(uv_fs_t* req) {
  #if defined(__linux__)            ||                                           \
      defined(_AIX71)               ||                                           \
 -    defined(__sun)                ||                                           \
-     defined(__HAIKU__)
-   struct timespec ts[2];
-   ts[0] = uv__fs_to_timespec(req->atime);
+     defined(__HAIKU__)            ||                                           \
+     defined(__GNU__)              ||                                           \
+     defined(__OpenBSD__)
