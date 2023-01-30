d29ea671f3c8b52763d45f31f1e764420c7e8055
John Marino <draco@marino.st>
2023-01-30 02:26:49 +0000

    Complete rework of ncurses
    - split into primary, tools, dev, docs, man subpackages
    - Drop the double compile
    - Include terminfo tree-based database
    - Drop db5 hashed database
    - USES=terminfo, terminfo keyword to be dropped
    - Add support for port terminfo (the reason @terminfo can be dropped)
      (from FreeBSD)

---
+++ bucket_5C/ncurses/files/special.mk
@@ -0,0 +1,11 @@
+FILES_DOCS=\
+	ANNOUNCE \
+	NEWS \
+	README \
+	doc/hackguide.doc \
+	doc/ncurses-intro.doc \
+	doc/html/index.html \
+	doc/html/announce.html \
+	doc/html/hackguide.html \
+	doc/html/ncurses-intro.html \
+
