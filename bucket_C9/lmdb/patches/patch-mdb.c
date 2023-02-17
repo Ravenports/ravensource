f3143838bb79ae4c55a9f7783d0589395db3ed5c
John Marino <draco@marino.st>
2023-02-17 03:07:08 +0000

    lmdb: upgrade and split out

--- bucket_C9/lmdb/patches/patch-mdb.c
+++ bucket_C9/lmdb/patches/patch-mdb.c
@@ -1,6 +1,6 @@
---- mdb.c.orig	2021-02-04 20:46:33 UTC
+--- mdb.c.orig	2023-02-08 18:41:37 UTC
 +++ mdb.c
-@@ -132,6 +132,8 @@ typedef SSIZE_T	ssize_t;
+@@ -131,6 +131,8 @@ typedef SSIZE_T	ssize_t;
  # define MDB_FDATASYNC		fsync
  #elif defined(ANDROID)
  # define MDB_FDATASYNC		fsync
