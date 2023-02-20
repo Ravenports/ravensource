270b5e6bc2cc8ea45dda3db5178acdcce0b0c7a1
John Marino <draco@marino.st>
2023-02-20 22:42:46 +0000

    libnice: patch header to fix farstream build

---
+++ bucket_1C/libnice/patches/patch-agent_interfaces.h
@@ -0,0 +1,11 @@
+--- agent/interfaces.h.orig	2023-01-07 15:51:15 UTC
++++ agent/interfaces.h
+@@ -37,7 +37,7 @@
+  */
+ 
+ #include <glib.h>
+-#include <address.h>
++#include "address.h"
+ 
+ G_BEGIN_DECLS
+ 
