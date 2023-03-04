62a7ba75e20a3af006f96bf9c4aa99d6e8e4d0ea
John Marino <draco@marino.st>
2023-03-04 13:34:23 +0000

    replace ffmpeg with ffmpeg6

--- bucket_03/ffmpeg/patches/patch-libavformat_rtsp.c
+++ bucket_03/ffmpeg/patches/patch-libavformat_rtsp.c
@@ -1,6 +1,6 @@
---- libavformat/rtsp.c.orig	2022-07-22 17:58:39 UTC
+--- libavformat/rtsp.c.orig	2023-02-27 20:43:45 UTC
 +++ libavformat/rtsp.c
-@@ -1673,7 +1673,12 @@ int ff_rtsp_make_setup_request(AVFormatC
+@@ -1674,7 +1674,12 @@ int ff_rtsp_make_setup_request(AVFormatC
              }
              if (ttl > 0)
                  snprintf(optbuf, sizeof(optbuf), "?ttl=%d", ttl);
@@ -14,7 +14,7 @@
                          namebuf, sizeof(namebuf), NULL, 0, NI_NUMERICHOST);
              ff_url_join(url, sizeof(url), "rtp", NULL, namebuf,
                          port, "%s", optbuf);
-@@ -1907,8 +1912,13 @@ redirect:
+@@ -1908,8 +1913,13 @@ redirect:
          goto fail;
      }
      if (!getpeername(tcp_fd, (struct sockaddr*) &peer, &peer_len)) {
@@ -30,7 +30,7 @@
      }
  
      /* request options supported by the server; this also detects server
-@@ -2423,7 +2433,11 @@ static int sdp_read_header(AVFormatConte
+@@ -2424,7 +2434,11 @@ static int sdp_read_header(AVFormatConte
              const char *p;
  
              err = getnameinfo((struct sockaddr*) &rtsp_st->sdp_ip,
