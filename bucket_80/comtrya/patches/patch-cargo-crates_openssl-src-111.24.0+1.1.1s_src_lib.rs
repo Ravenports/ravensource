14498c08cfbd12312ba09726d21b1d29748780e3
John Marino <draco@marino.st>
2023-03-25 08:29:22 -0500

    comtrya: always use gmake for openssl (fixes linux build)

--- bucket_80/comtrya/patches/patch-cargo-crates_openssl-src-111.24.0+1.1.1s_src_lib.rs
+++ bucket_80/comtrya/patches/patch-cargo-crates_openssl-src-111.24.0+1.1.1s_src_lib.rs
@@ -1,10 +1,21 @@
 --- cargo-crates/openssl-src-111.24.0+1.1.1s/src/lib.rs.orig	2006-07-24 01:21:28 UTC
 +++ cargo-crates/openssl-src-111.24.0+1.1.1s/src/lib.rs
-@@ -56,6 +56,7 @@ impl Build {
-         if host.contains("dragonfly")
-             || host.contains("freebsd")
-             || host.contains("openbsd")
-+            || host.contains("netbsd")
-             || host.contains("solaris")
-             || host.contains("illumos")
-         {
+@@ -52,17 +52,7 @@ impl Build {
+     }
+ 
+     fn cmd_make(&self) -> Command {
+-        let host = &self.host.as_ref().expect("HOST dir not set")[..];
+-        if host.contains("dragonfly")
+-            || host.contains("freebsd")
+-            || host.contains("openbsd")
+-            || host.contains("solaris")
+-            || host.contains("illumos")
+-        {
+-            Command::new("gmake")
+-        } else {
+-            Command::new("make")
+-        }
++        Command::new("gmake")
+     }
+ 
+     #[cfg(windows)]
