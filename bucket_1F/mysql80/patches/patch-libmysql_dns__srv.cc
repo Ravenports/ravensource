7f1a992d49d01a86b2c20b7f0d1ad62dd77be6ce
John Marino <draco@marino.st>
2023-03-31 19:30:58 +0200

    mysql80: support mbsd

--- bucket_1F/mysql80/patches/patch-libmysql_dns__srv.cc
+++ bucket_1F/mysql80/patches/patch-libmysql_dns__srv.cc
@@ -5,7 +5,7 @@
  #ifdef HAVE_UNIX_DNS_SRV
  
 -#ifdef __FreeBSD__
-+#if defined __FreeBSD__ || defined __DragonFly__ || defined __NetBSD__
++#if defined __FreeBSD__ || defined __DragonFly__ || defined __NetBSD__ || defined __MidnightBSD__
  #include <netinet/in.h>
  #endif
  #include <netdb.h>
