989eca6a47e7c53bae6ad3401908afbbb38c2696
John Marino <draco@marino.st>
2023-03-31 22:47:09 +0200

    kf5-extra-cmakes-modules: recognize MidnightBSD

--- bucket_85/kf5-extra-cmake-modules/patches/patch-kde-modules_KDEMetaInfoPlatformCheck.cmake
+++ bucket_85/kf5-extra-cmake-modules/patches/patch-kde-modules_KDEMetaInfoPlatformCheck.cmake
@@ -1,6 +1,6 @@
 --- kde-modules/KDEMetaInfoPlatformCheck.cmake.orig	2023-01-12 21:43:20 UTC
 +++ kde-modules/KDEMetaInfoPlatformCheck.cmake
-@@ -46,6 +46,14 @@ if (NOT "${KF_IGNORE_PLATFORM_CHECK}")
+@@ -46,6 +46,18 @@ if (NOT "${KF_IGNORE_PLATFORM_CHECK}")
                  if (CMAKE_SYSTEM_NAME MATCHES "OpenBSD")
                      set(_MetainfoFoundSupportedPlatform true)
                  endif()
@@ -10,6 +10,10 @@
 +                endif()
 +            elseif (${CMAKE_MATCH_1} STREQUAL "NetBSD")
 +                if (CMAKE_SYSTEM_NAME MATCHES "NetBSD")
++                    set(_MetainfoFoundSupportedPlatform true)
++                endif()
++            elseif (${CMAKE_MATCH_1} STREQUAL "MidnightBSD")
++                if (CMAKE_SYSTEM_NAME MATCHES "MidnightBSD")
 +                    set(_MetainfoFoundSupportedPlatform true)
 +                endif()
              elseif (${CMAKE_MATCH_1} STREQUAL "Windows")
