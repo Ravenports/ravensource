f4ab77c6a95bb2c91198b25db3eb14b98d08cc56
John Marino <draco@marino.st>
2023-04-01 00:53:43 +0200

    xorg-server: add mbsd support

---
+++ bucket_E7/xorg-server/patches/patch-hw_xfree86_os-support_xf86__OSlib.h
@@ -0,0 +1,11 @@
+--- hw/xfree86/os-support/xf86_OSlib.h.orig	2023-02-07 01:16:51 UTC
++++ hw/xfree86/os-support/xf86_OSlib.h
+@@ -241,7 +241,7 @@ extern _X_HIDDEN char xf86SolarisFbDev[P
+ #ifdef SYSCONS_SUPPORT
+ #define COMPAT_SYSCONS
+ #if defined(__FreeBSD__) || defined(__FreeBSD_kernel__) || defined(__DragonFly__)
+-#if defined(__DragonFly__)  || (__FreeBSD_kernel_version >= 410000)
++#if defined(__DragonFly__)  || (__FreeBSD_kernel_version >= 410000) || defined(__MidnightBSD__)
+ #include <sys/consio.h>
+ #include <sys/kbio.h>
+ #else
