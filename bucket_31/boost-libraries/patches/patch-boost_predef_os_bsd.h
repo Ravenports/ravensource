02fc59c87a0391da4241d30c412355a749fb5642
John Marino <draco@marino.st>
2023-03-31 04:58:57 +0200

    boost: support mbsd

---
+++ bucket_31/boost-libraries/patches/patch-boost_predef_os_bsd.h
@@ -0,0 +1,26 @@
+--- boost/predef/os/bsd.h.orig	2023-03-16 13:57:31 UTC
++++ boost/predef/os/bsd.h
+@@ -29,6 +29,7 @@ version number macro to match:
+ 
+ * `BOOST_OS_BSD_DRAGONFLY` http://en.wikipedia.org/wiki/DragonFly_BSD[DragonFly BSD]
+ * `BOOST_OS_BSD_FREE` http://en.wikipedia.org/wiki/Freebsd[FreeBSD]
++* `BOOST_OS_BSD_MIDNIGHT` http://en.wikipedia.org/wiki/Midnightbsd[MidnightBSD]
+ * `BOOST_OS_BSD_BSDI` http://en.wikipedia.org/wiki/BSD/OS[BSDi BSD/OS]
+ * `BOOST_OS_BSD_NET` http://en.wikipedia.org/wiki/Netbsd[NetBSD]
+ * `BOOST_OS_BSD_OPEN` http://en.wikipedia.org/wiki/Openbsd[OpenBSD]
+@@ -53,6 +54,7 @@ of BSD. If the above variants is detecte
+ #include <boost/predef/os/bsd/bsdi.h>
+ #include <boost/predef/os/bsd/dragonfly.h>
+ #include <boost/predef/os/bsd/free.h>
++#include <boost/predef/os/bsd/midnight.h>
+ #include <boost/predef/os/bsd/open.h>
+ #include <boost/predef/os/bsd/net.h>
+ 
+@@ -95,6 +97,7 @@ of BSD. If the above variants is detecte
+ #include <boost/predef/os/bsd/bsdi.h>
+ #include <boost/predef/os/bsd/dragonfly.h>
+ #include <boost/predef/os/bsd/free.h>
++#include <boost/predef/os/bsd/midnight.h>
+ #include <boost/predef/os/bsd/open.h>
+ #include <boost/predef/os/bsd/net.h>
+ 
---
+++ bucket_31/boost-libraries/patches/patch-boost_predef_os_bsd_midnight.h
@@ -0,0 +1,62 @@
+--- boost/predef/os/bsd/midnight.h.orig	2023-03-16 13:57:11 UTC
++++ boost/predef/os/bsd/midnight.h
+@@ -0,0 +1,59 @@
++/*
++Copyright Rene Rivera 2012-2015
++Distributed under the Boost Software License, Version 1.0.
++(See accompanying file LICENSE_1_0.txt or copy at
++http://www.boost.org/LICENSE_1_0.txt)
++*/
++
++#ifndef BOOST_PREDEF_OS_BSD_FREE_H
++#define BOOST_PREDEF_OS_BSD_FREE_H
++
++#include <boost/predef/os/bsd.h>
++
++/* tag::reference[]
++= `BOOST_OS_BSD_MIDNIGHT`
++
++http://en.wikipedia.org/wiki/Midnightbsd[MidnightBSD] operating system.
++
++[options="header"]
++|===
++| {predef_symbol} | {predef_version}
++
++| `+__MidnightBSD__+` | {predef_detection}
++
++| `+__MidnightBSD_version+` | V.R.P
++|===
++*/ // end::reference[]
++
++#define BOOST_OS_BSD_MIDNIGHT BOOST_VERSION_NUMBER_NOT_AVAILABLE
++
++#if !defined(BOOST_PREDEF_DETAIL_OS_DETECTED) && ( \
++    defined(__MidnightBSD__) \
++    )
++#   ifndef BOOST_OS_BSD_AVAILABLE
++#       undef BOOST_OS_BSD
++#       define BOOST_OS_BSD BOOST_VERSION_NUMBER_AVAILABLE
++#       define BOOST_OS_BSD_AVAILABLE
++#   endif
++#   undef BOOST_OS_BSD_FREE
++#   include <sys/param.h>
++#   if defined(__MidnightBSD_version)
++#           define BOOST_OS_BSD_MIDNIGHT \
++                BOOST_PREDEF_MAKE_10_VVRRPPP(__MidnightBSD_version)
++
++#   else
++#       define BOOST_OS_BSD_MIDNIGHT BOOST_VERSION_NUMBER_AVAILABLE
++#   endif
++#endif
++
++#if BOOST_OS_BSD_MIDNIGHT
++#   define BOOST_OS_BSD_MIDNIGHT_AVAILABLE
++#   include <boost/predef/detail/os_detected.h>
++#endif
++
++#define BOOST_OS_BSD_MIDNIGHT_NAME "Midnight BSD"
++
++#endif
++
++#include <boost/predef/detail/test.h>
++BOOST_PREDEF_DECLARE_TEST(BOOST_OS_BSD_MIDNIGHT,BOOST_OS_BSD_MIDNIGHT_NAME)
