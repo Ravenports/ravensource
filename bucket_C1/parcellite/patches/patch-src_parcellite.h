e63955088a6a2828ade23571b7a905bfd399eb7f
John Marino <draco@marino.st>
2023-04-05 04:45:35 +0000

    parcellite: upgrade, split out man page

---
+++ bucket_C1/parcellite/patches/patch-src_parcellite.h
@@ -0,0 +1,11 @@
+--- src/parcellite.h.orig	2023-03-11 19:15:01 UTC
++++ src/parcellite.h
+@@ -22,7 +22,7 @@
+ #include <gtk/gtk.h>
+ #include <string.h>
+ #include <unistd.h>
+-#include <config.simple.h>
++#include <config.h>
+ #include <stdlib.h>
+ #define PARCELLITE_PROG_NAME "parcellite"
+ #define PARCELLITE_ICON PARCELLITE_PROG_NAME
