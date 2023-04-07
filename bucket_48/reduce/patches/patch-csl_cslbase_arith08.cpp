bb501c4f8244bb56295df8c7af65b532ca0bc9d8
John Marino <draco@marino.st>
2023-04-07 15:26:36 +0000

    reduce: upgrade

--- bucket_48/reduce/patches/patch-csl_cslbase_arith08.cpp
+++ bucket_48/reduce/patches/patch-csl_cslbase_arith08.cpp
@@ -1,6 +1,6 @@
---- csl/cslbase/arith08.cpp.orig	2022-01-20 22:10:59 UTC
+--- csl/cslbase/arith08.cpp.orig	2023-01-02 21:45:46 UTC
 +++ csl/cslbase/arith08.cpp
-@@ -847,13 +847,13 @@ inline int nlz(uint64_t x)
+@@ -880,13 +880,13 @@ inline int nlz(uint64_t x)
  #ifndef POPCOUNT_DEFINED
  #ifdef __GNUC__
  
@@ -16,14 +16,14 @@
  {   x = (x & 0x5555555555555555U) + (x >> 1 & 0x5555555555555555U);
      x = (x & 0x3333333333333333U) + (x >> 2 & 0x3333333333333333U);
      x = x + (x >> 4) & 0x0f0f0f0f0f0f0f0fU;
-@@ -868,19 +868,19 @@ inline int popcount(uint64_t x)
- static LispObject Llogcount(LispObject env, LispObject a)
- {   if (is_fixnum(a))
+@@ -902,19 +902,19 @@ static LispObject Llogcount(LispObject e
+ {   SingleValued fn;
+     if (is_fixnum(a))
      {   intptr_t n = int_of_fixnum(a);
--        if (a >= 0) return onevalue(fixnum_of_int(popcount(n)));
--        else return onevalue(fixnum_of_int(popcount(~n)));
-+        if (a >= 0) return onevalue(fixnum_of_int(my_popcount(n)));
-+        else return onevalue(fixnum_of_int(my_popcount(~n)));
+-        if (a >= 0) return fixnum_of_int(popcount(n));
+-        else return fixnum_of_int(popcount(~n));
++        if (a >= 0) return fixnum_of_int(my_popcount(n));
++        else return fixnum_of_int(my_popcount(~n));
      }
      else if (is_bignum(a))
      {   size_t len = (length_of_header(numhdr(a)) - CELL)/4;
@@ -38,5 +38,5 @@
 -                n += popcount(bignum_digits(a)[i]);
 +                n += my_popcount(bignum_digits(a)[i]);
          }
-         return onevalue(fixnum_of_int(n));
+         return fixnum_of_int(n);
      }
