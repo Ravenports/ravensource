0df4d05edb6602380101a6ae51bfdd94721fe9d7
John Marino <draco@marino.st>
2023-04-08 17:13:16 +0000

    rarian: upgrade

--- bucket_64/rarian/patches/patch-librarian_rarian-main.c
+++ bucket_64/rarian/patches/patch-librarian_rarian-main.c
@@ -1,11 +1,11 @@
---- librarian/rarian-main.c.orig	2008-09-01 18:40:21 UTC
+--- librarian/rarian-main.c.orig	2023-02-22 02:43:03 UTC
 +++ librarian/rarian-main.c
-@@ -215,7 +215,7 @@ past:
-     path = getenv ("XDG_DATA_DIRS");
+@@ -207,7 +207,7 @@ past:
+     path = getenv("XDG_DATA_DIRS");
  
-     if (!path || !strcmp (path, "")) {
+     if (!path || !strcmp(path, "")) {
 -        path = "/usr/local/share/:/usr/share/";
 +        path = "/usr/local/share/";
      }
-     cur_path = path;
+     scan_dirs = rrn_str_split(path, ':');
  
