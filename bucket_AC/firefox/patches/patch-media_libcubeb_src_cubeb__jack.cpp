c6499f9b3a0f8397f1cdaf3bcba033cefef2173b
John Marino <draco@marino.st>
2023-04-22 17:59:00 +0000

    firefox: upgrade

--- bucket_AC/firefox/patches/patch-media_libcubeb_src_cubeb__jack.cpp
+++ bucket_AC/firefox/patches/patch-media_libcubeb_src_cubeb__jack.cpp
@@ -1,11 +1,11 @@
---- media/libcubeb/src/cubeb_jack.cpp.orig	2020-04-29 21:00:35 UTC
+--- media/libcubeb/src/cubeb_jack.cpp.orig	2023-04-14 16:28:02 UTC
 +++ media/libcubeb/src/cubeb_jack.cpp
 @@ -8,7 +8,7 @@
   */
  #define _DEFAULT_SOURCE
  #define _BSD_SOURCE
--#ifndef __FreeBSD__
-+#if !(defined __FreeBSD__ || defined __DragonFly__)
+-#if !defined(__FreeBSD__) && !defined(__NetBSD__)
++#if !defined(__FreeBSD__) && !defined(__NetBSD__) && !defined(__DragonFly__)
  #define _POSIX_SOURCE
  #endif
- #include <dlfcn.h>
+ #include "cubeb-internal.h"
