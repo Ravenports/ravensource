7ecce214730a5a48a78f6eeec4197da0050ba60f
John Marino <draco@marino.st>
2023-04-23 20:16:32 +0200

    sbcl: add midnightbsd support

--- bucket_FA/sbcl/patches/patch-make-config.sh
+++ bucket_FA/sbcl/patches/patch-make-config.sh
@@ -1,7 +1,16 @@
 Linux LARGEFILE option does not seem to build.
 
---- make-config.sh.orig	2022-10-30 19:42:45 UTC
+--- make-config.sh.orig	2023-03-28 19:42:52 UTC
 +++ make-config.sh
+@@ -286,7 +286,7 @@ case `uname` in
+         ;;
+     *BSD)
+         case `uname` in
+-            FreeBSD)
++            FreeBSD|MidnightBSD)
+                 sbcl_os="freebsd"
+                 ;;
+             GNU/kFreeBSD)
 @@ -523,7 +523,6 @@ case "$sbcl_os" in
          # src/runtime/Config.foo-linux too.
          case "$sbcl_arch" in
