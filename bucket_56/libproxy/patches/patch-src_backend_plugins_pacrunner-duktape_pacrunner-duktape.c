cec992249804888584e9eac0d6648c13f4a9245c
John Marino <draco@marino.st>
2023-05-27 12:36:17 +0000

    upgrade libproxy, file moved

--- bucket_56/libproxy/patches/patch-src_backend_plugins_pacrunner-duktape_pacrunner-duktape.c
+++ bucket_56/libproxy/patches/patch-src_backend_plugins_pacrunner-duktape_pacrunner-duktape.c
@@ -1,7 +1,7 @@
---- src/backend/plugins/pacrunner-duktape/pacrunner-duktape.c.orig	2023-05-15 13:09:42 UTC
+--- src/backend/plugins/pacrunner-duktape/pacrunner-duktape.c.orig	2023-05-24 06:14:23 UTC
 +++ src/backend/plugins/pacrunner-duktape/pacrunner-duktape.c
-@@ -28,6 +28,10 @@
- #include <netdb.h>
+@@ -29,6 +29,10 @@
+ #include <netinet/in.h>
  #endif
  
 +#if defined(__FreeBSD__) || defined(__DragonFly__) || defined(__NetBSD__)
