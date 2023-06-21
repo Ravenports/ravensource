e408fe8e9e386d48b810e62960c2730823654d09
John Marino <draco@marino.st>
2023-06-21 04:37:02 +0000

    skia: upgrade and split

---
+++ bucket_90/skia/patches/patch-third__party_externals_piex_src_image__type__recognition_image__type__recognition__lite.cc
@@ -0,0 +1,10 @@
+--- third_party/externals/piex/src/image_type_recognition/image_type_recognition_lite.cc.orig	2022-02-17 16:43:38 UTC
++++ third_party/externals/piex/src/image_type_recognition/image_type_recognition_lite.cc
+@@ -26,6 +26,7 @@
+ 
+ #include <algorithm>
+ #include <cassert>
++#include <cstring>
+ #include <string>
+ #include <vector>
+ 
