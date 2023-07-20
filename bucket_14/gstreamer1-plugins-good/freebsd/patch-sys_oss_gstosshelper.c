1d58058f2bb06c61e9a2c7d68c8f1234672d702b
John Marino <draco@marino.st>
2023-07-20 18:07:33 +0000

    upgrade gstreamer

--- bucket_14/gstreamer1-plugins-good/freebsd/patch-sys_oss_gstosshelper.c
+++ bucket_14/gstreamer1-plugins-good/freebsd/patch-sys_oss_gstosshelper.c
@@ -1,4 +1,4 @@
---- sys/oss/gstosshelper.c.orig	2023-05-19 08:23:19 UTC
+--- sys/oss/gstosshelper.c.orig	2023-07-20 14:22:48 UTC
 +++ sys/oss/gstosshelper.c
 @@ -82,9 +82,9 @@ GstCaps *
  gst_oss_helper_probe_caps (gint fd)
