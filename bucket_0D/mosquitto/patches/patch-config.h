64e35741c0dda3addec9153ca549f44ea87553c2
John Marino <draco@marino.st>
2023-07-24 00:21:05 +0000

    mosquitto: fix build on netbsd

--- bucket_0D/mosquitto/patches/patch-config.h
+++ bucket_0D/mosquitto/patches/patch-config.h
@@ -1,11 +1,18 @@
 --- config.h.orig	2022-08-16 13:34:02 UTC
 +++ config.h
-@@ -6,7 +6,7 @@
+@@ -6,10 +6,14 @@
  
  #ifdef __APPLE__
  #  define __DARWIN_C_SOURCE
 -#elif defined(__FreeBSD__) || defined(__NetBSD__) || defined(__SYMBIAN32__)
-+#elif defined(__FreeBSD__) || defined(__NetBSD__) || defined(__SYMBIAN32__) || defined(__DragonFly__)
++#elif defined(__FreeBSD__) || defined(__SYMBIAN32__) || defined(__DragonFly__)
  #  define _XOPEN_SOURCE 700
  #  define __BSD_VISIBLE 1
  #  define HAVE_NETINET_IN_H
++#elif defined(__NetBSD__)
++#  define _XOPEN_SOURCE 700
++#  define _NETBSD_SOURCE 1
++#  define HAVE_NETINET_IN_H
+ #elif defined(__QNX__)
+ #  define _XOPEN_SOURCE 600
+ #  define __BSD_VISIBLE 1
