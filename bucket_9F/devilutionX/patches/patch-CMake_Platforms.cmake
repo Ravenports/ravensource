79ffb3c06e0c0952177f9b750c6076e04461a1b1
John Marino <draco@marino.st>
2023-07-25 12:08:21 +0000

    devilutionX: fix build on netbsd

--- bucket_9F/devilutionX/patches/patch-CMake_Platforms.cmake
+++ bucket_9F/devilutionX/patches/patch-CMake_Platforms.cmake
@@ -1,6 +1,6 @@
---- CMake/Platforms.cmake.orig	2023-06-18 17:54:38 UTC
+--- CMake/Platforms.cmake.orig	2023-07-25 11:59:49 UTC
 +++ CMake/Platforms.cmake
-@@ -6,7 +6,7 @@ if(HAIKU)
+@@ -6,14 +6,14 @@ if(HAIKU)
    include(platforms/haiku)
  endif()
  
@@ -9,3 +9,11 @@
    if(CMAKE_SYSTEM_NAME MATCHES "NetBSD")
      add_definitions(-D_NETBSD_SOURCE)
    else()
+-    add_definitions(-D_BSD_SOURCE)
+     set(UBSAN OFF)
+   endif()
+   set(ASAN OFF)
++  add_definitions(-D_BSD_SOURCE)
+   add_definitions(-DO_LARGEFILE=0 -Dstat64=stat -Dlstat64=lstat -Dlseek64=lseek -Doff64_t=off_t -Dfstat64=fstat -Dftruncate64=ftruncate)
+ endif()
+ 
