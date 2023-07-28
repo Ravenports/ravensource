b5da9b509961b32a111972802926bbb0c32d2026
John Marino <draco@marino.st>
2023-07-28 11:26:21 -0500

    qt6-qtbase: fix manifest for linux

--- bucket_49/qt6-qtbase/files/special.mk
+++ bucket_49/qt6-qtbase/files/special.mk
@@ -3,3 +3,11 @@ PLIST_SUB+=EVDEV-OFF="@comment "
 .else
 PLIST_SUB+=EVDEV-OFF=
 .endif
+
+.if ${OPSYS} == Linux
+PLIST_SUB+=FSWTYPE=inotify
+PLIST_SUB+=DSSTYPE=udev
+.else
+PLIST_SUB+=FSWTYPE=kqueue
+PLIST_SUB+=DSSTYPE=static
+.endif
