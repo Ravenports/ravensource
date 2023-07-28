25abdb556e06f0dc667d2d00c1b25f85df59b142
John Marino <draco@marino.st>
2023-07-28 16:38:44 +0000

    bring back mesa gallium drivers on netbsd

---
+++ bucket_F0/mesa/patches/patch-src_gallium_drivers_llvmpipe_lp__scene.c
@@ -0,0 +1,13 @@
+--- src/gallium/drivers/llvmpipe/lp_scene.c.orig	2023-07-21 12:42:42 UTC
++++ src/gallium/drivers/llvmpipe/lp_scene.c
+@@ -25,6 +25,10 @@
+  *
+  **************************************************************************/
+ 
++#ifdef __NetBSD__
++#define _OPENBSD_SOURCE
++#include <stdlib.h> /* for reallocarray */
++#endif
+ #include "util/u_framebuffer.h"
+ #include "util/u_math.h"
+ #include "util/u_memory.h"
