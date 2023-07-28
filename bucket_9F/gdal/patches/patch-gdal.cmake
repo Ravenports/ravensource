4ed007618024b3a5ea251daf141e28d8aa70fd19
John Marino <draco@marino.st>
2023-07-28 02:39:40 +0000

    gdal: fix build on dragonfly

--- bucket_9F/gdal/patches/patch-gdal.cmake
+++ bucket_9F/gdal/patches/patch-gdal.cmake
@@ -5,7 +5,7 @@
    include(CheckLinkerFlag)
    check_linker_flag(C "-Wl,--no-undefined" HAS_NO_UNDEFINED)
 -  if (HAS_NO_UNDEFINED AND (NOT "${CMAKE_CXX_FLAGS}" MATCHES "-fsanitize") AND NOT CMAKE_SYSTEM_NAME MATCHES "OpenBSD")
-+  if (HAS_NO_UNDEFINED AND (NOT "${CMAKE_CXX_FLAGS}" MATCHES "-fsanitize") AND NOT CMAKE_SYSTEM_NAME MATCHES "OpenBSD|MidnightBSD|FreeBSD")
++  if (HAS_NO_UNDEFINED AND (NOT "${CMAKE_CXX_FLAGS}" MATCHES "-fsanitize") AND NOT CMAKE_SYSTEM_NAME MATCHES "OpenBSD|MidnightBSD|FreeBSD|DragonFly")
      string(APPEND CMAKE_SHARED_LINKER_FLAGS " -Wl,--no-undefined")
      string(APPEND CMAKE_MODULE_LINKER_FLAGS " -Wl,--no-undefined")
    endif ()
