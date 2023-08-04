6d573b216fba039454e8867a39f515567aaa32b5
Michael Reim <kraileth@elderlinux.org>
2023-08-04 08:02:50 +0200

    texlive-bin: upgrade

--- bucket_B8/texlive-bin/files/special.mk
+++ bucket_B8/texlive-bin/files/special.mk
@@ -12,20 +12,23 @@ PROGRAMS0=	afm2pl afm2tfm aleph autosp axohelp bbox bg5conv \
 		epsffit eptex euptex extconv gftodvi gftopk gftype \
 		gregorio gsftopk
 
-PROGRAMS1=	hbf2gf kpseaccess kpsereadlink kpsestat kpsewhich \
-		lacheck luajittex luatex mag makeindex makejvf \
-		mendex mf mf-nowin mflua mfluajit mft mmafm mmpfb \
-		mpost msxlint odvicopy odvitype omfonts otangle \
-		otfinfo otftotfm otp2ocp outocp patgen pbibtex pdfclose \
-		pdfopen pdftex pdftosrc pdvitype pfb2pfa pk2bm pktogf \
-		pktype pltotf pmpost pmxab pooltype ppltotf prepmx ps2pk \
-		psbook psnup psresize psselect pstops ptex ptftopl
+PROGRAMS1=	hbf2gf hishrink histretch hitex kpseaccess kpsereadlink \
+		kpsestat kpsewhich lacheck luajittex luatex mag \
+		makeindex makejvf mendex mf mf-nowin mflua mfluajit mft \
+		mmafm mmpfb mpost msxlint odvicopy odvitype omfonts \
+		otangle otfinfo otftotfm otp2ocp outocp patgen pbibtex \
+		pdfclose pdfopen pdftex pdftosrc pdvitype pfb2pfa pk2bm \
+		pktogf pktype pltotf pmpost pmxab pooltype ppltotf \
+		prepmx ps2pk psbook psnup psresize psselect pstops
 
-PROGRAMS2=	scor2prt sjisconv synctex t1ascii t1asm t1binary \
-		t1disasm t1dotlessj t1lint t1mac t1rawafm t1reencode \
-		t1testpage t1unmac t4ht tangle teckit_compile tex \
-		tex2aspc tex2xindy tex4ht tftopl tie ttf2afm ttf2pk \
-		ttf2tfm ttfdump ttftotype42 updvitype upbibtex upmendex \
-		upmpost uppltotf uptex uptftopl vftovp vlna vptovf \
-		weave wofm2opl wopl2ofm wovf2ovp wovp2ovf xdvi-xaw \
-		xdvipdfmx xetex luahbtex luajithbtex xml2pmx
+PROGRAMS2=	ptex ptftopl scor2prt sjisconv synctex t1ascii t1asm \
+		t1binary t1disasm t1dotlessj t1lint t1mac t1rawafm \
+		t1reencode t1testpage t1unmac t4ht tangle teckit_compile \
+		tex tex2aspc tex2xindy tex4ht tftopl tie ttf2afm ttf2pk \
+		ttf2tfm ttfdump ttftotype42 twill updvitype upbibtex \
+		upmendex upmpost uppltotf uptex uptftopl vftovp vlna \
+		vptovf weave wofm2opl wopl2ofm wovf2ovp wovp2ovf \
+		xdvi-xaw xdvipdfmx xetex luahbtex luajithbtex xml2pmx
+
+STUBS=		context contextjit luatools mtxrun mtxrunjit texexec \
+		texmfstart
