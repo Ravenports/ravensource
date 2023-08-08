e9b346f740e79f8880c527c1df94a4b140bbed50
John Marino <draco@marino.st>
2023-08-07 22:34:06 -0500

    liborcus17: add missing headers

---
+++ bucket_88/liborcus17/patches/patch-include_orcus_base64.hpp
@@ -0,0 +1,10 @@
+--- include/orcus/base64.hpp.orig	2021-12-16 01:11:41 UTC
++++ include/orcus/base64.hpp
+@@ -11,6 +11,7 @@
+ #include "env.hpp"
+ #include <vector>
+ #include <string>
++#include <cstdint>
+ 
+ namespace orcus {
+ 
