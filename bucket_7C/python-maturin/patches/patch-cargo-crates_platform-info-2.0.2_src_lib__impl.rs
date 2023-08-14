4bcf24a8b75b2a92b6241554ab6c549b1271897a
John Marino <draco@marino.st>
2023-08-14 00:20:23 +0000

    maturin: upgrade

---
+++ bucket_7C/python-maturin/patches/patch-cargo-crates_platform-info-2.0.2_src_lib__impl.rs
@@ -0,0 +1,11 @@
+--- cargo-crates/platform-info-2.0.2/src/lib_impl.rs.orig	2006-07-24 01:21:28 UTC
++++ cargo-crates/platform-info-2.0.2/src/lib_impl.rs
+@@ -46,6 +46,8 @@ const HOST_OS_NAME: &str = if cfg!(all(
+     "NetBSD"
+ } else if cfg!(target_os = "openbsd") {
+     "OpenBSD"
++} else if cfg!(target_os = "dragonfly") {
++    "DragonFly"
+ } else if cfg!(target_vendor = "apple") {
+     "Darwin"
+ } else if cfg!(target_os = "fuchsia") {
--- bucket_7C/python-maturin/patches/patch-src_python__interpreter_get__interpreter__metadata.py
+++ bucket_7C/python-maturin/patches/patch-src_python__interpreter_get__interpreter__metadata.py
@@ -1,4 +1,4 @@
---- src/python_interpreter/get_interpreter_metadata.py.orig	2023-06-10 12:17:57 UTC
+--- src/python_interpreter/get_interpreter_metadata.py.orig	2023-08-06 08:59:35 UTC
 +++ src/python_interpreter/get_interpreter_metadata.py
 @@ -19,6 +19,13 @@ if platform.python_implementation() == "
  else:
