1f7e6c21e9759758a503b3d01a262e8405afb4fa
John Marino <draco@marino.st>
2023-08-17 12:13:54 +0000

    abseil-cpp: fix build on netbsd

--- bucket_47/abseil-cpp/patches/patch-absl_base_internal_sysinfo.cc
+++ bucket_47/abseil-cpp/patches/patch-absl_base_internal_sysinfo.cc
@@ -1,6 +1,6 @@
 --- absl/base/internal/sysinfo.cc.orig	2023-08-07 18:40:00 UTC
 +++ absl/base/internal/sysinfo.cc
-@@ -26,11 +26,11 @@
+@@ -26,11 +26,15 @@
  #include <unistd.h>
  #endif
  
@@ -10,11 +10,15 @@
  #endif
  
 -#if defined(__APPLE__) || defined(__FreeBSD__)
++#if defined(__NetBSD__)
++#include <lwp.h> // For _lwp_self()
++#endif
++
 +#if defined(__APPLE__) || defined(__FreeBSD__) || defined(__DragonFly__)
  #include <sys/sysctl.h>
  #endif
  
-@@ -432,6 +432,12 @@ pid_t GetTID() {
+@@ -432,6 +436,18 @@ pid_t GetTID() {
    return static_cast<pid_t>(tid);
  }
  
@@ -22,6 +26,12 @@
 +
 +pid_t GetTID() {
 +  return syscall(SYS_lwp_gettid);
++}
++
++#elif defined(__NetBSD__)
++
++pid_t GetTID() {
++  return static_cast<pid_t>(_lwp_self());
 +}
 +
  #elif defined(__native_client__)
