02376327c932449b0ee2f8c7101e6256110b6bcf
John Marino <draco@marino.st>
2023-08-20 05:07:25 +0200

    opencollada: add #include <cstdint>

---
+++ bucket_D1/opencollada/patches/patch-DAEValidator_library_src_DaeValidator.cpp
@@ -0,0 +1,10 @@
+--- DAEValidator/library/src/DaeValidator.cpp.orig	2018-11-26 22:43:10 UTC
++++ DAEValidator/library/src/DaeValidator.cpp
+@@ -4,6 +4,7 @@
+ #include "Strings.h"
+ #include "StringUtil.h"
+ #include <cmath>
++#include <cstdint>
+ #include "no_warning_iomanip"
+ #include "no_warning_iostream"
+ #include <set>
