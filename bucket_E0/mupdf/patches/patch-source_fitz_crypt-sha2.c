dcb3cbaebde2bfe37dc38efa0d611a223bef77db
John Marino <draco@marino.st>
2023-08-22 23:57:57 +0000

    mupdf: upgrade

--- bucket_E0/mupdf/patches/patch-source_fitz_crypt-sha2.c
+++ bucket_E0/mupdf/patches/patch-source_fitz_crypt-sha2.c
@@ -2,7 +2,7 @@ $NetBSD: patch-ae,v 1.5 2016/02/04 11:50:24 leot Exp $
 
 Avoid namespace conflicts with bswap(3).
 
---- source/fitz/crypt-sha2.c.orig	2023-06-16 14:26:38 UTC
+--- source/fitz/crypt-sha2.c.orig	2023-08-22 13:54:15 UTC
 +++ source/fitz/crypt-sha2.c
 @@ -19,7 +19,7 @@ static inline int isbigendian(void)
  	return *(char*)&one == 0;
