2ff3f672b25c8ea81aaa60dba7a46a71ceaa2ca6
John Marino <draco@marino.st>
2023-08-24 03:42:09 +0000

    minizip: follow zlib upgrade

--- bucket_C3/minizip/patches/patch-contrib_minizip_ioapi.h
+++ bucket_C3/minizip/patches/patch-contrib_minizip_ioapi.h
@@ -1,11 +1,11 @@
---- ioapi.h.orig	2022-10-10 09:39:33 UTC
+--- ioapi.h.orig	2023-08-03 18:17:07 UTC
 +++ ioapi.h
 @@ -50,7 +50,7 @@
  #define ftello64 ftell
  #define fseeko64 fseek
  #else
--#if defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__NetBSD__)
-+#if defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__NetBSD__) || defined(__DragonFly__)
+-#if defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__NetBSD__) || defined(__HAIKU__) || defined(MINIZIP_FOPEN_NO_64)
++#if defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__NetBSD__) || defined(__DragonFly__) || defined(__HAIKU__) || defined(MINIZIP_FOPEN_NO_64)
  #define fopen64 fopen
  #define ftello64 ftello
  #define fseeko64 fseeko
