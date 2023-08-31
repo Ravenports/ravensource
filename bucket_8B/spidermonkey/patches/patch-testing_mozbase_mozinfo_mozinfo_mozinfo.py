179d4f1eb0092e15f74781ccac7753acb1f4dd0d
John Marino <draco@marino.st>
2023-08-31 16:44:45 +0000

    spidermonkey: upgrade

--- bucket_8B/spidermonkey/patches/patch-testing_mozbase_mozinfo_mozinfo_mozinfo.py
+++ bucket_8B/spidermonkey/patches/patch-testing_mozbase_mozinfo_mozinfo_mozinfo.py
@@ -1,4 +1,4 @@
---- ../../testing/mozbase/mozinfo/mozinfo/mozinfo.py.orig	2023-07-24 16:54:16 UTC
+--- ../../testing/mozbase/mozinfo/mozinfo/mozinfo.py.orig	2023-08-21 13:43:29 UTC
 +++ ../../testing/mozbase/mozinfo/mozinfo/mozinfo.py
 @@ -137,7 +137,7 @@ elif system == "Linux":
  
