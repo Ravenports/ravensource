4ffad26e34b64b71f565e9abbcb82b89b7e356cc
John Marino <draco@marino.st>
2023-09-02 17:31:22 -0500

    freeimpi: adjust patch for linux

--- bucket_3D/freeipmi/patches/patch-ipmi-oem-ipmi-oem.h
+++ bucket_3D/freeipmi/patches/patch-ipmi-oem-ipmi-oem.h
@@ -1,10 +1,12 @@
 --- ipmi-oem/ipmi-oem.h.orig	2023-06-05 22:58:26 UTC
 +++ ipmi-oem/ipmi-oem.h
-@@ -20,6 +20,7 @@
+@@ -20,6 +20,9 @@
  #define IPMI_OEM_H
  
  #include <freeipmi/freeipmi.h>
++#if !defined(__linux__)
 +#include <sys/limits.h>
++#endif
  
  #include "tool-cmdline-common.h"
  #include "pstdout.h"
