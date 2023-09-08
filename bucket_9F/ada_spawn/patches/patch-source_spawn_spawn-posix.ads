528bfca44030b504c29672f271d2deb07df7e381
John Marino <draco@marino.st>
2023-09-08 03:19:16 +0000

    ada_spawn: specific upgrade for ada language server

--- bucket_9F/ada_spawn/patches/patch-source_spawn_spawn-posix.ads
+++ bucket_9F/ada_spawn/patches/patch-source_spawn_spawn-posix.ads
@@ -1,6 +1,6 @@
---- source/spawn/spawn-posix.ads.orig	2022-09-26 13:44:48 UTC
+--- source/spawn/spawn-posix.ads.orig	2023-05-18 09:24:48 UTC
 +++ source/spawn/spawn-posix.ads
-@@ -154,7 +154,7 @@ package Spawn.Posix is
+@@ -156,7 +156,7 @@ package Spawn.Posix is
       (fd     : Interfaces.C.int;
        buf    : out Interfaces.C.char_array;
        buflen : Interfaces.C.size_t) return Interfaces.C.int
