47ac6807ee2811e88831d2fe9472dfc081c25fa2
John Marino <draco@marino.st>
2023-09-20 17:04:48 -0500

    PragmARC: tweak
    Change gcc13 patch at recommendation of author
    https://github.com/jrcarter/PragmARC/issues/13
    
    Tweak gprinstall parameters to make selectable the three library versions.

--- bucket_82/PragmARC/patches/patch-pragmarc-b__strings.ads
+++ bucket_82/PragmARC/patches/patch-pragmarc-b__strings.ads
@@ -4,21 +4,19 @@ https://gcc.gnu.org/bugzilla/show_bug.cgi?id=110488
 
 --- pragmarc-b_strings.ads.orig	2023-07-15 15:24:51 UTC
 +++ pragmarc-b_strings.ads
-@@ -21,7 +21,7 @@ pragma Assertion_Policy (Check);
- pragma Unsuppress (All_Checks);
- 
- package PragmARC.B_Strings is
--   type B_String (Max_Length : Positive := 1024) is tagged limited private;
-+   type B_String (Max_Length : Positive) is tagged private;
+@@ -24,7 +24,7 @@ package PragmARC.B_Strings is
+    type B_String (Max_Length : Positive := 1024) is tagged limited private;
     -- Default initial value is Null_B_String
  
-    Null_B_String : constant B_String; -- A string of zero characters
-@@ -51,7 +51,7 @@ package PragmARC.B_Strings is
-    function ">"  (Left : in B_String; Right : in B_String) return Boolean;
-    function ">=" (Left : in B_String; Right : in B_String) return Boolean;
- private -- PragmARC.B_Strings
--   type B_String (Max_Length : Positive := 1024) is tagged limited record
-+   type B_String (Max_Length : Positive) is tagged record
-       Len   : Natural := 0;
+-   Null_B_String : constant B_String; -- A string of zero characters
++   Null_B_String : constant B_String (Max_Length => 1); -- A string of zero characters
+ 
+    function To_String (Source : in B_String) return String with
+       Post => To_String'Result'First = 1 and To_String'Result'Last = Source.Length;
+@@ -56,5 +56,5 @@ private -- PragmARC.B_Strings
        Value : String (1 .. Max_Length) := (1 .. Max_Length => ' ');
     end record;
+ 
+-   Null_B_String : constant B_String := (Max_Length => 1, others => <>);
++   Null_B_String : constant B_String (Max_Length => 1) := (Max_Length => 1, others => <>);
+ end PragmARC.B_Strings;
