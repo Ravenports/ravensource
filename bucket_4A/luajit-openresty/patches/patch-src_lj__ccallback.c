35004cec91ebc52d0b9e785cfc2aab43358e40db
John Marino <draco@marino.st>
2023-09-22 16:38:42 +0000

    luajit-openresty: upgrade

--- bucket_4A/luajit-openresty/patches/patch-src_lj__ccallback.c
+++ bucket_4A/luajit-openresty/patches/patch-src_lj__ccallback.c
@@ -1,4 +1,4 @@
---- src/lj_ccallback.c.orig	2022-04-05 12:12:47 UTC
+--- src/lj_ccallback.c.orig	2023-09-10 14:56:47 UTC
 +++ src/lj_ccallback.c
 @@ -20,6 +20,7 @@
  #include "lj_mcode.h"
--- bucket_4A/luajit-openresty/patches/patch-src_lj__prng.c
+++ bucket_4A/luajit-openresty/patches/patch-src_lj__prng.c
@@ -1,6 +1,6 @@
---- src/lj_prng.c.orig	2022-04-05 12:12:47 UTC
+--- src/lj_prng.c.orig	2023-09-10 14:56:47 UTC
 +++ src/lj_prng.c
-@@ -109,6 +109,7 @@ static PRGR libfunc_rgr;
+@@ -113,6 +113,7 @@ static PRGR libfunc_rgr;
  #include <sys/syscall.h>
  #else
  
@@ -8,8 +8,8 @@
  #if LJ_TARGET_OSX && !LJ_TARGET_IOS
  /*
  ** In their infinite wisdom Apple decided to disallow getentropy() in the
-@@ -124,6 +125,7 @@ static PRGR libfunc_rgr;
- #elif (LJ_TARGET_BSD && !defined(__NetBSD__)) || LJ_TARGET_SOLARIS || LJ_TARGET_CYGWIN
+@@ -128,6 +129,7 @@ static PRGR libfunc_rgr;
+ #elif (LJ_TARGET_BSD && !defined(__NetBSD__)) || LJ_TARGET_SOLARIS || LJ_TARGET_CYGWIN || LJ_TARGET_QNX
  #define LJ_TARGET_HAS_GETENTROPY	1
  #endif
 +# endif
