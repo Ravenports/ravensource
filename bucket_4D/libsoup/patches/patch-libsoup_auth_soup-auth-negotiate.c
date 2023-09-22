c413b3f3415123bcbaa6b42c607655fa66b8f2de
John Marino <draco@marino.st>
2023-09-22 19:04:47 +0000

    libsoup: upgrade

--- bucket_4D/libsoup/patches/patch-libsoup_auth_soup-auth-negotiate.c
+++ bucket_4D/libsoup/patches/patch-libsoup_auth_soup-auth-negotiate.c
@@ -1,4 +1,4 @@
---- libsoup/auth/soup-auth-negotiate.c.orig	2023-04-21 01:37:39 UTC
+--- libsoup/auth/soup-auth-negotiate.c.orig	2023-09-15 15:00:37 UTC
 +++ libsoup/auth/soup-auth-negotiate.c
 @@ -13,7 +13,11 @@
  #include <string.h>
