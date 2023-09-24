cb1c372fa353e1c2bdd1516b643cbea663927915
John Marino <draco@marino.st>
2023-09-24 17:19:51 +0000

    cairo: upgrade (switched to meson)

--- bucket_E0/cairo/patches/patch-util_cairo-missing_getline.c
+++
@@ -1,9 +0,0 @@
---- util/cairo-missing/getline.c.orig	2018-08-17 01:10:53 UTC
-+++ util/cairo-missing/getline.c
-@@ -87,4 +87,6 @@ getline (char	**lineptr,
-     return ret;
- }
- #undef GETLINE_BUFFER_SIZE
-+#else
-+int solaris_ld_requires_at_least_one_symbol = 0;
- #endif
