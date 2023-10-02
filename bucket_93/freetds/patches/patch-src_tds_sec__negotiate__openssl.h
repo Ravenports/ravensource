bccc611b3dc96d8b7ef0f5ce68b2b3e5a411cbc5
John Marino <draco@marino.st>
2023-10-02 21:30:59 +0000

    freetds: upgrade

--- bucket_93/freetds/patches/patch-src_tds_sec__negotiate__openssl.h
+++ bucket_93/freetds/patches/patch-src_tds_sec__negotiate__openssl.h
@@ -1,4 +1,4 @@
---- src/tds/sec_negotiate_openssl.h.orig	2021-06-04 12:12:02 UTC
+--- src/tds/sec_negotiate_openssl.h.orig	2023-09-12 12:07:08 UTC
 +++ src/tds/sec_negotiate_openssl.h
 @@ -37,6 +37,11 @@
  #error HAVE_OPENSSL not defines, this file should not be included
