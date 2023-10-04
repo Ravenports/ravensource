24e67b5189a44af46cbf79177f8dc809a4dcd97a
John Marino <draco@marino.st>
2023-10-04 02:37:29 +0000

    thunderbird: major upgrade

---
+++ bucket_67/thunderbird/patches/patch-toolkit_components_protobuf_src_google_protobuf_stubs_port.h
@@ -0,0 +1,11 @@
+--- toolkit/components/protobuf/src/google/protobuf/stubs/port.h.orig	2023-09-28 22:51:59 UTC
++++ toolkit/components/protobuf/src/google/protobuf/stubs/port.h
+@@ -59,7 +59,7 @@
+ #else
+ #ifdef __APPLE__
+ #include <machine/endian.h>  // __BYTE_ORDER
+-#elif defined(__FreeBSD__)
++#elif defined(__FreeBSD__) || defined(__DragonFly__)
+ #include <sys/endian.h>  // __BYTE_ORDER
+ #elif (defined(sun) || defined(__sun)) && (defined(__SVR4) || defined(__svr4__))
+ #include <sys/isa_defs.h>  // __BYTE_ORDER
--- bucket_67/thunderbird/patches/patch-tools_profiler_core_platform.cpp
+++ bucket_67/thunderbird/patches/patch-tools_profiler_core_platform.cpp
@@ -4,12 +4,11 @@ Date:   Mon Mar 28 21:54:35 2022 +0200
 
     FreeBSD can use sched_getcpu() from 1400046/1300524 on
 
-File gcc13-fix.patch of Package MozillaThunderbird
-https://build.opensuse.org/package/view_file/openSUSE:Factory/MozillaThunderbird/gcc13-fix.patch?expand=1
-
---- tools/profiler/core/platform.cpp.orig	2023-07-07 14:39:18 UTC
+diff --git tools/profiler/core/platform.cpp tools/profiler/core/platform.cpp
+index 92bcc1100687..2f17a88942a8 100644
+--- tools/profiler/core/platform.cpp
 +++ tools/profiler/core/platform.cpp
-@@ -115,6 +115,20 @@
+@@ -111,6 +111,20 @@
  #  include <cpuid.h>
  #endif
  
@@ -30,61 +29,7 @@ https://build.opensuse.org/package/view_file/openSUSE:Factory/MozillaThunderbird
  #if defined(GP_OS_windows)
  #  include <processthreadsapi.h>
  
-@@ -1106,7 +1120,7 @@ class ActivePS {
-         continue;
-       }
-       ThreadRegistry::OffThreadRef::RWFromAnyThreadWithLock lockedThreadData =
--          offThreadRef.LockedRWFromAnyThread();
-+          offThreadRef.GetLockedRWFromAnyThread();
-       MOZ_RELEASE_ASSERT(array.append(ProfiledThreadListElement{
-           profiledThreadData->Info().RegisterTime(),
-           lockedThreadData->GetJSContext(), profiledThreadData}));
-@@ -4050,7 +4064,7 @@ void SamplerThread::Run() {
- 
-             if (threadStackSampling) {
-               ThreadRegistry::OffThreadRef::RWFromAnyThreadWithLock
--                  lockedThreadData = offThreadRef.LockedRWFromAnyThread();
-+                  lockedThreadData = offThreadRef.GetLockedRWFromAnyThread();
-               // Suspend the thread and collect its stack data in the local
-               // buffer.
-               mSampler.SuspendAndSampleAndResumeThread(
-@@ -4764,7 +4778,7 @@ static ProfilingStack* locked_register_t
-             aLock, aOffThreadRef.UnlockedConstReaderCRef().Info());
-     if (threadProfilingFeatures != ThreadProfilingFeatures::NotProfiled) {
-       ThreadRegistry::OffThreadRef::RWFromAnyThreadWithLock
--          lockedRWFromAnyThread = aOffThreadRef.LockedRWFromAnyThread();
-+          lockedRWFromAnyThread = aOffThreadRef.GetLockedRWFromAnyThread();
- 
-       ProfiledThreadData* profiledThreadData = ActivePS::AddLiveProfiledThread(
-           aLock, MakeUnique<ProfiledThreadData>(
-@@ -5629,7 +5643,7 @@ static void locked_profiler_start(PSLock
-         ActivePS::ProfilingFeaturesForThread(aLock, info);
-     if (threadProfilingFeatures != ThreadProfilingFeatures::NotProfiled) {
-       ThreadRegistry::OffThreadRef::RWFromAnyThreadWithLock lockedThreadData =
--          offThreadRef.LockedRWFromAnyThread();
-+          offThreadRef.GetLockedRWFromAnyThread();
-       ProfiledThreadData* profiledThreadData = ActivePS::AddLiveProfiledThread(
-           aLock, MakeUnique<ProfiledThreadData>(info));
-       lockedThreadData->SetProfilingFeaturesAndData(threadProfilingFeatures,
-@@ -5853,7 +5867,7 @@ void profiler_ensure_started(PowerOfTwo3
-     }
- 
-     ThreadRegistry::OffThreadRef::RWFromAnyThreadWithLock lockedThreadData =
--        offThreadRef.LockedRWFromAnyThread();
-+        offThreadRef.GetLockedRWFromAnyThread();
- 
-     lockedThreadData->ClearProfilingFeaturesAndData(aLock);
- 
-@@ -6196,7 +6210,7 @@ static void locked_unregister_thread(
-   // thread that is in the process of disappearing.
- 
-   ThreadRegistration::OnThreadRef::RWOnThreadWithLock lockedThreadData =
--      aOnThreadRef.LockedRWOnThread();
-+      aOnThreadRef.GetLockedRWOnThread();
- 
-   ProfiledThreadData* profiledThreadData =
-       lockedThreadData->GetProfiledThreadData(lock);
-@@ -6520,6 +6534,19 @@ void profiler_mark_thread_awake() {
+@@ -6368,6 +6382,19 @@ void profiler_mark_thread_awake() {
      cpuId = ebx >> 24;
    }
  #  endif
@@ -104,12 +49,3 @@ https://build.opensuse.org/package/view_file/openSUSE:Factory/MozillaThunderbird
  #else
    cpuId = sched_getcpu();
  #endif
-@@ -6717,7 +6744,7 @@ void profiler_clear_js_context() {
-         // The profiler mutex must be locked before the ThreadRegistration's.
-         PSAutoLock lock;
-         ThreadRegistration::OnThreadRef::RWOnThreadWithLock lockedThreadData =
--            aOnThreadRef.LockedRWOnThread();
-+            aOnThreadRef.GetLockedRWOnThread();
- 
-         if (ProfiledThreadData* profiledThreadData =
-                 lockedThreadData->GetProfiledThreadData(lock);
