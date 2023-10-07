6a7b8dc388f1bd693866f771b2df551357c139af
John Marino <draco@marino.st>
2023-10-07 20:51:32 +0000

    add python 3.12 (first pass).
    1) crypt will not build on DragonFly because crypt_r not supported.
       This might cause issues later.
    2) This port needs to be split, which needs additional ravenadm
       support.

---
+++ bucket_6C/python312/patches/patch-Modules___multiprocessing_posixshmem.c
@@ -0,0 +1,12 @@
+--- Modules/_multiprocessing/posixshmem.c.orig	2023-10-02 11:48:14 UTC
++++ Modules/_multiprocessing/posixshmem.c
+@@ -8,6 +8,9 @@ posixshmem - A Python extension that pro
+ 
+ // for shm_open() and shm_unlink()
+ #ifdef HAVE_SYS_MMAN_H
++# ifdef __sun__
++# define _XPG4_2
++# endif
+ #include <sys/mman.h>
+ #endif
+ 
---
+++ bucket_6C/python312/patches/patch-Modules_nismodule.c
@@ -0,0 +1,11 @@
+--- Modules/nismodule.c.orig	2023-10-02 11:48:14 UTC
++++ Modules/nismodule.c
+@@ -116,7 +116,7 @@ nis_mapname(char *map, int *pfix)
+     return map;
+ }
+ 
+-#if defined(__APPLE__) || defined(__OpenBSD__) || defined(__FreeBSD__)
++#if defined(__APPLE__) || defined(__OpenBSD__) || defined(__FreeBSD__) || defined(__DragonFly__)
+ typedef int (*foreachfunc)(unsigned long, char *, int, char *, int, void *);
+ #else
+ typedef int (*foreachfunc)(int, char *, int, char *, int, char *);
