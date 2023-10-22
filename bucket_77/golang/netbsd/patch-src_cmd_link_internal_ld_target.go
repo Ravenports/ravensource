307cbf7668ccbccdc15cdbe787a210ed5808ab88
John Marino <draco@marino.st>
2023-10-22 05:40:16 +0000

    golang: make go executable usable on netbsd (fixes syncthing)

---
+++ bucket_77/golang/netbsd/patch-src_cmd_link_internal_ld_target.go
@@ -0,0 +1,20 @@
+--- src/cmd/link/internal/ld/target.go.orig	2023-10-09 17:04:35 UTC
++++ src/cmd/link/internal/ld/target.go
+@@ -72,16 +72,7 @@ func (t *Target) IsDynlinkingGo() bool {
+ // UseRelro reports whether to make use of "read only relocations" aka
+ // relro.
+ func (t *Target) UseRelro() bool {
+-	switch t.BuildMode {
+-	case BuildModeCArchive, BuildModeCShared, BuildModeShared, BuildModePIE, BuildModePlugin:
+-		return t.IsELF || t.HeadType == objabi.Haix || t.HeadType == objabi.Hdarwin
+-	default:
+-		if t.HeadType == objabi.Hdarwin && t.IsARM64() {
+-			// On darwin/ARM64, everything is PIE.
+-			return true
+-		}
+-		return t.linkShared || (t.HeadType == objabi.Haix && t.LinkMode == LinkExternal)
+-	}
++	return false
+ }
+ 
+ //
