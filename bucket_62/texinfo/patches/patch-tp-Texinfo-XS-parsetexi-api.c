5e5fff6196d628720f19e65613b191ac537c193e
John Marino <draco@marino.st>
2023-10-26 16:03:07 +0000

    texinfo: upgrade

--- bucket_62/texinfo/patches/patch-tp-Texinfo-XS-parsetexi-api.c
+++ bucket_62/texinfo/patches/patch-tp-Texinfo-XS-parsetexi-api.c
@@ -1,4 +1,4 @@
---- tp/Texinfo/XS/parsetexi/api.c.orig	2022-10-23 12:56:25 UTC
+--- tp/Texinfo/XS/parsetexi/api.c.orig	2023-10-15 11:39:58 UTC
 +++ tp/Texinfo/XS/parsetexi/api.c
 @@ -30,7 +30,9 @@
  
