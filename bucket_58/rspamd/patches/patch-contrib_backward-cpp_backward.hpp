d8e90426732dee0b92f1e04bf86c47f4b922b845
John Marino <draco@marino.st>
2023-10-28 18:34:10 +0200

    rspamd: add <cstdint> to fix build on mbsd

---
+++ bucket_58/rspamd/patches/patch-contrib_backward-cpp_backward.hpp
@@ -0,0 +1,10 @@
+--- contrib/backward-cpp/backward.hpp.orig	2023-10-27 07:38:19 UTC
++++ contrib/backward-cpp/backward.hpp
+@@ -415,6 +415,7 @@ extern "C" uintptr_t _Unwind_GetIPInfo(_
+ #ifdef BACKWARD_ATLEAST_CXX11
+ #include <unordered_map>
+ #include <utility> // for std::swap
++#include <cstdint>
+ namespace backward {
+ namespace details {
+ template <typename K, typename V> struct hashtable {
