08e776fa45759d7772ea84715712fafcba562945
John Marino <draco@marino.st>
2023-10-28 14:13:18 +0000

    clang: restore equivalent of corrupted midnightbsd patch

--- bucket_EC/clang/patches/patch-lib_Basic_Targets_OSTargets.h
+++ bucket_EC/clang/patches/patch-lib_Basic_Targets_OSTargets.h
@@ -1,6 +1,14 @@
 --- lib/Basic/Targets/OSTargets.h.orig	2023-10-17 06:20:52 UTC
 +++ lib/Basic/Targets/OSTargets.h
-@@ -425,12 +425,22 @@ protected:
+@@ -213,6 +213,7 @@ protected:
+     if (CCVersion == 0U)
+       CCVersion = Release * 100000U + 1U;
+ 
++//  Builder.defineMacro("__MidnightBSD__", "3");
+     Builder.defineMacro("__FreeBSD__", Twine(Release));
+     Builder.defineMacro("__FreeBSD_cc_version", Twine(CCVersion));
+     Builder.defineMacro("__KPRINTF_ATTRIBUTE__");
+@@ -425,12 +426,22 @@ protected:
      Builder.defineMacro("__unix__");
      if (Opts.POSIXThreads)
        Builder.defineMacro("_REENTRANT");
