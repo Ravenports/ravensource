03a51d0a9f766b3cf5e61a1340aa7589821280d0
John Marino <draco@marino.st>
2023-10-29 03:05:38 +0000

    ffmpeg5: change major version of swresample
    There is a conflict with swresample library between ffmpeg5 and
    ffmpeg6.  To allow them both primary subpackages to be installed
    at the same time, we've changed the major version from 4 to 40.

---
+++ bucket_0E/ffmpeg5/patches/patch-libswresample_version__major.h
@@ -0,0 +1,10 @@
+--- libswresample/version_major.h.orig	2023-02-27 20:43:45 UTC
++++ libswresample/version_major.h
+@@ -26,6 +26,6 @@
+  * Libswresample version macros
+  */
+ 
+-#define LIBSWRESAMPLE_VERSION_MAJOR   4
++#define LIBSWRESAMPLE_VERSION_MAJOR   40
+ 
+ #endif /* SWRESAMPLE_VERSION_MAJOR_H */
