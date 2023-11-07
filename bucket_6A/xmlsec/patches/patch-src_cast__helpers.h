a141a53c277b1df97c44b35fcc7efb7fb2896051
Michael Reim <kraileth@elderlinux.org>
2023-11-07 16:42:21 +0100

    xmlsec: upgrade

--- bucket_6A/xmlsec/patches/patch-src_cast__helpers.h
+++ bucket_6A/xmlsec/patches/patch-src_cast__helpers.h
@@ -1,4 +1,4 @@
---- src/cast_helpers.h.orig	2023-04-12 16:45:12 UTC
+--- src/cast_helpers.h.orig	2023-06-06 13:00:47 UTC
 +++ src/cast_helpers.h
 @@ -16,6 +16,7 @@
  #error "cast_helpers.h file contains private xmlsec definitions and should not be used outside xmlsec or xmlsec-$crypto libraries"
