a6feb28757997526a7715d987a86a954fd82a526
Michael Reim <kraileth@elderlinux.org>
2023-11-07 20:11:46 +0100

    upx: upgrade

--- bucket_01/upx/patches/patch-src-packer.h
+++ bucket_01/upx/patches/patch-src-packer.h
@@ -1,4 +1,4 @@
---- src/packer.h.orig	2023-10-26 09:09:48 UTC
+--- src/packer.h.orig	2023-11-01 11:00:03 UTC
 +++ src/packer.h
 @@ -191,7 +191,7 @@ protected:
      void addLoader(C, C, C, C, C, C, C, C, C);
