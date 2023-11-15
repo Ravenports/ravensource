ab9209cbaa5ad1719c0fdf726a9c227381a5d5ba
John Marino <draco@marino.st>
2023-11-15 13:40:18 +0000

    shared-mime-info: upgrade

--- bucket_EC/shared-mime-info/patches/patch-src_update-mime-database.cpp
+++ bucket_EC/shared-mime-info/patches/patch-src_update-mime-database.cpp
@@ -1,10 +1,10 @@
---- src/update-mime-database.cpp.orig	2023-10-07 11:16:03 UTC
+--- src/update-mime-database.cpp.orig	2023-11-12 08:52:33 UTC
 +++ src/update-mime-database.cpp
-@@ -2158,7 +2158,7 @@ static void check_in_path_xdg_data(const
+@@ -2159,7 +2159,7 @@ static void check_in_path_xdg_data(const
  
  	env = getenv("XDG_DATA_DIRS");
  	if (!env)
--		env = "/usr/local/share/"PATH_SEPARATOR"/usr/share/";
+-		env = "/usr/local/share/" PATH_SEPARATOR "/usr/share/";
 +		env = "/usr/local/share/";
  	dirs = g_strsplit(env, PATH_SEPARATOR, 0);
  	g_return_if_fail(dirs != NULL);
