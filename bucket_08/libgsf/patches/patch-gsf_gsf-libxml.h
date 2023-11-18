c9d9b7eb2ceb2527e4e6ccfab60c7fb0015a24e8
John Marino <draco@marino.st>
2023-11-18 07:07:33 +0000

    libgsf: fix build after libxml2 update

---
+++ bucket_08/libgsf/patches/patch-gsf_gsf-libxml.h
@@ -0,0 +1,10 @@
+--- gsf/gsf-libxml.h.orig	2023-03-02 01:51:15 UTC
++++ gsf/gsf-libxml.h
+@@ -24,6 +24,7 @@
+ 
+ #include <gsf/gsf-fwd.h>
+ #include <libxml/tree.h>
++#include <libxml/parser.h>
+ 
+ G_BEGIN_DECLS
+ 
