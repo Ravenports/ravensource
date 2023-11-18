9e1c6f008e275c7bcd894d9702a3fbf35f4677ca
John Marino <draco@marino.st>
2023-11-18 05:35:35 +0000

    npm: upgrade

--- bucket_66/npm/patches/patch-node__modules_node-gyp_lib_configure.js
+++ bucket_66/npm/patches/patch-node__modules_node-gyp_lib_configure.js
@@ -1,27 +1,11 @@
 --- node_modules/node-gyp/lib/configure.js.orig	1985-10-26 08:15:00 UTC
 +++ node_modules/node-gyp/lib/configure.js
-@@ -15,7 +15,7 @@ if (win) {
- }
+@@ -20,7 +20,7 @@ async function configure (gyp, argv) {
+   let nodeDir
+   const release = processRelease(argv, gyp, process.version, process.release)
  
- function configure (gyp, argv, callback) {
--  var python
-+  var python = '@PYTHONBIN@'
-   var buildDir = path.resolve('build')
-   var buildBinsDir = path.join(buildDir, 'node_gyp_bins')
-   var configNames = ['config.gypi', 'common.gypi']
-@@ -23,14 +23,7 @@ function configure (gyp, argv, callback)
-   var nodeDir
-   var release = processRelease(argv, gyp, process.version, process.release)
+-  const python = await findPython(gyp.opts.python)
++  const python = '@PYTHONBIN@'
+   return getNodeDir()
  
--  findPython(gyp.opts.python, function (err, found) {
--    if (err) {
--      callback(err)
--    } else {
--      python = found
--      getNodeDir()
--    }
--  })
-+  getNodeDir()
- 
-   function getNodeDir () {
-     // 'python' should be set by now
+   async function getNodeDir () {
