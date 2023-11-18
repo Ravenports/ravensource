ffab8971dad9f7e67c06d230e33522fe4f07fd33
John Marino <draco@marino.st>
2023-11-18 05:00:50 +0000

    mupdf: upgrade

--- bucket_E0/mupdf/patches/patch-source_fitz_load-jpx.c
+++ bucket_E0/mupdf/patches/patch-source_fitz_load-jpx.c
@@ -13,7 +13,7 @@ $NetBSD: patch-source_fitz_load-jpx.c,v 1.10 2020/05/07 20:42:57 leot Exp $
 
   From OpenBSD ports textproc/mupdf/patch-source_fitz_load-jpx_c,v 1.5.
 
---- source/fitz/load-jpx.c.orig	2023-11-06 13:26:29 UTC
+--- source/fitz/load-jpx.c.orig	2023-11-15 14:55:24 UTC
 +++ source/fitz/load-jpx.c
 @@ -163,19 +163,7 @@ void opj_free(void *ptr)
  
