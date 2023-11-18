6b339625a4a9e1c5417f12e7fa3c8c404f8cd0a4
John Marino <draco@marino.st>
2023-11-18 16:15:18 +0000

    wireshark: upgrade (difficult).  no more dev subpackage

---
+++ bucket_FA/wireshark/patches/patch-cmake_modules_FindLua.cmake
@@ -0,0 +1,11 @@
+--- cmake/modules/FindLua.cmake.orig	2023-11-15 18:30:18 UTC
++++ cmake/modules/FindLua.cmake
+@@ -179,7 +179,7 @@ if (LUA_VERSION_STRING)
+ endif ()
+ 
+ find_library(LUA_LIBRARY
+-  NAMES ${_lua_library_names} lua
++  NAMES ${_lua_library_names} lua-5.2
+   NAMES_PER_DIR
+   HINTS
+     ${LUA_HINTS}
--- bucket_FA/wireshark/patches/patch-extcap.c
+++ bucket_FA/wireshark/patches/patch-extcap.c
@@ -1,4 +1,4 @@
---- extcap.c.orig	2023-10-04 22:31:39 UTC
+--- extcap.c.orig	2023-11-15 18:30:18 UTC
 +++ extcap.c
 @@ -22,6 +22,7 @@
  #include <process.h>
