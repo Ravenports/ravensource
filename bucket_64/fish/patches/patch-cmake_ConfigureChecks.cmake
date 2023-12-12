ff1e1a787e9a384b1d3552e3dad9db696c1dee88
John Marino <draco@marino.st>
2023-12-12 23:19:19 +0000

    fish: upgrade

--- bucket_64/fish/patches/patch-cmake_ConfigureChecks.cmake
+++ bucket_64/fish/patches/patch-cmake_ConfigureChecks.cmake
@@ -1,4 +1,4 @@
---- cmake/ConfigureChecks.cmake.orig	2023-03-25 06:50:41 UTC
+--- cmake/ConfigureChecks.cmake.orig	2023-12-05 14:16:18 UTC
 +++ cmake/ConfigureChecks.cmake
 @@ -215,8 +215,8 @@ int main () {
  # Check if tputs needs a function reading an int or char.
