27b270bcffc62584f12f6c7e6ce4d02abfa03db9
John Marino <draco@marino.st>
2023-12-12 18:42:52 +0000

    sbcl: upgrade

--- bucket_FA/sbcl/patches/patch-make-config.sh
+++ bucket_FA/sbcl/patches/patch-make-config.sh
@@ -1,6 +1,6 @@
 Linux LARGEFILE option does not seem to build.
 
---- make-config.sh.orig	2023-09-28 06:54:11 UTC
+--- make-config.sh.orig	2023-11-27 16:39:18 UTC
 +++ make-config.sh
 @@ -301,7 +301,7 @@ case `uname` in
          ;;
