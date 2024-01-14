0b61004b07038d9fc65f61d3715d8d71465fd21d
John Marino <draco@marino.st>
2024-01-14 06:24:26 +0000

    cppcheck: upgrade

--- bucket_66/cppcheck/patches/patch-cli_cppcheckexecutor.cpp
+++ bucket_66/cppcheck/patches/patch-cli_cppcheckexecutor.cpp
@@ -1,4 +1,4 @@
---- cli/cppcheckexecutor.cpp.intermediate	2023-12-23 06:29:47 UTC
+--- cli/cppcheckexecutor.cpp.orig	2024-01-13 14:24:29 UTC
 +++ cli/cppcheckexecutor.cpp
 @@ -51,6 +51,7 @@
  #include <sstream> // IWYU pragma: keep
