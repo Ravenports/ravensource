df8e28a3f64e7177c6108abfa8b1b35165c7e901
John Marino <draco@marino.st>
2024-01-24 23:18:17 +0000

    puppet: upgrade

--- bucket_98/puppet/patches/patch-lib_puppet_provider_service_daemontools.rb
+++ bucket_98/puppet/patches/patch-lib_puppet_provider_service_daemontools.rb
@@ -1,17 +1,17 @@
 Daemontools program locations
 
---- lib/puppet/provider/service/daemontools.rb.orig	2023-10-26 18:38:45 UTC
+--- lib/puppet/provider/service/daemontools.rb.orig	2023-12-21 23:41:03 UTC
 +++ lib/puppet/provider/service/daemontools.rb
-@@ -40,7 +40,7 @@ Puppet::Type.type(:service).provide :dae
+@@ -41,7 +41,7 @@ Puppet::Type.type(:service).provide :dae
  
    EOT
  
--  commands :svc  => "/usr/bin/svc", :svstat => "/usr/bin/svstat"
-+  commands :svc  => "%%PREFIX%%/bin/svc", :svstat => "%%PREFIX%%/bin/svstat"
+-  commands :svc => "/usr/bin/svc", :svstat => "/usr/bin/svstat"
++  commands :svc => "%%PREFIX%%/bin/svc", :svstat => "%%PREFIX%%/bin/svstat"
  
    class << self
      attr_writer :defpath
-@@ -87,7 +87,7 @@ Puppet::Type.type(:service).provide :dae
+@@ -88,7 +88,7 @@ Puppet::Type.type(:service).provide :dae
    # find the service dir on this node
    def servicedir
      unless @servicedir
