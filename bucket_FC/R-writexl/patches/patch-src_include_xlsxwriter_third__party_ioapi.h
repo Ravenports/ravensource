4394ddc74a8fe96da2b092dc6d607c606c89c67b
John Marino <draco@marino.st>
2024-02-10 17:30:01 +0000

    R-writexl: regenerate patches to fix build

--- bucket_FC/R-writexl/patches/patch-src_include_xlsxwriter_third__party_ioapi.h
+++ bucket_FC/R-writexl/patches/patch-src_include_xlsxwriter_third__party_ioapi.h
@@ -1,11 +1,11 @@
---- src/include/xlsxwriter/third_party/ioapi.h.orig	2023-01-06 17:19:12 UTC
+--- src/include/xlsxwriter/third_party/ioapi.h.orig	2024-02-09 14:22:57 UTC
 +++ src/include/xlsxwriter/third_party/ioapi.h
 @@ -56,7 +56,7 @@
  #define ftello64 ftell
  #define fseeko64 fseek
  #else
--#if defined(__FreeBSD__) || defined(__OpenBSD__)
-+#if defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__DragonFly__) || defined(__NetBSD__)
+-#if defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__NetBSD__) || defined(__HAIKU__) || defined(MINIZIP_FOPEN_NO_64)
++#if defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__DragonFly__) || defined(__NetBSD__) || defined(__HAIKU__) || defined(MINIZIP_FOPEN_NO_64)
  #define fopen64 fopen
  #define ftello64 ftello
  #define fseeko64 fseeko
--- bucket_FC/R-writexl/patches/patch-src_minizip_ioapi.h
+++ bucket_FC/R-writexl/patches/patch-src_minizip_ioapi.h
@@ -1,11 +1,11 @@
---- src/minizip/ioapi.h.orig	2023-01-06 17:19:12 UTC
+--- src/minizip/ioapi.h.orig	2024-02-09 14:23:29 UTC
 +++ src/minizip/ioapi.h
 @@ -56,7 +56,7 @@
  #define ftello64 ftell
  #define fseeko64 fseek
  #else
--#if defined(__FreeBSD__) || defined(__OpenBSD__)
-+#if defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__DragonFly__) || defined(__NetBSD__)
+-#if defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__NetBSD__) || defined(__HAIKU__) || defined(MINIZIP_FOPEN_NO_64)
++#if defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__DragonFly__) || defined(__NetBSD__) || defined(__HAIKU__) || defined(MINIZIP_FOPEN_NO_64)
  #define fopen64 fopen
  #define ftello64 ftello
  #define fseeko64 fseeko
