ebdfb6235d2a40ebc84f48a3c911989197840c66
Michael Reim <kraileth@elderlinux.org>
2024-02-11 17:45:40 +0100

    libuv: upgrade

--- bucket_AD/libuv/patches/patch-src_unix_fs.c
+++ bucket_AD/libuv/patches/patch-src_unix_fs.c
@@ -1,6 +1,6 @@
---- src/unix/fs.c.orig	2023-06-30 18:31:51 UTC
+--- src/unix/fs.c.orig	2023-11-06 18:17:17 UTC
 +++ src/unix/fs.c
-@@ -1098,13 +1098,13 @@ static ssize_t uv__fs_sendfile(uv_fs_t*
+@@ -1026,13 +1026,13 @@ static ssize_t uv__fs_sendfile(uv_fs_t*
  static ssize_t uv__fs_utime(uv_fs_t* req) {
  #if defined(__linux__)                                                         \
      || defined(_AIX71)                                                         \
@@ -15,7 +15,7 @@
      || defined(__DragonFly__)                                                 \
      || defined(__FreeBSD__)                                                   \
      || defined(__NetBSD__)                                                    \
-@@ -1137,7 +1137,6 @@ static ssize_t uv__fs_utime(uv_fs_t* req
+@@ -1065,7 +1065,6 @@ static ssize_t uv__fs_utime(uv_fs_t* req
  static ssize_t uv__fs_lutime(uv_fs_t* req) {
  #if defined(__linux__)            ||                                           \
      defined(_AIX71)               ||                                           \
