a725d6fe4a311560fcb71e7289a5ca2cf553de25
John Marino <draco@marino.st>
2024-02-21 22:10:17 +0000

    upgrade php 8.2 and 8.3 (all)

--- bucket_22/lighttpd/patches/patch-src_ck.h
+++ bucket_22/lighttpd/patches/patch-src_ck.h
@@ -1,16 +1,10 @@
---- src/ck.h.orig	2023-10-31 02:35:56 UTC
+--- src/ck.h.orig	2024-02-20 04:42:50 UTC
 +++ src/ck.h
-@@ -18,11 +18,12 @@
- #endif
- #endif
- #include "first.h"
--#if defined(__FreeBSD__) || defined(__sun)
-+#if defined(__FreeBSD__) || defined(__sun) || defined(__DragonFly__)
- #ifndef _RSIZE_T_DEFINED /* expecting __EXT1_VISIBLE 1 and _RSIZE_T_DEFINED */
+@@ -23,6 +23,7 @@
  #define _RSIZE_T_DEFINED
  typedef size_t rsize_t;
  #endif
 +#include <stdlib.h>
  #include <errno.h>
- #endif
- 
+ #if defined(__DragonFly__)
+ typedef int errno_t;
