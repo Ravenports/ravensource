332c771f0c9c155e82b5f215666bc0307452cf38
John Marino <draco@marino.st>
2024-02-21 23:15:18 +0000

    po4a: upgrade

--- bucket_05/po4a/patches/patch-Po4aBuilder.pm
+++ bucket_05/po4a/patches/patch-Po4aBuilder.pm
@@ -1,6 +1,6 @@
---- Po4aBuilder.pm.orig	2022-09-04 14:32:26 UTC
+--- Po4aBuilder.pm.orig	2024-01-28 23:33:27 UTC
 +++ Po4aBuilder.pm
-@@ -118,8 +118,8 @@ sub ACTION_install {
+@@ -125,8 +125,8 @@ sub ACTION_install {
      $mandir =~ s,/man3$,,;
      $self->install_path(man => $mandir);
  
