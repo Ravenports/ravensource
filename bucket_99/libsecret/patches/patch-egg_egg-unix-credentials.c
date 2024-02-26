92d7ad3ac533f4d0389e8a7627f2b5a822fe696f
John Marino <draco@marino.st>
2024-02-25 18:11:18 -0600

    libsecret: patch freebsd to restore build

---
+++ bucket_99/libsecret/patches/patch-egg_egg-unix-credentials.c
@@ -0,0 +1,13 @@
+--- egg/egg-unix-credentials.c.orig	2024-02-23 09:53:23 UTC
++++ egg/egg-unix-credentials.c
+@@ -48,6 +48,10 @@
+ #include <ucred.h>
+ #endif
+ 
++#if defined(__FreeBSD__)
++#undef LOCAL_CREDS
++#endif
++
+ int
+ egg_unix_credentials_read (int sock, pid_t *pid, uid_t *uid)
+ {
