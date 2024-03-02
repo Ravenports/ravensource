55a10d00afab26d951f87b8553668106aaefba15
Michael Reim <kraileth@elderlinux.org>
2024-03-02 14:47:23 +0100

    rspamd: upgrade

--- bucket_58/rspamd/patches/patch-contrib_backward-cpp_backward.hpp
+++ bucket_58/rspamd/patches/patch-contrib_backward-cpp_backward.hpp
@@ -1,4 +1,4 @@
---- contrib/backward-cpp/backward.hpp.orig	2024-01-19 12:45:38 UTC
+--- contrib/backward-cpp/backward.hpp.orig	2024-02-22 16:24:56 UTC
 +++ contrib/backward-cpp/backward.hpp
 @@ -415,6 +415,7 @@ extern "C" uintptr_t _Unwind_GetIPInfo(_
  #ifdef BACKWARD_ATLEAST_CXX11
