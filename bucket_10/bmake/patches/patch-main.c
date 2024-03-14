099b7ebad246bd63a6bccd445ba130ff123d5ab5
Michael Reim <kraileth@elderlinux.org>
2024-03-14 20:29:24 +0100

    bmake: upgrade

--- bucket_10/bmake/patches/patch-main.c
+++ bucket_10/bmake/patches/patch-main.c
@@ -1,6 +1,6 @@
---- main.c.orig	2024-01-07 01:44:09 UTC
+--- main.c.orig	2024-03-01 20:24:38 UTC
 +++ main.c
-@@ -1438,6 +1438,12 @@ main_Init(int argc, char **argv)
+@@ -1424,6 +1424,12 @@ main_Init(int argc, char **argv)
  		Global_Set_ReadOnly(".MAKE.UID", buf);
  		snprintf(buf, sizeof buf, "%u", getgid());
  		Global_Set_ReadOnly(".MAKE.GID", buf);
