38f500e4dd35dbb2e866c1c964e1e659574c1345
John Marino <draco@marino.st>
2024-04-11 04:17:50 +0000

    npm: upgrade

--- bucket_66/npm/patches/patch-node__modules_node-gyp_lib_configure.js
+++ bucket_66/npm/patches/patch-node__modules_node-gyp_lib_configure.js
@@ -1,6 +1,6 @@
 --- node_modules/node-gyp/lib/configure.js.orig	1985-10-26 08:15:00 UTC
 +++ node_modules/node-gyp/lib/configure.js
-@@ -20,7 +20,7 @@ async function configure (gyp, argv) {
+@@ -24,7 +24,7 @@ async function configure (gyp, argv) {
    let nodeDir
    const release = processRelease(argv, gyp, process.version, process.release)
  
