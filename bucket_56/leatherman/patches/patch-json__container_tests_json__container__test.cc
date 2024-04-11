40d0eba801a3e922e7f3a15cc463dc72f84d0a84
John Marino <draco@marino.st>
2024-04-11 02:56:05 +0000

    leatherman: upgrade

--- bucket_56/leatherman/patches/patch-json__container_tests_json__container__test.cc
+++ bucket_56/leatherman/patches/patch-json__container_tests_json__container__test.cc
@@ -1,4 +1,4 @@
---- json_container/tests/json_container_test.cc.orig	2024-02-20 16:43:08 UTC
+--- json_container/tests/json_container_test.cc.orig	2024-03-01 18:47:06 UTC
 +++ json_container/tests/json_container_test.cc
 @@ -22,11 +22,13 @@ TEST_CASE("JsonContainer::JsonContainer
      std::string json_value {};
--- bucket_56/leatherman/patches/patch-locale_src_locale.cc
+++ bucket_56/leatherman/patches/patch-locale_src_locale.cc
@@ -1,4 +1,4 @@
---- locale/src/locale.cc.orig	2024-02-20 16:43:08 UTC
+--- locale/src/locale.cc.orig	2024-03-01 18:47:06 UTC
 +++ locale/src/locale.cc
 @@ -44,6 +44,9 @@ namespace leatherman { namespace locale
              gen.add_messages_domain(domain);
