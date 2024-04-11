fcd86f1926bd3aa7fb4f2b8d8240eb903d33bc84
John Marino <draco@marino.st>
2024-04-11 04:13:52 +0000

    upgrade all versions of php

--- Scripts/Ravenports_Mk/raven.versions.mk
+++ Scripts/Ravenports_Mk/raven.versions.mk
@@ -127,9 +127,9 @@ MYSQL_mariadb-11_VERSION=	11.3.2
 
 PHP_DEFAULT?=		8.2
 
-PHP_8.1_VERSION=	8.1.27
-PHP_8.2_VERSION=	8.2.17
-PHP_8.3_VERSION=	8.3.4
+PHP_8.1_VERSION=	8.1.28
+PHP_8.2_VERSION=	8.2.18
+PHP_8.3_VERSION=	8.3.5
 
 #-------------------------------------------------------------------------
 # Ravenports system root versions per platform
