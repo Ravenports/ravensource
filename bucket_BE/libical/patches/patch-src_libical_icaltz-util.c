7e281d87248276d1989ee9c2f234b8f5b1bdceb5
John Marino <draco@marino.st>
2024-04-22 04:13:04 +0000

    libical: fix build (not sure why it broke on Dfly)

--- bucket_BE/libical/patches/patch-src_libical_icaltz-util.c
+++ bucket_BE/libical/patches/patch-src_libical_icaltz-util.c
@@ -1,13 +1,11 @@
 --- src/libical/icaltz-util.c.orig	2024-03-31 10:26:42 UTC
 +++ src/libical/icaltz-util.c
-@@ -53,6 +53,13 @@
+@@ -53,6 +53,11 @@
  #define bswap_32 swap32
  #endif
  
 +#if defined(__FreeBSD__) || defined(__DragonFly__) || defined(__NetBSD__)
 +#define bswap_64 bswap64
-+#endif
-+#if defined(__NetBSD__)
 +#define bswap_32 bswap32
 +#endif
 +
