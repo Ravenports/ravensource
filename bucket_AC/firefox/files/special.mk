c629d9b73d77235c42df08d910cd807906eec84e
John Marino <draco@marino.st>
2024-04-22 20:50:12 +0000

    firefox: upgrade

--- bucket_AC/firefox/files/special.mk
+++ bucket_AC/firefox/files/special.mk
@@ -1,5 +1,4 @@
 .if defined(BUILD_WITH_CCACHE)
-# QMAKE_ARGS+=	--with-ccache=${LOCALBASE}/bin/sccache
 MAKE_ENV+=	SCCACHE_DIR=${CCACHE_DIR}/sccache
 MAKE_ENV+=	RUSTC_WRAPPER=${LOCALBASE}/bin/sccache
 .endif
