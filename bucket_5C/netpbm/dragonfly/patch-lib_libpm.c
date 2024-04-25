f32ec283f6408b14f7eb2ea41239a901b0a553ab
John Marino <draco@marino.st>
2024-04-25 21:18:12 +0000

    netpbm: upgrade

--- bucket_5C/netpbm/dragonfly/patch-lib_libpm.c
+++ bucket_5C/netpbm/dragonfly/patch-lib_libpm.c
@@ -1,4 +1,4 @@
---- lib/libpm.c.orig	2023-09-29 18:36:47 UTC
+--- lib/libpm.c.orig	2024-04-11 21:05:07 UTC
 +++ lib/libpm.c
 @@ -17,6 +17,7 @@
  #include <unistd.h>
@@ -8,7 +8,7 @@
  #include <string.h>
  #include <errno.h>
  #include <setjmp.h>
-@@ -840,7 +841,7 @@ pm_arg0toprogname(const char arg0[]) {
+@@ -849,7 +850,7 @@ pm_arg0toprogname(const char arg0[]) {
  unsigned int
  pm_randseed(void) {
  
