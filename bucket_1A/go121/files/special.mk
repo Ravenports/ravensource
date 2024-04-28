4dca019951e2b06b5895212b1a10f2123c1f1b32
John Marino <draco@marino.st>
2024-04-28 23:45:43 +0000

    add go121 port

---
+++ bucket_1A/go121/files/special.mk
@@ -0,0 +1,15 @@
+.if ${ARCH_STANDARD} == "x86_64"
+GOARCH=amd64
+.elif ${ARCH_STANDARD} == "i386"
+GOARCH=i386
+.else
+GOARCH=unknown
+.endif
+
+.if ${OPSYS} == "MidnightBSD"
+GOSYS=freebsd
+.else
+GOSYS=${OPSYS:tl}
+.endif
+
+PREPEND_PATH=	${LOCALBASE}/go119/bin
