0b92ec08e7f92c622f11cc1f6877949b78b5c6c6
John Marino <draco@marino.st>
2024-04-29 01:02:38 +0000

    golang: adjust cert path to use ravenports certs

---
+++ bucket_77/golang/patches/patch-src_crypto_x509_root__bsd.go
@@ -0,0 +1,21 @@
+--- src/crypto/x509/root_bsd.go.orig	2024-03-29 15:27:02 UTC
++++ src/crypto/x509/root_bsd.go
+@@ -8,15 +8,11 @@ package x509
+ 
+ // Possible certificate files; stop after finding one.
+ var certFiles = []string{
+-	"/usr/local/etc/ssl/cert.pem",            // FreeBSD
+-	"/etc/ssl/cert.pem",                      // OpenBSD
+-	"/usr/local/share/certs/ca-root-nss.crt", // DragonFly
+-	"/etc/openssl/certs/ca-certificates.crt", // NetBSD
++	"/raven/share/certs/ca-root-nss.crt", 
+ }
+ 
+ // Possible directories with certificate files; all will be read.
+ var certDirectories = []string{
+-	"/etc/ssl/certs",         // FreeBSD 12.2+
+-	"/usr/local/share/certs", // FreeBSD
+-	"/etc/openssl/certs",     // NetBSD
++	"/raven/share/certs",
++	"/etc/ssl/certs",	// FreeBSD 12+
+ }
---
+++ bucket_77/golang/patches/patch-src_crypto_x509_root__linux.go
@@ -0,0 +1,23 @@
+--- src/crypto/x509/root_linux.go.orig	2024-03-29 15:27:02 UTC
++++ src/crypto/x509/root_linux.go
+@@ -8,18 +8,13 @@ import "internal/goos"
+ 
+ // Possible certificate files; stop after finding one.
+ var certFiles = []string{
+-	"/etc/ssl/certs/ca-certificates.crt",                // Debian/Ubuntu/Gentoo etc.
+-	"/etc/pki/tls/certs/ca-bundle.crt",                  // Fedora/RHEL 6
+-	"/etc/ssl/ca-bundle.pem",                            // OpenSUSE
+-	"/etc/pki/tls/cacert.pem",                           // OpenELEC
+-	"/etc/pki/ca-trust/extracted/pem/tls-ca-bundle.pem", // CentOS/RHEL 7
+-	"/etc/ssl/cert.pem",                                 // Alpine Linux
++	"/raven/share/certs/ca-root-nss.crt",
+ }
+ 
+ // Possible directories with certificate files; all will be read.
+ var certDirectories = []string{
++	"/raven/share/certs",
+ 	"/etc/ssl/certs",     // SLES10/SLES11, https://golang.org/issue/12139
+-	"/etc/pki/tls/certs", // Fedora/RHEL
+ }
+ 
+ func init() {
