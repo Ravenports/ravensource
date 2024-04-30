06eba03e85db0e3d2b950f0966333dc7d630bb17
John Marino <draco@marino.st>
2024-04-30 03:59:39 +0000

    add script to package golang bootstrap compiler

--- Scripts/assembler/create_golang_bootstrap.sh
+++ Scripts/assembler/create_golang_bootstrap.sh
@@ -1,21 +1,22 @@
 #!/bin/sh
 #
-# This script build a bootstrap package for a specific version of OpenJDK
+# This script build a bootstrap package for a specific version of Go
 # and specific to the the platform that the package is built on.
 # It accepts three arguments:
 #   1) Version in the format "x.yy.z
 #   2) optional: revision + epoch value
 #
-# These value correspond to the latest version of OpenJDK in the repository.
+# These value correspond to the packaged version of Go that will be
+# repackaged as a bootstrap compiler.
 #
-# e.g. <DISTDIR>/openjdk18-primary-standard-18.0.2_1.tzst
-# would require invocation of: create_openjdk_bootstrap.sh 18.0.2 1
+# e.g. <DISTDIR>/golang-single-standard-1.22.2.tzst
+# would require invocation of: create_golang_bootstrap.sh 1.22.2
 #
-# Output: <DISTDIR}/bootstrap-openjdk-18.0.2-dragonfly-x86_64.tzst (dragonfly)
-#         <DISTDIR}/bootstrap-openjdk-18.0.2-freebsd-amd64.tzst    (freebsd)
+# Output: <DISTDIR}/bootstrap-golang-1.22.2-dragonfly-x86_64.tzst (dragonfly)
+#         <DISTDIR}/bootstrap-golang-1.22.2-freebsd-amd64.tzst    (freebsd)
 
 if [ $# -lt 1 ]; then
-   echo "Usage: $0 openjdk_version <revision,epoch>"
+   echo "Usage: $0 golang_version <revision,epoch>"
    exit 0
 fi
 REVEPOCH=
@@ -28,11 +29,10 @@ SCRIPTSDIR=$(cd ${THISDIR} && pwd -P)
 DISTDIR=$(/raven/bin/ravenadm dev info F)
 BBASE=$(/raven/bin/ravenadm dev info J)
 PKGDIR=$(/raven/bin/ravenadm dev info H)/All
-ASSY="${BBASE}/openjdk-assy"
+ASSY="${BBASE}/golang-assy"
 OPSYS=$(uname -s)
 MYTAR="/raven/share/raven/sysroot/${OPSYS}/usr/bin/tar"
-MAJOR=$(echo "${1}" | awk -F'.' '{print $1}')
-RPKG="${PKGDIR}/openjdk${MAJOR}-primary-standard-${1}${REVEPOCH}.tzst"
+RPKG="${PKGDIR}/golang-single-standard-${1}${REVEPOCH}.tzst"
 
 rm -rf ${ASSY}
 
@@ -42,17 +42,25 @@ if [ ! -f ${RPKG} ]; then
 fi
 
 TARGET=$(/raven/sbin/ravensw info -F "${RPKG}" | awk '/^Architecture/ {n=split($3,t,":"); arch = length(t) == 4 ? t[3] "_" t[4] : t[3]; print t[1] "-" arch}')
-TPKG="bootstrap-openjdk-${1}-${TARGET}.tzst"
+TPKG="bootstrap-golang-${1}-${TARGET}.tzst"
 
 mkdir -p ${ASSY}
 echo "Extracting ${RPKG} ..."
 (cd ${ASSY} && ${MYTAR} -xf ${RPKG})
-echo "Bootstrap javac at ${ASSY}/raven/openjdk${MAJOR}/bin"
+echo "Bootstrap Go at ${ASSY}/raven"
+# remove documentation
+rm -rf ${ASSY}/raven/go/api
+rm -rf ${ASSY}/raven/go/doc
+rm ${ASSY}/raven/go/CONTRIBUTING.md
+rm ${ASSY}/raven/go/LICENSE
+rm ${ASSY}/raven/go/PATENTS
+rm ${ASSY}/raven/go/SECURITY.md
+find ${ASSY}/raven/go \( -name "*.html" -o -name "*.css" -o -name "*.js" -o -name "*.png" -o -name "README*" \) -delete
 sleep 2
 
 rm -f ${DISTDIR}/${RPKG}
 echo "Creating and relocating ${TPKG} to ${DISTDIR} directory"
-(cd ${ASSY}/raven && ${MYTAR} -c --zstd -f ${TPKG} openjdk${MAJOR}) && \
+(cd ${ASSY}/raven && ${MYTAR} -c --zstd -f ${TPKG} bin go) && \
 mv ${ASSY}/raven/${TPKG} ${DISTDIR}/
 
 echo "cleaning everything up."
