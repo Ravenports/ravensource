3197c9deba49bb3c18c57c245b745c0e0ec66e97
John Marino <draco@marino.st>
2024-05-02 04:31:27 +0000

    webkit2: try to fix build on netbsd

---
+++ bucket_58/webkit2/patches/patch-Source_ThirdParty_ANGLE_src_common_system__utils__linux.cpp
@@ -0,0 +1,13 @@
+--- Source/ThirdParty/ANGLE/src/common/system_utils_linux.cpp.orig	2024-03-16 06:45:47 UTC
++++ Source/ThirdParty/ANGLE/src/common/system_utils_linux.cpp
+@@ -56,6 +56,10 @@ void SetCurrentThreadName(const char *na
+ {
+     // There's a 15-character (16 including '\0') limit.  If the name is too big (and ERANGE is
+     // returned), just ignore the name.
++if defined(__NetBSD__)
++    pthread_setname_np(pthread_self(), "%s", (void *)name);
++#else
+     pthread_setname_np(pthread_self(), name);
++#endif
+ }
+ }  // namespace angle
