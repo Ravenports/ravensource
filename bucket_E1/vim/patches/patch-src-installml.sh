1457b6a316692170e3b3b53381d3e0bab38ab46f
Michael Reim <kraileth@elderlinux.org>
2024-05-11 08:29:51 +0200

    vim: upgrade

--- bucket_E1/vim/patches/patch-src-installml.sh
+++ bucket_E1/vim/patches/patch-src-installml.sh
@@ -1,4 +1,4 @@
---- src/installml.sh.orig	2024-04-19 13:26:21 UTC
+--- src/installml.sh.orig	2024-05-03 16:27:51 UTC
 +++ src/installml.sh
 @@ -53,16 +53,16 @@ if test $what = "install" -a \( -f $dest
     fi
