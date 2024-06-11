74237bcb0a1ca29765dea2ae9e7ca17952efa170
Michael Reim <kraileth@elderlinux.org>
2024-06-11 20:49:35 +0200

    xfce4-power-manager: upgrade

--- bucket_5E/xfce4-power-manager/patches/patch-src_xfpm-pm-helper.c
+++ bucket_5E/xfce4-power-manager/patches/patch-src_xfpm-pm-helper.c
@@ -1,4 +1,4 @@
---- src/xfpm-pm-helper.c.orig	2023-05-08 18:20:55 UTC
+--- src/xfpm-pm-helper.c.orig	2023-11-30 15:37:59 UTC
 +++ src/xfpm-pm-helper.c
 @@ -77,6 +77,10 @@
  #define UP_BACKEND_SUSPEND_COMMAND  "/usr/sbin/zzz"
