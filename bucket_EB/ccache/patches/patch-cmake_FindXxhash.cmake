cb98ab0c53136b0e70982dafa4e0a0f707fe545e
John Marino <draco@marino.st>
2024-06-17 13:12:31 +0000

    ccache: upgrade

---
+++ bucket_EB/ccache/patches/patch-cmake_FindXxhash.cmake
@@ -0,0 +1,11 @@
+--- cmake/FindXxhash.cmake.orig	2024-05-29 20:00:36 UTC
++++ cmake/FindXxhash.cmake
+@@ -39,7 +39,7 @@ if(_download_xxhash)
+   include(FetchContent)
+   FetchContent_Declare(
+     Xxhash
+-    URL "https://github.com/Cyan4973/xxhash/archive/refs/tags/v${_xxhash_version_string}.tar.gz"
++    URL "file:///distfiles/ccache/v${_xxhash_version_string}.tar.gz"
+     URL_HASH SHA256=baee0c6afd4f03165de7a4e67988d16f0f2b257b51d0e3cb91909302a26a79c4
+   )
+ 
---
+++ bucket_EB/ccache/patches/patch-cmake_FindZstd.cmake
@@ -0,0 +1,11 @@
+--- cmake/FindZstd.cmake.orig	2024-05-29 20:00:36 UTC
++++ cmake/FindZstd.cmake
+@@ -5,7 +5,7 @@ if(DEPS STREQUAL "DOWNLOAD" OR DEP_ZSTD
+   set(_download_zstd TRUE)
+ else()
+   find_path(ZSTD_INCLUDE_DIR zstd.h)
+-  find_library(ZSTD_LIBRARY zstd)
++  find_library(ZSTD_LIBRARY zstd_pic)
+   if(ZSTD_INCLUDE_DIR AND ZSTD_LIBRARY)
+     file(READ "${ZSTD_INCLUDE_DIR}/zstd.h" _zstd_h)
+     string(REGEX MATCH "#define ZSTD_VERSION_MAJOR +([0-9]+).*#define ZSTD_VERSION_MINOR +([0-9]+).*#define ZSTD_VERSION_RELEASE +([0-9]+)" _ "${_zstd_h}")
