a7d3e32a73c606df9d6e544b3c2a5971d593a293
John Marino <draco@marino.st>
2024-06-18 19:52:45 +0000

    libcmis: fix libxml2 fallout

---
+++ bucket_CF/libcmis/patches/patch-inc_libcmis_xml-utils.hxx
@@ -0,0 +1,10 @@
+--- inc/libcmis/xml-utils.hxx.orig	2023-11-13 20:06:58 UTC
++++ inc/libcmis/xml-utils.hxx
+@@ -37,6 +37,7 @@
+ #include <libxml/tree.h>
+ #include <libxml/xpathInternals.h>
+ #include <libxml/xmlwriter.h>
++#include <libxml/parser.h>
+ 
+ #include "libcmis/exception.hxx"
+ #include "libcmis/libcmis-api.h"
