19db71d996311c31e720abbdfcc1cf99ec870154
John Marino <draco@marino.st>
2024-06-18 12:07:57 +0000

    nsd: untested fix for freebsd

---
+++ bucket_16/nsd/patches/patch-simdzone_src_generic_endian.h
@@ -0,0 +1,11 @@
+--- simdzone/src/generic/endian.h.orig	2024-06-13 10:24:08 UTC
++++ simdzone/src/generic/endian.h
+@@ -73,6 +73,8 @@
+ #define le32toh(x) OSSwapLittleToHostInt32(x)
+ #define le64toh(x) OSSwapLittleToHostInt64(x)
+ 
++#elif defined(__FreeBSD__)
++#include <sys/endian.h>
+ #else
+ #include <endian.h>
+ #endif
