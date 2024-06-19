fecbeab5c746c3ce6ab3afac6044db4a036eca21
John Marino <draco@marino.st>
2024-06-19 21:47:01 +0000

    perl modules: replace 536 option with 540 option
    Drop 3 orphaned cpan modules (rather than add to top level)
    
    Requires ravenadm 2.23 (not yet released)

--- Scripts/cpan/scrape_cpan.php
+++ Scripts/cpan/scrape_cpan.php
@@ -8,9 +8,9 @@ $CPAN_CACHE   = "/var/cache/cpan";
 $SPECS_DIR    = $CPAN_CACHE . "/specs";
 $EXTS         = array("tgz" => ".tar.gz", "zip" => ".zip", "tbz" =>".tar.bz2");
 
-$VA           = 536;    # single point of change when perl
-$VB           = 538;    # series are changed in ravenports
-$AUTOPERL     = "5.38";
+$VA           = 538;    # single point of change when perl
+$VB           = 540;    # series are changed in ravenports
+$AUTOPERL     = "5.40";
 $ravensource_directory = "";
 $PERL_VERSION_A = "";
 $PERL_VERSION_B = "";
