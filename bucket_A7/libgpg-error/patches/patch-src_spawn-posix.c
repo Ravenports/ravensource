f9a60238b9202582a9d6864bafdf74fd1d50c9d2
John Marino <draco@marino.st>
2024-06-20 14:37:02 +0000

    libgpg-error: upgrade

---
+++ bucket_A7/libgpg-error/patches/patch-src_spawn-posix.c
@@ -0,0 +1,13 @@
+--- src/spawn-posix.c.orig	2024-06-19 07:33:41 UTC
++++ src/spawn-posix.c
+@@ -55,6 +55,10 @@
+ # include <dirent.h>
+ #endif /*__linux__ */
+ 
++#if defined(__FreeBSD__) || defined(__DragonFly__)
++extern char **environ;
++#endif
++
+ #include "gpgrt-int.h"
+ 
+ 
