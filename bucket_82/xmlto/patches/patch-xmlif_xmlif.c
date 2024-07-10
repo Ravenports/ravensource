dccb4e5cf4d957ca566fe5751d0c80ba09fc4097
John Marino <draco@marino.st>
2024-07-10 04:42:28 +0000

    xmlto: upgrade

--- bucket_82/xmlto/patches/patch-xmlif_xmlif.c
+++ bucket_82/xmlto/patches/patch-xmlif_xmlif.c
@@ -1,6 +1,6 @@
---- xmlif/xmlif.c.orig	2015-11-10 12:05:27 UTC
+--- xmlif/xmlif.c.orig	2024-06-11 18:42:54 UTC
 +++ xmlif/xmlif.c
-@@ -1306,7 +1306,7 @@ static void process_else()
+@@ -1300,7 +1300,7 @@ static void process_else()
  
  #define INITIAL 0
  #define attrib 1
@@ -9,17 +9,17 @@
  
  #ifndef YY_NO_UNISTD_H
  /* Special case for "unistd.h", since it is non-ANSI. We include it way
-@@ -1594,7 +1594,7 @@ YY_RULE_SETUP
+@@ -1597,7 +1597,7 @@ YY_RULE_SETUP
  case 8:
  YY_RULE_SETUP
- #line 217 "xmlif/xmlif.l"
+ #line 218 "xmlif/xmlif.l"
 -{BEGIN(val);}
 +{BEGIN(unique_val);}
  	YY_BREAK
  case 9:
  YY_RULE_SETUP
-@@ -1636,7 +1636,7 @@ ECHO;
- #line 1637 "xmlif/xmlif.c"
+@@ -1639,7 +1639,7 @@ ECHO;
+ #line 1639 "xmlif/xmlif.c"
  case YY_STATE_EOF(INITIAL):
  case YY_STATE_EOF(attrib):
 -case YY_STATE_EOF(val):
