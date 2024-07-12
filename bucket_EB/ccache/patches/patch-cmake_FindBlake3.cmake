61fd6f774186cb8a93e7dac86485ccc5dc78709a
John Marino <draco@marino.st>
2024-07-12 17:26:51 +0000

    ccache: fix build after fmt update

---
+++ bucket_EB/ccache/patches/patch-cmake_FindBlake3.cmake
@@ -0,0 +1,11 @@
+--- cmake/FindBlake3.cmake.orig	2024-06-30 18:46:01 UTC
++++ cmake/FindBlake3.cmake
+@@ -5,7 +5,7 @@ if(DEP_BLAKE3 STREQUAL "BUNDLED")
+   message(STATUS "Using bundled Blake3 as requested")
+ else()
+   find_path(BLAKE3_INCLUDE_DIR blake3.h)
+-  find_library(BLAKE3_LIBRARY blake3)
++  find_library(BLAKE3_LIBRARY blake3_pic)
+   if(BLAKE3_INCLUDE_DIR)
+     file(READ "${BLAKE3_INCLUDE_DIR}/blake3.h" _blake3_h)
+     string(REGEX MATCH "#define BLAKE3_VERSION_STRING \"([0-9]+).([0-9]+).*([0-9]+)\"" _ "${_blake3_h}")
