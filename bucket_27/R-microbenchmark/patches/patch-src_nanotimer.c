db7960339ed2698e330f8a0c05d7e03a4de1a651
John Marino <draco@marino.st>
2024-07-13 18:05:48 +0000

    blind fix for R-microbenchmark build on FreeBSD

---
+++ bucket_27/R-microbenchmark/patches/patch-src_nanotimer.c
@@ -0,0 +1,8 @@
+--- src/nanotimer.c.orig	2023-04-24 15:21:27 UTC
++++ src/nanotimer.c
+@@ -1,4 +1,5 @@
+ #include <stdint.h>
++#include <time.h>
+ #include <R.h>
+ #include <Rmath.h>
+ #include <Rinternals.h>
