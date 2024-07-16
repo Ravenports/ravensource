1c5b28ccb1c76ecb3beae302c98b410f21629116
John Marino <draco@marino.st>
2024-07-16 04:49:04 +0000

    imlib2: upgrade

--- bucket_38/imlib2/patches/patch-src_bin_imlib2__view.c
+++ bucket_38/imlib2/patches/patch-src_bin_imlib2__view.c
@@ -1,14 +1,12 @@
-$NetBSD: patch-src_bin_imlib2__view.c,v 1.1 2020/02/20 15:14:57 nia Exp $
-
 Include sys/time.h for timeval.
 
---- src/bin/imlib2_view.c.orig	2024-02-03 15:58:21 UTC
+--- src/bin/imlib2_view.c.orig	2024-07-06 09:58:30 UTC
 +++ src/bin/imlib2_view.c
 @@ -3,6 +3,7 @@
  
  #include <X11/Xlib.h>
  #include <X11/keysym.h>
 +#include <sys/time.h>
+ #include <poll.h>
  #include <stdio.h>
  #include <stdlib.h>
- #include <string.h>
