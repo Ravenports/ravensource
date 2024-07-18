200e7c56f829bf3f835909dfdeeb0a85bdc99cc2
John Marino <draco@marino.st>
2024-07-18 21:05:27 +0000

    add mozjs128 (newest spidermonkey version)

---
+++ bucket_6D/mozjs128/patches/patch-testing_mozbase_mozinfo_mozinfo_mozinfo.py
@@ -0,0 +1,11 @@
+--- ../../testing/mozbase/mozinfo/mozinfo/mozinfo.py.orig	2024-07-04 16:20:34 UTC
++++ ../../testing/mozbase/mozinfo/mozinfo/mozinfo.py
+@@ -106,7 +106,7 @@ elif system == "Linux":
+ 
+     info["os"] = "linux"
+     info["linux_distro"] = distribution
+-elif system in ["DragonFly", "FreeBSD", "NetBSD", "OpenBSD"]:
++elif system in ["DragonFly", "FreeBSD", "NetBSD", "OpenBSD", "MidnightBSD"]:
+     info["os"] = "bsd"
+     version = os_version = sys.platform
+ elif system == "Darwin":
