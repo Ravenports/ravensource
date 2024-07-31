40128e1d2c8b3cde8db64ad1552f4927b1f334b8
John Marino <draco@marino.st>
2024-07-31 03:08:02 +0000

    rspamd: upgrade

--- bucket_58/rspamd/patches/patch-contrib_backward-cpp_backward.hpp
+++ bucket_58/rspamd/patches/patch-contrib_backward-cpp_backward.hpp
@@ -1,4 +1,4 @@
---- contrib/backward-cpp/backward.hpp.orig	2024-02-22 16:24:56 UTC
+--- contrib/backward-cpp/backward.hpp.orig	2024-07-23 10:37:27 UTC
 +++ contrib/backward-cpp/backward.hpp
 @@ -415,6 +415,7 @@ extern "C" uintptr_t _Unwind_GetIPInfo(_
  #ifdef BACKWARD_ATLEAST_CXX11
