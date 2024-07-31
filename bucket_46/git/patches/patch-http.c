becc7339f87758ae319dfffcf0a87bc5bf2cb134
John Marino <draco@marino.st>
2024-07-31 02:34:11 +0000

    git: upgrade and merge variants
    introduce gui, svn, and contrib subpackages to allow the convergence
    of the two gui variants.

--- bucket_46/git/patches/patch-http.c
+++ bucket_46/git/patches/patch-http.c
@@ -1,14 +1,14 @@
---- http.c.orig	2024-04-30 15:08:30 UTC
+--- http.c.orig	2024-07-29 14:24:50 UTC
 +++ http.c
-@@ -2183,7 +2183,11 @@ static int http_request_reauth(const cha
- 			return HTTP_START_FAILED;
- 		}
- 		rewind(result);
+@@ -2273,7 +2273,11 @@ static int http_request_reauth(const cha
+ 				return HTTP_START_FAILED;
+ 			}
+ 			rewind(result);
 +#if defined(__MidnightBSD__)
-+		if (ftruncate(fileno((FILE *)result), 0) < 0) {
++			if (ftruncate(fileno((FILE *)result), 0) < 0) {
 +#else
- 		if (ftruncate(fileno(result), 0) < 0) {
+ 			if (ftruncate(fileno(result), 0) < 0) {
 +#endif
- 			error_errno("unable to truncate a file");
- 			return HTTP_START_FAILED;
- 		}
+ 				error_errno("unable to truncate a file");
+ 				return HTTP_START_FAILED;
+ 			}
