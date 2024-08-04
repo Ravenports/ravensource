d1bc91a141a60328dc188359482286a54d6c3e4c
John Marino <draco@marino.st>
2024-08-04 15:09:04 +0000

    mark libgtop broken on netbsd -- somebody needs to add a bunch of patches for it

--- bucket_56/libgtop/patches/patch-sysdeps_common_mountlist.c
+++ bucket_56/libgtop/patches/patch-sysdeps_common_mountlist.c
@@ -9,7 +9,19 @@
  static const char *
  fstype_to_string (short t)
  {
-@@ -309,7 +309,7 @@ read_filesystem_list (void)
+@@ -298,7 +298,11 @@ read_filesystem_list (void)
+ 
+ #ifdef MOUNTED_GETMNTINFO	/* 4.4BSD.  */
+   {
++#if defined(STAT_STATVFS) && !defined(__DragonFly__)
++    struct statvfs *fsp;
++#else
+     struct statfs *fsp;
++#endif
+     int entries;
+ 
+     entries = getmntinfo (&fsp, MNT_NOWAIT);
+@@ -309,7 +313,7 @@ read_filesystem_list (void)
  	me = (struct mount_entry *) g_malloc (sizeof (struct mount_entry));
  	me->me_devname = g_strdup (fsp->f_mntfromname);
  	me->me_mountdir = g_strdup (fsp->f_mntonname);
