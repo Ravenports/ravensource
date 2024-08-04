d8768f48a6d315f0a0d2864281c8a00e77ad0118
John Marino <draco@marino.st>
2024-08-04 15:17:20 +0000

    p7zip: remove netbsd-9 specific patch to fix build on netbsd 10

--- bucket_06/p7zip/patches/patch-CPP_7zip_Archive_Zip_ZipItem.cpp
+++ bucket_06/p7zip/patches/patch-CPP_7zip_Archive_Zip_ZipItem.cpp
@@ -4,7 +4,7 @@
        size_t dlen = slen * 4;
        const char* dest = s_utf8.GetBuf_SetEnd(dlen + 1); // (source length * 4) + null termination
  
-+#if defined(__NetBSD__) || defined(__sun)
++#if defined(__sun)
 +      size_t done = iconv(cd, (const char**)&src, &slen, (char**)&dest, &dlen);
 +#else
        size_t done = iconv(cd, (char**)&src, &slen, (char**)&dest, &dlen);
