7c0984982670202f0f02c8f7d2239f3dbd2fd868
John Marino <draco@marino.st>
2024-08-05 22:18:22 +0000

    vim: upgrade

--- bucket_E1/vim/patches/patch-src-installml.sh
+++ bucket_E1/vim/patches/patch-src-installml.sh
@@ -1,4 +1,4 @@
---- src/installml.sh.orig	2024-06-18 19:01:23 UTC
+--- src/installml.sh.orig	2024-08-05 18:24:11 UTC
 +++ src/installml.sh
 @@ -53,16 +53,16 @@ if test $what = "install" -a \( -f $dest
     fi
