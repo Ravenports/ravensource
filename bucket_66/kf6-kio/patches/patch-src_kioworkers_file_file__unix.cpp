a323e1767ae41f0c01a89c40389fef97ad03793c
John Marino <draco@marino.st>
2024-08-13 19:11:03 +0000

    upgrade kf6 framework

--- bucket_66/kf6-kio/patches/patch-src_kioworkers_file_file__unix.cpp
+++ bucket_66/kf6-kio/patches/patch-src_kioworkers_file_file__unix.cpp
@@ -1,6 +1,6 @@
---- src/kioworkers/file/file_unix.cpp.orig	2024-04-05 10:57:02 UTC
+--- src/kioworkers/file/file_unix.cpp.orig	2024-08-02 11:13:59 UTC
 +++ src/kioworkers/file/file_unix.cpp
-@@ -530,6 +530,9 @@ WorkerResult FileProtocol::tryChangeFile
+@@ -415,6 +415,9 @@ WorkerResult FileProtocol::tryChangeFile
  #if HAVE_SYS_XATTR_H || HAVE_SYS_EXTATTR_H
  bool FileProtocol::copyXattrs(const int src_fd, const int dest_fd)
  {
@@ -10,7 +10,7 @@
      // Get the list of keys
      ssize_t listlen = 0;
      QByteArray keylist;
-@@ -645,6 +648,7 @@ bool FileProtocol::copyXattrs(const int
+@@ -530,6 +533,7 @@ bool FileProtocol::copyXattrs(const int
          next_key();
      }
      return true;
