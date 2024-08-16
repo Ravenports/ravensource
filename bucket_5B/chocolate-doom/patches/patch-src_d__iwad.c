f9c95539a04ac946f9c2a53109b2458f5ac882ed
John Marino <draco@marino.st>
2024-08-16 01:50:24 +0000

    chocolate-doom: upgrade

--- bucket_5B/chocolate-doom/patches/patch-src_d__iwad.c
+++ bucket_5B/chocolate-doom/patches/patch-src_d__iwad.c
@@ -1,6 +1,6 @@
---- src/d_iwad.c.orig	2020-06-25 03:10:06 UTC
+--- src/d_iwad.c.intermediate	2024-08-16 01:30:22 UTC
 +++ src/d_iwad.c
-@@ -652,13 +652,14 @@ static void AddXdgDirs(void)
+@@ -677,13 +677,14 @@ static void AddXdgDirs(void)
      if (env == NULL)
      {
          // (Trailing / omitted from paths, as it is added below)
@@ -13,6 +13,6 @@
      // XDG_DATA_DIRS mechanism, through which it can be overridden.
      AddIWADPath(env, "/games/doom");
 +    AddIWADPath(env, "/games/doomengine");
+     AddIWADPath(env, "/doom");
  
      // The convention set by RBDOOM-3-BFG is to install Doom 3: BFG
-     // Edition into this directory, under which includes the Doom
