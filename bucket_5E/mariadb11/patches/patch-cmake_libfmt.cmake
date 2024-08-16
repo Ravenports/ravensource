92b771a8432dfe811dfd1379fd4fe03742d9b834
John Marino <draco@marino.st>
2024-08-16 04:36:06 +0000

    mariadb: upgrade several

--- bucket_5E/mariadb11/patches/patch-cmake_libfmt.cmake
+++ bucket_5E/mariadb11/patches/patch-cmake_libfmt.cmake
@@ -1,13 +1,11 @@
---- cmake/libfmt.cmake.orig	2024-05-26 18:15:37 UTC
+--- cmake/libfmt.cmake.orig	2024-08-05 08:49:38 UTC
 +++ cmake/libfmt.cmake
-@@ -15,8 +15,8 @@ MACRO(BUNDLE_LIBFMT)
+@@ -15,7 +15,7 @@ MACRO(BUNDLE_LIBFMT)
    ExternalProject_Add(
      libfmt
      PREFIX   "${dir}"
--    URL      "https://github.com/fmtlib/fmt/archive/refs/tags/8.0.1.zip"
--    URL_MD5  e77873199e897ca9f780479ad68e25b1
-+    URL      "file:///distfiles/fmtlib-fmt-8.0.1.tar.gz"
-+    URL_MD5  7d5af964c6633ef90cd6a47be3afe6a0
+-    URL      "https://github.com/fmtlib/fmt/releases/download/11.0.1/fmt-11.0.1.zip"
++    URL      "file:///distfiles/fmt-11.0.1.zip"
+     URL_MD5  5f3915e2eff60e7f70c558120592100d
      INSTALL_COMMAND ""
      CONFIGURE_COMMAND ""
-     BUILD_COMMAND ""
