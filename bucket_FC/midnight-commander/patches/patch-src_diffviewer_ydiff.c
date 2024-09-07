db5c1c157c1c28bb8ef918f0e735b53f195c7903
John Marino <draco@marino.st>
2024-09-07 22:23:24 +0000

    midnight-commander: upgrade

--- bucket_FC/midnight-commander/patches/patch-src_diffviewer_ydiff.c
+++ bucket_FC/midnight-commander/patches/patch-src_diffviewer_ydiff.c
@@ -1,9 +1,9 @@
---- src/diffviewer/ydiff.c.orig	2023-08-06 13:26:05 UTC
+--- src/diffviewer/ydiff.c.orig	2024-08-08 07:49:18 UTC
 +++ src/diffviewer/ydiff.c
-@@ -822,7 +822,7 @@ dff_execute (const char *args, const cha
+@@ -821,7 +821,7 @@ dff_execute (const char *args, const cha
      /* escape potential $ to avoid shell variable substitutions in popen() */
-     file1_esc = strutils_shell_escape (file1);
-     file2_esc = strutils_shell_escape (file2);
+     file1_esc = str_shell_escape (file1);
+     file2_esc = str_shell_escape (file2);
 -    cmd = g_strdup_printf ("diff %s %s %s %s %s", args, extra, opt, file1_esc, file2_esc);
 +    cmd = g_strdup_printf ("gdiff %s %s %s %s %s", args, extra, opt, file1_esc, file2_esc);
      g_free (file1_esc);
