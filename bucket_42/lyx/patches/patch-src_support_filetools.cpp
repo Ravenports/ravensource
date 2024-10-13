bce99c74c9f3a8d5dbaafaee05388bc52d905d5a
John Marino <draco@marino.st>
2024-10-13 21:06:01 +0000

    lyx: upgrade

--- bucket_42/lyx/patches/patch-src_support_filetools.cpp
+++ bucket_42/lyx/patches/patch-src_support_filetools.cpp
@@ -1,4 +1,4 @@
---- src/support/filetools.cpp.orig	2024-05-25 21:51:22 UTC
+--- src/support/filetools.cpp.orig	2024-09-24 22:21:37 UTC
 +++ src/support/filetools.cpp
 @@ -64,6 +64,8 @@
  #include <sstream>
