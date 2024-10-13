e817022292bcb58ff9e0f145c8b3db803306cc12
John Marino <draco@marino.st>
2024-10-13 00:36:50 +0000

    mpv: upgrade

--- bucket_15/mpv/patches/patch-osdep_threads-posix.h
+++ bucket_15/mpv/patches/patch-osdep_threads-posix.h
@@ -2,11 +2,11 @@ $NetBSD: patch-osdep_threads-posix.h,v 1.1 2024/01/01 15:50:14 leot Exp $
 
 Add support for NetBSD.
 
---- osdep/threads-posix.h.orig	2023-12-31 11:13:03.078250654 +0000
+--- osdep/threads-posix.h.orig	2024-09-23 17:36:41 UTC
 +++ osdep/threads-posix.h
-@@ -232,6 +232,8 @@ static inline void mp_thread_set_name(co
+@@ -236,6 +236,8 @@ static inline void mp_thread_set_name(co
      pthread_set_name_np(pthread_self(), name);
- #elif HAVE_OSX_THREAD_NAME
+ #elif HAVE_MAC_THREAD_NAME
      pthread_setname_np(name);
 +#elif HAVE_NETBSD_THREAD_NAME
 +    pthread_setname_np(pthread_self(), "%s", name);
