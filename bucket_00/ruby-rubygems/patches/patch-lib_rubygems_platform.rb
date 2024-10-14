84d378a6147d8a3063e6bdd69f74204f0b97042f
John Marino <draco@marino.st>
2024-10-14 12:06:35 +0000

    ruby-rubygems: upgrade

--- bucket_00/ruby-rubygems/patches/patch-lib_rubygems_platform.rb
+++ bucket_00/ruby-rubygems/patches/patch-lib_rubygems_platform.rb
@@ -1,4 +1,4 @@
---- lib/rubygems/platform.rb.orig	2024-09-24 18:16:16 UTC
+--- lib/rubygems/platform.rb.orig	2024-10-03 18:08:40 UTC
 +++ lib/rubygems/platform.rb
 @@ -132,6 +132,7 @@ class Gem::Platform
                          @cpu = "x86" if @cpu.nil? && os =~ /32$/
