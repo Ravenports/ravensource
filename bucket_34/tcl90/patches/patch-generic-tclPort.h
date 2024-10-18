c448a99f98be4af1f97dc518df0c3b1eeae7f5b5
John Marino <draco@marino.st>
2024-10-18 00:43:45 +0000

    first draft of tcl90

---
+++ bucket_34/tcl90/patches/patch-generic-tclPort.h
@@ -0,0 +1,11 @@
+--- generic/tclPort.h.orig	2024-08-28 16:34:22 UTC
++++ generic/tclPort.h
+@@ -20,7 +20,7 @@
+ #if defined(_WIN32)
+ #   include "tclWinPort.h"
+ #else
+-#   include "tclUnixPort.h"
++#   include "../unix/tclUnixPort.h"
+ #endif
+ #include "tcl.h"
+ 
