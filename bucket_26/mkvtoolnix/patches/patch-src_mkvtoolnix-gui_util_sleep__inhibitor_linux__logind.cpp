3947950c6a8f6e12c4e3bb236e5ca06d0abc8656
John Marino <draco@marino.st>
2024-10-19 19:05:55 +0000

    mkvtoolnix: upgrade

--- bucket_26/mkvtoolnix/patches/patch-src_mkvtoolnix-gui_util_sleep__inhibitor_linux__logind.cpp
+++ bucket_26/mkvtoolnix/patches/patch-src_mkvtoolnix-gui_util_sleep__inhibitor_linux__logind.cpp
@@ -1,4 +1,4 @@
---- src/mkvtoolnix-gui/util/sleep_inhibitor/linux_logind.cpp.orig	2024-09-07 11:10:02 UTC
+--- src/mkvtoolnix-gui/util/sleep_inhibitor/linux_logind.cpp.orig	2024-10-19 13:14:27 UTC
 +++ src/mkvtoolnix-gui/util/sleep_inhibitor/linux_logind.cpp
 @@ -1,6 +1,6 @@
  #include "common/common_pch.h"
