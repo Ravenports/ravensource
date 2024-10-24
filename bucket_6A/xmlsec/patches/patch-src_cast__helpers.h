83e7a8d0221f36f70152832787e0cb21af70fd6f
John Marino <draco@marino.st>
2024-10-24 19:29:05 +0000

    xmlsec: upgrade

--- bucket_6A/xmlsec/patches/patch-src_cast__helpers.h
+++ bucket_6A/xmlsec/patches/patch-src_cast__helpers.h
@@ -1,4 +1,4 @@
---- src/cast_helpers.h.orig	2024-04-09 14:46:44 UTC
+--- src/cast_helpers.h.orig	2024-10-22 13:05:39 UTC
 +++ src/cast_helpers.h
 @@ -16,6 +16,7 @@
  #error "cast_helpers.h file contains private xmlsec definitions and should not be used outside xmlsec or xmlsec-$crypto libraries"
