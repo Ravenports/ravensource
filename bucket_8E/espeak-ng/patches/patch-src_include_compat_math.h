44caa87783c03134996050dff5d56b4cad7dcaaa
John Marino <draco@marino.st>
2024-10-27 16:09:32 +0000

    espeak-ng: fix build on netbsd

---
+++ bucket_8E/espeak-ng/patches/patch-src_include_compat_math.h
@@ -0,0 +1,11 @@
+--- src/include/compat/math.h.orig	2022-06-21 14:53:53 UTC
++++ src/include/compat/math.h
+@@ -19,6 +19,8 @@
+ #ifndef MATH_H_COMPAT_SHIM
+ #define MATH_H_COMPAT_SHIM
+ 
++#define _NETBSD_SOURCE
++
+ #ifdef _MSC_VER
+ #define _USE_MATH_DEFINES // For M_PI, etc.
+ #if _MSC_VER >= 1900 // Visual C++ 14 (Visual Studio 2015) and above...
