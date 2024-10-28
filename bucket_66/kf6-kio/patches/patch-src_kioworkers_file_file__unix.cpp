94f70f92c43e01e888d26b21a64e7f73679c5757
John Marino <draco@marino.st>
2024-10-28 01:54:29 +0100

    kf6-kio: attempt to fix build on netbsd

--- bucket_66/kf6-kio/patches/patch-src_kioworkers_file_file__unix.cpp
+++ bucket_66/kf6-kio/patches/patch-src_kioworkers_file_file__unix.cpp
@@ -1,6 +1,6 @@
---- src/kioworkers/file/file_unix.cpp.orig	2024-08-02 11:13:59 UTC
+--- src/kioworkers/file/file_unix.cpp.orig	2024-10-04 15:07:52 UTC
 +++ src/kioworkers/file/file_unix.cpp
-@@ -415,6 +415,9 @@ WorkerResult FileProtocol::tryChangeFile
+@@ -413,6 +413,9 @@ WorkerResult FileProtocol::tryChangeFile
  #if HAVE_SYS_XATTR_H || HAVE_SYS_EXTATTR_H
  bool FileProtocol::copyXattrs(const int src_fd, const int dest_fd)
  {
@@ -10,7 +10,7 @@
      // Get the list of keys
      ssize_t listlen = 0;
      QByteArray keylist;
-@@ -530,6 +533,7 @@ bool FileProtocol::copyXattrs(const int
+@@ -528,6 +531,7 @@ bool FileProtocol::copyXattrs(const int
          next_key();
      }
      return true;
@@ -18,3 +18,21 @@
  }
  #endif // HAVE_SYS_XATTR_H || HAVE_SYS_EXTATTR_H
  
+@@ -949,7 +953,7 @@ WorkerResult FileProtocol::listDir(const
+     // qDebug() << "========= LIST " << url << "details=" << details << " =========";
+     UDSEntry entry;
+ 
+-#ifndef HAVE_DIRENT_D_TYPE
++#if !HAVE_DIRENT_D_TYPE
+     QT_STATBUF st;
+ #endif
+     QT_DIRENT *ep;
+@@ -970,7 +974,7 @@ WorkerResult FileProtocol::listDir(const
+          */
+         if (details == KIO::StatBasic) {
+             entry.fastInsert(KIO::UDSEntry::UDS_NAME, filename);
+-#ifdef HAVE_DIRENT_D_TYPE
++#if HAVE_DIRENT_D_TYPE
+             entry.fastInsert(KIO::UDSEntry::UDS_FILE_TYPE, (ep->d_type == DT_DIR) ? S_IFDIR : S_IFREG);
+             const bool isSymLink = (ep->d_type == DT_LNK);
+ #else
