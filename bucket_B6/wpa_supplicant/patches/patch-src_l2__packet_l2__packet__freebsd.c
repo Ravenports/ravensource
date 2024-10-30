0616a3236cf35767f5d4e4f182d334087817d266
John Marino <draco@marino.st>
2024-10-30 03:20:47 +0000

    wpa_supplicant: fix build on netbsd

--- bucket_B6/wpa_supplicant/patches/patch-src_l2__packet_l2__packet__freebsd.c
+++ bucket_B6/wpa_supplicant/patches/patch-src_l2__packet_l2__packet__freebsd.c
@@ -1,4 +1,4 @@
---- src/l2_packet/l2_packet_freebsd.c.orig	2022-01-16 20:51:29 UTC
+--- src/l2_packet/l2_packet_freebsd.c.orig	2024-07-20 18:04:37 UTC
 +++ src/l2_packet/l2_packet_freebsd.c
 @@ -8,7 +8,11 @@
   */
@@ -13,3 +13,13 @@
  #include <net/bpf.h>
  #endif /* __APPLE__ */
  #include <pcap.h>
+@@ -20,7 +24,9 @@
+ #include <sys/sysctl.h>
+ #endif /* __sun__ */
+ 
++#if !defined(__NetBSD__)
+ #include <net/ethernet.h>
++#endif
+ #include <net/if.h>
+ #include <net/if_dl.h>
+ #include <net/route.h>
