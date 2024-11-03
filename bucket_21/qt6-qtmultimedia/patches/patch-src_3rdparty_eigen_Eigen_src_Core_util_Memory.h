3f902e743efa596aad2fba2076db8306607c6255
John Marino <draco@marino.st>
2024-11-03 22:22:55 +0000

    qt6-qtmultimedia: ensure __builtin_alloca is used (for netbsd)

---
+++ bucket_21/qt6-qtmultimedia/patches/patch-src_3rdparty_eigen_Eigen_src_Core_util_Memory.h
@@ -0,0 +1,15 @@
+--- src/3rdparty/eigen/Eigen/src/Core/util/Memory.h.orig	2024-06-08 01:28:28 UTC
++++ src/3rdparty/eigen/Eigen/src/Core/util/Memory.h
+@@ -585,11 +585,7 @@ template<typename T> EIGEN_DEVICE_FUNC T
+ // you can overwrite Eigen's default behavior regarding alloca by defining EIGEN_ALLOCA
+ // to the appropriate stack allocation function
+ #if ! defined EIGEN_ALLOCA && ! defined EIGEN_GPU_COMPILE_PHASE
+-  #if EIGEN_OS_LINUX || EIGEN_OS_MAC || (defined alloca)
+-    #define EIGEN_ALLOCA alloca
+-  #elif EIGEN_COMP_MSVC
+-    #define EIGEN_ALLOCA _alloca
+-  #endif
++    #define EIGEN_ALLOCA __builtin_alloca
+ #endif
+ 
+ // With clang -Oz -mthumb, alloca changes the stack pointer in a way that is
