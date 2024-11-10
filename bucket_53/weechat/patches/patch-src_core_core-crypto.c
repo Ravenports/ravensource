fb0d67269ee2e8130121b98edde637c2e9d997ec
John Marino <draco@marino.st>
2024-11-10 00:41:47 +0000

    weechat: upgrade

--- bucket_53/weechat/patches/patch-src_core_core-crypto.c
+++ bucket_53/weechat/patches/patch-src_core_core-crypto.c
@@ -1,8 +1,8 @@
---- src/core/core-crypto.c.orig	2024-09-08 09:49:21 UTC
+--- src/core/core-crypto.c.orig	2024-10-30 11:13:22 UTC
 +++ src/core/core-crypto.c
-@@ -31,6 +31,10 @@
- #include <math.h>
- #include <gcrypt.h>
+@@ -37,6 +37,10 @@
+ #include <endian.h>
+ #endif
  
 +#if defined __DragonFly__ || defined __MidnightBSD__
 +#include <sys/endian.h>
