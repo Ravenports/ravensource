1c8f1acdedee08ac51141f83497231a8f1d546ae
John Marino <draco@marino.st>
2024-11-18 04:11:02 +0000

    arj: fix build on gcc14

--- bucket_7C/arj/patches/patch-environ.c
+++ bucket_7C/arj/patches/patch-environ.c
@@ -1,6 +1,11 @@
---- environ.c.orig	2021-10-27 00:22:50 UTC
+--- environ.c.orig	2024-11-18 04:07:44 UTC
 +++ environ.c
-@@ -58,7 +58,7 @@
+@@ -54,11 +54,12 @@
+  #include <utime.h>
+  #include <sys/time.h>                  /* LIBC high-resolution timing */
+  #include <sys/resource.h>              /* Priority control */
++ #include <sys/wait.h>
+  #if defined(linux)
    #include <sys/ioctl.h>
    #include <sys/statfs.h>
    #include <sys/statvfs.h>
@@ -9,7 +14,7 @@
    #include <sys/param.h>
    #include <sys/mount.h>
   #elif defined(__QNXNTO__)
-@@ -2061,7 +2061,7 @@ FILE *file_open(char *name, char *mode)
+@@ -2061,7 +2062,7 @@ FILE *file_open(char *name, char *mode)
        /* ASR fix 01/10/2003 -- re-fix to handle umask 022 correctly */
        if((handle=open(name, oflag, 0644))==-1)
         return(NULL);
@@ -18,7 +23,7 @@
        {
         close(handle);
         return(NULL);
-@@ -2289,7 +2289,7 @@ unsigned long file_getfree(char *name)
+@@ -2289,7 +2290,7 @@ unsigned long file_getfree(char *name)
    else
     return((LONG_MAX/(spclu*bps)<fclu)?LONG_MAX:spclu*bps*fclu);
   #elif TARGET==UNIX
@@ -27,7 +32,7 @@
     struct statvfs vfs;
  
     if(statvfs(name, &vfs)==-1)
-@@ -3008,7 +3008,7 @@ void get_exe_name(char *dest, char *arg)
+@@ -3008,7 +3009,7 @@ void get_exe_name(char *dest, char *arg)
      they are missing altogether, the corresponding code will gracefully
      terminate. */
   #if SFX_LEVEL==ARJ
@@ -36,7 +41,7 @@
   #elif SFX_LEVEL==ARJSFXV
    strcpy(dest, "./arjsfxv");
   #elif SFX_LEVEL==ARJSFX
-@@ -3016,7 +3016,7 @@ void get_exe_name(char *dest, char *arg)
+@@ -3016,7 +3017,7 @@ void get_exe_name(char *dest, char *arg)
   #elif SFX_LEVEL==ARJSFXJR
    strcpy(dest, "./arjsfxjr");
   #elif defined(REARJ)
