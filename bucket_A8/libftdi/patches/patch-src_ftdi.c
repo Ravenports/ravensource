95d0889658102cfa2e6917e101a0725b08fc8249
John Marino <draco@marino.st>
2024-11-18 02:16:26 +0000

    libftdi: fix build on gcc14

---
+++ bucket_A8/libftdi/patches/patch-src_ftdi.c
@@ -0,0 +1,10 @@
+--- src/ftdi.c.orig	2012-03-15 09:58:44 UTC
++++ src/ftdi.c
+@@ -32,6 +32,7 @@
+ #include <string.h>
+ #include <errno.h>
+ #include <stdio.h>
++#include <stdlib.h>
+ 
+ #include "ftdi.h"
+ 
