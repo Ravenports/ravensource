af8a5c73f0f2a017b2fb6d4a88a8599512a69f12
John Marino <draco@marino.st>
2024-11-18 01:26:08 +0000

    qt6-qtquick3d: fix on *BSD (caught by gcc14)

---
+++ bucket_D1/qt6-qtquick3d/patches/patch-src_3rdparty_assimp_src_contrib_unzip_ioapi.c
@@ -0,0 +1,11 @@
+--- src/3rdparty/assimp/src/contrib/unzip/ioapi.c.orig	2024-10-02 15:10:33 UTC
++++ src/3rdparty/assimp/src/contrib/unzip/ioapi.c
+@@ -14,7 +14,7 @@
+         #define _CRT_SECURE_NO_WARNINGS
+ #endif
+ 
+-#if defined(__APPLE__) || defined(IOAPI_NO_64) || defined(__HAIKU__) || defined(MINIZIP_FOPEN_NO_64)
++#if !defined(__linux__)
+ // In darwin and perhaps other BSD variants off_t is a 64 bit value, hence no need for specific 64 bit functions
+ #define FOPEN_FUNC(filename, mode) fopen(filename, mode)
+ #define FTELLO_FUNC(stream) ftello(stream)
