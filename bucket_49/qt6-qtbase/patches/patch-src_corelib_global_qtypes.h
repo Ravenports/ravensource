d8f231998eea85a923f09fbeb0423b232352c381
John Marino <draco@marino.st>
2024-11-18 16:30:34 +0000

    qt6 base: drop native float to avoid __extendhfsf2, add cups dev dep

---
+++ bucket_49/qt6-qtbase/patches/patch-src_corelib_global_qtypes.h
@@ -0,0 +1,28 @@
+Avoid __extendhfsf2 and friends
+
+--- src/corelib/global/qtypes.h.orig	2024-10-01 10:46:30 UTC
++++ src/corelib/global/qtypes.h
+@@ -271,23 +271,8 @@ using qsizetype = QIntegerForSizeof<std:
+ 
+ // Define a native float16 type
+ namespace QtPrivate {
+-#if defined(__STDCPP_FLOAT16_T__)
+-#  define QFLOAT16_IS_NATIVE        1
+-using NativeFloat16Type = std::float16_t;
+-#elif defined(Q_CC_CLANG) && defined(__FLT16_MAX__) && 0
+-// disabled due to https://github.com/llvm/llvm-project/issues/56963
+-#  define QFLOAT16_IS_NATIVE        1
+-using NativeFloat16Type = decltype(__FLT16_MAX__);
+-#elif defined(Q_CC_GNU_ONLY) && defined(__FLT16_MAX__) && defined(__ARM_FP16_FORMAT_IEEE)
+-#  define QFLOAT16_IS_NATIVE        1
+-using NativeFloat16Type = __fp16;
+-#elif defined(Q_CC_GNU_ONLY) && defined(__FLT16_MAX__) && defined(__SSE2__)
+-#  define QFLOAT16_IS_NATIVE        1
+-using NativeFloat16Type = _Float16;
+-#else
+ #  define QFLOAT16_IS_NATIVE        0
+ using NativeFloat16Type = void;
+-#endif
+ } // QtPrivate
+ 
+ #endif // __cplusplus
