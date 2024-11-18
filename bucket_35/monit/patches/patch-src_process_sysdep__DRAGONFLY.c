55268e912345e3ade87e916e899cc06b6686dfe0
John Marino <draco@marino.st>
2024-11-18 20:10:49 +0000

    monit: fix build on gcc14

--- bucket_35/monit/patches/patch-src_process_sysdep__DRAGONFLY.c
+++ bucket_35/monit/patches/patch-src_process_sysdep__DRAGONFLY.c
@@ -1,6 +1,14 @@
 --- src/process/sysdep_DRAGONFLY.c.orig	2024-10-11 11:02:44 UTC
 +++ src/process/sysdep_DRAGONFLY.c
-@@ -141,7 +141,8 @@ bool init_systeminfo_sysdep(void) {
+@@ -74,6 +74,7 @@
+ #endif
+ 
+ #include "monit.h"
++#include "system/Time.h"
+ #include "ProcessTree.h"
+ #include "process_sysdep.h"
+ 
+@@ -141,7 +142,8 @@ bool init_systeminfo_sysdep(void) {
   * @return treesize > 0 if succeeded otherwise 0.
   */
  int init_processtree_sysdep(ProcessTree_T **reference, ProcessEngine_Flags pflags) {
