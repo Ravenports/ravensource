2d6e347fb54011375195dba16961003f61221c10
John Marino <draco@marino.st>
2024-11-25 15:30:17 -0600

    expect: fix build on linux

--- bucket_FF/expect/patches/patch-pty__termios.c
+++ bucket_FF/expect/patches/patch-pty__termios.c
@@ -1,17 +1,21 @@
 --- pty_termios.c.orig	2018-02-02 19:15:52 UTC
 +++ pty_termios.c
-@@ -77,6 +77,10 @@ with openpty which supports 4000 while p
+@@ -77,6 +77,14 @@ with openpty which supports 4000 while p
  #include <sys/sysmacros.h>
  #endif
  
 +#ifdef HAVE_OPENPTY
-+#include <util.h>
++# ifdef __linux__
++# include <pty.h>
++# else
++# include <util.h>
++# endif
 +#endif
 +
  #ifdef HAVE_PTYTRAP
  #include <sys/ptyio.h>
  #endif
-@@ -102,6 +106,7 @@ with openpty which supports 4000 while p
+@@ -102,6 +110,7 @@ with openpty which supports 4000 while p
  #include "exp_tty_in.h"
  #include "exp_rename.h"
  #include "exp_pty.h"
