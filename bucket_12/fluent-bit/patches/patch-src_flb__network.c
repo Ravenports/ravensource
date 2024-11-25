d1767f9945343ad86fbd9e0e62d601f958ed044c
John Marino <draco@marino.st>
2024-11-25 17:05:28 -0600

    fluent-bit: fix build on linux

--- bucket_12/fluent-bit/patches/patch-src_flb__network.c
+++ bucket_12/fluent-bit/patches/patch-src_flb__network.c
@@ -1,26 +1,17 @@
 --- src/flb_network.c.orig	2024-11-17 03:19:24 UTC
 +++ src/flb_network.c
-@@ -603,9 +603,6 @@ static int net_connect_async(int fd,
+@@ -603,9 +603,7 @@ static int net_connect_async(int fd,
              }
  
              /* Connection is broken, not much to do here */
 -#if ((defined(_POSIX_C_SOURCE) && _POSIX_C_SOURCE >= 200112L) ||    \
 -     (defined(_XOPEN_SOURCE) || _XOPEN_SOURCE - 0L >= 600L)) &&     \
 -  (!defined(_GNU_SOURCE))
++#if !defined(_GNU_SOURCE)
              ret = strerror_r(error, so_error_buf, sizeof(so_error_buf));
              if (ret == 0) {
                  str = so_error_buf;
-@@ -614,9 +611,6 @@ static int net_connect_async(int fd,
-                 flb_errno();
-                 return -1;
-             }
--#else
--            str = strerror_r(error, so_error_buf, sizeof(so_error_buf));
--#endif
-             flb_error("[net] TCP connection failed: %s:%i (%s)",
-                       u->tcp_host, u->tcp_port, str);
-             return -1;
-@@ -1920,7 +1914,7 @@ static int net_address_unix_socket_peer_
+@@ -1920,7 +1918,7 @@ static int net_address_unix_socket_peer_
                                                  int output_buffer_size,
                                                  size_t *output_data_size)
  {
@@ -29,7 +20,7 @@
      unsigned int peer_credentials_size;
      struct ucred peer_credentials;
  #endif
-@@ -1938,7 +1932,7 @@ static int net_address_unix_socket_peer_
+@@ -1938,7 +1936,7 @@ static int net_address_unix_socket_peer_
          return -1;
      }
  
