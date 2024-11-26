d96c634a8da87ce5315a26cb2abb06e2f915a6ab
John Marino <draco@marino.st>
2024-11-26 17:01:05 -0600

    timidity: fix build on linux with gcc14

--- bucket_85/timidity/patches/patch-timidity_vorbis__a.c
+++ bucket_85/timidity/patches/patch-timidity_vorbis__a.c
@@ -1,10 +1,11 @@
 --- timidity/vorbis_a.c.orig	2008-04-01 02:35:08 UTC
 +++ timidity/vorbis_a.c
-@@ -27,6 +27,7 @@
- #endif /* HAVE_CONFIG_H */
+@@ -28,6 +28,8 @@
  #include "interface.h"
  #include <stdio.h>
-+#include <stdlib.h>
  #include <string.h>
++#include <stdlib.h>
++#include <time.h>
  
  #ifdef AU_VORBIS
+ 
