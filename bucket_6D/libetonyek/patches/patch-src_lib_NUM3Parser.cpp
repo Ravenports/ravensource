5903ca971314bb777951b367ef7207714142990e
Michael Reim <kraileth@elderlinux.org>
2024-12-01 08:39:57 +0100

    libetonyek: upgrade

--- bucket_6D/libetonyek/patches/patch-src_lib_NUM3Parser.cpp
+++ bucket_6D/libetonyek/patches/patch-src_lib_NUM3Parser.cpp
@@ -1,8 +1,8 @@
---- src/lib/NUM3Parser.cpp.orig	2018-12-28 17:45:35 UTC
+--- src/lib/NUM3Parser.cpp.orig	2024-11-01 18:04:41 UTC
 +++ src/lib/NUM3Parser.cpp
-@@ -8,6 +8,7 @@
-  */
+@@ -9,6 +9,7 @@
  
+ #include <algorithm>
  #include <functional>
 +#include <algorithm>
  
