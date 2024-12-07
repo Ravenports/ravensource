a20b78e56d97df4bd2ed67b31cb817bdf0cea7d4
John Marino <draco@marino.st>
2024-12-07 01:16:18 +0000

    mozjs128: upgrade

--- bucket_6D/mozjs128/patches/patch-testing_mozbase_mozinfo_mozinfo_mozinfo.py
+++ bucket_6D/mozjs128/patches/patch-testing_mozbase_mozinfo_mozinfo_mozinfo.py
@@ -1,4 +1,4 @@
---- ../../testing/mozbase/mozinfo/mozinfo/mozinfo.py.orig	2024-10-21 23:59:58 UTC
+--- ../../testing/mozbase/mozinfo/mozinfo/mozinfo.py.orig	2024-11-28 18:48:51 UTC
 +++ ../../testing/mozbase/mozinfo/mozinfo/mozinfo.py
 @@ -106,7 +106,7 @@ elif system == "Linux":
  
