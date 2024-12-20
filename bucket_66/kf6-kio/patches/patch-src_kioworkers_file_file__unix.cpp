f6141287b4460539bb735f5373569ed4573805a1
John Marino <draco@marino.st>
2024-12-20 22:30:33 +0000

    upgrade kf6 ports

--- bucket_66/kf6-kio/patches/patch-src_kioworkers_file_file__unix.cpp
+++ bucket_66/kf6-kio/patches/patch-src_kioworkers_file_file__unix.cpp
@@ -1,4 +1,4 @@
---- src/kioworkers/file/file_unix.cpp.orig	2024-10-04 15:07:52 UTC
+--- src/kioworkers/file/file_unix.cpp.orig	2024-12-06 11:52:22 UTC
 +++ src/kioworkers/file/file_unix.cpp
 @@ -413,6 +413,9 @@ WorkerResult FileProtocol::tryChangeFile
  #if HAVE_SYS_XATTR_H || HAVE_SYS_EXTATTR_H
@@ -18,21 +18,3 @@
  }
  #endif // HAVE_SYS_XATTR_H || HAVE_SYS_EXTATTR_H
  
-@@ -949,7 +953,7 @@ WorkerResult FileProtocol::listDir(const
-     // qDebug() << "========= LIST " << url << "details=" << details << " =========";
-     UDSEntry entry;
- 
--#ifndef HAVE_DIRENT_D_TYPE
-+#if !HAVE_DIRENT_D_TYPE
-     QT_STATBUF st;
- #endif
-     QT_DIRENT *ep;
-@@ -970,7 +974,7 @@ WorkerResult FileProtocol::listDir(const
-          */
-         if (details == KIO::StatBasic) {
-             entry.fastInsert(KIO::UDSEntry::UDS_NAME, filename);
--#ifdef HAVE_DIRENT_D_TYPE
-+#if HAVE_DIRENT_D_TYPE
-             entry.fastInsert(KIO::UDSEntry::UDS_FILE_TYPE, (ep->d_type == DT_DIR) ? S_IFDIR : S_IFREG);
-             const bool isSymLink = (ep->d_type == DT_LNK);
- #else
