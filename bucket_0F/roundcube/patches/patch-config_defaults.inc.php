0376618214afb2f7f3c9623617a033472b7dcdd2
John Marino <draco@marino.st>
2024-12-21 20:00:13 +0000

    roundcube: switch from pspell to enchant spellspeck (the former dropped from php84)

--- bucket_0F/roundcube/patches/patch-config_defaults.inc.php
+++ bucket_0F/roundcube/patches/patch-config_defaults.inc.php
@@ -1,8 +1,20 @@
---- config/defaults.inc.php.orig	2024-08-04 09:45:14 UTC
+--- config/defaults.inc.php.orig	2024-09-01 07:31:36 UTC
 +++ config/defaults.inc.php
-@@ -941,8 +941,8 @@ $config['spellcheck_dictionary'] = false
+@@ -925,7 +925,7 @@ $config['show_real_foldernames'] = false
+ $config['quota_zero_as_unlimited'] = false;
+ 
+ // Make use of the built-in spell checker.
+-$config['enable_spellcheck'] = false;
++$config['enable_spellcheck'] = true;
+ 
+ // Enables spellchecker exceptions dictionary.
+ // Setting it to 'shared' will make the dictionary shared by all users.
+@@ -939,10 +939,10 @@ $config['spellcheck_dictionary'] = false
+ // Since Google shut down their public spell checking service, the default settings
+ // connect to http://spell.roundcube.net which is a hosted service provided by Roundcube.
  // You can connect to any other googie-compliant service by setting 'spellcheck_uri' accordingly.
- $config['spellcheck_engine'] = 'googie';
+-$config['spellcheck_engine'] = 'googie';
++$config['spellcheck_engine'] = 'enchant';
  
 -// For locally installed Nox Spell Server or After the Deadline services,
 -// please specify the URI to call it.
