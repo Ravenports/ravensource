bedb51fa08bcb8e2dfc12bde06426cbedf1ff372
John Marino <draco@marino.st>
2025-01-05 05:31:21 +0000

    rofi: upgrade

--- bucket_99/rofi/patches/patch-source_rofi.c
+++ bucket_99/rofi/patches/patch-source_rofi.c
@@ -1,8 +1,8 @@
---- source/rofi.c.orig	2022-08-09 13:47:23 UTC
+--- source/rofi.c.orig	2024-12-31 22:52:29 UTC
 +++ source/rofi.c
-@@ -37,6 +37,9 @@
- #include <stdlib.h>
+@@ -38,6 +38,9 @@
  #include <string.h>
+ #include <sys/stat.h>
  #include <sys/types.h>
 +#if defined __FreeBSD__ || defined __NetBSD__
 +#include <sys/stat.h>
