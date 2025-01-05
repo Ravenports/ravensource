a0b73b8060ceaa77c8bd08bfe9c624351e60c4db
Michael Reim <kraileth@elderlinux.org>
2025-01-05 21:18:00 +0100

    Add musepack

---
+++ bucket_08/musepack/patches/patch-libmpcdec_requant.h
@@ -0,0 +1,15 @@
+--- libmpcdec/requant.h.orig	2009-02-23 19:44:04.000000000 +0100
++++ libmpcdec/requant.h	2025-01-05 20:57:23.780000000 +0100
+@@ -47,9 +47,9 @@
+ 
+ 
+ /* C O N S T A N T S */
+-const mpc_uint8_t      Res_bit [18];     ///< Bits per sample for chosen quantizer
+-const MPC_SAMPLE_FORMAT __Cc    [1 + 18]; ///< Requantization coefficients
+-const mpc_int16_t       __Dc    [1 + 18]; ///< Requantization offset
++extern const mpc_uint8_t      Res_bit [18];     ///< Bits per sample for chosen quantizer
++extern const MPC_SAMPLE_FORMAT __Cc    [1 + 18]; ///< Requantization coefficients
++extern const mpc_int16_t       __Dc    [1 + 18]; ///< Requantization offset
+ 
+ #define Cc (__Cc + 1)
+ #define Dc (__Dc + 1)
