9dbb1693a60a2e2ad529d49fdd93c8a1414f27ba
John Marino <draco@marino.st>
2025-01-05 01:33:12 +0000

    python-numpy: upgrade (v13 isn't building?)

--- bucket_E9/python-numpy/patches/patch-numpy_core_include_numpy_npy__math.h
+++ bucket_E9/python-numpy/patches/patch-numpy_core_include_numpy_npy__math.h
@@ -1,6 +1,6 @@
---- numpy/core/include/numpy/npy_math.h.orig	2024-02-05 21:17:48 UTC
-+++ numpy/core/include/numpy/npy_math.h
-@@ -285,7 +285,11 @@ NPY_INPLACE npy_longdouble npy_tanl(npy_
+--- numpy/_core/include/numpy/npy_math.h.orig	2024-12-21 18:26:32 UTC
++++ numpy/_core/include/numpy/npy_math.h
+@@ -288,7 +288,11 @@ NPY_INPLACE npy_longdouble npy_tanl(npy_
  NPY_INPLACE npy_longdouble npy_expl(npy_longdouble x);
  NPY_INPLACE npy_longdouble npy_sqrtl(npy_longdouble x);
  NPY_INPLACE npy_longdouble npy_hypotl(npy_longdouble x, npy_longdouble y);
@@ -12,7 +12,7 @@
  NPY_INPLACE npy_longdouble npy_atan2l(npy_longdouble x, npy_longdouble y);
  NPY_INPLACE npy_longdouble npy_powl(npy_longdouble x, npy_longdouble y);
  NPY_INPLACE npy_longdouble npy_modfl(npy_longdouble x, npy_longdouble* y);
-@@ -303,14 +307,22 @@ NPY_INPLACE npy_longdouble npy_modfl(npy
+@@ -306,14 +310,22 @@ NPY_INPLACE npy_longdouble npy_modfl(npy
  #define npy_cbrtl cbrtl
  #define npy_log10l log10l
  #define npy_logl logl
