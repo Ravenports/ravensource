baa8752aa6805260c55c67cb5f57a60b0dae1844
John Marino <draco@marino.st>
2025-01-06 00:45:48 +0000

    xinit: upgrade

--- bucket_F1/xinit/patches/patch-startx.cpp
+++ bucket_F1/xinit/patches/patch-startx.cpp
@@ -1,26 +1,26 @@
 #
 # generate displayname as dictated in the xauth manpage
 #
---- startx.cpp.orig	2022-12-03 22:18:43 UTC
+--- startx.cpp.orig	2025-01-05 18:25:56 UTC
 +++ startx.cpp
-@@ -168,7 +168,7 @@ while [ x"$1" != x ]; do
+@@ -177,7 +177,7 @@ while [ "$1" != "" ]; do
  	else
  	    XCOMM display must be the FIRST server argument
- 	    if [ x"$serverargs" = x ] && @@
+ 	    if [ "$serverargs" = "" ] && @@
 -		 expr "$1" : ':[0-9][0-9]*$' > /dev/null 2>&1; then
 +		expr \( "$1" \) : ':[0-9][0-9]*$' > /dev/null 2>&1; then
  		display="$1"
  	    else
  		serverargs="$serverargs $1"
-@@ -287,9 +287,9 @@ EOF
+@@ -300,9 +300,9 @@ EOF
      XCOMM now add the same credentials to the client authority file
      XCOMM if '$displayname' already exists do not overwrite it as another
      XCOMM server may need it. Add them to the '$xserverauthfile' instead.
 -    for displayname in $authdisplay $hostname$authdisplay; do
 +    for displayname in $authdisplay $hostname/unix$authdisplay; do
-         authcookie=`XAUTH list "$displayname" @@
--        | sed -n "s/.*$displayname[[:space:]*].*[[:space:]*]//p"` 2>/dev/null;
-+        | sed -n "s|.*$displayname[[:space:]*].*[[:space:]*]||p"` 2>/dev/null;
+         authcookie=$(xauth list "$displayname" @@
+-        | sed -n 's/.*'"$displayname"'[[:space:]*].*[[:space:]*]//p' 2>/dev/null);
++        | sed -n 's|.*'"$displayname"'[[:space:]*].*[[:space:]*]||p' 2>/dev/null);
          if [ "z${authcookie}" = "z" ] ; then
-             XAUTH -q << EOF 
+             $xauth -q << EOF
  add $displayname . $mcookie
