bc9515d6c265ba9e6709f9ee06973339790961e7
John Marino <draco@marino.st>
2025-01-11 15:53:03 +0000

    git: upgrade (some difficulty)

--- bucket_46/git/patches/patch-http.c
+++ bucket_46/git/patches/patch-http.c
@@ -1,13 +1,13 @@
---- http.c.orig	2024-10-07 14:56:19 UTC
+--- http.c.orig	2025-01-10 17:43:46 UTC
 +++ http.c
-@@ -2296,7 +2296,11 @@ static int http_request_reauth(const cha
+@@ -2241,7 +2241,11 @@ static int http_request_reauth(const cha
  				return HTTP_START_FAILED;
  			}
- 			rewind(result);
+ 			rewind(f);
 +#if defined(__MidnightBSD__)
-+			if (ftruncate(fileno((FILE *)result), 0) < 0) {
++			if (ftruncate(fileno((FILE *)f), 0) < 0) {
 +#else
- 			if (ftruncate(fileno(result), 0) < 0) {
+ 			if (ftruncate(fileno(f), 0) < 0) {
 +#endif
  				error_errno("unable to truncate a file");
  				return HTTP_START_FAILED;
