b7ee7c9cb22efbb2b543776b3dbcdd6a829feb38
John Marino <draco@marino.st>
2025-01-14 22:58:02 +0000

    rsync: fix build on DF

--- bucket_9C/rsync/patches/patch-popt_findme.c
+++ bucket_9C/rsync/patches/patch-popt_findme.c
@@ -1,10 +1,12 @@
 --- popt/findme.c.orig	2006-11-09 00:57:55 UTC
 +++ popt/findme.c
-@@ -6,6 +6,7 @@
+@@ -6,6 +6,9 @@
     file accompanying popt source distributions, available from 
     ftp://ftp.rpm.org/pub/rpm/dist. */
  
++#if defined(__linux__)
 +#include "rsync.h"
++#endif
  #include "system.h"
  #include "findme.h"
  
