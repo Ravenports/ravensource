fa1ca75fda8904f9d9a188dd70a3c80846c3e277
John Marino <draco@marino.st>
2025-01-14 15:34:03 -0600

    rsync: upgrade

---
+++ bucket_9C/rsync/patches/patch-popt_findme.c
@@ -0,0 +1,10 @@
+--- popt/findme.c.orig	2006-11-09 00:57:55 UTC
++++ popt/findme.c
+@@ -6,6 +6,7 @@
+    file accompanying popt source distributions, available from 
+    ftp://ftp.rpm.org/pub/rpm/dist. */
+ 
++#include "rsync.h"
+ #include "system.h"
+ #include "findme.h"
+ 
