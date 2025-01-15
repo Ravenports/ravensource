ca01900195e40694a03c1e44a88133c9d7e4615c
John Marino <draco@marino.st>
2025-01-15 05:12:35 +0000

    lyx: upgrade

--- bucket_42/lyx/patches/patch-src_support_filetools.cpp
+++ bucket_42/lyx/patches/patch-src_support_filetools.cpp
@@ -1,4 +1,4 @@
---- src/support/filetools.cpp.orig	2024-09-24 22:21:37 UTC
+--- src/support/filetools.cpp.orig	2025-01-09 19:37:38 UTC
 +++ src/support/filetools.cpp
 @@ -64,6 +64,8 @@
  #include <sstream>
