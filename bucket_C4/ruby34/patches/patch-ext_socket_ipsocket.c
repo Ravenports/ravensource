f19af0a8667850e56f6b6f410def8ce1f156695b
John Marino <draco@marino.st>
2025-01-25 21:32:34 +0100

    ruby34: fix build on midnight

---
+++ bucket_C4/ruby34/patches/patch-ext_socket_ipsocket.c
@@ -0,0 +1,12 @@
+--- ext/socket/ipsocket.c.orig	2024-12-25 07:43:20 UTC
++++ ext/socket/ipsocket.c
+@@ -10,6 +10,9 @@
+ 
+ #include "rubysocket.h"
+ #include <stdio.h>
++#if !defined(EAI_ADDRFAMILY)
++#define EAI_ADDRFAMILY 0
++#endif
+ 
+ struct inetsock_arg
+ {
