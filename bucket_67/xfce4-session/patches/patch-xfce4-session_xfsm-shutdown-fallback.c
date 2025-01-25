dff82c5d208bbfb3605bd08fab7fe34a636d9bf1
John Marino <draco@marino.st>
2025-01-25 20:35:57 +0100

    xfce4-session: fix build on midnightBSD

--- bucket_67/xfce4-session/patches/patch-xfce4-session_xfsm-shutdown-fallback.c
+++ bucket_67/xfce4-session/patches/patch-xfce4-session_xfsm-shutdown-fallback.c
@@ -1,10 +1,15 @@
 --- xfce4-session/xfsm-shutdown-fallback.c.orig	2024-11-04 16:33:53 UTC
 +++ xfce4-session/xfsm-shutdown-fallback.c
-@@ -30,10 +30,8 @@
+@@ -30,10 +30,13 @@
  
  #if defined(__DragonFly__) || defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__)
  #define __BACKEND_TYPE_BSD__ 1
 -#ifdef HAVE_SYS_SYSCTL_H
++# if defined(__MidnightBSD__)
++# define __BSD_VISIBLE 1
++#include <stddef.h>
++#include <sys/types.h>
++# endif
  #include <sys/sysctl.h>
  #endif
 -#endif
