f53c514aac98f175b5958b4725e73afcbfa464f3
John Marino <draco@marino.st>
2025-01-26 06:00:46 +0100

    lldb: support midnightbsd

--- bucket_23/lldb/patches/patch-cmake_modules_LLDBConfig.cmake
+++ bucket_23/lldb/patches/patch-cmake_modules_LLDBConfig.cmake
@@ -5,7 +5,7 @@
  # Figure out if lldb could use lldb-server.  If so, then we'll
  # ensure we build lldb-server when an lldb target is being built.
 -if (CMAKE_SYSTEM_NAME MATCHES "Android|Darwin|FreeBSD|Linux|NetBSD|Windows")
-+if (CMAKE_SYSTEM_NAME MATCHES "Android|Darwin|FreeBSD|Linux|NetBSD|DragonFly|Windows")
++if (CMAKE_SYSTEM_NAME MATCHES "Android|Darwin|FreeBSD|Linux|NetBSD|DragonFly|Windows|MidnightBSD")
    set(LLDB_CAN_USE_LLDB_SERVER ON)
  else()
    set(LLDB_CAN_USE_LLDB_SERVER OFF)
---
+++ bucket_23/lldb/patches/patch-source_Plugins_SymbolLocator_Default_SymbolLocatorDefault.cpp
@@ -0,0 +1,11 @@
+--- source/Plugins/SymbolLocator/Default/SymbolLocatorDefault.cpp.orig	2025-01-14 09:41:02 UTC
++++ source/Plugins/SymbolLocator/Default/SymbolLocatorDefault.cpp
+@@ -145,7 +145,7 @@ std::optional<FileSpec> SymbolLocatorDef
+       FileSystem::Instance().Resolve(file_spec);
+       debug_file_search_paths.AppendIfUnique(file_spec);
+     }
+-#if defined(__FreeBSD__)
++#if defined(__FreeBSD__) && !defined(__MidnightBSD__)
+     // Add $LOCALBASE/lib/debug directory, where LOCALBASE is
+     // usually /usr/local, but may be adjusted by the end user.
+     {
