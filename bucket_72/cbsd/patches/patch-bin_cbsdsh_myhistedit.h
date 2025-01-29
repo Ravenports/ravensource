c3d4cbb8d23251853300565cda56e23bf0925b0a
John Marino <draco@marino.st>
2025-01-29 02:32:39 +0000

    cbsd: upgrade

--- bucket_72/cbsd/patches/patch-bin_cbsdsh_myhistedit.h
+++ bucket_72/cbsd/patches/patch-bin_cbsdsh_myhistedit.h
@@ -1,4 +1,4 @@
---- bin/cbsdsh/myhistedit.h.orig	2025-01-06 19:12:01 UTC
+--- bin/cbsdsh/myhistedit.h.orig	2025-01-20 21:22:54 UTC
 +++ bin/cbsdsh/myhistedit.h
 @@ -32,15 +32,7 @@
   * $FreeBSD: head/bin/sh/myhistedit.h 326025 2017-11-20 19:49:47Z pfg $
