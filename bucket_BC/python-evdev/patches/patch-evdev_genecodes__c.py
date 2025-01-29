79d680ad42674b56478207274270b888b3395cdd
John Marino <draco@marino.st>
2025-01-29 13:53:12 +0000

    python-evdev: repatch, but still broken on DF

---
+++ bucket_BC/python-evdev/patches/patch-evdev_genecodes__c.py
@@ -0,0 +1,11 @@
+--- evdev/genecodes_c.py.orig	2025-01-20 22:19:18 UTC
++++ evdev/genecodes_c.py
+@@ -33,7 +33,7 @@ uname = " ".join((uname[0], *uname[2:]))
+ # -----------------------------------------------------------------------------
+ template_ecodes = r"""
+ #include <Python.h>
+-#ifdef __FreeBSD__
++#if defined(__FreeBSD__) && !defined(__MidnightBSD__)
+ #include <dev/evdev/input.h>
+ #else
+ #include <linux/input.h>
