933f56125e09f87b2451377a437ec9d84a396d07
John Marino <draco@marino.st>
2025-01-30 07:09:18 +0000

    add sdl3 port (could use more work, but more or less equivalent to sdl2)

---
+++ bucket_E5/sdl3/patches/patch-include_SDL3_SDL__endian.h
@@ -0,0 +1,15 @@
+--- include/SDL3/SDL_endian.h.orig	2025-01-21 18:51:21 UTC
++++ include/SDL3/SDL_endian.h
+@@ -140,10 +140,10 @@ _m_prefetch(void *__P)
+ #else
+ #error Unsupported endianness
+ #endif
+-#elif defined(SDL_PLATFORM_OPENBSD) || defined(__DragonFly__)
++#elif defined(SDL_PLATFORM_OPENBSD)
+ #include <endian.h>
+ #define SDL_BYTEORDER  BYTE_ORDER
+-#elif defined(SDL_PLATFORM_FREEBSD) || defined(SDL_PLATFORM_NETBSD)
++#elif defined(SDL_PLATFORM_FREEBSD) || defined(SDL_PLATFORM_NETBSD) || defined(__DragonFly__)
+ #include <sys/endian.h>
+ #define SDL_BYTEORDER  BYTE_ORDER
+ /* predefs from newer gcc and clang versions: */
