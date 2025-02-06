33132cc5bb826902c54450e85d44305fb7dd1938
John Marino <draco@marino.st>
2025-02-06 00:08:36 -0600

    kyua: fix build on freebsd

---
+++ bucket_D7/kyua/patches/patch-os_freebsd_utils_jail.cpp
@@ -0,0 +1,10 @@
+--- os/freebsd/utils/jail.cpp.orig	2025-01-01 05:46:21 UTC
++++ os/freebsd/utils/jail.cpp
+@@ -27,6 +27,7 @@
+ // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ 
+ #include "os/freebsd/utils/jail.hpp"
++#include <cstring>
+ 
+ extern "C" {
+ #include <limits.h>
