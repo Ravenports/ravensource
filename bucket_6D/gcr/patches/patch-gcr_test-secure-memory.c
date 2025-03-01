56537b488b94770cecc491d3b6384723f3910831
John Marino <draco@marino.st>
2025-03-01 22:03:51 +0000

    gcr: upgrade

--- bucket_6D/gcr/patches/patch-gcr_test-secure-memory.c
+++ bucket_6D/gcr/patches/patch-gcr_test-secure-memory.c
@@ -1,4 +1,4 @@
---- gcr/test-secure-memory.c.orig	2024-01-06 09:47:26 UTC
+--- gcr/test-secure-memory.c.orig	2025-02-05 11:40:17 UTC
 +++ gcr/test-secure-memory.c
 @@ -54,6 +54,10 @@ find_non_zero (gpointer mem, gsize len)
  static gsize
