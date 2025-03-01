e583c8e29a106a09083c5e0b1d7dd10213666c18
John Marino <draco@marino.st>
2025-03-01 17:50:20 +0000

    emacs: upgrade, mark desktop variant broken

--- bucket_4B/emacs/patches/patch-src_process.h
+++ bucket_4B/emacs/patches/patch-src_process.h
@@ -1,4 +1,4 @@
---- src/process.h.orig	2024-01-06 12:56:31 UTC
+--- src/process.h.orig	2025-02-17 22:38:58 UTC
 +++ src/process.h
 @@ -24,6 +24,9 @@ along with GNU Emacs.  If not, see <http
  #endif
