e8c9c1d2bf3c6e16da5ca8cacca1ea0783ba5ee8
John Marino <draco@marino.st>
2025-03-25 00:48:51 +0000

    npm: upgrade

--- bucket_66/npm/patches/patch-node__modules_@npmcli_config_lib_index.js
+++ bucket_66/npm/patches/patch-node__modules_@npmcli_config_lib_index.js
@@ -1,6 +1,6 @@
 --- node_modules/@npmcli/config/lib/index.js.orig	1985-10-26 08:15:00 UTC
 +++ node_modules/@npmcli/config/lib/index.js
-@@ -315,6 +315,9 @@ class Config {
+@@ -326,6 +326,9 @@ class Config {
  
      if (this.env.PREFIX) {
        this.globalPrefix = this.env.PREFIX
