c4d38d88350c98ab950b9ea28866dc38aae0e2b8
John Marino <draco@marino.st>
2025-03-25 17:37:35 +0000

    qpdf: upgrade

--- bucket_F3/qpdf/patches/patch-libqpdf_QUtil.cc
+++ bucket_F3/qpdf/patches/patch-libqpdf_QUtil.cc
@@ -1,6 +1,6 @@
---- libqpdf/QUtil.cc.orig	2024-06-07 12:39:46 UTC
+--- libqpdf/QUtil.cc.orig	2025-03-09 12:28:21 UTC
 +++ libqpdf/QUtil.cc
-@@ -893,6 +893,9 @@ QUtil::get_current_qpdf_time()
+@@ -896,6 +896,9 @@ QUtil::get_current_qpdf_time()
  # if HAVE_TM_GMTOFF
      // tm_gmtoff is seconds after UTC
      int tzoff = -static_cast<int>(ltime.tm_gmtoff / 60);
