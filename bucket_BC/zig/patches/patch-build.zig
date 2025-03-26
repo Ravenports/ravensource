a252ed5371c8ec3a13bff4a029783d5704382c3f
John Marino <draco@marino.st>
2025-03-26 04:32:47 +0000

    zig: upgrade

--- bucket_BC/zig/patches/patch-build.zig
+++ bucket_BC/zig/patches/patch-build.zig
@@ -1,36 +1,31 @@
---- build.zig.orig	2024-06-06 19:05:11 UTC
+--- build.zig.orig	2025-03-04 22:31:02 UTC
 +++ build.zig
-@@ -707,6 +707,7 @@ fn addCmakeCfgOptionsToExe(
+@@ -783,6 +783,7 @@ fn addCmakeCfgOptionsToExe(
                      else => |e| return e,
                  };
-                 exe.linkSystemLibrary("unwind");
+                 mod.linkSystemLibrary("unwind", .{});
 +                exe.addRPath(.{ .cwd_relative = "@GCC_LIBDIR@" });
              },
              .ios, .macos, .watchos, .tvos, .visionos => {
-                 exe.linkLibCpp();
-@@ -714,25 +715,14 @@ fn addCmakeCfgOptionsToExe(
+                 mod.link_libcpp = true;
+@@ -790,20 +791,10 @@ fn addCmakeCfgOptionsToExe(
              .windows => {
-                 if (exe.rootModuleTarget().abi != .msvc) exe.linkLibCpp();
+                 if (target.abi != .msvc) mod.link_libcpp = true;
              },
 -            .freebsd => {
--                if (static) {
--                    try addCxxKnownPath(b, cfg, exe, b.fmt("libc++.{s}", .{lib_suffix}), null, need_cpp_includes);
--                    try addCxxKnownPath(b, cfg, exe, b.fmt("libgcc_eh.{s}", .{lib_suffix}), null, need_cpp_includes);
--                } else {
--                    try addCxxKnownPath(b, cfg, exe, b.fmt("libc++.{s}", .{lib_suffix}), null, need_cpp_includes);
--                }
+-                try addCxxKnownPath(b, cfg, exe, b.fmt("libc++.{s}", .{lib_suffix}), null, need_cpp_includes);
+-                if (static) try addCxxKnownPath(b, cfg, exe, b.fmt("libgcc_eh.{s}", .{lib_suffix}), null, need_cpp_includes);
 -            },
-             .openbsd => {
-                 try addCxxKnownPath(b, cfg, exe, b.fmt("libc++.{s}", .{lib_suffix}), null, need_cpp_includes);
-                 try addCxxKnownPath(b, cfg, exe, b.fmt("libc++abi.{s}", .{lib_suffix}), null, need_cpp_includes);
-             },
+-            .openbsd => {
+-                // - llvm requires libexecinfo which has conflicting symbols with libc++abi
+-                // - only an issue with .a linking
+-                // - workaround is to link c++abi dynamically
+-                try addCxxKnownPath(b, cfg, exe, b.fmt("libc++.{s}", .{target.dynamicLibSuffix()[1..]}), null, need_cpp_includes);
+-                try addCxxKnownPath(b, cfg, exe, b.fmt("libc++abi.{s}", .{target.dynamicLibSuffix()[1..]}), null, need_cpp_includes);
+-            },
 -            .netbsd, .dragonfly => {
--                if (static) {
--                    try addCxxKnownPath(b, cfg, exe, b.fmt("libstdc++.{s}", .{lib_suffix}), null, need_cpp_includes);
--                    try addCxxKnownPath(b, cfg, exe, b.fmt("libgcc_eh.{s}", .{lib_suffix}), null, need_cpp_includes);
--                } else {
--                    try addCxxKnownPath(b, cfg, exe, b.fmt("libstdc++.{s}", .{lib_suffix}), null, need_cpp_includes);
--                }
+-                try addCxxKnownPath(b, cfg, exe, b.fmt("libstdc++.{s}", .{lib_suffix}), null, need_cpp_includes);
+-                if (static) try addCxxKnownPath(b, cfg, exe, b.fmt("libgcc_eh.{s}", .{lib_suffix}), null, need_cpp_includes);
 +            .netbsd, .dragonfly, .freebsd => {
 +                exe.addObjectFile(.{ .cwd_relative = "@GCC_LIBDIR@/libstdc++.a" });
 +                exe.addObjectFile(.{ .cwd_relative = "@GCC_TARGET_LIBDIR@/libgcc_eh.a" });
---
+++ bucket_BC/zig/patches/patch-lib_std_zig_LibCInstallation.zig
@@ -0,0 +1,45 @@
+--- lib/std/zig/LibCInstallation.zig.orig	2025-03-04 22:31:02 UTC
++++ lib/std/zig/LibCInstallation.zig
+@@ -973,23 +973,29 @@ pub fn resolveCrtPaths(
+     };
+     switch (target.os.tag) {
+         .dragonfly => {
+-            const gccv: []const u8 = if (target.os.version_range.semver.isAtLeast(.{
+-                .major = 5,
+-                .minor = 4,
+-                .patch = 0,
+-            }) orelse true) "gcc80" else "gcc54";
++            const gcc_dir_path: Path = .{
++                .root_dir = std.Build.Cache.Directory.cwd(),
++                .sub_path = "@GCC_TARGET_LIBDIR@",
++            };
++            return .{
++                .crt0 = if (crt_basenames.crt0) |basename| try crt_dir_path.join(arena, basename) else null,
++                .crti = if (crt_basenames.crti) |basename| try crt_dir_path.join(arena, basename) else null,
++                .crtn = if (crt_basenames.crtn) |basename| try crt_dir_path.join(arena, basename) else null,
++                .crtbegin = if (crt_basenames.crtbegin) |basename| try gcc_dir_path.join(arena, basename) else null,
++                .crtend = if (crt_basenames.crtend) |basename| try gcc_dir_path.join(arena, basename) else null,
++            };
++        },
++        .freebsd => {
++            const gcc_dir_path: Path = .{
++                .root_dir = std.Build.Cache.Directory.cwd(),
++                .sub_path = "@GCC_TARGET_LIBDIR@",
++            };
+             return .{
+                 .crt0 = if (crt_basenames.crt0) |basename| try crt_dir_path.join(arena, basename) else null,
+                 .crti = if (crt_basenames.crti) |basename| try crt_dir_path.join(arena, basename) else null,
+-                .crtbegin = if (crt_basenames.crtbegin) |basename| .{
+-                    .root_dir = crt_dir_path.root_dir,
+-                    .sub_path = try fs.path.join(arena, &.{ crt_dir_path.sub_path, gccv, basename }),
+-                } else null,
+-                .crtend = if (crt_basenames.crtend) |basename| .{
+-                    .root_dir = crt_dir_path.root_dir,
+-                    .sub_path = try fs.path.join(arena, &.{ crt_dir_path.sub_path, gccv, basename }),
+-                } else null,
+                 .crtn = if (crt_basenames.crtn) |basename| try crt_dir_path.join(arena, basename) else null,
++                .crtbegin = if (crt_basenames.crtbegin) |basename| try gcc_dir_path.join(arena, basename) else null,
++                .crtend = if (crt_basenames.crtend) |basename| try gcc_dir_path.join(arena, basename) else null,
+             };
+         },
+         .haiku => {
