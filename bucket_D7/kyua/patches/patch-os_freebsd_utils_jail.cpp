48d2cb0327f4b1fe100b92feb9b07930ed8226d5
John Marino <draco@marino.st>
2025-04-01 00:01:41 +0000

    kyua: upgrade

--- bucket_D7/kyua/patches/patch-os_freebsd_utils_jail.cpp
+++ bucket_D7/kyua/patches/patch-os_freebsd_utils_jail.cpp
@@ -1,4 +1,4 @@
---- os/freebsd/utils/jail.cpp.orig	2025-01-01 05:46:21 UTC
+--- os/freebsd/utils/jail.cpp.orig	2025-03-29 21:43:20 UTC
 +++ os/freebsd/utils/jail.cpp
 @@ -27,6 +27,7 @@
  // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
