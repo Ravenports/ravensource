757be4a0ecd7720810865098a38bd7f429e30e2b
John Marino <draco@marino.st>
2025-04-06 20:05:27 +0000

    netpbm: Upgrade

--- bucket_5C/netpbm/freebsd/patch-lib_libpm.c
+++ bucket_5C/netpbm/freebsd/patch-lib_libpm.c
@@ -1,4 +1,4 @@
---- lib/libpm.c.orig	2024-06-28 23:33:22 UTC
+--- lib/libpm.c.orig	2025-03-28 19:47:55 UTC
 +++ lib/libpm.c
 @@ -17,6 +17,7 @@
  #include <unistd.h>
@@ -8,7 +8,7 @@
  #include <string.h>
  #include <errno.h>
  #include <setjmp.h>
-@@ -849,7 +850,7 @@ pm_arg0toprogname(const char arg0[]) {
+@@ -837,7 +838,7 @@ pm_arg0toprogname(const char arg0[]) {
  unsigned int
  pm_randseed(void) {
  
