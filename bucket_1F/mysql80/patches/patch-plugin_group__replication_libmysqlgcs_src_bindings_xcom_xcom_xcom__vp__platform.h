219d4d9262263a5ed5442ca3decb50376d9e7e92
John Marino <draco@marino.st>
2025-04-11 02:39:29 +0000

    mysql80: fix build on netbsd

---
+++ bucket_1F/mysql80/patches/patch-plugin_group__replication_libmysqlgcs_src_bindings_xcom_xcom_xcom__vp__platform.h
@@ -0,0 +1,17 @@
+--- plugin/group_replication/libmysqlgcs/src/bindings/xcom/xcom/xcom_vp_platform.h.orig	2024-12-16 09:20:55 UTC
++++ plugin/group_replication/libmysqlgcs/src/bindings/xcom/xcom/xcom_vp_platform.h
+@@ -37,13 +37,11 @@
+ #endif
+ #endif
+ 
+-#ifdef __APPLE__
+-#if __APPLE__
++#ifdef __NetBSD__
+ 
+ /* xdr_uint64_t and xdr_uint32_t are not defined on OSX */
+ #define xdr_uint64_t xdr_u_int64_t
+ #define xdr_uint32_t xdr_u_int32_t
+ #endif
+-#endif
+ 
+ #endif
