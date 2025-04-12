9d033c3622a008acf401d49bf68c8b0b50dd8811
John Marino <draco@marino.st>
2025-04-12 06:16:46 +0000

    pipewire: fix build on netbsd

--- bucket_AA/pipewire/patches/patch-src_pipewire_conf.c
+++ bucket_AA/pipewire/patches/patch-src_pipewire_conf.c
@@ -21,3 +21,12 @@
  	sprintf(tmp_name, "%s.tmp", name);
  	if ((fd = openat(sfd, tmp_name,  O_CLOEXEC | O_CREAT | O_WRONLY | O_TRUNC, 0600)) < 0) {
  		res = -errno;
+@@ -472,7 +470,7 @@ SPA_EXPORT
+ int pw_conf_load_conf(const char *prefix, const char *name, struct pw_properties *conf)
+ {
+ 	char path[PATH_MAX];
+-	char fname[PATH_MAX + 256];
++	char fname[PATH_MAX + 1024];
+ 	int i, res, level = 0;
+ 	spa_autoptr(pw_properties) override = NULL;
+ 	const char *dname;
