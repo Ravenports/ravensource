6b83c012454df199ae9aea5e30c60af8d2b285e4
John Marino <draco@marino.st>
2025-04-25 17:57:03 +0000

    wxwidgets: upgrade

--- bucket_3D/wxWidgets/patches/patch-src_stc_scintilla_src_Editor.cxx
+++ bucket_3D/wxWidgets/patches/patch-src_stc_scintilla_src_Editor.cxx
@@ -1,4 +1,4 @@
---- src/stc/scintilla/src/Editor.cxx.orig	2025-03-17 22:14:26 UTC
+--- src/stc/scintilla/src/Editor.cxx.orig	2025-04-24 19:21:09 UTC
 +++ src/stc/scintilla/src/Editor.cxx
 @@ -18,6 +18,7 @@
  #include <map>
