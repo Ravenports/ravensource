bef9822a075722d59625d6ede7c6d7fd9dc3fc58
John Marino <draco@marino.st>
2025-05-11 17:14:16 +0000

    nmap: fix patch for freebsd

--- bucket_E5/nmap/patches/patch-libdnet-stripped_src_intf.c
+++ bucket_E5/nmap/patches/patch-libdnet-stripped_src_intf.c
@@ -1,11 +1,14 @@
---- libdnet-stripped/src/intf.c.orig	2016-07-27 20:11:27 UTC
+--- libdnet-stripped/src/intf.c.orig	2025-03-31 19:30:47 UTC
 +++ libdnet-stripped/src/intf.c
-@@ -163,7 +163,7 @@ intf_open(void)
+@@ -178,7 +178,11 @@ intf_open(void)
  #if defined(SIOCGLIFCONF) || defined(SIOCGIFNETMASK_IN6) || defined(SIOCGIFNETMASK6)
  		if ((intf->fd6 = socket(AF_INET6, SOCK_DGRAM, 0)) < 0) {
  #  ifdef EPROTONOSUPPORT
--			if (errno != EPROTONOSUPPORT)
++#    if defined(__FreeBSD__) || defined(__DragonFly__)
 +			if (errno != EPROTONOSUPPORT && errno != EAFNOSUPPORT)
- #  endif
++#    else
+ 			if (errno != EPROTONOSUPPORT)
++#    endif
+ #endif
  				return (intf_close(intf));
  		}
