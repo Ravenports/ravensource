ba0bc1e466188ecf02937cacd01b08a0e9b88ac8
John Marino <draco@marino.st>
2025-05-13 05:36:30 +0000

    add qtcreator port

---
+++ bucket_6B/qtcreator/patches/patch-src_libs_3rdparty_sqlite_sqlite3.c
@@ -0,0 +1,11 @@
+--- src/libs/3rdparty/sqlite/sqlite3.c.orig	2025-04-10 08:57:18 UTC
++++ src/libs/3rdparty/sqlite/sqlite3.c
+@@ -14337,7 +14337,7 @@ struct fts5_api {
+ ** But _XOPEN_SOURCE define causes problems for Mac OS X, so omit
+ ** it.
+ */
+-#if !defined(_XOPEN_SOURCE) && !defined(__DARWIN__) && !defined(__APPLE__)
++#if defined(_POSIX_C_SOURCE) && !defined(_XOPEN_SOURCE)
+ #  define _XOPEN_SOURCE 600
+ #endif
+ 
---
+++ bucket_6B/qtcreator/patches/patch-src_tools_process__stub_main.cpp
@@ -0,0 +1,36 @@
+--- src/tools/process_stub/main.cpp.orig	2025-04-10 08:57:18 UTC
++++ src/tools/process_stub/main.cpp
+@@ -22,10 +22,9 @@
+ #include <unistd.h>
+ #endif
+ 
+-#ifdef Q_OS_LINUX
++#ifdef Q_OS_UNIX
+ #include <sys/ptrace.h>
+ #include <sys/wait.h>
+-#include <sys/prctl.h>
+ #endif
+ 
+ #include <iostream>
+@@ -254,6 +253,9 @@ void onInferiorStarted()
+     // In debug mode we use the poll timer to send the pid.
+     if (!debugMode)
+         sendPid(inferiorId);
++#elif defined(Q_OS_FREEBSD) || defined(Q_OS_DRAGONFLY)
++    ptrace(PT_DETACH, inferiorId, 0, SIGSTOP);
++    sendPid(inferiorId);
+ #elif defined(Q_OS_LINUX)
+     if (debugMode) {
+         qCInfo(log) << "Waiting for SIGTRAP from inferiors execve ...";
+@@ -288,6 +290,11 @@ void setupUnixInferior()
+             // Suspend ourselves ...
+             raise(SIGSTOP);
+         });
++#elif defined(Q_OS_FREEBSD) || defined(Q_OS_DRAGONFLY)
++        // PT_TRACE_ME will stop execution of the child process as soon as execve is called.
++        inferiorProcess.setChildProcessModifier([] {
++            ptrace(PT_TRACE_ME, 0, 0, 0);
++        });
+ #elif defined(Q_OS_LINUX)
+         // PTRACE_TRACEME will stop execution of the child process as soon as execve is called.
+         inferiorProcess.setChildProcessModifier([] {
