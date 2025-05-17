24f0a126de1a644461908350bd1beeaf5917c75a
John Marino <draco@marino.st>
2025-05-17 12:12:58 +0000

    Drop R-s2 and everything that depends on it
    R-s2 has become extremely obnoxious to maintain.  It seems that it exists
    to support R-DCluster which currently nothing uses.  I am going to assume
    DCluster was orphaned and all the R-s2 nonsense is to support the orphan.
    Nope.  Kill it, the orphan, and 2 other dependencies.
    
    Obviously I can bring them back if circumstances change.

--- bucket_FB/R-s2/patches/patch-tools_build__absl.sh
+++
@@ -1,8 +0,0 @@
---- tools/build_absl.sh.orig	2025-01-07 11:51:24 UTC
-+++ tools/build_absl.sh
-@@ -1,4 +1,4 @@
--
-+#!/bin/sh
- # https://cran.r-project.org/doc/manuals/r-release/R-exts.html#Using-cmake
- 
- : ${R_HOME=`R RHOME`}
