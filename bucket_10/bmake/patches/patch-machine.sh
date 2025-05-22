6c2f410d2b27802cd7033276d8b6b88ebb2dca63
John Marino <draco@marino.st>
2025-05-21 21:37:03 -0500

    bmake: rotate freebsd versions, drop old dragonfly variant

---
+++ bucket_10/bmake/patches/patch-machine.sh
@@ -0,0 +1,7 @@
+--- machine.sh.orig	2023-01-17 18:30:21 UTC
++++ machine.sh
+@@ -1,3 +1,4 @@
++#!/bin/sh
+ :
+ # This is mostly redundant.
+ # These days I use the pseudo machine "host" when building for host
