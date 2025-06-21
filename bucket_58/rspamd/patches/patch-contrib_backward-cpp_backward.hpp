a3787fabcec87dfc1e9ab74393d2018d12779612
John Marino <draco@marino.st>
2025-06-21 23:35:24 +0000

    rspamd: upgrade

--- bucket_58/rspamd/patches/patch-contrib_backward-cpp_backward.hpp
+++ bucket_58/rspamd/patches/patch-contrib_backward-cpp_backward.hpp
@@ -1,4 +1,4 @@
---- contrib/backward-cpp/backward.hpp.orig	2025-03-08 14:11:50 UTC
+--- contrib/backward-cpp/backward.hpp.orig	2025-06-17 09:32:30 UTC
 +++ contrib/backward-cpp/backward.hpp
 @@ -415,6 +415,7 @@ extern "C" uintptr_t _Unwind_GetIPInfo(_
  #ifdef BACKWARD_ATLEAST_CXX11
