acad621eb4e378796e02c4cf275ccbbdd164569f
John Marino <draco@marino.st>
2025-06-27 00:20:54 +0000

    colord: upgrade

--- bucket_DB/colord/patches/patch-lib_colord_cd-icc.c
+++ bucket_DB/colord/patches/patch-lib_colord_cd-icc.c
@@ -1,7 +1,7 @@
 Apprently g_file_query_exists is failing with latest glib20.
 Ignore attempt to create existing directory to allow colord to build.
 
---- lib/colord/cd-icc.c.orig	2024-01-22 12:41:38 UTC
+--- lib/colord/cd-icc.c.orig	2025-06-23 14:06:37 UTC
 +++ lib/colord/cd-icc.c
 @@ -1501,7 +1501,9 @@ cd_icc_save_file_mkdir_parents (GFile *f
  	if (g_file_query_exists (parent_dir, NULL))
