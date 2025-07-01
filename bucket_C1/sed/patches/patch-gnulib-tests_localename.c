d9b9907c067600a6a279586d9ba275677558304f
John Marino <draco@marino.st>
2025-07-01 22:20:18 +0000

    sed: fix build on sunos

---
+++ bucket_C1/sed/patches/patch-gnulib-tests_localename.c
@@ -0,0 +1,11 @@
+--- gnulib-tests/localename.c.orig	2022-10-09 14:42:28 UTC
++++ gnulib-tests/localename.c
+@@ -45,7 +45,7 @@
+ # if defined __sun
+ #  if HAVE_GETLOCALENAME_L
+ /* Solaris >= 12.  */
+-extern char * getlocalename_l(int, locale_t);
++extern const char * getlocalename_l(int, locale_t);
+ #  elif HAVE_SOLARIS114_LOCALES
+ #   include <sys/localedef.h>
+ #  endif
