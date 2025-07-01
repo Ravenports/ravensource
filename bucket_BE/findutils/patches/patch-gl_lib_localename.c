8ecd2c7514a288f43ace2fb5ec9ad9aedfe437cf
John Marino <draco@marino.st>
2025-07-01 22:32:26 +0000

    findutils: fix build on sunos

---
+++ bucket_BE/findutils/patches/patch-gl_lib_localename.c
@@ -0,0 +1,11 @@
+--- gl/lib/localename-unsafe.c.orig	2022-10-09 14:42:28 UTC
++++ gl/lib/localename-unsafe.c
+@@ -45,7 +45,7 @@
+ # if defined __sun
+ #  if HAVE_GETLOCALENAME_L
+ /* Solaris >= 12.  */
+-extern char * getlocalename_l(int, locale_t);
++extern const char * getlocalename_l(int, locale_t);
+ #  elif HAVE_SOLARIS114_LOCALES
+ #   include <sys/localedef.h>
+ #  endif
