b4e494fc8f4bfb399b3ef9d03538d81b0ba89e37
John Marino <draco@marino.st>
2025-07-04 20:07:09 +0000

    nvi: add BYTECHAR option to disable widechar
    nvi was broken on OmniOS (even the solaris 2.10 version which used
    to work).  After a lot of debugging, I found the L##ch macro to
    convert from character strings to wide strings seemingly did not
    work, which was causing the initialization of the options to fail.
    
    My guess is that L'string' is somehow converting to UTF-8 (variable
    width string) instead of a fixed-width wide string.
    
    I need nvi working so I created an option to build NVI without
    widechar functionality and NVI seems to work fine like that.
    
    This required a new sunos patch for prototype mismatch correction
    on solaris.

---
+++ bucket_6A/nvi/patches/patch-common_util.c
@@ -0,0 +1,26 @@
+--- common/util.c.orig	2007-11-18 16:41:42 UTC
++++ common/util.c
+@@ -163,7 +163,11 @@ enum nresult
+ nget_uslong(SCR *sp, u_long *valp, const CHAR_T *p, CHAR_T **endp, int base)
+ {
+ 	errno = 0;
++#if defined(__sun__)
++	*valp = STRTOUL(p, (char **)endp, base);
++#else
+ 	*valp = STRTOUL(p, endp, base);
++#endif
+ 	if (errno == 0)
+ 		return (NUM_OK);
+ 	if (errno == ERANGE && *valp == ULONG_MAX)
+@@ -181,7 +185,11 @@ enum nresult
+ nget_slong(SCR *sp, long int *valp, const CHAR_T *p, CHAR_T **endp, int base)
+ {
+ 	errno = 0;
++#if defined(__sun__)
++	*valp = STRTOL(p, (char **)endp, base);
++#else
+ 	*valp = STRTOL(p, endp, base);
++#endif
+ 	if (errno == 0)
+ 		return (NUM_OK);
+ 	if (errno == ERANGE) {
