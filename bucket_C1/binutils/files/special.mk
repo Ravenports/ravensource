2e25a326e878040d2e05bead5e832d2d639f92eb
John Marino <draco@marino.st>
2025-07-06 01:00:26 +0000

    binutils: fix build on sunos

--- bucket_C1/binutils/files/special.mk
+++ bucket_C1/binutils/files/special.mk
@@ -7,3 +7,9 @@ CONFIGURE_ARGS+=	--disable-gold
 CONFIGURE_ARGS+=	--disable-plugins
 PLIST_SUB+=		GOLD="@comment "
 .endif
+
+# for non-GNU linkers, ensure DT_RPATH set instead of DT_RUNPATH
+# See rtld man page, we need to be invulnerable to LD_LIBRARY_PATH
+.if ${OPSYS} != SunOS
+LDFLAGS+= -Wl,--disable-new-dtags
+.endif
