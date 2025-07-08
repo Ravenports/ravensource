a57421b1084780b19acc240fd603ff99affe7bba
John Marino <draco@marino.st>
2025-07-08 23:28:56 +0000

    gobject-introspection: fix build on sunos

--- bucket_52/gobject-introspection/patches/patch-giscanner_ccompiler.py
+++ bucket_52/gobject-introspection/patches/patch-giscanner_ccompiler.py
@@ -9,3 +9,12 @@
                  args.append('-Wl,--no-as-needed')
  
          for library_path in libpaths:
+@@ -292,7 +292,7 @@ class CCompiler(object):
+ 
+         # Ensure libraries are always linked as we are going to use ldd to work
+         # out their names later
+-        if os.name != 'nt' and sys.platform != 'darwin':
++        if os.name != 'nt' and sys.platform != 'darwin' and sys.platform != 'sunos5':
+             args.append('-Wl,--no-as-needed')
+ 
+         for library in libraries:
