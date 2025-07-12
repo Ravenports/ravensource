70c7f8c90f229f6cc67647a6e393d7039a0ad321
John Marino <draco@marino.st>
2025-07-12 17:51:25 +0000

    git: upgrade

--- bucket_46/git/patches/patch-http.c
+++ bucket_46/git/patches/patch-http.c
@@ -1,4 +1,4 @@
---- http.c.orig	2025-06-16 05:42:57 UTC
+--- http.c.orig	2025-06-16 05:11:33 UTC
 +++ http.c
 @@ -2283,7 +2283,11 @@ static int http_request_reauth(const cha
  				return HTTP_START_FAILED;
