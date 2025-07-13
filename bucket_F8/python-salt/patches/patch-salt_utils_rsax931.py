3c72c239bac19b756f136c0867e45022f27e4237
John Marino <draco@marino.st>
2025-07-13 12:55:35 +0000

    salt: upgrade

--- bucket_F8/python-salt/patches/patch-salt_utils_rsax931.py
+++ bucket_F8/python-salt/patches/patch-salt_utils_rsax931.py
@@ -1,4 +1,4 @@
---- salt/utils/rsax931.py.orig	2025-06-26 18:26:26 UTC
+--- salt/utils/rsax931.py.orig	2025-07-10 17:36:30 UTC
 +++ salt/utils/rsax931.py
 @@ -92,7 +92,7 @@ def _find_libcrypto():
                  # This could be /opt/tools/lib (Global Zone) or
