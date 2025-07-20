3c3ed58da73a9c94fb858317c91f18e6579884e6
John Marino <draco@marino.st>
2025-07-20 17:57:41 +0000

    weechat: upgrade

--- bucket_53/weechat/patches/patch-src_core_core-crypto.c
+++ bucket_53/weechat/patches/patch-src_core_core-crypto.c
@@ -1,7 +1,7 @@
---- src/core/core-crypto.c.orig	2025-04-18 18:39:02 UTC
+--- src/core/core-crypto.c.orig	2025-07-19 10:01:43 UTC
 +++ src/core/core-crypto.c
-@@ -40,6 +40,10 @@
- #define htobe64 OSSwapHostToBigInt64
+@@ -50,6 +50,10 @@
+ #define BE_INT64 htobe64
  #endif
  
 +#if defined __DragonFly__ || defined __MidnightBSD__
