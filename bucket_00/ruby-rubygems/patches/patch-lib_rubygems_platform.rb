41b4307e7b46c0836e1bb3f8d27eb06ff50ac99e
John Marino <draco@marino.st>
2025-07-20 19:29:17 +0000

    ruby-rubygems: upgrade

--- bucket_00/ruby-rubygems/patches/patch-lib_rubygems_platform.rb
+++ bucket_00/ruby-rubygems/patches/patch-lib_rubygems_platform.rb
@@ -1,10 +1,10 @@
---- lib/rubygems/platform.rb.orig	2025-04-10 20:30:23 UTC
+--- lib/rubygems/platform.rb.orig	2025-07-14 07:07:38 UTC
 +++ lib/rubygems/platform.rb
-@@ -133,6 +133,7 @@ class Gem::Platform
-                         @cpu = "x86" if @cpu.nil? && os =~ /32$/
+@@ -122,6 +122,7 @@ class Gem::Platform
+                         @cpu = "x86" if @cpu.nil? && os.end_with?("32")
                          [os, version]
-                       when /netbsdelf/ then             ["netbsdelf", nil]
-+                      when /netbsd/ then                ["netbsd",    $1]
-                       when /openbsd(\d+\.\d+)?/ then    ["openbsd",   $1]
-                       when /solaris(\d+\.\d+)?/ then    ["solaris",   $1]
-                       when /wasi/ then                  ["wasi",      nil]
+                       when /netbsdelf/ then                  ["netbsdelf", nil]
++                      when /netbsd/ then                     ["netbsd",    $1]
+                       when /openbsd-?(\d+\.\d+)?/ then       ["openbsd",   $1]
+                       when /solaris-?(\d+\.\d+)?/ then       ["solaris",   $1]
+                       when /wasi/ then                       ["wasi",      nil]
