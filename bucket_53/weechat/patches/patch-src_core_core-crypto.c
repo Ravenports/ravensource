994b986c156afa7d2eeae150800b77b80eb55261
John Marino <draco@marino.st>
2025-08-17 21:47:23 +0000

    weechat: upgrade

--- bucket_53/weechat/patches/patch-src_core_core-crypto.c
+++ bucket_53/weechat/patches/patch-src_core_core-crypto.c
@@ -1,4 +1,4 @@
---- src/core/core-crypto.c.orig	2025-07-19 10:01:43 UTC
+--- src/core/core-crypto.c.orig	2025-08-16 19:49:19 UTC
 +++ src/core/core-crypto.c
 @@ -50,6 +50,10 @@
  #define BE_INT64 htobe64
