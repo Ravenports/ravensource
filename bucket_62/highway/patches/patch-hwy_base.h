50cfc49c4bc165f9c0f1d5e042ffcd662710ee18
John Marino <draco@marino.st>
2025-08-17 17:50:34 +0000

    highway: upgrade

--- bucket_62/highway/patches/patch-hwy_base.h
+++ bucket_62/highway/patches/patch-hwy_base.h
@@ -1,15 +1,15 @@
---- hwy/base.h.orig	2024-05-31 15:31:10 UTC
+--- hwy/base.h.orig	2025-08-14 07:25:53 UTC
 +++ hwy/base.h
-@@ -1074,7 +1074,7 @@ HWY_API HWY_BITCASTSCALAR_CONSTEXPR To B
- #ifndef HWY_HAVE_SCALAR_F16_TYPE
+@@ -1205,7 +1205,7 @@ HWY_API HWY_BITCASTSCALAR_CONSTEXPR To B
  // Compiler supports _Float16, not necessarily with operators.
- #if HWY_NEON_HAVE_F16C || HWY_RVV_HAVE_F16_VEC || HWY_SSE2_HAVE_F16_TYPE
+ #if HWY_NEON_HAVE_F16C || HWY_RVV_HAVE_F16_VEC || HWY_SSE2_HAVE_F16_TYPE || \
+     __SPIRV_DEVICE__
 -#define HWY_HAVE_SCALAR_F16_TYPE 1
 +#define HWY_HAVE_SCALAR_F16_TYPE 0
  #else
  #define HWY_HAVE_SCALAR_F16_TYPE 0
  #endif
-@@ -1593,7 +1593,7 @@ HWY_F16_CONSTEXPR inline std::partial_or
+@@ -1730,7 +1730,7 @@ HWY_F16_CONSTEXPR inline std::partial_or
  
  // Compiler supports __bf16, not necessarily with operators.
  #if HWY_ARM_HAVE_SCALAR_BF16_TYPE || HWY_SSE2_HAVE_SCALAR_BF16_TYPE
