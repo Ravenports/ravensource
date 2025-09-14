277a17e4eab9a9544a1ec97d089f16458e37c284
John Marino <draco@marino.st>
2025-09-14 23:12:24 +0000

    exiv2: upgrade

--- bucket_36/exiv2/patches/patch-src_futils.cpp
+++ bucket_36/exiv2/patches/patch-src_futils.cpp
@@ -1,4 +1,4 @@
---- src/futils.cpp.orig	2025-02-10 15:10:32 UTC
+--- src/futils.cpp.orig	2025-08-31 08:31:02 UTC
 +++ src/futils.cpp
 @@ -33,9 +33,11 @@ namespace fs = std::experimental::filesy
  // clang-format on
