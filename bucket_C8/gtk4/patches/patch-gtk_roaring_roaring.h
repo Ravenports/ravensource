f2504653f58287504e6f05096713ea6b3174d65a
Michael Reim <kraileth@elderlinux.org>
2025-10-03 19:09:11 +0200

    gtk4: upgrade

--- bucket_C8/gtk4/patches/patch-gtk_roaring_roaring.h
+++ bucket_C8/gtk4/patches/patch-gtk_roaring_roaring.h
@@ -1,10 +1,10 @@
---- gtk/roaring/roaring.h.orig	2025-06-09 20:44:13 UTC
-+++ gtk/roaring/roaring.h
-@@ -59,7 +59,6 @@ enum {
+--- gtk/roaring/roaring.h.orig	2025-10-03 18:50:24.760858000 +0200
++++ gtk/roaring/roaring.h	2025-10-03 18:50:46.926883000 +0200
+@@ -89,7 +89,6 @@
  #include <stdint.h>
  #include <stdlib.h>  // will provide posix_memalign with _POSIX_C_SOURCE as defined above
- #if !(defined(__APPLE__)) && !(defined(__FreeBSD__)) && !(defined(__OpenBSD__))
+ #ifdef __GLIBC__
 -#include <malloc.h>  // this should never be needed but there are some reports that it is needed.
  #endif
  
- 
+ #ifdef __cplusplus
