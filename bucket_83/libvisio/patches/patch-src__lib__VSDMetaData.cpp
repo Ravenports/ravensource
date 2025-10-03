79113f19ca989564ffd81a607da00f92285082ae
Michael Reim <kraileth@elderlinux.org>
2025-10-03 16:16:52 +0200

    libvisio: upgrade

--- bucket_83/libvisio/patches/patch-src__lib__VSDMetaData.cpp
+++ bucket_83/libvisio/patches/patch-src__lib__VSDMetaData.cpp
@@ -1,6 +1,6 @@
---- src/lib/VSDMetaData.cpp.orig	2017-10-21 14:38:56 UTC
+--- src/lib/VSDMetaData.cpp.orig	2024-10-22 15:15:20 UTC
 +++ src/lib/VSDMetaData.cpp
-@@ -312,7 +312,7 @@ bool libvisio::VSDMetaData::parseTimes(l
+@@ -313,7 +313,7 @@ bool libvisio::VSDMetaData::parseTimes(l
    uint32_t firstDirSectorLocation = readU32(input);
  
    // Seek to the Root Directory Entry
