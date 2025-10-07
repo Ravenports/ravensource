80650b4b9b9e5bebf9c64b4df1dd57e2cb0dbd3f
Michael Reim <kraileth@elderlinux.org>
2025-10-07 16:19:43 +0200

    sbcl: upgrade

--- bucket_FA/sbcl/patches/patch-make-config.sh
+++ bucket_FA/sbcl/patches/patch-make-config.sh
@@ -1,6 +1,6 @@
 Linux LARGEFILE option does not seem to build.
 
---- make-config.sh.orig	2025-07-26 14:30:40 UTC
+--- make-config.sh.orig	2025-08-29 14:04:39 UTC
 +++ make-config.sh
 @@ -301,7 +301,7 @@ case `uname` in
          ;;
@@ -11,7 +11,7 @@ Linux LARGEFILE option does not seem to build.
                  sbcl_os="freebsd"
                  ;;
              GNU/kFreeBSD)
-@@ -577,7 +577,6 @@ case "$sbcl_os" in
+@@ -582,7 +582,6 @@ case "$sbcl_os" in
          # src/runtime/Config.foo-linux too.
          case "$sbcl_arch" in
  	    mips | arm | x86 | x86-64)
