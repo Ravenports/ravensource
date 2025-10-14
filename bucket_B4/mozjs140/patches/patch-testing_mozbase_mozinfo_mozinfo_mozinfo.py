d8d017b74eb81f07999094d705c5f1154c1bc71b
Michael Reim <kraileth@elderlinux.org>
2025-10-14 16:59:28 +0200

    mozjs140: upgrade

--- bucket_B4/mozjs140/patches/patch-testing_mozbase_mozinfo_mozinfo_mozinfo.py
+++ bucket_B4/mozjs140/patches/patch-testing_mozbase_mozinfo_mozinfo_mozinfo.py
@@ -1,4 +1,4 @@
---- ../../testing/mozbase/mozinfo/mozinfo/mozinfo.py.orig	2025-08-11 17:31:54 UTC
+--- ../../testing/mozbase/mozinfo/mozinfo/mozinfo.py.orig	2025-09-22 21:16:17 UTC
 +++ ../../testing/mozbase/mozinfo/mozinfo/mozinfo.py
 @@ -104,7 +104,7 @@ elif system == "Linux":
  
