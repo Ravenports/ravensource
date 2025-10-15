ed9706bbce27c50c74173ab9938549b5e1f51cdd
Michael Reim <kraileth@elderlinux.org>
2025-10-15 20:23:10 +0200

    qt6-qtbase: upgrade

--- Scripts/Ravenports_Mk/raven.information.mk
+++ Scripts/Ravenports_Mk/raven.information.mk
@@ -212,7 +212,7 @@ MIDNIGHT32_CC_TARGET=		i386-raven-freebsd12.3
 # ------------------------------------------------------------------------
 
 QT5_VERSION=			5.15.10
-QT6_VERSION=			6.9.1
+QT6_VERSION=			6.9.2
 LUMINA_VERSION=			1.6.2
 LUMINA_RELEASE_TAG=		1.6.2
 KDE6_FRAMEWORKS_VERSION=	6.18.0
