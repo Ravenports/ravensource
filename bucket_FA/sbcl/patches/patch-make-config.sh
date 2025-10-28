eef94d941a5ccb95143b2fcd25a3cc4009913c96
Michael Reim <kraileth@elderlinux.org>
2025-10-28 17:52:35 +0100

    sbcl: upgrade

--- bucket_FA/sbcl/patches/patch-make-config.sh
+++ bucket_FA/sbcl/patches/patch-make-config.sh
@@ -1,6 +1,6 @@
 Linux LARGEFILE option does not seem to build.
 
---- make-config.sh.orig	2025-08-29 14:04:39 UTC
+--- make-config.sh.orig	2025-09-29 07:54:10 UTC
 +++ make-config.sh
 @@ -301,7 +301,7 @@ case `uname` in
          ;;
