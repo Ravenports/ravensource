fd2928dcb991a08d2721219127bb2637f392fbf0
Michael Reim <kraileth@elderlinux.org>
2025-10-29 20:57:12 +0100

    php84-fileinfo: upgrade

--- bucket_1A/php84-fileinfo/patches/patch-libmagic_softmagic.c
+++ bucket_1A/php84-fileinfo/patches/patch-libmagic_softmagic.c
@@ -1,4 +1,4 @@
---- libmagic/softmagic.c.orig	2024-11-20 09:48:35 UTC
+--- libmagic/softmagic.c.orig	2025-09-23 14:14:32 UTC
 +++ libmagic/softmagic.c
 @@ -511,7 +511,7 @@ check_fmt(struct magic_set *ms, const ch
  	return rv;
