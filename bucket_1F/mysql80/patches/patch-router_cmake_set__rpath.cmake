280f26906ab88c7e9c9754c08689a22fd518ad3a
Michael Reim <kraileth@elderlinux.org>
2025-10-31 16:14:51 +0100

    mysql80: upgrade

--- bucket_1F/mysql80/patches/patch-router_cmake_set__rpath.cmake
+++ bucket_1F/mysql80/patches/patch-router_cmake_set__rpath.cmake
@@ -1,9 +1,9 @@
---- router/cmake/set_rpath.cmake.orig	2025-07-09 08:45:01 UTC
-+++ router/cmake/set_rpath.cmake
-@@ -28,8 +28,7 @@ ELSE()
+--- router/cmake/set_rpath.cmake.orig	2025-10-31 15:47:45.054929000 +0100
++++ router/cmake/set_rpath.cmake	2025-10-31 15:48:40.875994000 +0100
+@@ -27,8 +27,7 @@
+   SET(RPATH_ORIGIN "\$ORIGIN")
  ENDIF()
  
- SET(CMAKE_INSTALL_RPATH)
 -IF(INSTALL_LAYOUT STREQUAL "STANDALONE"
 -    OR INSTALL_LAYOUT STREQUAL "SVR4")
 +IF(FALSE)
