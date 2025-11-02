9a78feaea03a7c8f4b8bd4181336ad5ffc861c07
Michael Reim <kraileth@elderlinux.org>
2025-11-02 16:15:11 +0100

    libedit: upgrade

--- bucket_BC/libedit/patches/patch-src_terminal.c
+++ bucket_BC/libedit/patches/patch-src_terminal.c
@@ -1,9 +1,9 @@
---- src/terminal.c.orig 2023-08-27 07:25:53 UTC
+--- src/terminal.c.orig	2023-08-27 07:25:53 UTC
 +++ src/terminal.c
 @@ -68,14 +68,6 @@ __RCSID("$NetBSD: terminal.c,v 1.46 2023
  #include <term.h>
  #endif
-
+ 
 -#if defined(__sun)
 -extern int tgetent(char *, const char *);
 -extern int tgetflag(char *);
@@ -12,6 +12,6 @@
 -extern char* tgoto(const char*, int, int);
 -extern char* tgetstr(char*, char**);
 -#endif
-
+ 
  #ifdef _REENTRANT
  #include <pthread.h>
