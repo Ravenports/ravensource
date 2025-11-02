9adb359d51218a10b347d64ebc8a1a58af9ab3b5
Michael Reim <kraileth@elderlinux.org>
2025-11-02 13:16:31 +0100

    vim: upgrade

--- bucket_E1/vim/patches/patch-src_feature.h
+++ bucket_E1/vim/patches/patch-src_feature.h
@@ -2,7 +2,7 @@ This allows /raven/etc/vim/vimrc to be loaded as a system
 vimrc. Note that creating that file will prevent loading
 defaults.vim from Vim runtime.
 
---- src/feature.h.orig	2025-10-06 19:06:02 UTC
+--- src/feature.h.orig	2025-10-27 18:07:52 UTC
 +++ src/feature.h
 @@ -703,12 +703,12 @@
  /*
