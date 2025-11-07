0ce99dfae53e79072dc8e2b5c8da077193255bbb
John Marino <draco@marino.st>
2025-11-07 14:12:17 +0000

    update mediainfo

--- bucket_9F/libmediainfo/patches/patch-Source_ThirdParty_aes-gladman_brg__endian.h
+++ bucket_9F/libmediainfo/patches/patch-Source_ThirdParty_aes-gladman_brg__endian.h
@@ -1,4 +1,4 @@
---- Source/ThirdParty/aes-gladman/brg_endian.h.orig	2025-10-03 15:10:32 UTC
+--- Source/ThirdParty/aes-gladman/brg_endian.h.orig	2025-11-07 14:09:59 UTC
 +++ Source/ThirdParty/aes-gladman/brg_endian.h
 @@ -33,7 +33,7 @@ Issue Date: 10/09/2018
  /* Include files where endian defines and byteswap functions may reside */
