8df44e217fcd6092e07e2d4d8c87d5e1349cfe6f
John Marino <john.marino@toshibagcs.com>
2025-11-11 20:06:54 +0000

    nvi: needs to built like solaris on linux

--- bucket_6A/nvi/patches/patch-common_util.c
+++ bucket_6A/nvi/patches/patch-common_util.c
@@ -4,7 +4,7 @@
  nget_uslong(SCR *sp, u_long *valp, const CHAR_T *p, CHAR_T **endp, int base)
  {
  	errno = 0;
-+#if defined(__sun__)
++#if defined(__sun__) || defined(__linux__)
 +	*valp = STRTOUL(p, (char **)endp, base);
 +#else
  	*valp = STRTOUL(p, endp, base);
@@ -16,7 +16,7 @@
  nget_slong(SCR *sp, long int *valp, const CHAR_T *p, CHAR_T **endp, int base)
  {
  	errno = 0;
-+#if defined(__sun__)
++#if defined(__sun__) || defined(__linux__)
 +	*valp = STRTOL(p, (char **)endp, base);
 +#else
  	*valp = STRTOL(p, endp, base);
