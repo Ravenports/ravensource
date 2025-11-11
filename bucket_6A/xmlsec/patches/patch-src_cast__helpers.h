251dbffb0477e7389b6f36d4be812d1f6575eff7
John Marino <john.marino@toshibagcs.com>
2025-11-11 20:12:53 +0000

    xmlsec: upgrade, pick master site that won't shift

--- bucket_6A/xmlsec/patches/patch-src_cast__helpers.h
+++ bucket_6A/xmlsec/patches/patch-src_cast__helpers.h
@@ -1,4 +1,4 @@
---- src/cast_helpers.h.orig	2025-10-14 15:42:42 UTC
+--- src/cast_helpers.h.orig	2025-11-10 00:14:05 UTC
 +++ src/cast_helpers.h
 @@ -16,6 +16,7 @@
  #error "cast_helpers.h file contains private xmlsec definitions and should not be used outside xmlsec or xmlsec-$crypto libraries"
