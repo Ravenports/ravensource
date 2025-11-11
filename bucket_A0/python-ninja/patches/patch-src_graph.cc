6ec025efda011e7d1cc6e991aee3979ab1f7f0b4
John Marino <draco@marino.st>
2025-11-11 01:41:22 +0000

    python-ninja: hack dragonfly to avoid relinking
    The recorded mtime for output files on DragonFly are just wrong.
    The input file times seem to be correct.  I don't know what's going on
    here.  Rather than disabling this "obsolete output" check for all platforms,
    just disable it on DragonFly for now and observe.
    
    If there are mass failures on other platforms (like FreeBSD, NetBSD) this
    hack might need to be extended.

---
+++ bucket_A0/python-ninja/patches/patch-src_graph.cc
@@ -0,0 +1,23 @@
+The recorded mtime for output files on DragonFly are just wrong.
+The input file times seem to be correct.  I don't know what's going on
+here.  Rather than disabling this "obsolete output" check for all platforms,
+just disable it on DragonFly for now and observe.
+
+--- src/graph.cc.orig	2025-06-18 12:08:38 UTC
++++ src/graph.cc
+@@ -343,6 +343,7 @@ bool DependencyScan::RecomputeOutputDirt
+                              output->path().c_str());
+         return true;
+       }
++#if !defined(__DragonFly__)
+       if (most_recent_input && entry->mtime < most_recent_input->mtime()) {
+         // May also be dirty due to the mtime in the log being older than the
+         // mtime of the most recent input.  This can occur even when the mtime
+@@ -358,6 +359,7 @@ bool DependencyScan::RecomputeOutputDirt
+             entry->mtime, most_recent_input->mtime());
+         return true;
+       }
++#endif
+     }
+     if (!entry && !generator) {
+       explanations_.Record(output, "command line not found in log for %s",
