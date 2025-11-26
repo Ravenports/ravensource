0f3f3f5265cd75dfd07b9599d48c90bc8e5623dd
Michael Reim <kraileth@elderlinux.org>
2025-11-26 21:13:53 +0100

    freeglut: upgrade

--- bucket_2C/freeglut/patches/patch-src_fg__internal.h
+++ bucket_2C/freeglut/patches/patch-src_fg__internal.h
@@ -1,12 +1,13 @@
---- src/fg_internal.h.orig	2022-10-07 16:12:51 UTC
-+++ src/fg_internal.h
-@@ -137,9 +137,6 @@
-  * If so, perhaps there should be a config_guess.h for them. Alternatively,
-  * config guesses could be placed above, just after the config.h exclusion.
-  */
+--- src/fg_internal.h.orig	2025-11-26 21:09:49.131780000 +0100
++++ src/fg_internal.h	2025-11-26 21:10:05.151605000 +0100
+@@ -134,10 +134,6 @@
+ #    include <time.h>
+ #endif
+ 
 -#if defined(__FreeBSD__) || defined(__NetBSD__)
 -#    define HAVE_USB_JS 1
 -#endif
- 
+-
  #if defined(_MSC_VER) || defined(__WATCOMC__)
  /* strdup() is non-standard, for all but POSIX-2001 */
+ #define strdup   _strdup
