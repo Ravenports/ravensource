53162a8af4228897065ac5971b4f387dd9463cd3
Michael Reim <kraileth@elderlinux.org>
2025-11-28 19:25:56 +0100

    qt5-qtimageformats: upgrade

--- bucket_AD/qt5-qtimageformats/patches/patch-src_plugins_imageformats_jp2_qjp2handler.cpp
+++ bucket_AD/qt5-qtimageformats/patches/patch-src_plugins_imageformats_jp2_qjp2handler.cpp
@@ -1,4 +1,4 @@
---- src/plugins/imageformats/jp2/qjp2handler.cpp.orig	2024-11-08 13:36:15 UTC
+--- src/plugins/imageformats/jp2/qjp2handler.cpp.orig	2024-11-20 13:56:40 UTC
 +++ src/plugins/imageformats/jp2/qjp2handler.cpp
 @@ -38,6 +38,7 @@
  **
