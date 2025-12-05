e3bdb9f53f9fd520699cb6cc7233ead2c78cf6c7
John Marino <draco@marino.st>
2025-12-05 20:54:32 +0000

    drop cpp-hocon
    Apparently this was a puppet dependency that hasn't been used in years.
    The history shows that it consistently breaks with boost updates (and
    it's broken again with boost 1.89) but there's only fixing action, not
    updates.  Other repositories are just dropping it as well.

--- bucket_87/cpp-hocon/patches/patch-lib_src_path.cc
+++
@@ -1,7 +0,0 @@
---- lib/src/path.cc.orig	2019-12-12 09:56:44 UTC
-+++ lib/src/path.cc
-@@ -1,3 +1,4 @@
-+#include <algorithm>
- #include <hocon/path.hpp>
- #include <hocon/config_exception.hpp>
- #include <internal/path_builder.hpp>
