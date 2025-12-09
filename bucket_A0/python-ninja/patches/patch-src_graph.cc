924d67834b134a459fb1f148e34a9316eb661e3a
John Marino <draco@marino.st>
2025-12-09 13:56:11 +0000

    ninja: Avoid relinking on Midnight, same as DragonFly

--- bucket_A0/python-ninja/patches/patch-src_graph.cc
+++ bucket_A0/python-ninja/patches/patch-src_graph.cc
@@ -3,13 +3,15 @@ The input file times seem to be correct.  I don't know what's going on
 here.  Rather than disabling this "obsolete output" check for all platforms,
 just disable it on DragonFly for now and observe.
 
+Also seen on MidnightBSD
+
 --- src/graph.cc.orig	2025-06-18 12:08:38 UTC
 +++ src/graph.cc
 @@ -343,6 +343,7 @@ bool DependencyScan::RecomputeOutputDirt
                               output->path().c_str());
          return true;
        }
-+#if !defined(__DragonFly__)
++#if !(defined(__DragonFly__) || defined(__MidnightBSD__))
        if (most_recent_input && entry->mtime < most_recent_input->mtime()) {
          // May also be dirty due to the mtime in the log being older than the
          // mtime of the most recent input.  This can occur even when the mtime
