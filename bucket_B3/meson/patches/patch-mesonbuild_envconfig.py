bbe97e978c553cbf9aa5f4986db7c2890bd6970c
John Marino <draco@marino.st>
2025-12-09 17:01:27 +0000

    meson: upgrade

--- bucket_B3/meson/patches/patch-mesonbuild_envconfig.py
+++ bucket_B3/meson/patches/patch-mesonbuild_envconfig.py
@@ -1,6 +1,6 @@
---- mesonbuild/envconfig.py.orig	2025-08-24 17:28:31 UTC
+--- mesonbuild/envconfig.py.orig	2025-12-09 16:59:30 UTC
 +++ mesonbuild/envconfig.py
-@@ -354,7 +354,7 @@ class MachineInfo(HoldableObject):
+@@ -360,7 +360,7 @@ class MachineInfo(HoldableObject):
  
      def is_freebsd(self) -> bool:
          """Machine is FreeBSD?"""
@@ -9,3 +9,21 @@
  
      def is_sunos(self) -> bool:
          """Machine is illumos or Solaris?"""
+@@ -579,6 +579,8 @@ def detect_cpu_family(compilers: T.Dict[
+         trial = detect_windows_arch(compilers)
+     elif mesonlib.is_freebsd() or mesonlib.is_netbsd() or mesonlib.is_openbsd() or mesonlib.is_qnx() or mesonlib.is_aix():
+         trial = platform.processor().lower()
++    elif mesonlib.is_dragonflybsd():
++        trial = platform.processor().lower()
+     else:
+         trial = platform.machine().lower()
+     if trial.startswith('i') and trial.endswith('86'):
+@@ -645,6 +647,8 @@ def detect_cpu(compilers: T.Dict[str, Co
+         trial = detect_windows_arch(compilers)
+     elif mesonlib.is_freebsd() or mesonlib.is_netbsd() or mesonlib.is_openbsd() or mesonlib.is_aix():
+         trial = platform.processor().lower()
++    elif mesonlib.is_dragonflybsd():
++        trial = platform.processor().lower()
+     else:
+         trial = platform.machine().lower()
+ 
