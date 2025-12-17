cc2ccfb9f6aec71bb2a0cde6e24d8f2629bfb97a
John Marino <draco@marino.st>
2025-12-17 01:33:23 +0000

    R-s2: fix build on NetBSD

--- bucket_FB/R-s2/patches/patch-src_s2_base_port.h
+++ bucket_FB/R-s2/patches/patch-src_s2_base_port.h
@@ -1,6 +1,6 @@
 --- src/s2/base/port.h.orig	2025-01-07 11:51:24 UTC
 +++ src/s2/base/port.h
-@@ -30,7 +30,7 @@
+@@ -30,9 +30,16 @@
  // -----------------------------------------------------------------------------
  
  // IS_LITTLE_ENDIAN, IS_BIG_ENDIAN
@@ -8,4 +8,13 @@
 +#if defined(__linux__) || defined(__ANDROID__) || defined(__FreeBSD__) || defined(__DragonFly__)
  #include <endian.h>
  
++#elif defined(__NetBSD__)
++
++#include <endian.h>
++#define __BYTE_ORDER _BYTE_ORDER
++#define __LITTLE_ENDIAN _LITTLE_ENDIAN
++#define __BIG_ENDIAN _BIG_ENDIAN
++
  #elif defined(__APPLE__)
+ 
+ // BIG_ENDIAN
