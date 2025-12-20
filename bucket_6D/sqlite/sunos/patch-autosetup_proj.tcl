b93a971710d5c54b2f1fe579fd5633d0307d3d6f
John Marino <draco@marino.st>
2025-12-20 23:50:27 +0000

    sqlite: fix build on omnios

--- bucket_6D/sqlite/sunos/patch-autosetup_proj.tcl
+++ bucket_6D/sqlite/sunos/patch-autosetup_proj.tcl
@@ -1,14 +1,18 @@
 Sunos link complains when -Wl,-soname is applied to an executable
 rather than a library.  The test is bad!!  Force test success.
 
---- autosetup/proj.tcl.orig	Tue Jul  1 15:14:59 2025
-+++ autosetup/proj.tcl	Tue Jul  1 15:15:55 2025
-@@ -1157,13 +1157,8 @@
+--- autosetup/proj.tcl.orig	2025-11-28 17:50:50 UTC
++++ autosetup/proj.tcl
+@@ -1196,17 +1196,8 @@ proc proj-check-rpath {} {
  #
  proc proj-check-soname {{libname "libfoo.so.0"}} {
    cc-with {-link 1} {
 -    if {[cc-check-flags "-Wl,-soname,${libname}"]} {
 -      define LDFLAGS_SONAME_PREFIX "-Wl,-soname,"
+-      return 1
+-    } elseif {[cc-check-flags "-Wl,+h,${libname}"]} {
+-      # HP-UX: https://sqlite.org/forum/forumpost/d80ecdaddd
+-      define LDFLAGS_SONAME_PREFIX "-Wl,+h,"
 -      return 1
 -    } else {
 -      define LDFLAGS_SONAME_PREFIX ""
