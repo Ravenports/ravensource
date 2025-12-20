5f7104e10150fab3d1f2f4ff878ef52989a48d91
John Marino <draco@marino.st>
2025-12-20 19:41:27 +0100

    R-s2: fix build on MidnightBSD

--- bucket_FB/R-s2/patches/patch-src_s2_base_port.h
+++ bucket_FB/R-s2/patches/patch-src_s2_base_port.h
@@ -1,11 +1,17 @@
 --- src/s2/base/port.h.orig	2025-01-07 11:51:24 UTC
 +++ src/s2/base/port.h
-@@ -30,9 +30,16 @@
+@@ -30,9 +30,22 @@
  // -----------------------------------------------------------------------------
  
  // IS_LITTLE_ENDIAN, IS_BIG_ENDIAN
 -#if defined(__linux__) || defined(__ANDROID__)
-+#if defined(__linux__) || defined(__ANDROID__) || defined(__FreeBSD__) || defined(__DragonFly__)
++#if defined(__MidnightBSD__)
++#include <sys/endian.h>
++#define __BYTE_ORDER _BYTE_ORDER
++#define __LITTLE_ENDIAN _LITTLE_ENDIAN
++#define __BIG_ENDIAN _BIG_ENDIAN
++ 
++#elif defined(__linux__) || defined(__ANDROID__) || defined(__FreeBSD__) || defined(__DragonFly__)
  #include <endian.h>
  
 +#elif defined(__NetBSD__)
