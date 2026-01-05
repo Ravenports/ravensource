09460cd6b7c91c26cde4b111b2e14477b3ce9147
Michael Reim <kraileth@elderlinux.org>
2026-01-05 17:01:35 +0100

    imlib2: upgrade

--- bucket_38/imlib2/patches/patch-src_bin_imlib2__view.c
+++ bucket_38/imlib2/patches/patch-src_bin_imlib2__view.c
@@ -1,6 +1,6 @@
 Include sys/time.h for timeval.
 
---- src/bin/imlib2_view.c.orig	2025-03-15 18:12:29 UTC
+--- src/bin/imlib2_view.c.orig	2025-04-06 13:23:11 UTC
 +++ src/bin/imlib2_view.c
 @@ -3,6 +3,7 @@
  
