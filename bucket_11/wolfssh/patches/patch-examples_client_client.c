b633f48369153d0102ca3d27e8e973e7401c0157
Michael Reim <kraileth@elderlinux.org>
2026-01-07 17:43:18 +0100

    wolfssh: upgrade

--- bucket_11/wolfssh/patches/patch-examples_client_client.c
+++ bucket_11/wolfssh/patches/patch-examples_client_client.c
@@ -1,4 +1,4 @@
---- examples/client/client.c.orig	2025-02-20 20:31:48 UTC
+--- examples/client/client.c.orig	2025-10-20 21:46:33 UTC
 +++ examples/client/client.c
 @@ -22,6 +22,8 @@
      #include <config.h>
--- bucket_11/wolfssh/patches/patch-examples_echoserver_echoserver.c
+++ bucket_11/wolfssh/patches/patch-examples_echoserver_echoserver.c
@@ -1,6 +1,6 @@
---- examples/echoserver/echoserver.c.orig	2025-02-20 20:31:48 UTC
+--- examples/echoserver/echoserver.c.orig	2025-10-20 21:46:33 UTC
 +++ examples/echoserver/echoserver.c
-@@ -69,9 +69,13 @@
+@@ -71,9 +71,13 @@
      #ifdef HAVE_PTY_H
          #include <pty.h>
      #endif
