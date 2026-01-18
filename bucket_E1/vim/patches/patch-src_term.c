5a74821117ab57a3d123c3910f73dd79f6bd2a8e
Michael Reim <kraileth@elderlinux.org>
2026-01-18 12:11:50 +0100

    vim: upgrade

--- bucket_E1/vim/patches/patch-src_term.c
+++ bucket_E1/vim/patches/patch-src_term.c
@@ -1,4 +1,4 @@
---- src/term.c.orig	2026-01-03 23:47:27 UTC
+--- src/term.c.orig	2026-01-10 18:14:13 UTC
 +++ src/term.c
 @@ -30,6 +30,7 @@
  #  include <termios.h>	    // seems to be required for some Linux
