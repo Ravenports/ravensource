0b76cd9f113ef490f49aa19c32816a84eecfa1f1
John Marino <draco@marino.st>
2026-01-19 14:21:38 +0000

    vlc: upgrade

--- bucket_54/vlc/patches/patch-modules_codec_opus__header.h
+++ bucket_54/vlc/patches/patch-modules_codec_opus__header.h
@@ -1,4 +1,4 @@
---- modules/codec/opus_header.h.orig	2025-11-13 22:17:54 UTC
+--- modules/codec/opus_header.h.orig	2025-12-23 10:49:20 UTC
 +++ modules/codec/opus_header.h
 @@ -29,6 +29,7 @@
  #define OPUS_HEADER_H
