36d93a76505756435cd4c6211e24b3ca7abefb9e
John Marino <draco@marino.st>
2026-01-23 21:49:46 +0000

    add new lua-5.5

---
+++ bucket_3E/lua55/patches/patch-src__liolib.c
@@ -0,0 +1,10 @@
+--- src/liolib.c.orig	2025-12-15 10:44:39 UTC
++++ src/liolib.c
+@@ -16,6 +16,7 @@
+ #include <stdio.h>
+ #include <stdlib.h>
+ #include <string.h>
++#include <unistd.h>
+ 
+ #include "lua.h"
+ 
