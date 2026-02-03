1d84611e089d369540c8e5eb14d500fb5d02060c
Michael Reim <kraileth@elderlinux.org>
2026-02-03 17:39:58 +0100

    lmdb: upgrade

--- bucket_C9/lmdb/patches/patch-mdb.c
+++ bucket_C9/lmdb/patches/patch-mdb.c
@@ -1,11 +1,11 @@
---- mdb.c.orig	2024-05-21 17:16:06 UTC
+--- mdb.c.orig	2026-01-26 19:02:01 UTC
 +++ mdb.c
-@@ -131,6 +131,8 @@ typedef SSIZE_T	ssize_t;
- # define MDB_FDATASYNC		fsync
- #elif defined(ANDROID)
+@@ -138,6 +138,8 @@ typedef SSIZE_T	ssize_t;
+ #elif defined(__HAIKU__)
+ # define MDB_USE_POSIX_SEM	1
  # define MDB_FDATASYNC		fsync
 +#elif defined(__FreeBSD_version) && __FreeBSD_version < 1101000
 +# define MDB_FDATASYNC		fsync
  #endif
  
- #ifndef _WIN32
+ /* NetBSD does not define union semun in sys/sem.h */
