54f3a6580dc2b02161be0651586b8fc0c2305d65
John Marino <draco@marino.st>
2026-02-18 03:15:18 +0000

    unzip: fix build on omnios, delete accidentally committed trash

--- bucket_14/unzip/patches/patch-unix_unix.c
+++ bucket_14/unzip/patches/patch-unix_unix.c
@@ -1,17 +1,18 @@
 --- unix/unix.c.orig	2009-01-23 23:31:26 UTC
 +++ unix/unix.c
-@@ -30,6 +30,10 @@
+@@ -30,6 +30,11 @@
  #define UNZIP_INTERNAL
  #include "unzip.h"
  
 +#if defined(__sun__)
 +#include <utime.h>
++#include <strings.h>
 +#endif
 +
  #ifdef SCO_XENIX
  #  define SYSNDIR
  #else  /* SCO Unix, AIX, DNIX, TI SysV, Coherent 4.x, ... */
-@@ -610,7 +614,7 @@ int mapname(__G__ renamed)
+@@ -610,7 +615,7 @@ int mapname(__G__ renamed)
                   * else allow 8-bit characters (e.g. UTF-8) in filenames:
                   */
                  if (uO.cflxflag ||
