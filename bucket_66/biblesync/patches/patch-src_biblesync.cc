34ec27b07a19373c591a6c46b32cb2b245dd75b6
John Marino <draco@marino.st>
2026-02-22 19:53:42 +0000

    biblesync: fix build on omnios

---
+++ bucket_66/biblesync/patches/patch-src_biblesync.cc
@@ -0,0 +1,10 @@
+--- src/biblesync.cc.orig	2020-05-24 17:57:02 UTC
++++ src/biblesync.cc
+@@ -17,6 +17,7 @@
+ // BibleSync - class implementation.
+ //
+ 
++#include <cstring>
+ #include <biblesync.hh>
+ 
+ using namespace std;
