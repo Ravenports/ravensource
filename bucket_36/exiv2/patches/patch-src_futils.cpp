f68153a1c1a785dc4981a6a93715b812ea2d018b
John Marino <draco@marino.st>
2026-02-24 05:05:11 +0000

    exiv2: fix build on omnios

--- bucket_36/exiv2/patches/patch-src_futils.cpp
+++ bucket_36/exiv2/patches/patch-src_futils.cpp
@@ -1,6 +1,16 @@
+Include image_int.hpp for Internal::stringFormat().
+
 --- src/futils.cpp.orig	2025-08-31 08:31:02 UTC
 +++ src/futils.cpp
-@@ -33,9 +33,11 @@ namespace fs = std::experimental::filesy
+@@ -6,6 +6,7 @@
+ #include "config.h"
+ #include "enforce.hpp"
+ #include "error.hpp"
++#include "image_int.hpp"
+ #include "utils.hpp"
+ 
+ // + standard includes
+@@ -33,9 +34,11 @@ namespace fs = std::experimental::filesy
  // clang-format on
  #endif
  
