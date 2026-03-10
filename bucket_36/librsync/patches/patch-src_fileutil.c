a8498873f1c72b26bd839a2701d48959ef068795
John Marino <draco@marino.st>
2026-03-10 17:22:17 +0000

    librsync: fix build on omnios

--- bucket_36/librsync/patches/patch-src_fileutil.c
+++ bucket_36/librsync/patches/patch-src_fileutil.c
@@ -1,12 +1,17 @@
+$NetBSD: patch-src_fileutil.c,v 1.1 2025/12/19 12:27:03 nia Exp $
+
+Maybe don't peek into internal libc symbols if we can avoid it.
+
+Fixes build on SunOS.
+
 --- src/fileutil.c.orig	2023-02-19 05:08:08 UTC
 +++ src/fileutil.c
-@@ -75,7 +75,9 @@
- 
- /* Use and prefer _fileno if it exists. */
- #ifdef HAVE__FILENO
-+# if !defined(__NetBSD__)
- #  define fileno(f) _fileno((f))
-+# endif
+@@ -74,7 +74,7 @@
  #endif
  
- FILE *rs_file_open(char const *filename, char const *mode, int force)
+ /* Use and prefer _fileno if it exists. */
+-#ifdef HAVE__FILENO
++#if defined(HAVE__FILENO) && !defined(HAVE_FILENO)
+ #  define fileno(f) _fileno((f))
+ #endif
+ 
