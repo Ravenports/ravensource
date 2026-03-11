5e747e7c221501bc9f51e84bbcf8eac632697c90
John Marino <draco@marino.st>
2026-03-11 03:33:32 +0000

    libwebsockets: fix build on omnios

---
+++ bucket_00/libwebsockets/patches/patch-lib_misc_diskcache.c
@@ -0,0 +1,33 @@
+--- lib/misc/diskcache.c.orig	2025-12-03 06:15:09 UTC
++++ lib/misc/diskcache.c
+@@ -280,6 +280,9 @@ lws_diskcache_trim(struct lws_diskcache_
+ 	size_t trimmed = 0;
+ 	struct dirent *de;
+ 	struct stat s;
++#if defined(__sun)
++	struct stat st;
++#endif
+ 	DIR *dir;
+ 
+ 	if (!lds->cache_subdir) {
+@@ -316,14 +319,16 @@ lws_diskcache_trim(struct lws_diskcache_
+ 		if (!de)
+ 			break;
+ 
++		lws_snprintf(filepath, sizeof(filepath), "%s/%s", dirpath, de->d_name);
++#if defined(__sun)
++		if (stat(filepath, &st) != 0 || !S_ISREG(st.st_mode))
++			continue;
++#else
+ 		if (de->d_type != DT_REG)
+ 			continue;
+-
++#endif
+ 		lds->agg_file_count++;
+ 
+-		lws_snprintf(filepath, sizeof(filepath), "%s/%s", dirpath,
+-			     de->d_name);
+-
+ 		fd = open(filepath, O_RDONLY);
+ 		if (fd < 0) {
+ 			lwsl_err("%s: cannot open %s\n", __func__, filepath);
--- bucket_00/libwebsockets/patches/patch-lib_plat_unix_unix-sockets.c
+++ bucket_00/libwebsockets/patches/patch-lib_plat_unix_unix-sockets.c
@@ -1,6 +1,21 @@
---- lib/plat/unix/unix-sockets.c.orig	2025-11-29 19:42:32 UTC
+--- lib/plat/unix/unix-sockets.c.orig	2025-12-03 06:15:09 UTC
 +++ lib/plat/unix/unix-sockets.c
-@@ -109,6 +109,7 @@ lws_plat_set_socket_options(struct lws_v
+@@ -52,6 +52,14 @@
+ 
+ #include <netinet/ip.h>
+ 
++#if defined(__sun) && !defined(IPV6_ADDR_PREFERENCES)
++# ifdef IPV6_SRC_PREFERENCES
++#define IPV6_ADDR_PREFERENCES IPV6_SRC_PREFERENCES
++# else
++#define IPV6_ADDR_PREFERENCES 0
++# endif
++#endif
++
+ int
+ lws_send_pipe_choked(struct lws *wsi)
+ {
+@@ -109,6 +117,7 @@ lws_plat_set_socket_options(struct lws_v
  	socklen_t optlen = sizeof(optval);
  
  #if defined(__APPLE__) || \
@@ -8,7 +23,7 @@
      defined(__FreeBSD__) || defined(__FreeBSD_kernel__) || \
      defined(__NetBSD__) || \
      defined(__OpenBSD__) || \
-@@ -126,6 +127,7 @@ lws_plat_set_socket_options(struct lws_v
+@@ -126,6 +135,7 @@ lws_plat_set_socket_options(struct lws_v
  			return 1;
  
  #if defined(__APPLE__) || \
@@ -16,7 +31,7 @@
      defined(__FreeBSD__) || defined(__FreeBSD_kernel__) || \
      defined(__NetBSD__) || \
      defined(__CYGWIN__) || defined(__OpenBSD__) || defined (__sun) || \
-@@ -179,6 +181,7 @@ lws_plat_set_socket_options(struct lws_v
+@@ -179,6 +189,7 @@ lws_plat_set_socket_options(struct lws_v
  	if (!unix_skt && setsockopt(fd, IPPROTO_TCP, TCP_NODELAY, (const void *)&optval, optlen) < 0)
  		return 1;
  #elif !defined(__APPLE__) && \
@@ -24,7 +39,7 @@
        !defined(__FreeBSD__) && !defined(__FreeBSD_kernel__) &&        \
        !defined(__NetBSD__) && \
        !defined(__OpenBSD__) && \
-@@ -239,6 +242,7 @@ lws_plat_set_socket_options_ip(lws_sockf
+@@ -239,6 +250,7 @@ lws_plat_set_socket_options_ip(lws_sockf
  #endif
  
  #if !defined(__APPLE__) && \
