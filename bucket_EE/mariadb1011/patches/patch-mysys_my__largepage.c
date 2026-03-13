659a4010766fe9dba4621949cfc27b1417a5b130
John Marino <draco@marino.st>
2026-03-13 13:12:25 +0000

    mariadb1011: fix build on omnios

---
+++ bucket_EE/mariadb1011/patches/patch-mysys_my__largepage.c
@@ -0,0 +1,20 @@
+$NetBSD: patch-mysys_my__largepage.c,v 1.2 2025/10/27 20:56:37 nia Exp $
+
+--- mysys/my_largepage.c.orig	2026-01-31 10:47:55 UTC
++++ mysys/my_largepage.c
+@@ -28,6 +28,7 @@
+ #endif
+ 
+ #ifdef HAVE_SOLARIS_LARGE_PAGES
++my_bool opt_super_large_pages= 0;
+ #if defined(__sun__) && defined(__GNUC__) && defined(__cplusplus) \
+     && defined(_XOPEN_SOURCE)
+ /* memcntl exist within sys/mman.h, but under-defines what is need to use it */
+@@ -203,7 +204,6 @@ int my_init_large_pages(void)
+   my_get_large_page_sizes(my_large_page_sizes);
+ 
+ #ifdef HAVE_SOLARIS_LARGE_PAGES
+-  extern my_bool opt_super_large_pages;
+   /*
+     tell the kernel that we want to use 4/256MB page for heap storage
+     and also for the stack. We use 4 MByte as default and if the
--- bucket_EE/mariadb1011/patches/patch-sql_mysqld.cc
+++ bucket_EE/mariadb1011/patches/patch-sql_mysqld.cc
@@ -1,5 +1,14 @@
 --- sql/mysqld.cc.orig	2026-01-31 10:47:55 UTC
 +++ sql/mysqld.cc
+@@ -422,7 +422,7 @@ char* opt_secure_file_priv;
+ my_bool lower_case_file_system= 0;
+ my_bool opt_large_pages= 0;
+ #ifdef HAVE_SOLARIS_LARGE_PAGES
+-my_bool opt_super_large_pages= 0;
++extern my_bool opt_super_large_pages;
+ #endif
+ my_bool opt_myisam_use_mmap= 0;
+ uint   opt_large_page_size= 0;
 @@ -4720,8 +4720,9 @@ static void init_ssl()
          sql_print_warning("SSL error: %s",buf);
        }
