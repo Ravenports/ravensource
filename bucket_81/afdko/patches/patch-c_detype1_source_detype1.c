eac32ab84ed9be8952b1b61c0403fd9a7b688158
John Marino <draco@marino.st>
2026-03-13 15:35:23 +0000

    afdko: commit forgotten patches for omnios

---
+++ bucket_81/afdko/patches/patch-c_detype1_source_detype1.c
@@ -0,0 +1,20 @@
+--- c/detype1/source/detype1.c.orig	2025-12-05 00:32:05 UTC
++++ c/detype1/source/detype1.c
+@@ -4,6 +4,7 @@
+ #include <stdarg.h>
+ #include <string.h>
+ #include <stdlib.h>
++#include <unistd.h>
+ #include <stdbool.h>
+ #ifdef _MSC_VER /* defined by Microsoft Compiler */
+ #include <io.h>
+@@ -524,7 +525,9 @@ static void usage(void) {
+ }
+ 
+ #ifndef _MSC_VER /* Unix */
++#if !defined(__sun__)
+ extern int getopt(int argc, char **argv, char *optstring);
++#endif
+ extern int optind;
+ extern char *optarg;
+ #else  /* dos */
---
+++ bucket_81/afdko/patches/patch-c_type1_source_type1.c
@@ -0,0 +1,20 @@
+--- c/type1/source/type1.c.orig	2025-12-05 00:32:05 UTC
++++ c/type1/source/type1.c
+@@ -4,6 +4,7 @@
+ #include <ctype.h>
+ #include <string.h>
+ #include <stdarg.h>
++#include <unistd.h>
+ #include <stdlib.h>
+ #ifdef _MSC_VER /* defined by Microsoft Compiler */
+ #include <fcntl.h>
+@@ -504,7 +505,9 @@ static void usage(void) {
+ }
+ 
+ #ifndef _MSC_VER /* Unix */
++#if !defined(__sun__)
+ extern int getopt(int argc, char **argv, char *optstring);
++#endif
+ extern int optind;
+ extern char *optarg;
+ #else  /* dos */
