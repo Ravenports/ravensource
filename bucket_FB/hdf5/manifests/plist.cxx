8ae20f36bfc62fdce0e78bbcdeae6d3f1afe7631
Michael Reim <kraileth@elderlinux.org>
2026-03-15 18:48:49 +0100

    hdf5: upgrade

--- bucket_FB/hdf5/manifests/plist.cxx
+++ bucket_FB/hdf5/manifests/plist.cxx
@@ -1,4 +1,4 @@
 lib/libhdf5_cpp.so.%%CPP_SOMAJOR%%
 lib/libhdf5_cpp.so.%%CPP_SOVERSION%%
-lib/libhdf5_hl_cpp.so.%%HL_SOMAJOR%%
-lib/libhdf5_hl_cpp.so.%%HL_SOVER%%
+lib/libhdf5_hl_cpp.so.%%HL_CPP_SOMAJOR%%
+lib/libhdf5_hl_cpp.so.%%HL_CPP_SOVER%%
