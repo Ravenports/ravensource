f8108a5162e5df456317347865c14b141506c981
John Marino <draco@marino.st>
2026-03-25 03:15:04 +0000

    tweak python shebang fix, affects 3 ports

--- Scripts/Ravenports_Mk/Scripts/fix-python-shebang.sh
+++ Scripts/Ravenports_Mk/Scripts/fix-python-shebang.sh
@@ -7,27 +7,31 @@
 
 PCMD="${3}"
 PENV="/usr/bin/env python"
-SARG="s|^\#![[:space:]]*${PENV}\([[:space:]]\)|\#!${PCMD}\1|; s|^\#![[:space:]]*${PENV}\$|\#!${PCMD}|"
+PY3C="/usr/bin/python3"
+SARG="1s|^\#![[:space:]]*${PENV}\([[:space:]]\)|\#!${PCMD}\1|; 1s|^\#![[:space:]]*${PENV}\$|\#!${PCMD}|"
+SAR2="1s|^\#![[:space:]]*${PY3C}\([[:space:]]\)|\#!${PCMD}\1|; 1s|^\#![[:space:]]*${PY3C}\$|\#!${PCMD}|"
 
 check_file() {
 	f="$1"
 	top_line=$(sed -n -e '1s/^#![[:space:]]*\([^[:space:]]*\)[[:space:]]*\([^[:space:]]*\).*/\1 \2/p;2q' "${f}")
 
-	interp=${top_line% * bridge} # Note: % * removes everything after the last space
-        interp=${top_line% *}
+	interp=${top_line% *}
+	interparg=${top_line#* }
 
 	case "${interp}" in
 	/usr/bin/env)
-		interparg=${top_line#* }
-		case "${interparg}" in
-		python)
+		if [ "$interparg" = "python" ]; then
 			echo "==> Adjusting /usr/bin/env shebang: ${f}"
 			sed -i'' -e "${SARG}" "${f}"
-			;;
-		esac
+		fi
+		;;
+	/usr/bin/python3)
+		echo "==> Adjusting /usr/bin/python3 shebang: ${f}"
+		sed -i'' -e "${SAR2}" "${f}"
+		;;
 	esac
 }
 
-find "${1}${2}" -type f -perm /111 -name "*.py" 2>/dev/null | while read -r ff; do
+find "${1}${2}" -type f -perm /111 2>/dev/null | while read -r ff; do
     check_file "$ff"
 done
