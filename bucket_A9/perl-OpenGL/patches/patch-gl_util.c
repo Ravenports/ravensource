b5659818d1add993da9c2c7f0601322b9b646e32
John Marino <draco@marino.st>
2026-03-25 05:48:31 +0000

    perl-OpenGL: regen patches to fix build

--- bucket_A9/perl-OpenGL/patches/patch-gl_util.c
+++ bucket_A9/perl-OpenGL/patches/patch-gl_util.c
@@ -1,6 +1,6 @@
---- gl_util.c.orig	2025-04-14 04:33:29 UTC
+--- gl_util.c.orig	2026-02-07 18:49:14 UTC
 +++ gl_util.c
-@@ -759,6 +759,12 @@ int gl_component_count(GLenum format, GL
+@@ -978,6 +978,12 @@ int gl_component_count(GLenum format, GL
  
  /* 18 */
  #ifdef GL_EXT_cmyka
