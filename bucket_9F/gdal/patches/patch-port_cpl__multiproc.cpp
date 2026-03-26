111d1a05b1d0efe093b251cde5cb1d1c753a2a77
John Marino <draco@marino.st>
2026-03-26 05:26:25 +0000

    gdal: upgrade

--- bucket_9F/gdal/patches/patch-port_cpl__multiproc.cpp
+++ bucket_9F/gdal/patches/patch-port_cpl__multiproc.cpp
@@ -1,4 +1,4 @@
---- port/cpl_multiproc.cpp.orig	2025-12-18 14:15:18 UTC
+--- port/cpl_multiproc.cpp.orig	2026-03-20 13:31:36 UTC
 +++ port/cpl_multiproc.cpp
 @@ -1365,7 +1365,11 @@ int CPLGetNumCPUs()
      nCPUs = 1;
