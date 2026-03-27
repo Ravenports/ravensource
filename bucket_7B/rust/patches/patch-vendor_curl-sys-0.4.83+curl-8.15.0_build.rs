983a1acb2ba7e7d4542014c1c3d4a24f44c5725c
John Marino <draco@marino.st>
2026-03-27 17:55:16 +0000

    rust: the ca-root patch returned from version 94.0, fixes secure downloads

---
+++ bucket_7B/rust/patches/patch-vendor_curl-sys-0.4.83+curl-8.15.0_build.rs
@@ -0,0 +1,10 @@
+--- vendor/curl-sys-0.4.83+curl-8.15.0/build.rs.orig	2026-03-25 01:22:28 UTC
++++ vendor/curl-sys-0.4.83+curl-8.15.0/build.rs
+@@ -106,6 +106,7 @@ fn main() {
+         .include("curl/lib")
+         .include("curl/include")
+         .define("BUILDING_LIBCURL", None)
++        .define("CURL_CA_BUNDLE", "\"@PREFIX@/share/certs/ca-root-nss.crt\"")
+         .define("CURL_DISABLE_DICT", None)
+         .define("CURL_DISABLE_GOPHER", None)
+         .define("CURL_DISABLE_IMAP", None)
