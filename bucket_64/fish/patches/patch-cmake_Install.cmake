dc34a876662c03ac55f95c4e5195abdd5299e651
John Marino <draco@marino.st>
2026-03-28 20:17:43 +0000

    fish: upgrade

--- bucket_64/fish/patches/patch-cmake_Install.cmake
+++ bucket_64/fish/patches/patch-cmake_Install.cmake
@@ -1,20 +1,20 @@
---- cmake/Install.cmake.orig	2026-01-07 07:34:20 UTC
+--- cmake/Install.cmake.orig	2026-03-28 04:56:37 UTC
 +++ cmake/Install.cmake
-@@ -108,7 +108,7 @@ install(FILES share/config.fish
- fish_create_dirs(${rel_datadir}/fish/vendor_completions.d ${rel_datadir}/fish/vendor_functions.d
-     ${rel_datadir}/fish/vendor_conf.d)
+@@ -126,7 +126,7 @@ fish_create_dirs(
+     ${rel_datadir}/fish/vendor_conf.d
+ )
  
 -fish_try_create_dirs(${rel_datadir}/pkgconfig)
 +fish_try_create_dirs(${rel_datadir}/../lib/pkgconfig)
  configure_file(fish.pc.in fish.pc.noversion @ONLY)
  
- add_custom_command(OUTPUT fish.pc
-@@ -121,7 +121,7 @@ add_custom_command(OUTPUT fish.pc
- add_custom_target(build_fish_pc ALL DEPENDS fish.pc)
+ add_custom_command(
+@@ -142,7 +142,7 @@ add_custom_target(build_fish_pc ALL DEPE
  
- install(FILES ${CMAKE_CURRENT_BINARY_DIR}/fish.pc
--        DESTINATION ${rel_datadir}/pkgconfig)
-+        DESTINATION ${rel_datadir}/../lib/pkgconfig)
+ install(
+     FILES ${CMAKE_CURRENT_BINARY_DIR}/fish.pc
+-    DESTINATION ${rel_datadir}/pkgconfig
++    DESTINATION ${rel_datadir}/../lib/pkgconfig
+ )
  
- install(DIRECTORY share/completions/
-         DESTINATION ${rel_datadir}/fish/completions
+ install(
