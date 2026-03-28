3717c0f8e86ab6fbdbdc6eab8688d86068214f29
John Marino <draco@marino.st>
2026-03-28 16:09:51 +0000

    vim: upgrade

--- bucket_E1/vim/patches/patch-src_term.c
+++ bucket_E1/vim/patches/patch-src_term.c
@@ -1,4 +1,4 @@
---- src/term.c.orig	2026-03-26 21:19:34 UTC
+--- src/term.c.orig	2026-03-28 10:07:27 UTC
 +++ src/term.c
 @@ -30,6 +30,7 @@
  #  include <termios.h>	    // seems to be required for some Linux
