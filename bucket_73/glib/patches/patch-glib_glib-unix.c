1e999d251e7b2794239fa31e7a432cc52b786bf9
John Marino <draco@marino.st>
2026-04-07 23:55:37 +0200

    glib: implement g_unix_fd_query_path for MidnightBSD
    The FreeBSD implmentation can't be used because MidnightBSD doesn't
    support F_GETPATH for fcntl.

--- bucket_73/glib/patches/patch-glib_glib-unix.c
+++ bucket_73/glib/patches/patch-glib_glib-unix.c
@@ -13,16 +13,55 @@
  #include <pwd.h>
  #include <unistd.h>
  
-@@ -967,7 +973,7 @@ g_unix_fd_query_path (int      fd,
+@@ -73,6 +79,9 @@
+ #if defined (__FreeBSD__)
+ #include <sys/user.h>
+ #endif  /* defined (__FreeBSD__ )*/
++#if defined (__MidnightBSD__)
++#include <sys/sysctl.h>
++#endif
+ 
+ G_STATIC_ASSERT (sizeof (ssize_t) == GLIB_SIZEOF_SSIZE_T);
+ G_STATIC_ASSERT (G_ALIGNOF (gssize) == G_ALIGNOF (ssize_t));
+@@ -967,7 +976,36 @@ g_unix_fd_query_path (int      fd,
    g_free (proc_path);
  
    return g_steal_pointer (&path);
 -#elif defined (__FreeBSD__) || defined(__DragonFly__)
++#elif defined (__MidnightBSD__)
++ char file_path[MAXPATHLEN] = {0};
++  size_t len = sizeof (struct kinfo_file);
++  struct kinfo_file kif;
++
++  // MIB for a specific FD: [Kernel, Process, File Descriptor, PID, FD]
++  int mib[5] = { CTL_KERN, KERN_PROC, KERN_PROC_FILEDESC, getpid(), fd };
++
++  // Query the kernel for just this specific FD
++  if (sysctl (mib, 5, &kif, &len, NULL, 0) < 0)
++    {
++      int errsv = errno;
++      g_set_error (error, G_FILE_ERROR, g_file_error_from_errno (errsv),
++                   "Error querying file information for FD %d: %s",
++                   fd, g_strerror (errsv));
++      return NULL;
++    }
++
++  // Ensure the path isn't empty (e.g., if it's a pipe or socket)
++  if (len == 0 || kif.kf_path[0] == '\0')
++    {
++      g_set_error (error, G_FILE_ERROR, g_file_error_from_errno (EBADF),
++                   "No path available for FD %d", fd);
++      return NULL;
++    }
++
++  g_strlcpy (file_path, kif.kf_path, sizeof (file_path));
++
++  return g_strdup (file_path);
 +#elif defined (__FreeBSD__)
    struct kinfo_file kf = {0};
  
    kf.kf_structsize = sizeof (kf);
-@@ -982,7 +988,7 @@ g_unix_fd_query_path (int      fd,
+@@ -982,7 +1020,7 @@ g_unix_fd_query_path (int      fd,
      }
  
    return g_strdup (kf.kf_path);
