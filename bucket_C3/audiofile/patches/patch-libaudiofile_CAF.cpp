287b1576bb8fde2a7317ac84dd89ca61a2c7e992
John Marino <draco@marino.st>
2026-04-08 01:51:15 +0000

    audiofile: fix build on omnios

---
+++ bucket_C3/audiofile/patches/patch-libaudiofile_CAF.cpp
@@ -0,0 +1,10 @@
+--- libaudiofile/CAF.cpp.orig	2013-03-06 05:30:03 UTC
++++ libaudiofile/CAF.cpp
+@@ -32,6 +32,7 @@
+ 
+ #include <stdint.h>
+ #include <string.h>
++#include <strings.h>
+ #include <string>
+ #include <vector>
+ 
