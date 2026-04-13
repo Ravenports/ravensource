3c3326d02fc28bf8a98ee16dceba96482d857817
John Marino <draco@marino.st>
2026-04-13 01:43:34 +0000

    qt5-qtbase: fix build on omnios

--- bucket_1A/qt5-qtbase/patches/patch-tests_auto_tools_moc_tst__moc.cpp
+++ bucket_1A/qt5-qtbase/patches/patch-tests_auto_tools_moc_tst__moc.cpp
@@ -1,25 +1,16 @@
-NetBSD compile problem:
-error: declaration of '__sF' as array of references
-  763 |         const QByteArray stderr = p.readAllStandardError(); \
-error: request for member 'data' in '& __sF[2]', which is of pointer type 'FILE*' (maybe you meant to use '->' ?)
-  764 |         QVERIFY2(stderr.isEmpty(), stderr.data()); \
+Another example of coders picking the stupidest possible names for variables.
+Fixes NetBSD and SunOS.
 
 --- tests/auto/tools/moc/tst_moc.cpp.orig	2025-06-05 14:07:26 UTC
 +++ tests/auto/tools/moc/tst_moc.cpp
-@@ -758,12 +758,16 @@ private:
+@@ -760,8 +760,8 @@ private:
  
- };
- 
-+#if defined(__NetBSD__)
-+#define VERIFY_NO_ERRORS(proc)
-+#else
  #define VERIFY_NO_ERRORS(proc) do { \
          auto &&p = proc; \
-         const QByteArray stderr = p.readAllStandardError(); \
-         QVERIFY2(stderr.isEmpty(), stderr.data()); \
+-        const QByteArray stderr = p.readAllStandardError(); \
+-        QVERIFY2(stderr.isEmpty(), stderr.data()); \
++        const QByteArray xstderr = p.readAllStandardError(); \
++        QVERIFY2(xstderr.isEmpty(), xstderr.data()); \
          QCOMPARE(p.exitCode(), 0); \
      } while (false)
-+#endif
  
- 
- void tst_Moc::initTestCase()
