b7f43d83789632ca1e5f9d04ff9a4d7fc65c85f7
John Marino <draco@marino.st>
2026-04-16 04:42:03 +0000

    extra-cmake-modules: stop --fatal-warnings on solaris linker

--- bucket_9E/kf6-extra-cmake-modules/patches/patch-kde-modules_KDECompilerSettings.cmake
+++ bucket_9E/kf6-extra-cmake-modules/patches/patch-kde-modules_KDECompilerSettings.cmake
@@ -12,3 +12,14 @@
  endif()
  
  if (CMAKE_SYSTEM_NAME STREQUAL GNU)
+@@ -564,8 +566,10 @@ if ((CMAKE_CXX_COMPILER_ID STREQUAL "GNU
+         (CMAKE_CXX_COMPILER_ID MATCHES "Clang" AND NOT APPLE) OR
+         (CMAKE_CXX_COMPILER_ID STREQUAL "Intel" AND NOT WIN32))
+     # Linker warnings should be treated as errors
++  if (NOT CMAKE_SYSTEM_NAME MATCHES "SunOS")
+     set(CMAKE_SHARED_LINKER_FLAGS "-Wl,--fatal-warnings ${CMAKE_SHARED_LINKER_FLAGS}")
+     set(CMAKE_MODULE_LINKER_FLAGS "-Wl,--fatal-warnings ${CMAKE_MODULE_LINKER_FLAGS}")
++  endif()
+ 
+     string(TOUPPER "CMAKE_CXX_FLAGS_${CMAKE_BUILD_TYPE}" compileflags)
+     if("${CMAKE_CXX_FLAGS} ${${compileflags}}" MATCHES "-fsanitize")
