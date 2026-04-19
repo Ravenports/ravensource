ad3023976d69c2be9dc28cdfc0f4cdb7aafe55e5
John Marino <draco@marino.st>
2026-04-19 13:46:33 +0000

    openblas: fix build on omnios

---
+++ bucket_21/openblas/patches/patch-c__check.pl
@@ -0,0 +1,11 @@
+--- c_check.pl.orig	2026-03-23 22:53:57 UTC
++++ c_check.pl
+@@ -401,7 +401,7 @@ $linker_a = "";
+ {
+     $link = `$compiler_name $flags -c ctest2.c -o ctest2.o 2>&1 && $compiler_name $flags $openmp -v ctest2.o -o ctest2 2>&1 && rm -f ctest2.o ctest2 ctest2.exe`;
+ 
+-    $link =~ s/\-Y\sP\,/\-Y/g;
++    $link =~ s/-Y\s*P,[^\s]*//g;
+ 
+     @flags = split(/[\s\,\n]/, $link);
+     # remove leading and trailing quotes from each flag.
---
+++ bucket_21/openblas/patches/patch-f__check.pl
@@ -0,0 +1,18 @@
+--- f_check.pl.orig	2026-03-23 22:53:57 UTC
++++ f_check.pl
+@@ -322,13 +322,8 @@ $linker_a = "";
+ 
+ if ($link ne "") {
+ 
+-    $link =~ s/\-Y\sP\,/\-Y/g;
+-    
+-    $link =~ s/\-R\s*/\-rpath\%/g;
+-
+-    $link =~ s/\-rpath\s+/\-rpath\%/g;
+-
+-    $link =~ s/\-rpath-link\s+/\-rpath-link\%/g;
++    # Delete -Y, any whitespace, and the following path string until the next whitespace
++    $link =~ s/-Y\s*P,[^\s]*//g;
+ 
+     @flags = split(/[\s\,\n]/, $link);
+     # remove leading and trailing quotes from each flag.
