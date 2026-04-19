cb2c0bb8d86dc945c22314aea169b0276b0f408a
John Marino <draco@marino.st>
2026-04-19 19:01:47 +0000

    mkvtoolnix: fix build on omnios

---
+++ bucket_26/mkvtoolnix/patches/patch-src_input_r__qtmp4.cpp
@@ -0,0 +1,11 @@
+--- src/input/r_qtmp4.cpp.orig	2026-01-02 12:35:08 UTC
++++ src/input/r_qtmp4.cpp
+@@ -1615,7 +1615,7 @@ qtmp4_reader_c::handle_display_matrix(qt
+     dmx.yaw = 180;
+   else
+     return;
+-  dmx.roll = 180 / M_PI * atan2(matrix[1][0], matrix[1][1]);
++  dmx.roll = 180 / M_PI * atan2(static_cast<double>(matrix[1][0]), static_cast<double>(matrix[1][1]));
+   ignored = false;
+ 
+   if (!dmx.yaw && !dmx.roll)
