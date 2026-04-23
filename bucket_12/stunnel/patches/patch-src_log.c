488c79a5b3f4f256c6611396119eda8ca6e5e269
John Marino <draco@marino.st>
2026-04-23 22:21:35 +0000

    stunnel: upgrade

--- bucket_12/stunnel/patches/patch-src_log.c
+++ bucket_12/stunnel/patches/patch-src_log.c
@@ -1,15 +1,15 @@
---- src/log.c.orig	2025-11-14 12:52:51 UTC
+--- src/log.c.orig	2026-03-19 19:18:37 UTC
 +++ src/log.c
-@@ -198,7 +198,7 @@ void s_vlog(int level, const char *forma
-         len=vsnprintf(NULL, 0, format, ap);
+@@ -194,7 +194,7 @@ void s_vlog(int level, const char *forma
+         va_end(aq);
          if(len>1024)
              len=1024;
 -        text=alloca((size_t)len+1);
 +        text=__builtin_alloca((size_t)len+1);
+         va_copy(aq, ap);
          len=vsnprintf(text, (size_t)len+1, format, aq);
          va_end(aq);
-         while(len>0 && text[len-1]=='\n')
-@@ -284,7 +284,7 @@ NOEXPORT void log_raw(SERVICE_OPTIONS *o
+@@ -282,7 +282,7 @@ NOEXPORT void log_raw(SERVICE_OPTIONS *o
      switch(log_mode) {
      case LOG_MODE_CONFIGURED:
          size=strlen(stamp)+strlen(id)+strlen(text)+4;
@@ -19,11 +19,11 @@
          if(level<=opt->log_level) {
  #if !defined(USE_WIN32) && !defined(__vms)
 @@ -302,7 +302,7 @@ NOEXPORT void log_raw(SERVICE_OPTIONS *o
-     case LOG_MODE_ERROR:
+             return;
          /* don't log the id or the time stamp */
          size=strlen(text)+5;
 -        line=alloca(size);
 +        line=__builtin_alloca(size);
-         if(level>=0 && level<=7) /* just in case */
-             snprintf(line, size, "[%c] %s", "***!:.  "[level], text);
-         else
+         if(level>=LOG_EMERG && level<=LOG_NOTICE)
+             snprintf(line, size, "[%c] %s", "***!:."[level], text);
+         else /* invalid level */
