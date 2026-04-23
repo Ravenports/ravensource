4106dd0244ac781fbabb0f0f6b9708d64077c471
John Marino <draco@marino.st>
2026-04-23 20:00:04 +0000

    update filezilla, libfilezilla, and tweak fzssh

--- bucket_96/libfilezilla/patches/patch-lib_local__filesys.cpp
+++ bucket_96/libfilezilla/patches/patch-lib_local__filesys.cpp
@@ -1,4 +1,4 @@
---- lib/local_filesys.cpp.orig	2025-08-13 17:10:42 UTC
+--- lib/local_filesys.cpp.orig	2025-12-10 15:59:22 UTC
 +++ lib/local_filesys.cpp
 @@ -15,6 +15,7 @@
  #include <sys/types.h>
--- bucket_D7/filezilla/patches/patch-src_interface_Mainfrm.h
+++ bucket_D7/filezilla/patches/patch-src_interface_Mainfrm.h
@@ -1,4 +1,4 @@
---- src/interface/Mainfrm.h.orig	2025-04-02 14:21:43 UTC
+--- src/interface/Mainfrm.h.orig	2026-04-01 03:30:02 UTC
 +++ src/interface/Mainfrm.h
 @@ -1,6 +1,7 @@
  #ifndef FILEZILLA_INTERFACE_MAINFRM_HEADER
