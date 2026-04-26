eb1c6f35fcda905d799098b0d5bb4746012fc8e1
John Marino <draco@marino.st>
2026-04-26 20:56:05 +0000

    R-pak: fix build on omnios

---
+++ bucket_44/R-pak/patches/patch-src_library_cli_src_sha1.c
@@ -0,0 +1,10 @@
+--- src/library/cli/src/sha1.c.orig	2026-02-04 13:26:07 UTC
++++ src/library/cli/src/sha1.c
+@@ -194,6 +194,7 @@ static void bin2str(char *to, const unsi
+ #include "errors.h"
+ 
+ #include <stdint.h>
++#include <string.h>
+ 
+ #include <Rinternals.h>
+ 
