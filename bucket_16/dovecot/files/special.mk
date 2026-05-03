c116ed9bf92931c372c7438d99471c30fdf39aeb
John Marino <draco@marino.st>
2026-05-03 21:22:43 +0000

    dovecot: add SMF support
    - Drop release 2.2 => 2.3 notes
    - add new vmail user and group
    - Handle missing hibernate on non FreeBSD15 and linux platforms
    - Fix provide default dovecot.conf file
    - Add /raven/var/mail directory (owned by vmail)

--- bucket_16/dovecot/files/special.mk
+++ bucket_16/dovecot/files/special.mk
@@ -1,7 +1,12 @@
+HIBERNATE=
+
 .if "${OPSYS}" == "FreeBSD"
 . if "${MAJOR}" == "15"
 PLIST_SUB+=	FREEBSD15=
+HIBERNATE=	\#
 . else
 PLIST_SUB+= 	FREEBSD15="@comment "
 . endif
+.elif "${OPSYS}" == "Linux"
+HIBERNATE=	\#
 .endif
