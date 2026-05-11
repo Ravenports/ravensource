323c2580f7d507aa02eb317e19610619abc238ce
John Marino <draco@marino.st>
2026-05-11 20:14:43 +0000

    libkiwix: upgrade

--- bucket_95/libkiwix/patches/patch-src_tools_networkTools.cpp
+++ bucket_95/libkiwix/patches/patch-src_tools_networkTools.cpp
@@ -1,4 +1,4 @@
---- src/tools/networkTools.cpp.orig	2025-11-30 15:37:21 UTC
+--- src/tools/networkTools.cpp.orig	2026-05-09 15:31:46 UTC
 +++ src/tools/networkTools.cpp
 @@ -45,6 +45,7 @@
  #include <netinet/in.h>
