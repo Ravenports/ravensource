33637e675be1dcca7f8efd24d8b2c5a45a1b35b5
John Marino <draco@marino.st>
2026-05-12 13:55:12 +0000

    roundcube: upgrade

--- bucket_0F/roundcube/patches/patch-program_lib_Roundcube_rcube__message.php
+++ bucket_0F/roundcube/patches/patch-program_lib_Roundcube_rcube__message.php
@@ -1,11 +1,11 @@
---- program/lib/Roundcube/rcube_message.php.orig	2026-03-29 09:48:47 UTC
+--- program/lib/Roundcube/rcube_message.php.orig	2026-05-10 06:37:11 UTC
 +++ program/lib/Roundcube/rcube_message.php
-@@ -915,8 +915,7 @@ class rcube_message
-                 else if (
+@@ -1006,8 +1006,7 @@ class rcube_message
+                 elseif (
                      preg_match('/^(inline|attach)/', $mail_part->disposition)
                      || !empty($mail_part->headers['content-id'])
--                    || ($mail_part->filename &&
--                        (empty($mail_part->disposition) || preg_match('/^[a-z0-9!#$&.+^_-]+$/i', $mail_part->disposition)))
+-                    || ($mail_part->filename
+-                        && (empty($mail_part->disposition) || preg_match('/^[a-z0-9!#$&.+^_-]+$/i', $mail_part->disposition)))
 +                    || $mail_part->filename
                  ) {
                      // skip apple resource forks
