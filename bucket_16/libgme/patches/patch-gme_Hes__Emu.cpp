3663c99560855546db5402f7ddf417a86925c125
John Marino <draco@marino.st>
2026-05-16 23:09:33 -0500

    libgme: fix build on freebsd

---
+++ bucket_16/libgme/patches/patch-gme_Hes__Emu.cpp
@@ -0,0 +1,10 @@
+--- gme/Hes_Emu.cpp.orig	2026-04-20 19:20:21.000000000 -0000
++++ gme/Hes_Emu.cpp
+@@ -5,6 +5,7 @@
+ #include "blargg_endian.h"
+ #include <string.h>
+ #include <algorithm>
++#include <ctime>
+ 
+ /* Copyright (C) 2006 Shay Green. This module is free software; you
+ can redistribute it and/or modify it under the terms of the GNU Lesser
