902e8cdb04ed1106021f7c28804a3883f5c37488
Michael Reim <kraileth@elderlinux.org>
2026-05-24 19:04:20 +0200

    ruby33: upgrade

--- bucket_9E/ruby33/patches/patch-tool_mkconfig.rb
+++ bucket_9E/ruby33/patches/patch-tool_mkconfig.rb
@@ -1,4 +1,4 @@
---- tool/mkconfig.rb.orig	2025-10-23 10:00:15 UTC
+--- tool/mkconfig.rb.orig	2025-10-23 10:00:15.000000000 -0000
 +++ tool/mkconfig.rb
 @@ -169,8 +169,9 @@ def vars.expand(val, config = self)
    val.replace(newval) unless newval == val
