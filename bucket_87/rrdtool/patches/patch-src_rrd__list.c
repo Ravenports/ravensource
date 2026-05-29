af41c59290f39a87885a1221009aa5e4c693e59b
John Marino <draco@marino.st>
2026-05-29 22:21:18 +0000

    rrdtool: upgrade

--- bucket_87/rrdtool/patches/patch-src_rrd__list.c
+++ bucket_87/rrdtool/patches/patch-src_rrd__list.c
@@ -2,7 +2,7 @@ $NetBSD: patch-src_rrd__list.c,v 1.2 2019/02/11 08:22:45 adam Exp $
 
 Need limits.h for PATH_MAX
 
---- src/rrd_list.c.orig	2024-07-29 07:08:58.000000000 -0000
+--- src/rrd_list.c.orig	2026-05-25 09:14:16 UTC
 +++ src/rrd_list.c
 @@ -13,6 +13,7 @@
  #endif
