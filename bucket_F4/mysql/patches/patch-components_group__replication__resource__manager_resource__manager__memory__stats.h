15a8123b43867fc0c3e125b7b0e5701ad4dcd8de
Michael Reim <kraileth@elderlinux.org>
2026-06-19 18:59:13 +0200

    mysql: upgrade

--- bucket_F4/mysql/patches/patch-components_group__replication__resource__manager_resource__manager__memory__stats.h
+++ bucket_F4/mysql/patches/patch-components_group__replication__resource__manager_resource__manager__memory__stats.h
@@ -1,4 +1,4 @@
---- components/group_replication_resource_manager/resource_manager_memory_stats.h.orig	2026-04-07 16:08:28.000000000 -0000
+--- components/group_replication_resource_manager/resource_manager_memory_stats.h.orig	2026-04-07 16:08:28 UTC
 +++ components/group_replication_resource_manager/resource_manager_memory_stats.h
 @@ -5,6 +5,7 @@
  #ifndef GR_RESOURCE_MANAGER_MEMORY_STATS
