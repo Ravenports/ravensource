2ce6db3a283ace12fc7df6270c37612a33ce6980
Michael Reim <kraileth@elderlinux.org>
2026-06-28 13:43:31 +0200

    tor: upgrade

--- bucket_A0/tor/patches/patch-doc_asciidoc-helper.sh
+++ bucket_A0/tor/patches/patch-doc_asciidoc-helper.sh
@@ -1,4 +1,4 @@
---- doc/asciidoc-helper.sh.orig	2026-05-07 14:41:42 UTC
+--- doc/asciidoc-helper.sh.orig	2026-06-01 15:08:43 UTC
 +++ doc/asciidoc-helper.sh
 @@ -12,11 +12,6 @@ if [ $# != 3 ]; then
      exit 1
