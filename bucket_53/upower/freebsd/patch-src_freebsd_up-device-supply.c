89abac137f7a5bc8e632126c59377f3cb79ec059
Michael Reim <kraileth@elderlinux.org>
2026-07-06 18:02:25 +0200

    upower: upgrade

--- bucket_53/upower/freebsd/patch-src_freebsd_up-device-supply.c
+++ bucket_53/upower/freebsd/patch-src_freebsd_up-device-supply.c
@@ -1,4 +1,4 @@
---- src/freebsd/up-device-supply.c.orig	2026-02-10 12:01:59.000000000 -0000
+--- src/freebsd/up-device-supply.c.orig	2026-04-01 08:20:02 UTC
 +++ src/freebsd/up-device-supply.c
 @@ -250,7 +250,7 @@ up_device_supply_battery_set_properties
  		      "energy", cap,
