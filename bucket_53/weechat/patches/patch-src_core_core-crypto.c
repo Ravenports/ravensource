4970c95afabf83ba2ffe48f9e2193702b010f549
Michael Reim <kraileth@elderlinux.org>
2026-07-06 17:49:49 +0200

    weechat: upgrade

--- bucket_53/weechat/patches/patch-src_core_core-crypto.c
+++ bucket_53/weechat/patches/patch-src_core_core-crypto.c
@@ -1,6 +1,6 @@
---- src/core/core-crypto.c.orig	2026-03-29 08:20:23 UTC
+--- src/core/core-crypto.c.orig	2026-06-07 07:25:09 UTC
 +++ src/core/core-crypto.c
-@@ -50,6 +50,10 @@
+@@ -53,6 +53,10 @@
  #define BE_INT64 htobe64
  #endif
  
