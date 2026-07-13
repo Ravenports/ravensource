3864b6053234b45d2a6b27079fc25c17aedec5a7
Michael Reim <kraileth@elderlinux.org>
2026-07-13 22:01:32 +0200

    wxWidgets: upgrade

--- bucket_3D/wxWidgets/patches/patch-src_stc_scintilla_src_Editor.cxx
+++ bucket_3D/wxWidgets/patches/patch-src_stc_scintilla_src_Editor.cxx
@@ -1,4 +1,4 @@
---- src/stc/scintilla/src/Editor.cxx.orig	2025-12-07 22:59:42 UTC
+--- src/stc/scintilla/src/Editor.cxx.orig	2026-02-28 12:50:12 UTC
 +++ src/stc/scintilla/src/Editor.cxx
 @@ -18,6 +18,7 @@
  #include <map>
