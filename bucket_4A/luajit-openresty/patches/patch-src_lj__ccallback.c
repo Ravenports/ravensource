8e4030d1663c708d6d5b7ad75c5afb14b8e37f96
Michael Reim <kraileth@elderlinux.org>
2026-07-19 10:01:59 +0200

    luajit-openresty: upgrade

--- bucket_4A/luajit-openresty/patches/patch-src_lj__ccallback.c
+++ bucket_4A/luajit-openresty/patches/patch-src_lj__ccallback.c
@@ -1,4 +1,4 @@
---- src/lj_ccallback.c.orig	2026-06-20 14:36:18 UTC
+--- src/lj_ccallback.c.orig	2026-06-30 15:09:49 UTC
 +++ src/lj_ccallback.c
 @@ -20,6 +20,7 @@
  #include "lj_mcode.h"
--- bucket_4A/luajit-openresty/patches/patch-src_lj__prng.c
+++ bucket_4A/luajit-openresty/patches/patch-src_lj__prng.c
@@ -1,4 +1,4 @@
---- src/lj_prng.c.orig	2026-06-20 14:36:18 UTC
+--- src/lj_prng.c.orig	2026-06-30 15:09:49 UTC
 +++ src/lj_prng.c
 @@ -113,6 +113,7 @@ static PRGR libfunc_rgr;
  #include <sys/syscall.h>
