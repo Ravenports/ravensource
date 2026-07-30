48ccc53eb5382413519c6747a24ade475cfb301e
Michael Reim <kraileth@elderlinux.org>
2026-07-30 18:04:18 +0200

    xfce4-power-manager: upgrade

--- bucket_5E/xfce4-power-manager/patches/patch-src_xfpm-pm-helper.c
+++ bucket_5E/xfce4-power-manager/patches/patch-src_xfpm-pm-helper.c
@@ -1,8 +1,8 @@
---- src/xfpm-pm-helper.c.orig	2024-06-09 15:35:09 UTC
+--- src/xfpm-pm-helper.c.orig	2024-11-21 06:26:21 UTC
 +++ src/xfpm-pm-helper.c
-@@ -77,6 +77,16 @@
- #define UP_BACKEND_SUSPEND_COMMAND  "/usr/sbin/zzz"
+@@ -82,6 +82,16 @@
  #define UP_BACKEND_HIBERNATE_COMMAND "/usr/sbin/ZZZ"
+ #define UP_BACKEND_HYBRID_SLEEP_COMMAND "/usr/bin/false"
  #endif
 +#ifdef __NetBSD__
 +#define UP_BACKEND_SUSPEND_COMMAND      "/sbin/sysctl -w hw.acpi.sleep.state=1"
