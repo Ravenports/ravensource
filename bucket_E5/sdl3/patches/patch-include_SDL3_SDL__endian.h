157906e2af1369d9fc90491fe2a9efd69b4aa818
Michael Reim <kraileth@elderlinux.org>
2026-08-04 06:26:02 +0200

    sdl3: upgrade

--- bucket_E5/sdl3/patches/patch-include_SDL3_SDL__endian.h
+++ bucket_E5/sdl3/patches/patch-include_SDL3_SDL__endian.h
@@ -1,4 +1,4 @@
---- include/SDL3/SDL_endian.h.orig	2026-05-31 17:38:41 UTC
+--- include/SDL3/SDL_endian.h.orig	2026-07-01 16:20:42 UTC
 +++ include/SDL3/SDL_endian.h
 @@ -140,10 +140,10 @@ _m_prefetch(void *__P)
  #else
