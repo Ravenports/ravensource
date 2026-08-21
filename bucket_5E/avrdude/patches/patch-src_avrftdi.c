cf399be9ef78497f361fc1b84f4150cc53eb738c
John Marino <draco@marino.st>
2026-08-21 00:03:05 +0000

    avrdude: upgrade

--- bucket_5E/avrdude/patches/patch-src_avrftdi.c
+++ bucket_5E/avrdude/patches/patch-src_avrftdi.c
@@ -1,4 +1,4 @@
---- src/avrftdi.c.orig	2025-06-23 09:03:44 UTC
+--- src/avrftdi.c.orig	2026-07-11 11:11:28 UTC
 +++ src/avrftdi.c
 @@ -30,6 +30,9 @@
  #include <unistd.h>
--- bucket_5E/avrdude/patches/patch-src_stk500.c
+++ bucket_5E/avrdude/patches/patch-src_stk500.c
@@ -1,6 +1,6 @@
---- src/stk500.c.orig	2025-06-23 09:03:44 UTC
+--- src/stk500.c.orig	2026-07-11 11:11:28 UTC
 +++ src/stk500.c
-@@ -992,7 +992,7 @@ static int set_memchr_a_div(const PROGRA
+@@ -1001,7 +1001,7 @@ static int set_memchr_a_div(const PROGRA
  
  static int stk500_paged_write(const PROGRAMMER *pgm, const AVRPART *p, const AVRMEM *m,
    unsigned int page_size, unsigned int addr, unsigned int n_bytes) {
