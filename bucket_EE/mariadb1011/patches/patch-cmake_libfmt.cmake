3e9b2f6f1e64e8103550be3056ba8b04caec8676
Michael Reim <kraileth@elderlinux.org>
2026-08-23 18:04:35 +0200

    mariadb1011: upgrade

--- bucket_EE/mariadb1011/patches/patch-cmake_libfmt.cmake
+++ bucket_EE/mariadb1011/patches/patch-cmake_libfmt.cmake
@@ -1,11 +1,15 @@
---- cmake/libfmt.cmake.orig	2026-05-23 15:01:04 UTC
-+++ cmake/libfmt.cmake
-@@ -15,7 +15,7 @@ MACRO(BUNDLE_LIBFMT)
-   ExternalProject_Add(
-     libfmt
-     PREFIX   "${dir}"
--    URL      "https://github.com/fmtlib/fmt/releases/download/11.0.2/fmt-11.0.2.zip"
-+    URL      "file:///distfiles/fmt-11.0.2.zip"
-     URL_MD5 c622dca45ec3fc95254c48370a9f7a1d
-     INSTALL_COMMAND ""
-     CONFIGURE_COMMAND ""
+--- cmake/libfmt.cmake.orig	2026-08-23 17:40:48.155373000 +0200
++++ cmake/libfmt.cmake	2026-08-23 17:43:02.210545000 +0200
+@@ -15,10 +15,10 @@
+   IF(CMAKE_CXX_COMPILER_ID STREQUAL "GNU" AND CMAKE_CXX_COMPILER_VERSION VERSION_LESS "4.9")
+     # it's for RHEL-7, gcc 4.8.5
+     # with newer fmt it complains about missing space in operator""_a()
+-    SET(source URL "https://github.com/fmtlib/fmt/releases/download/11.0.2/fmt-11.0.2.zip"
++    SET(source URL "file:///distfiles/fmt-11.0.2.zip"
+                URL_MD5 c622dca45ec3fc95254c48370a9f7a1d)
+   ELSE()
+-    SET(source URL "https://github.com/fmtlib/fmt/releases/download/12.2.0/fmt-12.2.0.zip"
++    SET(source URL "file:///distfiles/fmt-12.2.0.zip"
+                URL_MD5 6083253772ad29e73a4d5920be08918e)
+   ENDIF()
+ 
