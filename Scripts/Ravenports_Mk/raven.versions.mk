a95fbc6e45daf1ac85a76c2b111a88ef7d253673
Michael Reim <kraileth@elderlinux.org>
2026-08-23 19:50:06 +0200

    mariadb: upgrade

--- Scripts/Ravenports_Mk/raven.versions.mk
+++ Scripts/Ravenports_Mk/raven.versions.mk
@@ -119,7 +119,7 @@ MYSQL_mariadb-10.6_VERSION=	10.6.28
 MYSQL_mariadb-10.11_VERSION=	10.11.19
 MYSQL_mariadb-11.4_VERSION=	11.4.13
 MYSQL_mariadb-11.8_VERSION=	11.8.9
-MYSQL_mariadb-BE_VERSION=	12.3.2		# Bleeding Edge
+MYSQL_mariadb-BE_VERSION=	12.3.3		# Bleeding Edge
 
 #-------------------------------------------------------------------------
 # PHP (restricted to branches still receiving updates)
--- bucket_37/mariadb/patches/patch-cmake_libfmt.cmake
+++ bucket_37/mariadb/patches/patch-cmake_libfmt.cmake
@@ -1,11 +1,19 @@
---- cmake/libfmt.cmake.orig	2026-05-26 19:21:05 UTC
-+++ cmake/libfmt.cmake
-@@ -15,7 +15,7 @@ MACRO(BUNDLE_LIBFMT)
+--- cmake/libfmt.cmake.orig	2026-08-23 19:26:54.297538000 +0200
++++ cmake/libfmt.cmake	2026-08-23 19:29:48.762619000 +0200
+@@ -12,15 +12,8 @@
+     SET(fmt_byproducts BUILD_BYPRODUCTS ${LIBFMT_INCLUDE_DIR}/fmt/format-inl.h)
+   ENDIF()
+ 
+-  IF(CMAKE_CXX_COMPILER_ID STREQUAL "GNU" AND CMAKE_CXX_COMPILER_VERSION VERSION_LESS "4.9")
+-    # it's for RHEL-7, gcc 4.8.5
+-    # with newer fmt it complains about missing space in operator""_a()
+-    SET(source URL "https://github.com/fmtlib/fmt/releases/download/11.0.2/fmt-11.0.2.zip"
+-               URL_MD5 c622dca45ec3fc95254c48370a9f7a1d)
+-  ELSE()
+-    SET(source URL "https://github.com/fmtlib/fmt/releases/download/12.2.0/fmt-12.2.0.zip"
++  SET(source URL "file:///distfiles/fmt-12.2.0.zip"
+                URL_MD5 6083253772ad29e73a4d5920be08918e)
+-  ENDIF()
+ 
    ExternalProject_Add(
      libfmt
-     PREFIX   "${dir}"
--    URL "https://github.com/fmtlib/fmt/releases/download/12.1.0/fmt-12.1.0.zip"
-+    URL "file:///distfiles/fmt-12.1.0.zip"
-     URL_MD5 028c6979cad96a4260154f091885171a
-     INSTALL_COMMAND ""
-     CONFIGURE_COMMAND ""
