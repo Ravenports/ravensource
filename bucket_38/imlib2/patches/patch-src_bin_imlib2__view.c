1374eb11de4999c370e0fdf0a1c4d581d4ad74ea
John Marino <draco@marino.st>
2026-08-30 04:15:27 +0000

    imlib2: upgrade

--- bucket_38/imlib2/patches/patch-src_bin_imlib2__view.c
+++ bucket_38/imlib2/patches/patch-src_bin_imlib2__view.c
@@ -1,6 +1,6 @@
 Include sys/time.h for timeval.
 
---- src/bin/imlib2_view.c.orig	2025-04-06 13:23:11 UTC
+--- src/bin/imlib2_view.c.orig	2026-08-28 05:14:53 UTC
 +++ src/bin/imlib2_view.c
 @@ -3,6 +3,7 @@
  
