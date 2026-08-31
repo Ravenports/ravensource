d436d9f8304dba6d275d6de203ee237e69992293
John Marino <draco@marino.st>
2026-08-31 00:25:30 +0000

    exiv2: upgrade

--- bucket_36/exiv2/patches/patch-src_futils.cpp
+++ bucket_36/exiv2/patches/patch-src_futils.cpp
@@ -1,13 +1,14 @@
---- src/futils.cpp.orig	2026-03-15 21:35:48.575499000 +0100
-+++ src/futils.cpp	2026-03-15 21:36:46.608386000 +0100
-@@ -35,8 +35,10 @@
+--- src/futils.cpp.orig	2026-08-30 19:33:50 UTC
++++ src/futils.cpp
+@@ -35,9 +35,11 @@ namespace fs = std::experimental::filesy
  // clang-format on
  #endif
  
 +#ifndef __NetBSD__
  #if __has_include(<libproc.h>)
  #include <libproc.h>
-+#endif
  #endif
++#endif
  
  #if __has_include(<unistd.h>)
+ #include <unistd.h>  // for stat()
