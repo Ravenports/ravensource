a1b81198f5fc420cfd70683d17fc5391ae2b77c8
John Marino <draco@marino.st>
2026-09-03 04:02:58 +0000

    geos: upgrade

--- bucket_5D/geos/patches/patch-util_geosop_cxxopts.hpp
+++ bucket_5D/geos/patches/patch-util_geosop_cxxopts.hpp
@@ -1,4 +1,4 @@
---- util/geosop/cxxopts.hpp.orig	2025-10-27 15:13:40 UTC
+--- util/geosop/cxxopts.hpp.orig	2026-09-01 13:37:08 UTC
 +++ util/geosop/cxxopts.hpp
 @@ -677,7 +677,7 @@ namespace cxxopts
        integer_parser(text, value);
