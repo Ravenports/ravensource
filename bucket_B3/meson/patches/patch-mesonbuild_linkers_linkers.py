3d7d9721ebc2525f997b82a11e97d31b36af8391
John Marino <draco@marino.st>
2026-09-05 18:10:08 +0000

    meson: fix linkers code on omnios

--- bucket_B3/meson/patches/patch-mesonbuild_linkers_linkers.py
+++ bucket_B3/meson/patches/patch-mesonbuild_linkers_linkers.py
@@ -1,4 +1,4 @@
---- mesonbuild/linkers/linkers.py.orig	2026-08-21 01:29:42 UTC
+--- mesonbuild/linkers/linkers.py.orig	2026-09-05 18:01:49 UTC
 +++ mesonbuild/linkers/linkers.py
 @@ -731,7 +731,7 @@ class GnuLikeDynamicLinkerMixin(DynamicL
          return ['-pthread']
@@ -9,17 +9,18 @@
  
      def fatal_warnings(self) -> T.List[str]:
          return self._apply_prefix('--fatal-warnings')
-@@ -1604,7 +1604,12 @@ class SolarisDynamicLinker(PosixDynamicL
+@@ -1604,7 +1604,13 @@ class SolarisDynamicLinker(PosixDynamicL
      def get_link_whole_for(self, args: T.List[str]) -> T.List[str]:
          if not args:
              return args
 -        return self._apply_prefix('--whole-archive') + args + self._apply_prefix('--no-whole-archive')
-+        result = [self.prefix_arg + '-z', self.prefix_arg + 'allextract']
-+        for a in args:
-+            result.append(a)
-+        result.append(self.prefix_arg + '-z')
-+        result.append(self.prefix_arg + 'defaultextract')
-+        return result
++        return (
++            self._apply_prefix('-z') +
++            self._apply_prefix('allextract') +
++            args +
++            self._apply_prefix('-z') +
++            self._apply_prefix('defaultextract')
++        )
  
      def get_pie_args(self) -> T.List[str]:
          # Available in Solaris 11.2 and later
