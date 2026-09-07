7009b1a6edc651c92335b9bcd650a2b5add4ab6b
John Marino <draco@marino.st>
2026-09-07 04:29:10 +0000

    openldap: fix build with gcc15

--- bucket_7F/openldap/patches/patch-libraries_libldap_tls__o.c
+++ bucket_7F/openldap/patches/patch-libraries_libldap_tls__o.c
@@ -1,4 +1,5 @@
 Fix build with LibreSSL.
+Support pre-C23 compilers.
 
 --- libraries/libldap/tls_o.c.orig	2026-08-06 17:17:48 UTC
 +++ libraries/libldap/tls_o.c
@@ -38,7 +39,51 @@ Fix build with LibreSSL.
  #define	SSL_CTX_up_ref(ctx)	CRYPTO_add( &(ctx->references), 1, CRYPTO_LOCK_SSL_CTX )
  #endif
  	SSL_CTX_up_ref( c );
-@@ -931,7 +931,7 @@ tlso_session_my_dn( tls_session *sess, s
+@@ -543,7 +543,7 @@ tlso_ctx_init( struct ldapoptions *lo, s
+ 
+ 				while ((info = OSSL_STORE_load( sctx ))) {
+ 					switch (OSSL_STORE_INFO_get_type( info )) {
+-					case OSSL_STORE_INFO_CERT:
++					case OSSL_STORE_INFO_CERT: {
+ 						X509 *cert = OSSL_STORE_INFO_get0_CERT( info );
+ 						X509_STORE *store = SSL_CTX_get_cert_store( ctx );
+ 						if ( !X509_STORE_add_cert( store, cert ) ) {
+@@ -565,6 +565,7 @@ tlso_ctx_init( struct ldapoptions *lo, s
+ 							}
+ 						}
+ 						break;
++					}
+ 					default:
+ 						/* ignore other types */
+ 						break;
+@@ -753,7 +754,7 @@ tlso_ctx_init( struct ldapoptions *lo, s
+ 						return -1;
+ 					}
+ 					break;
+-				case OSSL_STORE_INFO_CERT:
++				case OSSL_STORE_INFO_CERT: {
+ 					X509 *cert = OSSL_STORE_INFO_get0_CERT(info);
+ 					int is_ca = X509_check_ca( cert );
+ 					if ( !is_ca && !SSL_CTX_use_certificate( ctx, cert )) {
+@@ -773,7 +774,8 @@ tlso_ctx_init( struct ldapoptions *lo, s
+ 						return -1;
+ 					}
+ 					break;
+-				case OSSL_STORE_INFO_CRL:
++				}
++				case OSSL_STORE_INFO_CRL: {
+ 					X509_STORE *x509_s = SSL_CTX_get_cert_store( ctx );
+ 					if ( !X509_STORE_add_crl( x509_s,
+ 							OSSL_STORE_INFO_get0_CRL(info) )) {
+@@ -785,6 +787,7 @@ tlso_ctx_init( struct ldapoptions *lo, s
+ 						return -1;
+ 					}
+ 					break;
++				}
+ 				default:
+ 					/* ignore other types */
+ 					break;
+@@ -931,7 +934,7 @@ tlso_session_my_dn( tls_session *sess, s
  	if (!x) return LDAP_INVALID_CREDENTIALS;
  	
  	xn = X509_get_subject_name(x);
@@ -47,7 +92,7 @@ Fix build with LibreSSL.
  	der_dn->bv_len = i2d_X509_NAME( xn, NULL );
  	der_dn->bv_val = xn->bytes->data;
  #else
-@@ -967,7 +967,7 @@ tlso_session_peer_dn( tls_session *sess,
+@@ -967,7 +970,7 @@ tlso_session_peer_dn( tls_session *sess,
  		return LDAP_INVALID_CREDENTIALS;
  
  	xn = X509_get_subject_name(x);
@@ -56,7 +101,7 @@ Fix build with LibreSSL.
  	der_dn->bv_len = i2d_X509_NAME( xn, NULL );
  	der_dn->bv_val = xn->bytes->data;
  #else
-@@ -1418,7 +1418,7 @@ struct tls_data {
+@@ -1418,7 +1421,7 @@ struct tls_data {
  	Sockbuf_IO_Desc		*sbiod;
  };
  
