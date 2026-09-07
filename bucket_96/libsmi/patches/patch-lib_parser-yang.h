1a42b1f0ea6e764d6a9483bee44baa017e8efd13
John Marino <draco@marino.st>
2026-09-07 03:56:09 +0000

    libsmi: fix build with gcc15 (from pkgsrc)

---
+++ bucket_96/libsmi/patches/patch-lib_parser-yang.h
@@ -0,0 +1,16 @@
+$NetBSD: patch-lib_parser-yang.h,v 1.2 2025/04/29 07:46:55 wiz Exp $
+
+Replace K&R prototype.
+
+--- lib/parser-yang.h.orig	2013-06-04 12:34:00 UTC
++++ lib/parser-yang.h
+@@ -13,7 +13,8 @@
+ 
+ 
+ 
+-extern int yangparse();
++struct Parser;
++extern int yangparse(struct Parser *);
+ 
+ 
+ 
