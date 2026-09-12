a4d4395012d5229c5aff5667c4ba60d2d15bcb6f
John Marino <draco@marino.st>
2026-09-11 21:28:37 -0500

    libcupfilters: forgot to commit patch for freebsd

---
+++ bucket_D7/libcupsfilters/patches/patch-cupsfilters_ipp-options.c
@@ -0,0 +1,10 @@
+--- cupsfilters/ipp-options.c.orig	2026-08-11 10:31:11 UTC
++++ cupsfilters/ipp-options.c
+@@ -14,6 +14,7 @@
+ #include <stdlib.h>
+ #include <string.h>
+ #include <ctype.h>
++#include <stdint.h>
+ #include <limits.h>
+ 
+ #  ifdef DEBUG
