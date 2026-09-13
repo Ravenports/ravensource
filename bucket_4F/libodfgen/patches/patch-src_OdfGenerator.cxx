375d751f2fb5e4a0ee56d32df20b30efd3e2f2ba
John Marino <john.marino@toshibagcs.com>
2026-09-13 16:08:15 +0000

    libodfgen: include missing header patch (fixes linux)

---
+++ bucket_4F/libodfgen/patches/patch-src_OdfGenerator.cxx
@@ -0,0 +1,10 @@
+--- src/OdfGenerator.cxx.orig	2021-01-06 10:16:44 UTC
++++ src/OdfGenerator.cxx
+@@ -32,6 +32,7 @@
+ 
+ #include <math.h>
+ 
++#include <cstdint>
+ #include <cctype>
+ #include <limits>
+ #include <memory>
