2d0a7a0da7ab9a91c77231ddd69d1ac85cd2c2f8
John Marino <john.marino@toshibagcs.com>
2026-09-13 16:19:43 +0000

    textlive-bin: bundled woff2 needs same header include was the system woff2

---
+++ bucket_B8/texlive-bin/patches/patch-texk_dvisvgm_dvisvgm-src_libs_woff2_include_woff2_output.h
@@ -0,0 +1,10 @@
+--- texk/dvisvgm/dvisvgm-src/libs/woff2/include/woff2/output.h.orig	2022-08-29 13:52:14 UTC
++++ texk/dvisvgm/dvisvgm-src/libs/woff2/include/woff2/output.h
+@@ -11,6 +11,7 @@
+ 
+ #include <algorithm>
+ #include <cstring>
++#include <cstdint>
+ #include <memory>
+ #include <string>
+ 
