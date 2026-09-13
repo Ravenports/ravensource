c447dd9c73f051116cac3f9df9ef61240ecda10d
John Marino <john.marino@toshibagcs.com>
2026-09-13 16:23:17 +0000

    webrtc-audio-processing: add header include (fix for linux)

---
+++ bucket_96/webrtc-audio-processing/patches/patch-webrtc_modules_audio__processing_aec3_multi__channel__content__detector.h
@@ -0,0 +1,10 @@
+--- webrtc/modules/audio_processing/aec3/multi_channel_content_detector.h.orig	2025-01-22 22:32:18 UTC
++++ webrtc/modules/audio_processing/aec3/multi_channel_content_detector.h
+@@ -13,6 +13,7 @@
+ 
+ #include <stddef.h>
+ 
++#include <cstdint>
+ #include <memory>
+ #include <optional>
+ #include <vector>
