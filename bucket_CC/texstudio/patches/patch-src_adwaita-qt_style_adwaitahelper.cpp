a384a0fba088096c51a0f642c2a7456948499e09
Michael Reim <kraileth@elderlinux.org>
2026-09-19 09:49:18 +0200

    texstudio: upgrade

--- bucket_CC/texstudio/patches/patch-src_adwaita-qt_style_adwaitahelper.cpp
+++ bucket_CC/texstudio/patches/patch-src_adwaita-qt_style_adwaitahelper.cpp
@@ -1,4 +1,4 @@
---- src/adwaita-qt/style/adwaitahelper.cpp.orig	2026-07-24 15:49:44 UTC
+--- src/adwaita-qt/style/adwaitahelper.cpp.orig	2026-08-14 17:43:46 UTC
 +++ src/adwaita-qt/style/adwaitahelper.cpp
 @@ -1526,10 +1526,10 @@ void Helper::renderProgressBarBusyConten
      QRectF contentRect;
